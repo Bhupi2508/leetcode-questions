a4faefcf7f281aeac2c09bdad76c2a879e93aa86
bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-14 22:21:55 +0530

    longest-common-prefix question added one more

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -33,4 +33,19 @@ var longestCommonPrefix = function (strs) {
     return result ? result : "";
 };
 
+// const longestCommonPrefix = (strs) => {
+//     if (!strs.length) return '';
+
+//     let prefix = strs[0];
+
+//     for (let i = strs.length; --i;) {
+//       for (;strs[i].indexOf(prefix) !== 0;) {
+//         prefix = prefix.substring(0, prefix.length - 1);
+//         if (!prefix.length) return '';
+//       }
+//     }
+
+//     return prefix;
+//   };
+
 longestCommonPrefix(["cir", "car"])
