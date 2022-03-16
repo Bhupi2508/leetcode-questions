079897422cf29db2c9f2a8e355856c95b5874355
bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-16 22:00:56 +0530

    add binary question added

---
+++ add-binary.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+    let carry = 0, i = 0, finalValue = '';
+    if (a == "0" && b == "0") {
+        return "0";
+    }
+
+    let length = a.length > b.length ? a.length : b.length;
+    a = a.split("").reverse();
+    b = b.split("").reverse();
+    
+    while (i < length) {
+        const firstVal = a[i] ? +a[i] : 0;
+        const SecondVal = b[i] ? +b[i] : 0;
+
+        const sum = firstVal + SecondVal + carry;
+        carry = Math.floor(sum / 2);
+        finalValue = (sum % 2) + finalValue
+        i++;
+    }
+
+    if (carry > 0) {
+        finalValue = carry + finalValue;
+    };
+    return finalValue;
+};
+
+console.log(addBinary("1010", "1011"))
