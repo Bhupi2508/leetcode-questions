daa39e8c48dbf7dd53dcad3081e91c14301eafed
bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-19 14:41:56 +0530

    word-pattern question updated

--- word-pattern.js
+++ word-pattern.js
@@ -15,37 +15,23 @@
  * @return {boolean}
  */
 var wordPattern = function (pattern, s) {
+
+    // Convert into array
     const secondString = s.replaceAll(" ", ",").split(",")
-
-    console.log("pattern ", pattern, pattern.length);
-    console.log("secondString ", secondString, secondString.length);
-
-    // If both values are same
-    if (pattern == s) return true;
 
     // If both lenght are not same
     if (pattern == null || s == null || pattern.length != secondString.length) return false;
 
 
     const a = {}, b = {};
-
     for (let i = 0; i < pattern.length; i++) {
-        console.log(pattern[i]);
-        console.log(secondString[i]);
-
-        console.log("first =========>>> ", a, " :::: ", a[pattern[i]]);
-        console.log("second =========>>>  ", b, " :::: ", b[secondString[i]]);
-
         // If value is ntot exist in object or undefined
         if (a[pattern[i]] !== b[secondString[i]]) {
-            console.log("If Condition ");
             return false;
         }
-            a[pattern[i]] = i + 1;
-            b[secondString[i]] = i + 1;
-            console.log("First value set =======>>> ", a);
-            console.log("Second value set =======>>> ", b, "\n");
-        
+        a[pattern[i]] = i + 1;
+        b[secondString[i]] = i + 1;
+
     }
     return true
 };
