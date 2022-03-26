6bb513e2792bdb3321b9e73ce083f68c6ecc2d5a
bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-26 23:28:54 +0530

    add string logic update

--- add-strings.js
+++ add-strings.js
@@ -16,7 +16,24 @@
  * @return {string}
  */
  var addStrings = function (num1, num2) {
-    return (BigInt(num1) + BigInt(num2)).toString();
-};
+    let len = num1.length > num2.length ? num1.length : num2.length;
 
-console.log(addStrings("11", "123"));
+    let i = num1.length - 1, j = num2.length - 1, sum = '', carry = 0;
+
+    while (len > 0) {
+        const a = i < 0 ? 0 : num1.charAt(i) - '0';
+        const b = j < 0 ? 0 : num2.charAt(j) - '0';
+
+        const val = carry + a + b;
+        sum = (val % 10) + sum;
+        carry = Math.floor(val / 10);
+        len--; i--; j--
+    }
+
+    if (carry > 0) return (carry + sum);
+    return sum;
+};
+console.log(addStrings("110992037024834", "7403274092"));
+//  var addStrings = function (num1, num2) {
+//     return (BigInt(num1) + BigInt(num2)).toString();
+// };
