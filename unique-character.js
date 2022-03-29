49e80b3fc935131ce7bc7a3fbc91a2aede135070
bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-03-29 23:30:12 +0530

    uniquestring question dded

---
+++ unique-character.js
@@ -0,0 +1,21 @@
+/**
+ * @Question :-
+ * Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+ * Example 1:
+        Input: s = "leetcode"
+        Output: 0
+...
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    const element = s.replace(s[i], "-");
+    if (element.indexOf(s[i]) == -1) return s.indexOf(s[i]);
+  }
+  return -1;
+};
+console.log(firstUniqChar("aabb"));
