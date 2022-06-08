cc433432f181dea59e6a2474622d94c24d94155d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-08 22:53:26 +0530

    0 :::: 0520-detect-capital.js leetcode question updated (2022-06-08)

--- dsa.js
+++ dsa.js
@@ -1,53 +1,58 @@
-// 515. Find Largest Value in Each Tree Row
-// Medium   55%
+// 520. Detect Capital
+// Easy   51%
 
-// You need to find the largest value in each row of a binary tree.
 
-// Example:
-// Input:
+// Given a word, you need to judge whether the usage of capitals in it is right
+// or not.
 
-//       1
-//      / \
-//     3   2
-//    / \   \
-//   5   3   9
+// We define the usage of capitals in a word to be right when one of the
+// following cases holds:
 
-// Output: [1, 3, 9]
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital if it has more than one letter,
+// like "Google".
+
+// Otherwise, we define that this word doesn't use capitals in a right way.
+
+// Example 1:
+
+// Input: "USA"
+// Output: True
+
+// Example 2:
+
+// Input: "FlaG"
+// Output: False
+
+// Note:
+// The input will be a non-empty word consisting of uppercase and lowercase latin
+// letters.
+
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *   this.val = val;
- *   this.left = this.right = null;
- * }
+ * @param {string} word
+ * @return {boolean}
  */
-
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-const largestValues = function(root) {
-  const result = []
-  function traverse(root, layer) {
-    if (root == null) return
-    if (result[layer] == null ||
-        root.val > result[layer]) result[layer] = root.val
-    traverse(root.left, layer + 1)
-    traverse(root.right, layer + 1)
-  }
-  traverse(root, 0)
-  return result
+const detectCapitalUse = function(word) {
+  const n = word.length
+  let lower = 0
+  for (let l of word) if (l > 'Z') lower++
+  return lower === n || lower === 0 || (word[0] < 'a' && lower === n - 1)
 }
 
-const TreeNode = require('../structs/TreeNode')
 ;[
-  [1,3,2,5,3,null,9],           // [1,3,9]
-].forEach(array => {
-  console.log(largestValues(TreeNode.from(array)))
+  'USA',                        // true
+  'FlaG',                       // false
+  'leetcode',                   // true
+  'Google',                     // true
+].forEach(word => {
+  console.log(detectCapitalUse(word))
 })
 
 // Solution:
-// 使用一个层级参数，进行遍历。
-// 每进入一个节点，就与该层中最大的数比较，若大于则设置为该层最大。
+// 记录小写字符的个数。
+// 如果数量等于 单词的长度 或 0，说明单词全为大写或全为小写，则是正规的。
+// 如果第一个大写，而后面全为小写，即 数量等于长度减一，也是正规的。
 
 // Submission Result: Accepted
