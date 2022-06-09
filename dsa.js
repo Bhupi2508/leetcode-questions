84e98de268c594db15e4018e74c6ca0955914d3d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-09 22:45:06 +0530

    0 :::: 50-powerxn.js question updated

--- dsa.js
+++ dsa.js
@@ -1,62 +1,26 @@
 /**
- * Key: see inline comments. O(N^2). Space O(1).
- *
- * @param {string} s
- * @return {string}
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
  */
-var longestPalindrome = function(s) {
-    if (!s) return s;
-    var longest = s.substring(0, 1);
-    for (var i = 0; i < s.length; i++) {
-        // odd
-        var tmp = getPalindromeSubstring(s, i, i);
-        if (tmp.length > longest.length) {
-            longest = tmp;
-        }
-
-        // even, get longest palindrome with center of i, i+1
-        tmp = getPalindromeSubstring(s, i, i + 1);
-        if (tmp.length > longest.length) {
-            longest = tmp;
-        }
-    }
-
-    return longest;
+var myPow = function(x, n) {
+  if (n === 0) return 1;
+  if (n < 0) {
+    n *= -1;
+    x = 1 / x;
+  }
+  return n % 2 === 0 ? myPow(x*x, n / 2) : x * myPow(x * x, Math.floor(n / 2));
 };
 
-// Given a center, either one letter or two letter,
-// Find longest palindrome
-var getPalindromeSubstring = function(s, i, j) {
-    while (i >= 0 && j < s.length && s[i] === s[j]) {
-        i--;
-        j++;
+// runtime error?
+var myPow = function(x, n) {
+    if (n === 0) {
+        return 1;
     }
 
-    return s.substring(i + 1, j);
-};
-
-
-// dp method, should work. memory exceed limit
-// bottom-up.
-var longestPalindrome = function(s) {
-    if (!s) return s;
-    var longest = s.substring(0, 1);
-    var length = s.length;
-    var dp = [];
-    var maxLength = 1;
-    for (var i = length - 1; i >= 0; i--) {
-        dp[i] = [];
-        for (var j = i; j < length; j++) {
-            // j - i <= 2 is important, in case i + 1 is out of array boundary.
-            if (s[i] === s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
-                dp[i][j] = true;
-                if (j - i + 1 > maxLength) {
-                    maxLength = j - i + 1;
-                    longest = s.substring(i, j + 1);
-                }
-            }
-        }
+    if (n === 1) {
+        return x;
     }
 
-    return longest;
+    return n > 0 ? myPow(x, n - 1) * x : x / myPow(x, -n - 1) * x;
 };
