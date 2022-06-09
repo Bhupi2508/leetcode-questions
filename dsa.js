5ad6e71994128d9358c28e2dd0043414e0bd358f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-09 22:53:17 +0530

    0 :::: 0459-repeated-substring-pattern.js leetcode question updated (2022-06-09)

--- dsa.js
+++ dsa.js
@@ -1,51 +1,71 @@
-// 458. Poor Pigs
-// Easy   40%
+// 459. Repeated Substring Pattern
+// Easy   38%
 
+// Given a non-empty string check if it can be constructed by taking a substring
+// of it and appending multiple copies of the substring together. You may assume
+// the given string consists of lowercase English letters only and its length
+// will not exceed 10000.
 
-// There are 1000 buckets, one and only one of them contains poison, the rest are
-// filled with water. They all look the same. If a pig drinks that poison it will
-// die within 15 minutes. What is the minimum amount of pigs you need to figure
-// out which bucket contains the poison within one hour.
+// Example 1:
 
-// Answer this question, and write an algorithm for the follow-up general case.
+// Input: "abab"
 
-// Follow-up:
+// Output: True
 
-// If there are n buckets and a pig drinking poison will die within m minutes,
-// how many pigs (x) you need to figure out the "poison" bucket within p minutes?
-// There is exact one bucket with poison.
+// Explanation: It's the substring "ab" twice.
+
+// Example 2:
+
+// Input: "aba"
+
+// Output: False
+
+// Example 3:
+
+// Input: "abcabcabcabc"
+
+// Output: True
+
+// Explanation: It's the substring "abc" four times. (And the substring "abcabc"
+// twice.)
 
 
 /**
- * @param {number} buckets
- * @param {number} minutesToDie
- * @param {number} minutesToTest
- * @return {number}
+ * @param {string} s
+ * @return {boolean}
  */
-const poorPigs = function(buckets, minutesToDie, minutesToTest) {
-  let pigs = 0
-  while (Math.pow(minutesToTest / minutesToDie + 1, pigs) < buckets) pigs++
-  return pigs
+const repeatedSubstringPattern = function(s) {
+  const n = s.length
+  for (let i = 2; i <= n; i++) {
+    if (!(n % i)) {
+      let p = -1, q = n / i - 1
+      while (q < n && s[++p] === s[++q]);
+      if (q >= n) return true
+    }
+  }
+  return false
+
+  // // more easy solution
+  // return (s + s).slice(1, s.length * 2 - 1).includes(s)
 }
 
 ;[
-  [1000, 15, 60],               // 5
-].forEach(args => {
-  console.log(poorPigs(...args))
+  'abab',                       // true
+  'aba',                        // false
+  'abcabcabcabc',               // true
+  'abac',                       // false
+  'ababab',                     // true
+].forEach(s => {
+  console.log(repeatedSubstringPattern(s))
 })
 
 // Solution:
-// 若有n只猪，则可将桶摆为n维体，每只猪负责一个象限。
-// n只猪都死在其象限的某个值上，这些在组合起来就能定位到有毒的桶了。
-// 时间定义了每个象限的范围。
 
-// 假设2只猪，一共45分钟，每15分钟试一次，16个桶。
-//  1  2  3  4
-//  5  6  7  8
-//  9 10 11 12
-// 13 14 15 16
-// 一只猪负责行，另一只负责列。每次一行（列）。最后一行（列）不用测试。
-// 比如第一只猪在测试第2行时死掉了，那么一定在这行。
-// 而第二只猪最后都没死，那么就说明有毒的桶在最后一列。即毒桶为8。
+// 因为要重复两次及以上，所以从重复的次数开始入手。
+// 从重复 2 次开始检查，不断递增，直到重复 n 次。
+// 如果重复次数不能整除字符串长度，则跳过。
+// 在能整除的次数中，从 p = [0, n / i]，检查
+// s[p]是否与 s[p + n / i]相同，如果全部都同，说明是重复子字符串组成。
+
 
 // Submission Result: Accepted
