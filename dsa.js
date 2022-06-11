b13575f917d1924baa09802cf0c3a56f19612640
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-11 22:54:36 +0530

    0 :::: 1108-defanging-an-ip-address.js leetcode question updated (2022-06-11)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,37 @@
-// 1103. Distribute Candies to People
-// Easy   60%
+// 1108. Defanging an IP Address
+// Easy   86%
 
 
-// We distribute some number of candies, to a row of n = num_people people in the
-// following way:
-// We then give 1 candy to the first person, 2 candies to the second person, and
-// so on until we give n candies to the last person.
-// Then, we go back to the start of the row, giving n + 1 candies to the first
-// person, n + 2 candies to the second person, and so on until we give 2 * n
-// candies to the last person.
-// This process repeats (with us giving one more candy each time, and moving to
-// the start of the row after we reach the end) until we run out of candies.  The
-// last person will receive all of our remaining candies (not necessarily one
-// more than the previous gift).
-// Return an array (of length num_people and sum candies) that represents the
-// final distribution of candies.
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
 
 // Example 1:
-// Input: candies = 7, num_people = 4
-// Output: [1,2,3,1]
-// Explanation:
-// On the first turn, ans[0] += 1, and the array is [1,0,0,0].
-// On the second turn, ans[1] += 2, and the array is [1,2,0,0].
-// On the third turn, ans[2] += 3, and the array is [1,2,3,0].
-// On the fourth turn, ans[3] += 1 (because there is only one candy left), and
-// the final array is [1,2,3,1].
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
 // Example 2:
-// Input: candies = 10, num_people = 3
-// Output: [5,2,3]
-// Explanation:
-// On the first turn, ans[0] += 1, and the array is [1,0,0].
-// On the second turn, ans[1] += 2, and the array is [1,2,0].
-// On the third turn, ans[2] += 3, and the array is [1,2,3].
-// On the fourth turn, ans[0] += 4, and the final array is [5,2,3].
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
 
 // Constraints:
-//     1 <= candies <= 10^9
-//     1 <= num_people <= 1000
+//     The given address is a valid IPv4 address.
 
 
 /**
- * @param {number} candies
- * @param {number} num_people
- * @return {number[]}
+ * @param {string} address
+ * @return {string}
  */
-const distributeCandies = function(candies, num_people) {
-  const ans = Array(num_people).fill(0)
-  let people = 0
-  for (let turn = 1; candies - turn > 0; turn++) {
-    ans[people] += turn
-    candies -= turn
-    people = (people + 1) % num_people
-  }
-  ans[people] += candies
-  return ans
+const defangIPaddr = function(address) {
+  return address.replace(/\./g, '[.]')
 }
 
 ;[
-  [7, 4],   // [1,2,3,1]
-  [10, 3],  // [5,2,3]
-  [11, 3],  // [5,3,3]
-].forEach(([candies, num_people]) => {
-  console.log(distributeCandies(candies, num_people))
+  '1.1.1.1',
+  '255.100.50.0',
+].forEach((address) => {
+  console.log(defangIPaddr(address))
 })
 
 // Solution:
-// 根据题意直接 循环遍历 每个人并分该轮数的糖数即可。
+// 使用replace函数，注意用正则表达的全局匹配，否则只替换一个。
 
 // Submission Result: Accepted
