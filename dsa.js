199e4c5e89e1a9ab55c0f0065b35d9ac7ec18fd4
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-11 22:55:09 +0530

    0 :::: 1408-string-matching-in-an-array.js leetcode question updated (2022-06-11)

--- dsa.js
+++ dsa.js
@@ -1,63 +1,61 @@
-// 1403. Minimum Subsequence in Non-Increasing Order
-// Easy   71%
+// 1408. String Matching in an Array
+// Easy   60%
 
 
-// Given the array nums, obtain a subsequence of the array whose sum of elements
-// is strictly greater than the sum of the non included elements in such
-// subsequence.
-// If there are multiple solutions, return the subsequence with minimum size and
-// if there still exist multiple solutions, return the subsequence with the
-// maximum total sum of all its elements. A subsequence of an array can be
-// obtained by erasing some (possibly zero) elements from the array.
-// Note that the solution with the given constraints is guaranteed to be unique.
-// Also return the answer sorted in non-increasing order.
+// Given an array of string words. Return all strings in words which is substring
+// of another word in any order.
+// String words[i] is substring of words[j], if can be obtained removing some
+// characters to left and/or right side of words[j].
 
 // Example 1:
-// Input: nums = [4,3,10,9,8]
-// Output: [10,9]
-// Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum
-// of their elements is strictly greater than the sum of elements not included,
-// however, the subsequence [10,9] has the maximum total sum of its elements.
+// Input: words = ["mass","as","hero","superhero"]
+// Output: ["as","hero"]
+// Explanation: "as" is substring of "mass" and "hero" is substring of
+// "superhero".
+// ["hero","as"] is also a valid answer.
 // Example 2:
-// Input: nums = [4,4,7,6,7]
-// Output: [7,7,6]
-// Explanation: The subsequence [7,7] has the sum of its elements equal to 14
-// which is not strictly greater than the sum of elements not included (14 = 4 +
-// 4 + 6). Therefore, the subsequence [7,6,7] is the minimal satisfying the
-// conditions. Note the subsequence has to returned in non-decreasing order.
+// Input: words = ["leetcode","et","code"]
+// Output: ["et","code"]
+// Explanation: "et", "code" are substring of "leetcode".
 // Example 3:
-// Input: nums = [6]
-// Output: [6]
+// Input: words = ["blue","green","bu"]
+// Output: []
 
 // Constraints:
-//     1 <= nums.length <= 500
-//     1 <= nums[i] <= 100
+//     1 <= words.length <= 100
+//     1 <= words[i].length <= 30
+//     words[i] contains only lowercase English letters.
+//     It's guaranteed that words[i] will be unique.
 
 
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {string[]} words
+ * @return {string[]}
  */
-const minSubsequence = function(nums) {
-  nums.sort((a, b) => b - a)
-  const sum = nums.reduce((pre, v) => pre + v, 0)
-  let i = 0, subSum = 0
-  while (subSum <= sum - subSum) subSum += nums[i++]
-  return nums.slice(0, i)
+const stringMatching = function(words) {
+  let res = new Set()
+  for (let i = 1; i < words.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let a = words[i], b = words[j]
+      if (a.length > b.length) [a, b] = [b, a]
+      if (b.indexOf(a) > -1) res.add(a)
+    }
+  }
+  return [...res]
 }
 
 ;[
-  [4,3,10,9,8],
-  [4,4,7,6,7],
-  [6],
-].forEach((nums) => {
-  console.log(minSubsequence(nums))
+  ['mass','as','hero','superhero'],
+  ['leetcode', 'et', 'code'],
+  ['blue', 'grenn', 'bu'],
+  ["leetcoder","leetcode","od","hamlet","am"],
+  ["jak","yjakdn","tj","yyjakdn"], //["jak","yjakdn"]
+].forEach((words) => {
+  console.log(stringMatching(words))
 })
 
 // Solution:
-// 1）排序，这样可从最大值开始遍历
-// 2）计算整个数组的总和 sum
-// 3）计算前 i 项的序列总和 subSum ，并比较剩余子序列和（sum - subSum)比较，当
-// subSum > sum-subSum 时，返回前 i 项即可。
+// 两两遍历，看长的字符串是否包含短的字符串。
+// 使用 Set 集合去重
 
 // Submission Result: Accepted
