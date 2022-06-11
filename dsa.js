3bf00ac7d1a96fa063920a877f7162a0b015b47b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-11 22:55:10 +0530

    0 :::: 1422-maximum-score-after-splitting-a-string.js leetcode question updated (2022-06-11)

--- dsa.js
+++ dsa.js
@@ -1,83 +1,84 @@
-// 1417. Reformat The String
-// Easy   56%
+// 1422. Maximum Score After Splitting a String
+// Easy   54%
 
 
-// Given alphanumeric string s. (Alphanumeric string is a string consisting of
-// lowercase English letters and digits).
-// You have to find a permutation of the string where no letter is followed by
-// another letter and no digit is followed by another digit. That is, no two
-// adjacent characters have the same type.
-// Return the reformatted string or return an empty string if it is impossible to
-// reformat the string.
+// Given a string s of zeros and ones, return the maximum score after splitting
+// the string into two non-empty substrings (i.e. left substring and right
+// substring).
+// The score after splitting a string is the number of zeros in the left
+// substring plus the number of ones in the right substring.
 
 // Example 1:
-// Input: s = "a0b1c2"
-// Output: "0a1b2c"
-// Explanation: No two adjacent characters have the same type in "0a1b2c".
-// "a0b1c2", "0a1b2c", "0c2a1b" are also valid permutations.
+// Input: s = "011101"
+// Output: 5
+// Explanation:
+// All possible ways of splitting s into two non-empty substrings are:
+// left = "0" and right = "11101", score = 1 + 4 = 5
+// left = "01" and right = "1101", score = 1 + 3 = 4
+// left = "011" and right = "101", score = 1 + 2 = 3
+// left = "0111" and right = "01", score = 1 + 1 = 2
+// left = "01110" and right = "1", score = 2 + 1 = 3
 // Example 2:
-// Input: s = "leetcode"
-// Output: ""
-// Explanation: "leetcode" has only characters so we cannot separate them by
-// digits.
+// Input: s = "00111"
+// Output: 5
+// Explanation: When left = "00" and right = "111", we get the maximum score = 2
+// + 3 = 5
 // Example 3:
-// Input: s = "1229857369"
-// Output: ""
-// Explanation: "1229857369" has only digits so we cannot separate them by
-// characters.
-// Example 4:
-// Input: s = "covid2019"
-// Output: "c2o0v1i9d"
-// Example 5:
-// Input: s = "ab123"
-// Output: "1a2b3"
+// Input: s = "1111"
+// Output: 3
 
 // Constraints:
-//     1 <= s.length <= 500
-//     s consists of only lowercase English letters and/or digits.
+//     2 <= s.length <= 500
+//     The string s consists of characters '0' and '1' only.
 
 
 /**
  * @param {string} s
- * @return {string}
+ * @return {number}
  */
-const reformat = function(s) {
-  const nums = [], digits = []
-  for (let c of s) {
-    if (Number.isInteger(c - 0)) nums.push(c)
-    else digits.push(c)
-  }
-  const n = nums.length, m = digits.length
-  if (Math.abs(n - m) > 1) return ''
-  let res = ''
-  if (n < m) {
-    for (let i = 0; i < n; i++) {
-      res += digits[i] + nums[i]
-    }
-    res += digits[m - 1]
-  } else {
-    for (let i = 0; i < m; i++) {
-      res += nums[i] + digits[i]
-    }
-    if (n !== m) res += nums[n - 1]
+const maxScore = function(s) {
+  const l = s.length
+  let left = s[0] == '0' ? 1 : 0, right = 0
+  for (let i = 1; i < l; i++) if (s[i] == '1') right++
+  let res = left + right
+  for (let i = 1; i < l - 1; i++) {
+    if (s[i] == '0') left++
+    else right--
+    res = Math.max(res, left + right)
   }
   return res
 }
 
+const batter = function(s) {
+  let l = s.length, max = -l - 1, zeros = 0, ones = 0
+  for (let i = 0; i < l; i++) {
+    if (s[i] === '0') zeros++
+    else ones++
+    if (i < l - 1) max = Math.max(max, zeros - ones)
+  }
+  return max + ones
+}
+
 ;[
-  'a0b1c2', // '0a1b2c'
-  'leetcode', // ''
-  '1229857369', // ''
-  'covid2019', // 'c2o0v1i9d'
-  'ab123', // '1a2b3'
-  'da0b1c2e', // ''
+  '011101', // 5
+  '00111',  // 5
+  '1111', // 3
+  '10', // 0
 ].forEach((s) => {
-  console.log(reformat(s))
+  console.log(maxScore(s))
+  console.log(batter(s))
 })
 
 // Solution:
-// 使用两个数组分别记录 s 字符串中的数字和字母
-// 若两数组的长度之差大于 1，则不能组成新字符串
-// 交替遍历两个数组组成新字符串，（长度大的先遍历，若相等则数字先）
+// 先分为 s[0] 和 s[1,...]，并记录左边 0 的个数 left 和右边 1 的个数 right
+// 从第二个字符开始遍历，过程中改变 left 和 right 的值
+// 遍历到 0 则 left+1，否则 right-1
+// 过程中记录 left+right 的最大值
+// 遍历了两遍
+
+// 遍历一遍的方法
+// Max( zeroL + oneR )
+// =Max( zeroL - oneL + oneL + oneR )
+// =Max( zeroL - oneL ) + oneTotal
 
 // Submission Result: Accepted
