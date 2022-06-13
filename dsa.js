8dc11a5a5e0200bf17a08862628fa4a08b94352b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-13 22:54:09 +0530

    1 :::: 0889-construct-binary-tree-from-preorder-and-postorder-traversal.js leetcode question updated (2022-06-13)

--- dsa.js
+++ dsa.js
@@ -1,75 +1,76 @@
-// 888. Fair Candy Swap
-// Easy   57%
+// 889. Construct Binary Tree from Preorder and Postorder Traversal
+// Medium   66%
 
 
-// Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th
-// bar of candy that Alice has, and B[j] is the size of the j-th bar of candy
-// that Bob has.
-// Since they are friends, they would like to exchange one candy bar each so that
-// after the exchange, they both have the same total amount of candy.  (The total
-// amount of candy a person has is the sum of the sizes of candy bars they have.)
-// Return an integer array ans where ans[0] is the size of the candy bar that
-// Alice must exchange, and ans[1] is the size of the candy bar that Bob must
-// exchange.
-// If there are multiple answers, you may return any one of them.  It is
-// guaranteed an answer exists.
+// Return any binary tree that matches the given preorder and postorder
+// traversals.
+// Values in the traversals pre and post are distinct positive integers.
 
 // Example 1:
-// Input: A = [1,1], B = [2,2]
-// Output: [1,2]
-// Example 2:
-// Input: A = [1,2], B = [2,3]
-// Output: [1,2]
-// Example 3:
-// Input: A = [2], B = [1,3]
-// Output: [2,3]
-// Example 4:
-// Input: A = [1,2,5], B = [2,4]
-// Output: [5,4]
+// Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
 
 // Note:
-//     1 <= A.length <= 10000
-//     1 <= B.length <= 10000
-//     1 <= A[i] <= 100000
-//     1 <= B[i] <= 100000
-//     It is guaranteed that Alice and Bob have different total amounts of candy.
-//     It is guaranteed there exists an answer.
+//     1 <= pre.length == post.length <= 30
+//     pre[] and post[] are both permutations of 1, 2, ..., pre.length.
+//     It is guaranteed an answer exists. If there exists multiple answers, you
+// can return any of them.
 
 
 /**
- * @param {number[]} A
- * @param {number[]} B
- * @return {number[]}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ * this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-const fairCandySwap = function(A, B) {
-  const diff = (A.reduce((s, v) => s + v, 0) - B.reduce((s, v) => s + v, 0)) / 2
+const TreeNode = require('../structs/TreeNode')
 
-  const set = new Set(A)
-  for (let b of B) {
-    if (set.has(b + diff)) return [b + diff, b]
+/**
+ * @param {number[]} pre
+ * @param {number[]} post
+ * @return {TreeNode}
+ */
+const constructFromPrePost = function(pre, post) {
+  const n = pre.length
+  if (n === 0) return null
+
+  let i = 0
+  while (i < n - 1 && pre[i + 1] !== post[n - 2]) i++
+
+  const root = new TreeNode(
+    pre[0],
+    constructFromPrePost(pre.slice(1, i + 1), post.slice(0, i)),
+    constructFromPrePost(pre.slice(i + 1), post.slice(i, n - 1))
+  )
+  return root
+}
+
+const better = function(pre, post) {
+  let preIndex = 0, postIndex = 0
+  function recursive() {
+    const root = new TreeNode(pre[preIndex++])
+    if (root.val !== post[postIndex]) root.left = recursive()
+    if (root.val !== post[postIndex]) root.right = recursive()
+    postIndex++
+    return root
   }
-  return []
+  return recursive()
 }
 
 ;[
-  // [[1,1], [2,2]], // [1,2]
-  // [[1,2], [2,3]], // [1,2]
-  // [[2], [1,3]],   // [2,3]
-  // [[1,2,5],[2,4]], // [5,4]
-  // [[35,17,4,24,10],[63,21]], // [24,21]
-  [[8,73,2,86,32], [56,5,67,100,31]], // [2, 31]
-].forEach(([A, B]) => {
-  console.log(fairCandySwap(A, B))
+  [[1,2,4,5,3,6,7,8], [4,5,2,6,8,7,3,1]],
+].forEach(([pre, post]) => {
+  // console.log(constructFromPrePost(pre, post))
+  console.log(better(pre, post))
 })
 
 // Solution:
-// 计算 A 的总和 与 B 的总和 的差 diff
-// 再分别找到 A 的一个值 a 和 B 的一个值 b，使得 2 * (a - b) == diff
-// T(M*N)
+// 1. 找到左子树数组和右子树数组，递归创建。
+// 每次都创建新数组，消耗了大量的空间
 
-// 更好的方法
-// 再寻找 a 和 b 时，使用 Set 数据结构
-// Set 也许是 Hash 的，寻找数值的速度为 O(1)
-// T(N)
+// 2. 使用两个全局变量
+// TODO #889 组织语言，理顺思路，解释清楚。
 
 // Submission Result: Accepted
