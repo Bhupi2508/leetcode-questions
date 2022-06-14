ade3d2fa33f35362afcb7ff195c9c7b9b56749a2
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-14 22:45:17 +0530

    1 :::: 96-uniqueBinarySearchTrees.js question updated

--- dsa.js
+++ dsa.js
@@ -1,47 +1,27 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * Key: Dynamic Programming.
+ * Binary search tree: all left nodes are less than root, and all right node are greater than root
+ * That means, given number n, if the root is i, only numbers between [0, i-1] appear on the left, while
+ * only numbers between [i+1, n] appear on the right. Thus, the total combination is m*n.
+ * Let count[i] be the total possible BST structure ways when the node number is i.
+ * count[i] = sum(count[k], count[i- k - 1]), k starts from 0 to i-1
+ * e.g. count[3] = count[0] * count[2] // when root is 1;
+ *                + count[1] * count[1] // when root is 2;
+ *                + count[2] * count[0] // when root is 3;
+ *
+ * @param {number} n
+ * @return {number}
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-var inorderTraversal = function(root) {
-    var stack = [];
-    var order = [];
-    while (stack.length > 0 || root) {
-        if (root) {
-            stack.push(root);
-            root = root.left;
-        } else {
-            var node = stack.pop();
-            order.push(node.val);
-            if (node.right) root = node.right;
+var numTrees = function(n) {
+    var count = [1, 1];
+
+    for (var i = 2; i <= n; i++) {
+        // give an intial value to count[i], otherwise, it will be NaN
+        count[i] = 0;
+        for (var j = 0; j < i; j++) {
+            count[i] += count[j] * count[i - j - 1];
         }
     }
 
-    return order;
-};
-
-// more concise and efficient
-// as long as the left child is not empty, push it to the stack
-// once left child reaches to the end (leaf), pop the last left, and visit,
-// then assign its right node to be root, and repeat the process on current root (the right child)
-var inorderTraversal = function(root) {
-    var stack = [];
-    var order = [];
-
-    while (root || stack.length > 0) {
-        while (root) {
-            stack.push(root);
-            root = root.left;
-        }
-        var node = stack.pop();
-        order.push(node.val);
-        root = node.right;
-    }
-    return order;
+    return count[n];
 };
