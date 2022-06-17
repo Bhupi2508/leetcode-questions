5893e852e123dc5f3c407437cff8513c61679939
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-17 22:52:49 +0530

    1 :::: 0168-excel-sheet-column-title.js leetcode question updated (2022-06-17)

--- dsa.js
+++ dsa.js
@@ -1,48 +1,45 @@
-// 167. Two Sum II - Input array is sorted
-// Easy   47%
+// 168. Excel Sheet Column Title
+// Easy   26%
 
 
-// Given an array of integers that is already sorted in ascending order, find two
-// numbers such that they add up to a specific target number.
+// Given a positive integer, return its corresponding column title as appear in
+// an Excel sheet.
 
-// The function twoSum should return indices of the two numbers such that they
-// add up to the target, where index1 must be less than index2. Please note that
-// your returned answers (both index1 and index2) are not zero-based.
+// For example:
 
-// You may assume that each input would have exactly one solution and you may not
-// use the same element twice.
+//     1 -> A
+//     2 -> B
+//     3 -> C
+//     ...
+//     26 -> Z
+//     27 -> AA
+//     28 -> AB
 
-// Input: numbers={2, 7, 11, 15}, target=9
-// Output: index1=1, index2=2
+// Credits:Special thanks to @ifanchu for adding this problem and creating all
+// test cases.
 
 
 /**
- * @param {number[]} numbers
- * @param {number} target
- * @return {number[]}
+ * @param {number} n
+ * @return {string}
  */
-const twoSum = function(numbers, target) {
-  let i = 0, j = numbers.length - 1
-  while (i < j) {
-    const sum = numbers[i] + numbers[j]
-    if (sum > target) j--
-    else if (sum < target) i++
-    else return [i + 1, j + 1]
+const convertToTitle = function(n) {
+  let result = ''
+  while (n-- > 0) {
+    result = String.fromCharCode((n % 26) + 65) + result
+    n = Math.trunc(n / 26)
   }
-  return [0, 0]
+  return result
 }
 
 ;[
-  [[2,7,11,15], 9],                  // [1,2]
-].forEach(args => {
-  console.log(twoSum(...args))
+  28,                           // 'AB'
+  26,                           // 'Z'
+].forEach(n => {
+  console.log(convertToTitle(n))
 })
 
 // Solution:
-// 因为已经排好序
-// 因此使用两个变量来保存数字下标，
-// 从外围开始选择两个数字，计算n[i] + n[j]，若大于target，说明大的数字太大了。
-// 若小于，说明选的小的数字太小了。
-// 若等于，则是答案。
+// 像是10进制转换成26进制一样。
 
 // Submission Result: Accepted
