1264ddf7343fcac2c7eff9a365c805d6ddea5651
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-21 22:53:27 +0530

    2 :::: 0526-beautiful-arrangement.js leetcode question updated (2022-06-21)

--- dsa.js
+++ dsa.js
@@ -1,56 +1,66 @@
-// 521. Longest Uncommon Subsequence I
-// Easy   55%
-
-
-// Given a group of two strings, you need to find the longest uncommon
-// subsequence of this group of two strings.
-// The longest uncommon subsequence is defined as the longest subsequence of one
-// of these strings and this subsequence should not be any subsequence of the
-// other strings.
-
-// A subsequence is a sequence that can be derived from one sequence by deleting
-// some characters without changing the order of the remaining elements.
-// Trivially, any string is a subsequence of itself and an empty string is a
-// subsequence of any string.
-
-// The input will be two strings, and the output needs to be the length of the
-// longest uncommon subsequence. If the longest uncommon subsequence doesn't
-// exist, return -1.
-
-// Example 1:
-
-// Input: "aba", "cdc"
-// Output: 3
-// Explanation: The longest uncommon subsequence is "aba" (or "cdc"), because
-// "aba" is a subsequence of "aba", but not a subsequence of any other strings in
-// the group of two strings.
-
-// Note:
-
-// Both strings' lengths will not exceed 100.
-// Only letters from a ~ z will appear in input strings.
-
-
-/**
- * @param {string} a
- * @param {string} b
- * @return {number}
- */
-const findLUSlength = function(a, b) {
-  return a === b ? -1 : Math.max(a.length, b.length)
-}
-
-;[
-  ['aba', 'cdc'],               // 3
-  ['aba', 'aba'],               // -1
-  ['abaa', 'aba'],              // 4
-  ['aba', 'abaa'],              // 4
-].forEach(args => {
-  console.log(findLUSlength(...args))
-})
-
-// Solution:
-// ??? 我还以为读错题目了呢。
-// 搞得我，试了好多个例子。
-
-// Submission Result: Accepted
+// 526. Beautiful Arrangement
+// Medium   54%
+
+
+// Suppose you have N integers from 1 to N. We define a beautiful arrangement as
+// an used that is constructed by these N numbers successfully if one of the
+// following is true for the ith position (1 <= i <= N) in this used:
+
+// 1. The number at the ith position is divisible by i.
+// 2. i is divisible by the number at the ith position.
+
+// Now given N, how many beautiful arrangements can you construct?
+
+// Example 1:
+
+// Input: 2
+// Output: 2
+// Explanation:
+
+// The first beautiful arrangement is [1, 2]:
+// Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+// Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+// The second beautiful arrangement is [2, 1]:
+// Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+// Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
+
+// Note: N is a positive integer and will not exceed 15.
+
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+const countArrangement = function(N) {
+  function iter(i, used) {
+    if (i === N + 1) return 1
+    let result = 0
+    for (let j = 1; j <= N; j++) {
+      if (!used[j] && (i % j === 0 || j % i === 0)) {
+        used[j] = true
+        result += iter(i + 1, used)
+        used[j] = false
+      }
+    }
+    return result
+  }
+  return iter(1, [])
+}
+
+;[
+  1,                            // 1
+  2,                            // 2
+  3,                            // 3
+  13,                           // 4237
+  14,                           // 10680
+].forEach(N => {
+  console.log(countArrangement(N))
+})
+
+// Solution:
+// 用一个数组记录某个数是否被使用过。
+// 使用递归迭代，
+// 找出每个位置可以放置的数。
+
+// Submission Result: Accepted
