9759bdb756404620f034588eea39f7c428357a3a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-21 22:55:11 +0530

    2 :::: 1431-kids-with-the-greatest-number-of-candies.js leetcode question updated (2022-06-21)

--- dsa.js
+++ dsa.js
@@ -1,84 +1,68 @@
-// 1422. Maximum Score After Splitting a String
-// Easy   54%
+// 1431. Kids With the Greatest Number of Candies
+// Easy   89%
 
 
-// Given a string s of zeros and ones, return the maximum score after splitting
-// the string into two non-empty substrings (i.e. left substring and right
-// substring).
-// The score after splitting a string is the number of zeros in the left
-// substring plus the number of ones in the right substring.
+// Given the array candies and the integer extraCandies, where candies[i]
+// represents the number of candies that the ith kid has.
+// For each kid check if there is a way to distribute extraCandies among the kids
+// such that he or she can have the greatest number of candies among them. Notice
+// that multiple kids can have the greatest number of candies.
 
 // Example 1:
-// Input: s = "011101"
-// Output: 5
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true]
 // Explanation:
-// All possible ways of splitting s into two non-empty substrings are:
-// left = "0" and right = "11101", score = 1 + 4 = 5
-// left = "01" and right = "1101", score = 1 + 3 = 4
-// left = "011" and right = "101", score = 1 + 2 = 3
-// left = "0111" and right = "01", score = 1 + 1 = 2
-// left = "01110" and right = "1", score = 2 + 1 = 3
+// Kid 1 has 2 candies and if he or she receives all extra candies (3) will have
+// 5 candies --- the greatest number of candies among the kids.
+// Kid 2 has 3 candies and if he or she receives at least 2 extra candies will
+// have the greatest number of candies among the kids.
+// Kid 3 has 5 candies and this is already the greatest number of candies among
+// the kids.
+// Kid 4 has 1 candy and even if he or she receives all extra candies will only
+// have 4 candies.
+// Kid 5 has 3 candies and if he or she receives at least 2 extra candies will
+// have the greatest number of candies among the kids.
 // Example 2:
-// Input: s = "00111"
-// Output: 5
-// Explanation: When left = "00" and right = "111", we get the maximum score = 2
-// + 3 = 5
+// Input: candies = [4,2,1,1,2], extraCandies = 1
+// Output: [true,false,false,false,false]
+// Explanation: There is only 1 extra candy, therefore only kid 1 will have the
+// greatest number of candies among the kids regardless of who takes the extra
+// candy.
 // Example 3:
-// Input: s = "1111"
-// Output: 3
+// Input: candies = [12,1,12], extraCandies = 10
+// Output: [true,false,true]
 
 // Constraints:
-//     2 <= s.length <= 500
-//     The string s consists of characters '0' and '1' only.
+//     2 <= candies.length <= 100
+//     1 <= candies[i] <= 100
+//     1 <= extraCandies <= 50
 
 
 /**
- * @param {string} s
- * @return {number}
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
  */
-const maxScore = function(s) {
-  const l = s.length
-  let left = s[0] == '0' ? 1 : 0, right = 0
-  for (let i = 1; i < l; i++) if (s[i] == '1') right++
-  let res = left + right
-  for (let i = 1; i < l - 1; i++) {
-    if (s[i] == '0') left++
-    else right--
-    res = Math.max(res, left + right)
+const kidsWithCandies = function(candies, extraCandies) {
+  let max = candies[0]
+  for (let c of candies) max = Math.max(max, c)
+  const res = []
+  for (let c of candies) {
+    res.push(c + extraCandies >= max)
   }
   return res
 }
 
-const batter = function(s) {
-  let l = s.length, max = -l - 1, zeros = 0, ones = 0
-  for (let i = 0; i < l; i++) {
-    if (s[i] === '0') zeros++
-    else ones++
-    if (i < l - 1) max = Math.max(max, zeros - ones)
-  }
-  return max + ones
-}
-
 ;[
-  '011101', // 5
-  '00111',  // 5
-  '1111', // 3
-  '10', // 0
-].forEach((s) => {
-  console.log(maxScore(s))
-  console.log(batter(s))
+  [[2,3,5,1,3], 3],
+  [[4,2,1,1,2], 1],
+  [[12,1,12], 10],
+].forEach(([candies, extraCandies]) => {
+  console.log(kidsWithCandies(candies, extraCandies))
 })
 
 // Solution:
-// 先分为 s[0] 和 s[1,...]，并记录左边 0 的个数 left 和右边 1 的个数 right
-// 从第二个字符开始遍历，过程中改变 left 和 right 的值
-// 遍历到 0 则 left+1，否则 right-1
-// 过程中记录 left+right 的最大值
-// 遍历了两遍
-
-// 遍历一遍的方法
-// Max( zeroL + oneR )
-// =Max( zeroL - oneL + oneL + oneR )
-// =Max( zeroL - oneL ) + oneTotal
+// 找出最大值，
+// 遍历数组，比较 candies[i] + extraCandies 是否大于 max 即可
 
 // Submission Result: Accepted
