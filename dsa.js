53de84504fa5a9703bce602a730bc665f5f5f2a6
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-22 22:55:08 +0530

    2 :::: 1399-count-largest-group.js leetcode question updated (2022-06-22)

--- dsa.js
+++ dsa.js
@@ -1,59 +1,80 @@
-// 1394. Find Lucky Integer in an Array
-// Easy   66%
+// 1399. Count Largest Group
+// Easy   64%
 
 
-// Given an array of integers arr, a lucky integer is an integer which has a
-// frequency in the array equal to its value.
-// Return a lucky integer in the array. If there are multiple lucky integers
-// return the largest of them. If there is no lucky integer return -1.
+// Given an integer n. Each number from 1 to n is grouped according to the sum of
+// its digits.
+// Return how many groups have the largest size.
 
 // Example 1:
-// Input: arr = [2,2,3,4]
-// Output: 2
-// Explanation: The only lucky number in the array is 2 because frequency[2] ==
-// 2.
+// Input: n = 13
+// Output: 4
+// Explanation: There are 9 groups in total, they are grouped according sum of
+// its digits of numbers from 1 to 13:
+// [1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9]. There are 4 groups
+// with largest size.
 // Example 2:
-// Input: arr = [1,2,2,3,3,3]
-// Output: 3
-// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+// Input: n = 2
+// Output: 2
+// Explanation: There are 2 groups [1], [2] of size 1.
 // Example 3:
-// Input: arr = [2,2,2,3,3]
-// Output: -1
-// Explanation: There are no lucky numbers in the array.
+// Input: n = 15
+// Output: 6
 // Example 4:
-// Input: arr = [5]
-// Output: -1
-// Example 5:
-// Input: arr = [7,7,7,7,7,7,7]
-// Output: 7
+// Input: n = 24
+// Output: 5
 
 // Constraints:
-//     1 <= arr.length <= 500
-//     1 <= arr[i] <= 500
+//     1 <= n <= 10^4
 
 
 /**
- * @param {number[]} arr
+ * @param {number} n
  * @return {number}
  */
-const findLucky = function(arr) {
-  const frequency = Array(501).fill(0)
-  for (let i of arr) frequency[i]++
-  for (let i = 500; i > 0; i--) if (frequency[i] === i) return i
-  return -1
+const countLargestGroup = function(n) {
+  const counts = []
+  for (let i = 1; i <= n; i++) {
+    let j = i, k = 0
+    while (j > 0) {
+      k += j % 10
+      j = (j / 10) >>> 0
+    }
+    counts[k-1] = (counts[k-1] || 0) + 1
+  }
+  let max = Math.max(...counts)
+  let res = 0
+  for (let c of counts) if (c === max) res++
+  return res
 }
 
 ;[
-  [2,2,3,4], // 2
-  [1,2,2,3,3,3], // 3
-  [2,2,2,3,3], // -1
-  [5], // -1
-  [7,7,7,7,7,7,7], // 7
-].forEach((arr) => {
-  console.log(findLucky(arr))
+  13, // 4
+  2,  // 2
+  15, // 6
+  24, // 5
+].forEach((n) => {
+  console.log(countLargestGroup(n))
 })
 
 // Solution:
-// 使用一个数组计数，再遍历该数组找到最大的幸运数。
+//  1  2  3  4  5  6  7  8  9  [10] [11]
+// 10 11 12 13 14 15 16 17 18   19
+//    20 21 22 23 24 25 26 27   28   29
+//       30 31 32 33 34 35 36
+//          40 ...
+//             50 ...
+//                60 ...
+//                   70 ...
+//                      80 ...
+//                         90 ...
+// 100 ...
+
+// 本想找一下规律的，枚举了一下，好像有点复杂，就放弃了。
+
+// 用一般思维方法
+// 先用数组计算并保存 1 到 n 的数字和
+// 再选出最大值 （其实遍历 1 到 n 时可以计算出来）
+// 最后计算最大值的个数
 
 // Submission Result: Accepted
