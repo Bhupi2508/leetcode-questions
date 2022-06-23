b695f0694b26af5ec0e4de336622461a561111d5
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-23 22:54:06 +0530

    2 :::: 0872-leaf-similar-trees.js leetcode question updated (2022-06-23)

--- dsa.js
+++ dsa.js
@@ -1,88 +1,65 @@
-// 868. Binary Gap
-// Easy   60%
+// 872. Leaf-Similar Trees
+// Easy   65%
 
 
-// Given a positive integer N, find and return the longest distance between two
-// consecutive 1's in the binary representation of N.
-// If there aren't two consecutive 1's, return 0.
+// Consider all the leaves of a binary tree.  From left to right order, the
+// values of those leaves form a leaf value sequence.
+// For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9,
+// 8).
+// Two binary trees are considered leaf-similar if their leaf value sequence is
+// the same.
+// Return true if and only if the two given trees with head nodes root1 and root2
+// are leaf-similar.
 
-// Example 1:
-// Input: 22
-// Output: 2
-// Explanation:
-// 22 in binary is 0b10110.
-// In the binary representation of 22, there are three ones, and two consecutive
-// pairs of 1's.
-// The first consecutive pair of 1's have distance 2.
-// The second consecutive pair of 1's have distance 1.
-// The answer is the largest of these two distances, which is 2.
-// Example 2:
-// Input: 5
-// Output: 2
-// Explanation:
-// 5 in binary is 0b101.
-// Example 3:
-// Input: 6
-// Output: 1
-// Explanation:
-// 6 in binary is 0b110.
-// Example 4:
-// Input: 8
-// Output: 0
-// Explanation:
-// 8 in binary is 0b1000.
-// There aren't any consecutive pairs of 1's in the binary representation of 8,
-// so we return 0.
-
-// Note:
-//     1 <= N <= 10^9
+// Constraints:
+//     Both of the given trees will have between 1 and 200 nodes.
+//     Both of the given trees will have values between 0 and 200
 
 
 /**
- * @param {number} N
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ * this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-const binaryGap = function(N) {
-  let result = 0, count = -Infinity
-  while (N > 0) {
-    if (N % 2) {
-      result = Math.max(result, count + 1)
-      count = 0
-    } else count++
-    N = N >>> 1
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+const leafSimilar = function(root1, root2) {
+  function leafSeq(node, array = []) {
+    if (node.left) leafSeq(node.left, array)
+    if (node.right) leafSeq(node.right, array)
+    if (node.left == null && node.right == null) array.push(node.val)
+    return array
   }
-  return result
+  const array1 = leafSeq(root1)
+  const array2 = leafSeq(root2)
+
+  for (let i = 0; i < array1.length; ++i) {
+    if (array1[i] !== array2[i]) return false
+  }
+  return array1.length === array2.length
 }
 
-const better = function(N) {
-  let result = 0
-  for (let d = -32; N > 0; N = N >>> 1, d++) {
-    if (N % 2) {
-      result = Math.max(result, d)
-      d = 0
-    }
-  }
-  return result
-}
-
+const TreeNode = require('../structs/TreeNode')
 ;[
-  22, // 2
-  5,  // 2
-  6,  // 1
-  8,  // 0
-  9,  // 3
-].forEach((N) => {
-  console.log(binaryGap(N))
-  console.log(better(N))
+  // [
+  //   [3,5,1,6,2,9,8,null,null,7,4],
+  //   [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8],
+  // ], // true
+  [
+    [1,2],
+    [2,2],
+  ], // true
+].forEach(([A, B]) => {
+  console.log(leafSimilar(TreeNode.from(A), TreeNode.from(B)))
 })
 
 // Solution:
-// 用一个变量记录中间产生的最大距离（即结果），另一个变量记录计算过程中的距离 (count)。
-// 先初始化 count 为无限小，表示还没有遇到 "1"，还不用计算距离。
-// 遇到 1 时，更新 result，并初始化 count = 0。
-
-// 更简洁的方法
-// 将 d 初始化为 -32 (因为 N 的最大的值为 31 位的)，
-// 优化过程。
+// 用深度遍历从左到右访问一颗树的每个叶子节点，并保存在一个数组当中
+// 遍历两棵树，得到两个数组，比较数组即可。
 
 // Submission Result: Accepted
