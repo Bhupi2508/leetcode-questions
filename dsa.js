a86608bde2d72f25d2fccd4c98cebde256767293
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-06-26 22:52:37 +0530

    3 :::: 0107-binary-tree-level-order-traversal-ii.js leetcode question updated (2022-06-26)

--- dsa.js
+++ dsa.js
@@ -1,22 +1,25 @@
-// 106. Construct Binary Tree from Inorder and Postorder Traversal
-// Medium   32%
+// 107. Binary Tree Level Order Traversal II
+// Easy   40%
 
-// Given inorder and postorder traversal of a tree, construct the binary tree.
+// Given a binary tree, return the bottom-up level order traversal of its nodes'
+// values. (ie, from left to right, level by level from leaf to root).
 
-// Note: You may assume that duplicates do not exist in the tree.
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
 
-// For example, given
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
 
-// inorder = [9,3,15,20,7]
-// postorder = [9,15,7,20,3]
+// return its bottom-up level order traversal as:
 
-// Return the following binary tree:
-
-//    3
-//   / \
-//  9  20
-//    /  \
-//   15   7
+// [
+//   [15,7],
+//   [9,20],
+//   [3]
+// ]
 
 
 /**
@@ -27,34 +30,35 @@
  * }
  */
 
-/**
- * @param {number[]} inorder
- * @param {number[]} postorder
- * @return {TreeNode}
- */
-const buildTree = function(inorder, postorder) {
-  const n = inorder.length
-  if (n === 0) return null
 
-  const root = new TreeNode(postorder[n - 1])
-  const i = inorder.indexOf(postorder[n - 1])
-  root.left = buildTree(inorder.slice(0, i), postorder.slice(0, i))
-  root.right = buildTree(inorder.slice(i + 1, n), postorder.slice(i, n - 1))
-  return root
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrderBottom = function(root) {
+  const res = []
+  function iter(root, level) {
+    if (root != null) {
+      if (level >= res.length) res.unshift([])
+      res[res.length - level - 1].push(root.val)
+      iter(root.left, level + 1)
+      iter(root.right, level + 1)
+    }
+  }
+  iter(root, 0)
+  return res
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  [[9,3,15,20,7], [9,15,7,20,3]],
-].forEach(args => {
-  console.log(buildTree(...args))
+  [3,9,20,null,null,15,7], // [ [ 15, 7 ], [ 9, 20 ], [ 3 ] ]
+].forEach(array => {
+  console.log(levelOrderBottom(TreeNode.from(array)))
 })
 
 // Solution:
-// 中序遍历的顺序是，左子树-根节点-右子树
-// 后序遍历的顺序是，左子树-右子树-根节点
-
-// 1. 后序数组的最后一个节点作为根节点；
-// 2. 找出根节点在中序数组中的位置，由此计算出左子树和右子树的长度；
-// 3. 分割出后序和中序数组中的左右子树，并分别递归生成左右子树。
+// 带一个层级参数，进行递归遍历。
+// 根据层级参数和数组的长度来构造数组，
+// 当层数大于或等于数组长度时，在数组头部插入一个代表新的层的数组。
 
 // Submission Result: Accepted
