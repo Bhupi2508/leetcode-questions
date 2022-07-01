dd7eb74d81c772374ddf993628f5841d826be890
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-01 22:52:26 +0530

    3 :::: 0075-sort-colors.js leetcode question updated (2022-07-01)

--- dsa.js
+++ dsa.js
@@ -1,75 +1,57 @@
-// 74. Search a 2D Matrix
-// Medium 34% locked:false
+// 75. Sort Colors
+// Medium   38%
 
-// Write an efficient algorithm that searches for a value in an m x n matrix.
-// This matrix has the following properties:
+// Given an array with n objects colored red, white or blue, sort them so that
+// objects of the same color are adjacent, with the colors in the order red,
+// white and blue.
 
-// 1. Integers in each row are sorted from left to right.
+// Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+// and blue respectively.
 
-// 2. The first integer of each row is greater than the last integer of the
-// previous row.
+// Note: You are not suppose to use the library's sort function for this
+// problem.
 
-// For example,
+// click to show follow up.
 
-// Consider the following matrix:
+// Follow up: A rather straight forward solution is a two-pass algorithm using
+// counting sort. First, iterate the array counting number of 0's, 1's, and 2's,
+// then overwrite array with total number of 0's, then 1's and followed by 2's.
 
-// [
-//   [1,   3,  5,  7],
-//   [10, 11, 16, 20],
-//   [23, 30, 34, 50]
-// ]
-
-// Given target = 3, return true.
+// Could you come up with an one-pass algorithm using only constant space?
 
 /**
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-const searchMatrix = function(matrix, target) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false
-  const m = matrix.length, n = matrix[0].length
-  let i = 0, j = m - 1
-  while (i < j) {
-    const mid = Math.trunc((i + j) / 2)
-    if (target < matrix[mid][0]) j = mid - 1
-    else if (target > matrix[mid][0]) i = mid + 1
-    else return true
+const sortColors = function(nums) {
+  const n = nums.length
+  const swap = (a, b) => [nums[a], nums[b]] = [nums[b], nums[a]]
+  for (let i = 0, redNum = 0, WhiteNum = 0; i < n; i++) {
+    if (nums[i] === 0) {
+      if (nums[redNum] === 1) WhiteNum--
+      swap(redNum++, i)
+    }
+    if (nums[i] === 1) swap(redNum + WhiteNum++, i)
   }
-
-  let row = (target < matrix[i][0]) ? i - 1 : i
-  if (row < 0) return false
-  ;[i, j] = [0, n - 1]
-  while (i <= j) {
-    const mid = Math.trunc((i + j) / 2)
-    if (target < matrix[row][mid]) j = mid - 1
-    else if (target > matrix[row][mid]) i = mid + 1
-    else return true
-  }
-
-  return false
 }
 
+;[
+  [2,0,1,2,1,2,0],
+].forEach(nums => {
+  console.log(nums)
+  sortColors(nums)
+  console.log(nums)
+})
 
-const treatAsList = function(matrix, target) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false
-  const m = matrix.length, n = matrix[0].length
-  let i = 0, j = m * n - 1
-  while (i <= j) {
-    const mid = (i + j) >> 1,
-          value = matrix[Math.trunc(mid / n)][mid % n]
-    if (target < value) j = mid - 1
-    else if (target > value) i = mid + 1
-    else return true
-  }
-  return false
-}
+// Solution:
+// 使用两个变量，分别记录当前已排序的数组中的 0 的个数和 1 的个数。
+// 遍历整个数组。
+// 若当前位置的数字为 0 ，则检查已排序的数组中的 0 后是否为 1 ，
+// 若为 1 ，则将 1 的个数减去一个，
+// 因为，下一步是将 0 与已排序的数组的所有 0 后的数字交换，
+// 这样，已排序中的数组中的 0 的个数会加一，而 1 的个数会减一，
+// 如果不是 1 ，也交换，但不必将 1 的个数减一，而 2 的个数并不需要记录；
+// 若当前位置的数字为 1（可能原来是0，与已排序的数组中的 1 交换了），
+// 则将该数与已排序的数组中的所有 1 后的数字交换，并将 1 的个数加一。
 
-const matrix = [
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-const n = 20
-console.log(searchMatrix(matrix, n))
-console.log(treatAsList(matrix, n))
+// Submission Result: Accepted
