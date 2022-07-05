584fb1bc1575805bd24595ba4e4c000ae172c4b5
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-05 22:54:27 +0530

    4 :::: 0998-maximum-binary-tree-ii.js leetcode question updated (2022-07-05)

--- dsa.js
+++ dsa.js
@@ -1,77 +1,97 @@
-// 997. Find the Town Judge
-// Easy   50%
+// 998. Maximum Binary Tree II
+// Medium   63%
 
 
-// In a town, there are N people labelled from 1 to N.  There is a rumor that one
-// of these people is secretly the town judge.
-// If the town judge exists, then:
-//     The town judge trusts nobody.
-//     Everybody (except for the town judge) trusts the town judge.
-//     There is exactly one person that satisfies properties 1 and 2.
-// You are given trust, an array of pairs trust[i] = [a, b] representing that the
-// person labelled a trusts the person labelled b.
-// If the town judge exists and can be identified, return the label of the town
-// judge.  Otherwise, return -1.
+// We are given the root node of a maximum tree: a tree where every node has a
+// value greater than any other value in its subtree.
+// Just as in the previous problem, the given tree was constructed from an list A
+// (root = Construct(A)) recursively with the following Construct(A) routine:
+//     If A is empty, return null.
+//     Otherwise, let A[i] be the largest element of A.  Create a root node with
+// value A[i].
+//     The left child of root will be Construct([A[0], A[1], ..., A[i-1]])
+//     The right child of root will be Construct([A[i+1], A[i+2], ..., A[A.length
+// - 1]])
+//     Return root.
+// Note that we were not given A directly, only a root node root = Construct(A).
+// Suppose B is a copy of A with the value val appended to it.  It is guaranteed
+// that B has unique values.
+// Return Construct(B).
 
-// Example 1:
-// Input: N = 2, trust = [[1,2]]
-// Output: 2
+// Example 1:               5
+//                         /
+//     4                  4
+//   /   \              /   \
+//  1     3   ->       1     3
+//       /                  /
+//      2                  2
+// Input: root = [4,1,3,null,null,2], val = 5
+// Output: [5,4,null,1,3,null,null,2]
+// Explanation: A = [1,4,2,3], B = [1,4,2,3,5]
 // Example 2:
-// Input: N = 3, trust = [[1,3],[2,3]]
-// Output: 3
+//
+//     5                  5
+//   /   \              /   \
+//  2     4            2     4
+//   \                  \     \
+//    1                  1     3
+// Input: root = [5,2,4,null,1], val = 3
+// Output: [5,2,4,null,1,null,3]
+// Explanation: A = [2,1,5,4], B = [2,1,5,4,3]
 // Example 3:
-// Input: N = 3, trust = [[1,3],[2,3],[3,1]]
-// Output: -1
-// Example 4:
-// Input: N = 3, trust = [[1,2],[2,3]]
-// Output: -1
-// Example 5:
-// Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
-// Output: 3
+//
+//     5                  5
+//   /   \              /   \
+//  2     3            2     4
+//   \                  \   /
+//    1                  1  3
+// Input: root = [5,2,3,null,1], val = 4
+// Output: [5,2,4,null,1,3]
+// Explanation: A = [2,1,5,3], B = [2,1,5,3,4]
 
-// Note:
-//     1 <= N <= 1000
-//     trust.length <= 10000
-//     trust[i] are all different
-//     trust[i][0] != trust[i][1]
-//     1 <= trust[i][0], trust[i][1] <= N
+// Constraints:
+//     1 <= B.length <= 100
 
 
 /**
- * @param {number} N
- * @param {number[][]} trust
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ * this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-const findJudge = function(N, trust) {
-  const count = Array(N + 1).fill(0)
-  for (let t of trust) {
-    count[t[0]]--
-    count[t[1]]++
+const TreeNode = require('../structs/TreeNode')
+
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const insertIntoMaxTree = function(root, val) {
+  if (root === null) return null
+  if (val > root.val) return new TreeNode(val, root)
+
+  let node = root
+  while (node.right && node.right.val > val) {
+    node = node.right
   }
-  for (let i = 1; i <= N; i++) {
-    if (count[i] === N - 1) return i
-  }
-  return -1
+  const newNode = new TreeNode(val, node.right)
+  node.right = newNode
+
+  return root
 }
 
 ;[
-  [2, [[1,2]]],              // 2
-  [3, [[1,3],[2,3]]],        // 3
-  [3, [[1,3],[2,3],[3,1]]],  // -1
-  [3, [[1,2],[2,3]]],        // -1
-  [4, [[1,3],[1,4],[2,3],[2,4],[4,3]]], // 3
-].forEach(([N, trust]) => {
-  console.log(findJudge(N, trust))
+  [[4,1,3,null,null,2], 5],
+].forEach(([array, val]) => {
+  console.log(insertIntoMaxTree(TreeNode.from(array), val))
 })
 
 // Solution:
-// 使用投票箱的方式来统计
-// 每个人一个箱子
-// 若有人去投票，则这个人的箱子就会被关闭，设为 -1
-// 最后查看箱子里的票，若有箱子的票数量为 N - 1，则该箱子的人为法官
-
-// 简化
-// 不用关闭箱子，而是允许箱子的票为负数
-// 投票的人，自己的箱子票数减1
+// 当 val 大于 根值时，构建新的根节点val，并将原树设为左子树。
+// 否则，
+// 使用迭代的方式找到 val 的位置，当 val 节点小于当前节点值时，则遍历其右节点，直到 val 大于
+// 当前节点值。（为了插入操作，用父节点的右子节点作为当前节点）
 
 // Submission Result: Accepted
