7d5311a635f87043ab3114fd96c7816b3a71c8dd
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-06 22:44:43 +0530

    4 :::: 134-gasStation.js question updated

--- dsa.js
+++ dsa.js
@@ -1,78 +1,30 @@
 /**
- * Solution: backtracking, DFS
- *
- * @param {string} s
- * @return {string[][]}
+ * @see inline comments
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
  */
-var partition = function(s) {
-    var result = [];
-    var results = [];
-    if (!s) return results;
-    helper(s, 0, result, results);
-    return results;
-};
+var canCompleteCircuit = function(gas, cost) {
+    var total = 0;
+    var sumRemaining = 0;
+    var start = 0;
 
-var helper = function(s, start, result, results) {
-    if (start >= s.length) {
-        results.push(result.slice());
-        result = [];
-    }
-
-    for (var i = start; i < s.length; i++) {
-        var newStr = s.substring(start, i + 1);
-        if (isPalindrome(newStr)) {
-          result.push(newStr);
-          helper(s, i + 1, result, results);
-          result.pop();
+    for (var i = 0; i < gas.length; i++) {
+        var remaining = gas[i] - cost[i];
+        // track total remaining gas until i
+        if (sumRemaining >= 0) {
+            sumRemaining += remaining;
+        } else {
+            // if total remaing is smaller than 0, set start from i,
+            start = i;
+            // now the total remaining is the remaining at current i
+            sumRemaining = remaining;
         }
-    }
-};
-
-var isPalindrome = function(s) {
-    if (!s || s.length === 1) return true;
-    var length = s.length;
-    for (var i = 0; i < length; i++) {
-        if (s[i] !== s[length - i - 1]) return false;
+        total += remaining;
     }
 
-    return true;
-};
-
-// testing cases
-// var s = 'aab';
-// console.log(partition(s));
-
-// second method, use a dp matrix. (isPal)
-// Same O(n^2), but faster
-var partition = function(s) {
-    var result = [];
-    var results = [];
-    if (!s) return results;
-    var length = s.length;
-    var isPal = [];
-    for (var i = length - 1; i >= 0; i--) {
-        isPal[i] = [];
-        for(var j = i; j < length; j++) {
-            if ((j - i <= 2 || isPal[i+1][j-1]) && s[i] == s[j])
-                isPal[i][j] = true;
-        }
-    }
-
-    helper(s, 0, result, results, isPal);
-    return results;
-};
-
-var helper = function(s, start, result, results, isPal) {
-    if (start >= s.length) {
-        results.push(result.slice());
-        return;
-    }
-
-    for (var i = start; i < s.length; i++) {
-        if (isPal[start][i]) {
-            result.push(s.substring(start, i + 1));
-            helper(s, i + 1, result, results, isPal);
-            result.pop();
-        }
-    }
+    // at the end, if total >= 0, it means there must be a circle to finish, and the
+    // start is the start
+    if (total >= 0) return start;
+    else return -1;
 };
