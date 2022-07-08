0d825d40b824e5bbcb85284feb13953b32bb37de
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-08 22:53:37 +0530

    4 :::: 0599-minimum-index-sum-of-two-lists.js leetcode question updated (2022-07-08)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,78 @@
-// 598. Range Addition II
-// Easy 48% locked:false
+// 599. Minimum Index Sum of Two Lists
+// Easy   46%
 
-// 598. Range Addition II
 
-// Given an m * n matrix M initialized with all 0's and several update
-// operations.
+// Suppose Andy and Doris want to choose a restaurant for dinner, and they both
+// have a list of favorite restaurants represented by strings.
 
-// Operations are represented by a 2D array, and each operation is represented
-// by an array with two positive integers a and b, which means M[i][j] should be
-// added by one for all 0 <= i < a and 0 <= j < b.
-
-// You need to count and return the number of maximum integers in the matrix
-// after performing all the operations.
+// You need to help them find out their common interest with the least list index
+// sum. If there is a choice tie between answers, output all of them with no
+// order requirement. You could assume there always exists an answer.
 
 // Example 1:
 
 // Input:
-// m = 3, n = 3
-// operations = [[2,2],[3,3]]
-// Output: 4
-// Explanation:
-// Initially, M =
-// [[0, 0, 0],
-//  [0, 0, 0],
-//  [0, 0, 0]]
+// ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+// ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+// Output: ["Shogun"]
+// Explanation: The only restaurant they both like is "Shogun".
 
-// After performing [2,2], M =
-// [[1, 1, 0],
-//  [1, 1, 0],
-//  [0, 0, 0]]
+// Example 2:
 
-// After performing [3,3], M =
-// [[2, 2, 1],
-//  [2, 2, 1],
-//  [1, 1, 1]]
-
-// So the maximum integer in M is 2, and there are four of it in M. So return 4.
+// Input:
+// ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+// ["KFC", "Shogun", "Burger King"]
+// Output: ["Shogun"]
+// Explanation: The restaurant they both like and have the least index sum is
+// "Shogun" with index sum 1 (0+1).
 
 // Note:
 
-//     The range of m and n is [1,40000].
-//     The range of a is [1,m], and the range of b is [1,n].
-//     The range of operations size won't exceed 10,000.
+// The length of both lists will be in the range of [1, 1000].
+// The length of strings in both lists will be in the range of [1, 30].
+// The index is starting from 0 to the list length minus 1.
+// No duplicates in both lists.
 
 
 /**
- * @param {number} m
- * @param {number} n
- * @param {number[][]} ops
- * @return {number}
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
  */
-const maxCount = function(m, n, ops) {
-  let a = m, b = n
-  for (let pair of ops) {
-    if (a > pair[0]) a = pair[0]
-    if (b > pair[1]) b = pair[1]
+const findRestaurant = function(list1, list2) {
+  const n = list1.length, m = list2.length
+  let result = [], index = Infinity
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (list1[i] === list2[j]) {
+        if (i + j <= index) {
+          if (i + j < index) result = []
+          result.push(list1[i])
+          index = i + j
+        }
+        break
+      }
+    }
   }
-
-  return a * b
+  return result
 }
+
+;[
+  [
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+  ],                            // ['Shogun']
+  [
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["KFC", "Shogun", "Burger King"]
+  ],                            // ['Shogun']
+].forEach(args => {
+  console.log(findRestaurant(...args))
+})
+
+// Solution:
+// 遍历 list1，每个元素 list1[i] 都再 list2 中找，找到第一个 list2[j]，
+// 比较记录中的 index 和 i + j，如小于 index 则将 result 清空再添加，
+// 如等于，则添加。
+
+// Submission Result: Accepted
