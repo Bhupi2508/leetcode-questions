b8593789e32534848fe5ea9dec9722aa4745fe83
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-10 22:54:18 +0530

    5 :::: 0946-validate-stack-sequences.js leetcode question updated (2022-07-10)

--- dsa.js
+++ dsa.js
@@ -1,68 +1,58 @@
-// 944. Delete Columns to Make Sorted
-// Easy   70%
+// 946. Validate Stack Sequences
+// Medium   61%
 
 
-// We are given an array A of N lowercase letter strings, all of the same length.
-// Now, we may choose any set of deletion indices, and for each string, we delete
-// all the characters in those indices.
-// For example, if we have an array A = ["abcdef","uvwxyz"] and deletion indices
-// {0, 2, 3}, then the final array after deletions is ["bef", "vyz"], and the
-// remaining columns of A are ["b","v"], ["e","y"], and ["f","z"].  (Formally,
-// the c-th column is [A[0][c], A[1][c], ..., A[A.length-1][c]].)
-// Suppose we chose a set of deletion indices D such that after deletions, each
-// remaining column in A is in non-decreasing sorted order.
-// Return the minimum possible value of D.length.
+// Given two sequences pushed and popped with distinct values, return true if and
+// only if this could have been the result of a sequence of push and pop
+// operations on an initially empty stack.
 
 // Example 1:
-// Input: ["cba","daf","ghi"]
-// Output: 1
-// Explanation:
-// After choosing D = {1}, each column ["c","d","g"] and ["a","f","i"] are in
-// non-decreasing sorted order.
-// If we chose D = {}, then a column ["b","a","h"] would not be in non-decreasing
-// sorted order.
+// Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+// Output: true
+// Explanation: We might do the following sequence:
+// push(1), push(2), push(3), push(4), pop() -> 4,
+// push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
 // Example 2:
-// Input: ["a","b"]
-// Output: 0
-// Explanation: D = {}
-// Example 3:
-// Input: ["zyx","wvu","tsr"]
-// Output: 3
-// Explanation: D = {0, 1, 2}
+// Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+// Output: false
+// Explanation: 1 cannot be popped before 2.
 
 // Note:
-//     1 <= A.length <= 100
-//     1 <= A[i].length <= 1000
+//     0 <= pushed.length == popped.length <= 1000
+//     0 <= pushed[i], popped[i] < 1000
+//     pushed is a permutation of popped.
+//     pushed and popped have distinct values.
 
 
 /**
- * @param {string[]} A
- * @return {number}
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
  */
-const minDeletionSize = function(A) {
-  let result = 0, p = A.length, q = A[0].length
-  for (let i = 0; i < q; i++) {
-    for (let j = 1; j < p; j++) {
-      if (A[j - 1][i] > A[j][i]) {
-        result++
-        break
-      }
+const validateStackSequences = function(pushed, popped) {
+  const stack = []
+  let i = 0
+  for (let x of pushed) {
+    stack.push(x)
+    while (stack.length && stack[stack.length - 1] === popped[i]) {
+      stack.pop()
+      i++
     }
   }
-  return result
+  return stack.length === 0
 }
 
 ;[
-  ['cba','daf','ghi'], // 1
-  ['a','b'],           // 0
-  ['zyx','wvu','tsr'], // 3
-].forEach((A) => {
-  console.log(minDeletionSize(A))
+  [[1,2,3,4,5], [4,5,3,2,1]], // true
+  [[1,2,3,4,5], [4,3,5,1,2]], // false
+  [[1,2,3,4,5], [4,3,5,2,1]], // true
+].forEach(([pushed, popped]) => {
+  console.log(validateStackSequences(pushed, popped))
 })
 
 // Solution:
-// 一列一列遍历，
-// 若为上一个大于下一个，则 result + 1, 跳出循环，
-// 遍历下一列。
+// 用一个栈 stack 来模拟一遍操作。
+// 每次将 pushed 中的数入栈后，都检查一遍栈顶的数是否与 popped[i] 相等，相等则出栈，再重复
+// 检查，直到不相等，再进将 pushed 的下一个数入栈。
 
 // Submission Result: Accepted
