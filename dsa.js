c3145ebf55c927d7a74196d15f25c38cafcea2f4
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-14 22:54:47 +0530

    5 :::: 1276-number-of-burgers-with-no-waste-of-ingredients.js leetcode question updated (2022-07-14)

--- dsa.js
+++ dsa.js
@@ -1,102 +1,58 @@
-// 1275. Find Winner on a Tic Tac Toe Game
-// Easy   53%
+// 1276. Number of Burgers with No Waste of Ingredients
+// Medium   50%
 
 
-// Tic-tac-toe is played by two players A and B on a 3 x 3 grid.
-// Here are the rules of Tic-Tac-Toe:
-//     Players take turns placing characters into empty squares (" ").
-//     The first player A always places "X" characters, while the second player B
-// always places "O" characters.
-//     "X" and "O" characters are always placed into empty squares, never on
-// filled ones.
-//     The game ends when there are 3 of the same (non-empty) character filling
-// any row, column, or diagonal.
-//     The game also ends if all squares are non-empty.
-//     No more moves can be played if the game is over.
-// Given an array moves where each element is another array of size 2
-// corresponding to the row and column of the grid where they mark their
-// respective character in the order in which A and B play.
-// Return the winner of the game if it exists (A or B), in case the game ends in
-// a draw return "Draw", if there are still movements to play return "Pending".
-// You can assume that moves is valid (It follows the rules of Tic-Tac-Toe), the
-// grid is initially empty and A will play first.
+// Given two integers tomatoSlices and cheeseSlices. The ingredients of different
+// burgers are as follows:
+//     Jumbo Burger: 4 tomato slices and 1 cheese slice.
+//     Small Burger: 2 Tomato slices and 1 cheese slice.
+// Return [total_jumbo, total_small] so that the number of remaining tomatoSlices
+// equal to 0 and the number of remaining cheeseSlices equal to 0. If it is not
+// possible to make the remaining tomatoSlices and cheeseSlices equal to 0 return
+// [].
 
 // Example 1:
-// Input: moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
-// Output: "A"
-// Explanation: "A" wins, he always plays first.
-// "X  "    "X  "    "X  "    "X  "    "X  "
-// "   " -> "   " -> " X " -> " X " -> " X "
-// "   "    "O  "    "O  "    "OO "    "OOX"
+// Input: tomatoSlices = 16, cheeseSlices = 7
+// Output: [1,6]
+// Explantion: To make one jumbo burger and 6 small burgers we need 4*1 + 2*6 =
+// 16 tomato and 1 + 6 = 7 cheese. There will be no remaining ingredients.
 // Example 2:
-// Input: moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
-// Output: "B"
-// Explanation: "B" wins.
-// "X  "    "X  "    "XX "    "XXO"    "XXO"    "XXO"
-// "   " -> " O " -> " O " -> " O " -> "XO " -> "XO "
-// "   "    "   "    "   "    "   "    "   "    "O  "
+// Input: tomatoSlices = 17, cheeseSlices = 4
+// Output: []
+// Explantion: There will be no way to use all ingredients to make small and
+// jumbo burgers.
 // Example 3:
-// Input: moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
-// Output: "Draw"
-// Explanation: The game ends in a draw since there are no moves to make.
-// "XXO"
-// "OOX"
-// "XOX"
+// Input: tomatoSlices = 4, cheeseSlices = 17
+// Output: []
+// Explantion: Making 1 jumbo burger there will be 16 cheese remaining and making
+// 2 small burgers there will be 15 cheese remaining.
 // Example 4:
-// Input: moves = [[0,0],[1,1]]
-// Output: "Pending"
-// Explanation: The game has not finished yet.
-// "X  "
-// " O "
-// "   "
+// Input: tomatoSlices = 0, cheeseSlices = 0
+// Output: [0,0]
+// Example 5:
+// Input: tomatoSlices = 2, cheeseSlices = 1
+// Output: [0,1]
 
 // Constraints:
-//     1 <= moves.length <= 9
-//     moves[i].length == 2
-//     0 <= moves[i][j] <= 2
-//     There are no repeated elements on moves.
-//     moves follow the rules of tic tac toe.
+//     0 <= tomatoSlices <= 10^7
+//     0 <= cheeseSlices <= 10^7
 
 
 /**
- * @param {number[][]} moves
- * @return {string}
+ * @param {number} tomatoSlices
+ * @param {number} cheeseSlices
+ * @return {number[]}
  */
-const tictactoe = function(moves) {
-  const gird = []
-  for (let i = 0; i < 3; i++) gird[i] = [0,0,0]
-  let i = 0
-  for (; i < moves.length; i++) {
-    let r = moves[i][0], c = moves[i][1]
-    gird[r][c] = i % 2 ? -1 : 1
-    if (
-      Math.abs(gird[r][0] + gird[r][1] + gird[r][2]) === 3 ||
-      Math.abs(gird[0][c] + gird[1][c] + gird[2][c]) === 3 ||
-      (r === c && Math.abs(gird[0][0] + gird[1][1] + gird[2][2]) === 3) ||
-      (r + c === 2 && Math.abs(gird[0][2] + gird[1][1] + gird[2][0]) === 3)
-    ) return i % 2 ? 'B' : 'A'
-  }
-  return i < 9 ? 'Pending' : 'Draw'
+const numOfBurgers = function(tomatoSlices, cheeseSlices) {
+
 }
 
 ;[
-  [[0,0],[2,0],[1,1],[2,1],[2,2]],
-  [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]],
-  [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]],
-  [[0,0],[1,1]],
-].forEach((moves) => {
-  console.log(tictactoe(moves))
+  [16, 7],
+].forEach(() => {
+
 })
 
 // Solution:
-// 创建一个 3*3 矩阵，全部填充 0，
-// A 下的位置填入 1， B 下的填入 -1
-// 每次下完，都检查一次棋盘
-// 检查下的棋子所在位置的行列，若在对角线上还需要检查对角线
-// 当 行、列、对角线 上的和的绝对值等于 3 （`1+1+1` 或 `-1-1-1`）时，则该次下棋则胜。
 
-//  1  0  0
-//  0  1  0
-// -1 -1  1
-
-// Submission Result: Accepted
+// Submission Result: Accept
