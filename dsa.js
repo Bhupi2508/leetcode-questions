4b77b1164008a4925581b74bc343ed6dbf6e0fe8
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-16 22:55:04 +0530

    5 :::: 1365-how-many-numbers-are-smaller-than-the-current-number.js leetcode question updated (2022-07-16)

--- dsa.js
+++ dsa.js
@@ -1,40 +1,58 @@
-// 1360. Number of Days Between Two Dates
-// Easy   49%
+// 1365. How Many Numbers Are Smaller Than the Current Number
+// Easy   85%
 
 
-// Write a program to count the number of days between two dates.
-// The two dates are given as strings, their format is YYYY-MM-DD as shown in the
-// examples.
+// Given the array nums, for each nums[i] find out how many numbers in the array
+// are smaller than it. That is, for each nums[i] you have to count the number of
+// valid j's such that j != i and nums[j] < nums[i].
+// Return the answer in an array.
 
 // Example 1:
-// Input: date1 = "2019-06-29", date2 = "2019-06-30"
-// Output: 1
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
 // Example 2:
-// Input: date1 = "2020-01-15", date2 = "2019-12-31"
-// Output: 15
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
 
 // Constraints:
-//     The given dates are valid dates between the years 1971 and 2100.
+//     2 <= nums.length <= 500
+//     0 <= nums[i] <= 100
 
 
 /**
- * @param {string} date1
- * @param {string} date2
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[]}
  */
-const daysBetweenDates = function(date1, date2) {
-  return Math.abs((new Date(date1) - new Date(date2)) / 86400000)
+const smallerNumbersThanCurrent = function(nums) {
+  const n = nums.length
+  const res = Array(n).fill(0)
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (nums[j] > nums[i]) res[j]++
+    }
+  }
+  return res
 }
 
 ;[
-  ['2019-06-29', '2019-06-30'],
-  ['2020-01-15', '2019-12-31'],
-].forEach(([date1, date2]) => {
-  console.log(daysBetweenDates(date1, date2))
+  [8,1,2,2,3],
+  [6,5,4,8],
+  [7,7,7,7],
+].forEach((nums) => {
+  console.log(smallerNumbersThanCurrent(nums))
 })
 
 // Solution:
-// 使用 Date API 解决（作弊）
-// TODO #1360 不使用 API
+// 初始化一个数组记录有多少数小于当前数。
+// 两两比较，大的下标加一。
 
 // Submission Result: Accepted
