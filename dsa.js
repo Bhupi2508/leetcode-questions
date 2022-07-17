870d7ee40e2f0297d065b9d4557543573eaf16a9
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-17 22:52:51 +0530

    6 :::: 0190-reverse-bits.js leetcode question updated (2022-07-17)

--- dsa.js
+++ dsa.js
@@ -1,59 +1,45 @@
-// 189. Rotate Array
-// Easy   24%
+// 190. Reverse Bits
+// Easy   29%
 
 
-// Rotate an array of n elements to the right by k steps.
+// Reverse bits of a given 32 bits unsigned integer.
 
-// For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
-// [5,6,7,1,2,3,4].
+// For example, given input 43261596 (represented in binary as
+// 00000010100101000001111010011100), return 964176192 (represented in binary as
+// 00111001011110000010100101000000).
 
-// Note:
-// Try to come up as many solutions as you can, there are at least 3 different
-// ways to solve this problem.
+// Follow up:
+// If this function is called many times, how would you optimize it?
 
-// [show hint]
+// Related problem: Reverse Integer
 
-// Hint:
-// Could you do it in-place with O(1) extra space?
-
-// Related problem: Reverse Words in a String II
-
-// Credits:Special thanks to @Freezen for adding this problem and creating all
-// test cases.
+// Credits:Special thanks to @ts for adding this problem and creating all test
+// cases.
 
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
  */
-const rotate = function(nums, k) {
-  // js-array-features
-  // k = k % nums.length
-  // for (let i = 0; i < k; i++) nums.unshift(nums.pop())
-
-  const n = nums.length
-  k = k % n
-  for (let j = 0; j < k; j++) {
-    const num = nums[n - 1]
-    for (let i = n - 1; i > 0; i--) {
-      nums[i] = nums[i - 1]
-    }
-    nums[0] = num
+const reverseBits = function(n) {
+  let result = 0
+  for (let i = 0; i < 32; i++) {
+    result = result * 2 + n % 2
+    n = n >>> 1
   }
+  return result
 }
 
 ;[
-  [[1], 0],                     // [1]
-  [[1,2,3,4], 3],               // [2,3,4,1]
-  [[1,2,3,4,5,6,7], 3],         // [5,6,7,1,2,3,4]
-  [[1,2,3,4,5,6,7,8,9,0], 3]    // [8,9,0,1,2,3,4,5,6,7]
-].forEach(args => {
-  console.log(rotate(...args))
+  43261596,                     // 964176192
+  1,                            // 2147483648
+  4294967295,                   // 4294967295
+].forEach(n => {
+  console.log(reverseBits(n))
 })
 
 // Solution:
-// 迭代k次，每次将最后一位移到最前面。
-// 或者作为一个队列，弹出最后一个，并弹入最前面。
+// 构造一个新的数字。
+// 像两个栈传递数据一样。
 
 // Submission Result: Accepted
