bffe0d28b49710576113d89b0e70617d09a6ab2e
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-18 22:45:03 +0530

    6 :::: 35-searchInsertPosition.js question updated

--- dsa.js
+++ dsa.js
@@ -1,31 +1,28 @@
 /**
- * Key: Two rounds of binary search, 1) first the left index, 2) find the right index
- *
  * @param {number[]} nums
  * @param {number} target
- * @return {number[]}
+ * @return {number}
  */
-var searchRange = function(nums, target) {
-    var lo = 0;
-    var hi = nums.length - 1;
-    var result = [-1, -1];
+// it not O(logN)
+var searchInsert = function(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+      if (nums[i] >= target) return i;
+  }
 
-    while (lo < hi) {
-        var mid = lo + Math.floor((hi - lo) / 2);
-        if (nums[mid] < target) lo = mid + 1;
-        else hi = mid;
-    }
-    if (nums[lo] !== target) return result;
-    else result[0] = lo;
+  return nums.length;
+};
 
-    hi = nums.length - 1;
-    while (lo < hi) {
-        // this +1 is very important!!! Make mid biased to the right
-        var mid = lo + Math.floor((hi - lo) / 2) + 1;
-        if (nums[mid] > target) hi = mid - 1;
-        else lo = mid;
-    }
-    result[1] = lo;
+// because it is a sorted array, we can use binary search.
+var searchInsert = function(nums, target) {
+  var low = 0;
+  var high = nums.length - 1;
 
-    return result;
+  while (low <= high) {
+      var mid = low + Math.floor((high - low) / 2);
+      if (nums[mid] === target) return mid;
+      if (target < nums[mid]) high = mid - 1;
+      else low = mid + 1;
+  }
+
+  return low;
 };
