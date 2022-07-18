a3cafb2e8c5fce3ac071eda7e984dce42ae613b1
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-18 22:45:01 +0530

    6 :::: 331-verifyBinaryTree.js question updated

--- dsa.js
+++ dsa.js
@@ -1,61 +1,34 @@
 /**
- * Binary search. In the rotated array, there must be a half of the array in sorted order.
- * @see the recursive method. If the target is in the sorted half, binary search the target in
- * this half. Otherwise, search the target in the other half. And recursively do this process until
- * the target is found.
- *
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
+ * Solution: use stack
+ * Note, stack[stack.length - 3] !== '#' is important. for the case "9, #, #,#,# "
+ * @param {string} preorder
+ * @return {boolean}
  */
-var search = function(nums, target) {
-    return searchHelper(nums, 0, nums.length - 1, target);
-};
+var isValidSerialization = function(preorder) {
+    var stack = [];
+    var i = 0;
+    var newStr = preorder.split(',');
+    var length = newStr.length;
 
-var searchHelper = function(nums, left, right, target) {
-    if (right < left) return -1;
-
-    var mid = left + Math.floor((right - left) / 2);
-
-    if (nums[mid] === target) return mid;
-
-    if (nums[left] <= nums[mid]) {
-        if (target >= nums[left] && target < nums[mid]) {
-            return searchHelper(nums, left, mid - 1, target);
-        } else {
-            return searchHelper(nums, mid + 1, right, target);
+    while (i < length) {
+        stack.push(newStr[i]);
+        while (stack.length > 2 && stack[stack.length - 1] === '#' &&
+            stack[stack.length - 2] === '#' && stack[stack.length - 3] !== '#') {
+            stack.pop();
+            stack.pop();
+            stack.pop();
+            stack.push('#');
         }
-    } else {
-        if (target <= nums[right] && target > nums[mid]) {
-            return searchHelper(nums, mid + 1, right, target);
-        } else {
-            return searchHelper(nums, left, mid - 1, target);
-        }
-    }
-};
-
-// Interative
-var search = function(nums, target) {
-    var left = 0;
-    var right = nums.length - 1;
-
-    while (left < right) {
-        var mid = left + Math.floor((right - left) / 2);
-        if (nums[mid] === target) return mid;
-        if (nums[left] <= nums[mid]) {
-            if (target >= nums[left] && target < nums[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        } else {
-            if (target <= nums[right] && target > nums[mid]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+        i++;
     }
 
-    return nums[left] === target ? left : -1;
+    if (stack.length === 1 && stack[0] === '#') {
+        return true;
+    }
+    return false;
 };
+
+
+// Testing cases
+// "9,3,4,#,#,1,#,#,2,#,6,#,#"
+// "1, #, #, #, #"
