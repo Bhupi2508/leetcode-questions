2e89ffc610f00e8b39529a94e7090a76a674cbd9
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-22 22:44:42 +0530

    6 :::: 127-wordLadder.js question updated

--- dsa.js
+++ dsa.js
@@ -1,37 +1,54 @@
 /**
- * Key: because no limitation, as long as the price is heigher than before, the profit
- * can be added to maxProfit.
+ * Solution: BFS
+ * use a queue to save candidates from wordList on each level.
+ * find next next candiate from the queue.
  *
- * @param {number[]} prices
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {Set} wordList
+ *   Note: wordList is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
  * @return {number}
  */
-// This solution in fact doesn't follow the rule (you're not allowed to buy and sell at the same time),
-// But mathmatically it is right.
-var maxProfit = function(prices) {
-    var maxProfit = 0;
-    for (var i = 1; i < prices.length; i++) {
-        var profit = prices[i] - prices[i - 1];
-        if (profit > 0) maxProfit += profit;
-    }
-
-    return maxProfit;
-};
-
-// 2nd try, my own solution
-var maxProfit = function(prices) {
-    var maxProfit = 0;
-    var minPrice = prices[0];
-    for (var i = 0; i < prices.length;) {
-        i++;
-        while (prices[i] > prices[i-1]) {
-            i++;
+var ladderLength = function(beginWord, endWord, wordList) {
+    if (!beginWord) return 0;
+    var queue = [beginWord];
+    var step = 1;
+    wordList.add(endWord);
+    while (queue.length > 0) {
+        var queueLength = queue.length;
+        for (var i = 0; i < queueLength; i++) {
+            var word = queue.shift();
+            if (word === endWord) return step;
+            for (var k = 0; k < word.length; k++) {
+                var tmp = word[k];
+                for (var j = 97; j <= 122; j++) {
+                    if (word.charCodeAt(k) !== j) {
+                        charAtK = String.fromCharCode(j);
+                        word = word.substring(0, k) + charAtK + word.substring(k + 1);
+                        if (wordList.has(word)) {
+                            // save the candidate to the queue of this level
+                            queue.push(word);
+                            // meanwhile, delete this candidate from wordList
+                            wordList.delete(word);
+                        }
+                    }
+                }
+                // reset word, for next character change
+                word = word.substring(0, k) + tmp + word.substring(k + 1);
+            }
         }
-        maxProfit += prices[i-1] - minPrice;
-        minPrice = prices[i];
+        step++;
     }
-    return maxProfit;
+
+    return 0;
 };
 
-// test cases
-// [5,4,1,2,7,8]
-// [1,2,5,4,6,8]
+// for testing
+// var a = 'hit';
+// var b = 'cog';
+// var set = new Set(["hot","dot","dog","lot","log"]);
+// var a = 'a';
+// var b = 'c';
+// var set = new Set(['a', 'b', 'c']);
+// console.log(ladderLength(a, b, set));
