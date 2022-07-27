5c716fd6005d2d29e298ed43b0567c79bf646672
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-07-27 22:44:54 +0530

    7 :::: 230-kthSmallestElementinBST.js question updated

--- dsa.js
+++ dsa.js
@@ -1,40 +1,49 @@
 /**
- * @see http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
- * @param {number[]} nums
- * @return {number[]}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var majorityElement = function(nums) {
-    var n1 = null;
-    var n2 = null;
-    var c1 = 0;
-    var c2 = 0;
+/**
+ * Key: just use inorderTraversal to traverse the tree. Inorder traversal
+ * travels the node in a ascending order because it is a BST...
+ *
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    var stack = [];
+    var node = root;
 
-    for (var i = 0; i < nums.length; i++) {
-        if (n1 !== null && n1 === nums[i]) {
-            c1++;
-        } else if (n2 !== null && n2 === nums[i]) {
-            c2++;
-        } else if (c1 === 0) {
-            n1 = nums[i];
-            c1++;
-        } else if (c2 === 0) {
-            n2 = nums[i];
-            c2++;
+    while (node || stack.length > 0) {
+        if (node) {
+            stack.push(node);
+            node = node.left;
         } else {
-            c1--;
-            c2--;
+            node = stack.pop();
+            k--;
+            if (k === 0) return node.val;
+            node = node.right;
         }
     }
+};
 
-    c1 = 0;
-    c2 = 0;
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] === n1) c1++;
-        if (nums[i] === n2) c2++;
+// second try
+var kthSmallest = function(root, k) {
+    var stack = [];
+
+    while (root || stack.length > 0) {
+        while (root) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        k--;
+        if (k === 0) return root.val;
+        root = root.right;
     }
 
-    var result = [];
-    if (c1 > Math.floor(nums.length / 3)) result.push(n1);
-    if (c2 > Math.floor(nums.length / 3)) result.push(n2);
-    return result;
+    return 0;
 };
