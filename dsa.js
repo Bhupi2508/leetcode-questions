c40a98ade7d314e63650d1cb19a47baee62c8917
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-03 22:44:58 +0530

    8 :::: 284-peekingIterator.java question updated

--- dsa.js
+++ dsa.js
@@ -1,18 +1,41 @@
-/**
- * @param {number[]} citations
- * @return {number}
- */
-// sort first. 
-var hIndex = function(citations) {
-    citations.sort(function(a, b) {
-        return a - b;
-    });
+// No JavaScript supported
+// peek() means peek next elment but pointer doesn't move (i.e. still points the next elment)
+// Solution, think the iterator as the the pointer, cache next value
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+class PeekingIterator implements Iterator<Integer> {
+    Integer next = null;
+    Iterator<Integer> iter;
 
-    var result = 0;
-    for (var i = 0, length = citations.length; i < length; i++) {
-        var min = Math.min(citations[i], length - i);
-        result = Math.max(result, min);
-    }
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+	    this.iter = iterator;
+	    if (iter.hasNext()) {
+	        this.next = iter.next();
+	    }
+	}
 
-    return result;
-};
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return this.next;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    Integer nextCache = next;
+	    if (iter.hasNext()) {
+	        next = iter.next();
+	    } else {
+	        next = null;
+	    }
+
+	    return nextCache;
+	}
+
+	@Override
+	public boolean hasNext() {
+	    return next != null;
+	}
+}
