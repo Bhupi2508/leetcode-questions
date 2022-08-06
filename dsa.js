a65c94e1a52761f078669831023a8167c5b67df1
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-06 22:54:48 +0530

    8 :::: 1290-convert-binary-number-in-a-linked-list-to-integer.js leetcode question updated (2022-08-06)

--- dsa.js
+++ dsa.js
@@ -1,82 +1,67 @@
-// 1287. Element Appearing More Than 25% In Sorted Array
-// Easy   60%
+// 1290. Convert Binary Number in a Linked List to Integer
+// Easy   80%
 
 
-// Given an integer array sorted in non-decreasing order, there is exactly one
-// integer in the array that occurs more than 25% of the time.
-// Return that integer.
+// Given head which is a reference node to a singly-linked list. The value of
+// each node in the linked list is either 0 or 1. The linked list holds the
+// binary representation of a number.
+// Return the decimal value of the number in the linked list.
 
 // Example 1:
-// Input: arr = [1,2,2,6,6,6,6,7,10]
-// Output: 6
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+// Example 2:
+// Input: head = [0]
+// Output: 0
+// Example 3:
+// Input: head = [1]
+// Output: 1
+// Example 4:
+// Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+// Output: 18880
+// Example 5:
+// Input: head = [0,0]
+// Output: 0
 
 // Constraints:
-//     1 <= arr.length <= 10^4
-//     0 <= arr[i] <= 10^5
+//     The Linked List is not empty.
+//     Number of nodes will not exceed 30.
+//     Each node's value is either 0 or 1.
 
 
 /**
- * @param {number[]} arr
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ * this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
  * @return {number}
  */
-const findSpecialInteger = function(arr) {
-  const minLength = Math.floor(arr.length / 4)
-  for (let i = 0; i < arr.length - minLength; i++) {
-    if (arr[i] === arr[i + minLength]) return arr[i]
+const getDecimalValue = function(head) {
+  let res = 0
+  while (head) {
+    res = res * 2 + head.val
+    head = head.next
   }
-  return arr[0]
+  return res
 }
 
-const recursion = function(arr) {
-  const n = arr.length
-  if (n === 1) return arr[0]
-
-  const minLength = Math.floor(n / 4) + 1
-  
-  function find(p, q) {
-    if (q === p || q - p < minLength - 1) return -1
-    if (q - p === minLength - 1) return arr[p] === arr[q] ? arr[p] : -1
-
-    const mid = Math.floor((p + q) / 2)
-
-    const left = find(p, mid)
-    if (left > 0) return left
-
-    const right = find(mid + 1, q)
-    if (right > 0) return right
-
-    for (let i = 0; i < minLength; i++) {
-      if (arr[mid - i] === arr[mid - i + minLength - 1]) {
-        return arr[mid - i]
-      }
-    }
-    return -1
-  }
-
-  return find(0, n - 1)
-}
-
+const ListNode = require('../structs/ListNode')
 ;[
-  [1],                         // 1
-  [0,0,1],                     // 0
-  [0,1,1,2],                   // 1
-  [1,2,3,3],                   // 3
-  [1,2,2,6,6,6,6,7,10],        // 6
-  [0,0,0,1,1,1,2,2,2,3,3,3,3], // 3
-  [1,1,2,2,3,3,3,3]            // 3
+  [1,0,1], // 5
+  [0], // 0
+  [1], // 1
+  [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0], //18880
+  [0,0], // 0
 ].forEach((arr) => {
-  console.log(findSpecialInteger(arr))
+  console.log(getDecimalValue(ListNode.from(arr)))
 })
 
 // Solution:
-// 算法一： 遍历一遍
-// 先根据 arr 长度计算 minLength (至少需要几个数是相同的)
-// 因为数组已经排序，
-// 遍历数组时，比较 arr[i] 和 arr[i + minLength]，相同即可返回该数
-
-// 算法二： 递归
-// 强行二分递归，算法难度增加，时间空间复杂度也不好计算
-// 编码时间增加，运行速度都不一定增加
-// 算法能力增强前，尽量写这样的算法
+// 遍历链表，每遍历一个节点，就将 res 乘 2（或左移一位），再加上该节点的值。
 
 // Submission Result: Accepted
