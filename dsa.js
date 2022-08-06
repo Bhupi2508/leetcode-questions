a4e29c42e0b4bda0435ce03943c1988b6eb1a127
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-06 22:53:40 +0530

    8 :::: 0643-maximum-average-subarray-i.js leetcode question updated (2022-08-06)

--- dsa.js
+++ dsa.js
@@ -1,82 +1,52 @@
-// 637. Average of Levels in Binary Tree
-// Easy   55%
+// 643. Maximum Average Subarray I
+// Easy   37%
 
-// Given a non-empty binary tree, return the average value of the nodes on each
-// level in the form of an array.
+
+// Given an array consisting of n integers, find the contiguous subarray of given
+// length k that has the maximum average value. And you need to output the
+// maximum average value.
 
 // Example 1:
 
-// Input:
-//     3
-//    / \
-//   9  20
-//     /  \
-//    15   7
-// Output: [3, 14.5, 11]
-// Explanation:
-// The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level
-// 2 is 11. Hence return [3, 14.5, 11].
+// Input: [1,12,-5,-6,50,3], k = 4
+// Output: 12.75
+// Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
 
 // Note:
 
-// The range of node's value is in the range of 32-bit signed integer.
+// 1 <= k <= n <= 30,000.
+// Elements of the given array will be in the range [-10,000, 10,000].
 
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *   this.val = val;
- *   this.left = this.right = null;
- * }
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
  */
-
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-const averageOfLevels = function(root) {
-  if (root == null) return []
-  const result = []
-  const queue = [root]
-  while (queue.length) {
-    let sum = 0, l = queue.length
-    for (let i = l; i > 0; i--) {
-      const node = queue.shift()
-      sum += node.val
-      if (node.left) queue.push(node.left)
-      if (node.right) queue.push(node.right)
-    }
-    result.push(sum / l)
+const findMaxAverage = function(nums, k) {
+  let result = -Infinity
+  for (let i = 0, sum = 0, n = nums.length; i < n; i++) {
+    sum += i < k ? nums[i] : nums[i] - nums[i - k]
+    if (i >= k - 1) result = Math.max(result, sum)
   }
-  return result
+  return result / k
 }
 
-
-const old = function(root) {
-  const result = []
-  function iter(root, i) {
-    if (root == null) return
-    if (!result[i]) result[i] = []
-    result[i].push(root.val)
-    iter(root.left, i + 1)
-    iter(root.right, i + 1)
-  }
-  iter(root, 0)
-  return result.map(arr => arr.reduce((sum, i) => sum + i) / arr.length)
-}
-
-const TreeNode = require('../structs/TreeNode')
 ;[
-  [3,9,20,null,null,15,7], // [3, 14.5, 11]
-].forEach((array) => {
-  console.log(averageOfLevels(TreeNode.from(array)))
+  [[1,12,-5,-6,50,3], 4],            // 12.75
+  [[1,2], 1],                        // 2
+  [[-1], 1],                         // -1
+].forEach((args) => {
+  console.log(findMaxAverage(...args))
 })
 
 // Solution:
-// 层遍历？
-// 应该是吧，有个参数i，记录层数。
-// 代码看起来好像还不够优雅。
+// 想象拥有一个长度为 k 的队列，随着队列不断沿着数组向前，新的元素不断进入队列的
+// 头部，而队列尾部的元素不断退出。队列满了之后，前进一位，与记录中的队列总和比
+// 较，更新记录。
+// 记录过程只记录总和，没必要算平均数，总和越大平均越大（k 确定）。
 
-// 使用 先进先出列表 完成层遍历
+// 编程经验：每次使用 Math.max(.min) 时，都应该注意两个参数的初始值。
+
 
 // Submission Result: Accepted
