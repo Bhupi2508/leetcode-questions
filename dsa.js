5345901cf146762350e45f5d81886dd5f6cf3d9c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-08 22:52:35 +0530

    9 :::: 0101-symmetric-tree.js leetcode question updated (2022-08-08)

--- dsa.js
+++ dsa.js
@@ -1,10 +1,26 @@
-// 100. Same Tree
-// Easy   46%
+// 101. Symmetric Tree
+// Easy   39%
 
-// Given two abinary trees, write a function to check if they are equal or not.
+// Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+// around its center).
 
-// Two binary trees are considered equal if they are structurally identical and
-// the nodes have the same value.
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
+
+// But the following [1,2,2,null,3,null,3] is not:
+
+//    1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+
+// Note: Bonus points if you could solve it both recursively and iteratively.
 
 /**
  * Definition for a binary tree node.
@@ -15,29 +31,34 @@
  */
 
 /**
- * @param {TreeNode} p
- * @param {TreeNode} q
+ * @param {TreeNode} root
  * @return {boolean}
  */
-const isSameTree = function(p, q) {
-  if (p === q) return true
-  if (p == null || q == null || p.val !== q.val) return false
-  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+const isSymmetric = function(root) {
+  if (root == null) return true
+  function iter(left, right) {
+    if (left === right) return true
+    if (left && right && left.val === right.val) {
+      return iter(left.left, right.right) && iter(left.right, right.left)
+    }
+    return false
+  }
+  return iter(root.left, root.right)
 }
-
 
 const TreeNode = require('../structs/TreeNode')
 ;[
-  [[1, 2, 2, 3, 4, 3, 4], [1, 2, 2, 3, 4, 3, 4]], // true
-].forEach(([p, q]) => {
-  console.log(isSameTree(TreeNode.from(p), TreeNode.from(q)))
+  [1, 2, 2, 3, 4, 4, 3],        // true
+  [1,2,2,null,3,null,3],        // false
+].forEach(array => {
+  console.log(isSymmetric(TreeNode.from(array)))
 })
 
 // Solution:
-// 递归遍历两棵树的相同位置的节点（包括空子节点，即 null）
-// 1. 如果两节点相同则返回 true，这种情况中只有一种可能，两节点同为 null,
-//    因为两个 js 对象的内容相同，指针不同，比较起来值是不同的。
-// 2. 如果两节点不同为 null, 只有一个为 null, 值必然不同。
-// 3. 两个值均不为 null, 且均不相同，值也不同。
+// 比较左右子节点对象是否相同，相同（同为 null）则返回 true
+// 比较左右子节点的值是否相同，相同则递归返回（&&运算）
+//  1. 左节点的左子树和右节点的右子树是否相同
+//  2. 左节点的右子树和右节点的左子树是否相同
+// 否则返回 false
 
 // Submission Result: Accepted
