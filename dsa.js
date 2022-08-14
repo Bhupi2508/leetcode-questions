f560522b52835d817dcee2407ec872670db9daca
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-14 22:54:02 +0530

    10 :::: 0836-rectangle-overlap.js leetcode question updated (2022-08-14)

--- dsa.js
+++ dsa.js
@@ -1,90 +1,70 @@
-// 833. Find And Replace in String
-// Medium   38%
+// 836. Rectangle Overlap
+// Easy   49%
 
 
-// To some string S, we will perform some replacement operations that replace
-// groups of letters with new ones (not necessarily the same size).
-
-// Each replacement operation has 3 parameters: a starting index i, a source word
-// x and a target word y.  The rule is that if x starts at position i in the
-// original string S, then we will replace that occurrence of x with y.  If not,
-// we do nothing.
-
-// For example, if we have S = "abcd" and we have some replacement operation i =
-// 2, x = "cd", y = "ffff", then because "cd" starts at position 2 in the
-// original string S, we will replace it with "ffff".
-
-// Using another example on S = "abcd", if we have both the replacement operation
-// i = 0, x = "ab", y = "eee", as well as another replacement operation i = 2, x
-// = "ec", y = "ffff", this second operation does nothing because in the original
-// string S[2] = 'c', which doesn't match x[0] = 'e'.
-
-// All these operations occur simultaneously.  It's guaranteed that there won't
-// be any overlap in replacement: for example, S = "abc", indexes = [0, 1],
-// sources = ["ab","bc"] is not a valid test case.
-
+// A rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) are the
+// coordinates of its bottom-left corner, and (x2, y2) are the coordinates of its
+// top-right corner.
+// Two rectangles overlap if the area of their intersection is positive.  To be
+// clear, two rectangles that only touch at the corner or edges do not overlap.
+// Given two (axis-aligned) rectangles, return whether they overlap.
 // Example 1:
-// Input: S = "abcd", indexes = [0,2], sources = ["a","cd"], targets =
-// ["eee","ffff"]
-// Output: "eeebffff"
-// Explanation: "a" starts at index 0 in S, so it's replaced by "eee".
-// "cd" starts at index 2 in S, so it's replaced by "ffff".
-
+// Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+// Output: true
 // Example 2:
-// Input: S = "abcd", indexes = [0,2], sources = ["ab","ec"], targets =
-// ["eee","ffff"]
-// Output: "eeecd"
-// Explanation: "ab" starts at index 0 in S, so it's replaced by "eee".
-// "ec" doesn't starts at index 2 in the original S, so we do nothing.
-
+// Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+// Output: false
 // Notes:
-//     0 <= indexes.length = sources.length = targets.length <= 100
-//     0 < indexes[i] < S.length <= 1000
-//     All characters in given inputs are lowercase letters.
+//     Both rectangles rec1 and rec2 are lists of 4 integers.
+//     All coordinates in rectangles will be between -10^9 and 10^9.
 
 
 /**
- * @param {string} S
- * @param {number[]} indexes
- * @param {string[]} sources
- * @param {string[]} targets
- * @return {string}
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
  */
-const findReplaceString = function(S, indexes, sources, targets) {
-  const operations = []
-  for (let i = 0; i < indexes.length; i++) {
-    operations.push([indexes[i], sources[i], targets[i]])
-  }
-  operations.sort((a, b) => b[0] - a[0])
-  for (let operation of operations) {
-    const index = operation[0]
-    const source = operation[1]
-    const substr = S.substr([index], source.length)
-    S = S.slice(0, index) + (substr === source ? operation[2] : substr) +
-      S.slice(index + source.length)
-  }
-  return S
+const isRectangleOverlap = function(rec1, rec2) {
+  return rec1[0] < rec2[2] && rec2[0] < rec1[2] && rec1[1] < rec2[3] && rec2[1] < rec1[3]
 }
 
 ;[
-  ['abcd', [0, 2], ['a', 'cd'], ['eee', 'ffff']], // 'eeebffff'
-  ['abcd', [0, 2], ['ab', 'ec'], ['eee', 'ffff']], // 'eeecd'
-  ['vmokgggqzp', [3,5,1], ['kg','ggq','mo'], ['s','so','bfr']], // 'vbfrssozp'
-].forEach((args) => {
-  console.log(findReplaceString(...args))
+  [[0,0,2,2], [1,1,3,3]], // true
+  [[0,0,1,1], [1,0,2,1]], // false
+  [[7,8,13,15], [10,8,12,20]], // true
+  [[1,1,3,2], [2,0,4,3]], // true
+].forEach(([rec1, rec2]) => {
+  console.log(isRectangleOverlap(rec1, rec2))
 })
 
 // Solution:
 
-// 方法 1：
-// 使用一个数组字符串保存不参与替换的和参与替换的子字符串。
-// 按 indexes 从头到尾的顺序，比较要替换的子字符串，若匹配，则将target放到数组，
-// 否则放原子字符串。
-// 最后将字符串拼合得到答案。
-// 缺陷：操作的下标没有排序，会导致不参与替换的子字符串错乱。
-// 解决方法：先排序操作。
+// 1. 判断是否存在一个矩形的四个点中的一个在另一个矩阵内
+// 【错误】不一定有点在另一个矩阵内才重合，例如一个矩阵的一条边横穿另一个矩阵
+const faild = function(rec1, rec2) {
+  function isInner(i, j) {
+    return rec1[0] < i && i < rec1[2] && rec1[1] < j && j < rec1[3]
+  }
+  return isInner(rec2[0], rec2[1]) ||
+    isInner(rec2[0], rec2[3]) ||
+    isInner(rec2[2], rec2[3]) ||
+    isInner(rec2[2], rec2[1])
+}
 
-// 方法 2：
-// 排序操作，从右到左操作。在原字符串上操作。
+// 2. 大神的思路(真的神！)
+// 先考虑 1D 情况
+// 若两条线 (a1,b1) (a2,b2) 重叠
+// a1------b1     | a1----b1          |           a1-----b1 |     a1------b1
+//     a2------b2 |         a2-----b2 | a2-----b2           | a2-------------b2
+// 则 a1 < b2 && a2 < b1
+
+// 再回到 2D 的情况
+// 若两个矩阵在 x 轴和 y 轴上都重叠，则它们在平面上重叠
+
+// x 轴上的点为 （rec1[0],rec1[2]) (rec2[0],rec2[2])
+// 即需 rec1[0] < rec2[2] && rec2[0] < rec1[2]
+
+// y 轴上的点为  (rec1[1],rec1[3]) (rec2[1],rec2[3])
+// 即需 rec1[1] < rec2[3] && rec2[1] < rec1[3]
 
 // Submission Result: Accepted
