bd14d4ac65f0b7aca82d6db549463f33c6a4d5ec
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-15 22:44:58 +0530

    10 :::: 29-DivideTwoIntegers.js question updated

--- dsa.js
+++ dsa.js
@@ -1,41 +1,27 @@
-// No JavaScript supported
-// peek() means peek next elment but pointer doesn't move (i.e. still points the next elment)
-// Solution, think the iterator as the the pointer, cache next value
-// Java Iterator interface reference:
-// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
-class PeekingIterator implements Iterator<Integer> {
-    Integer next = null;
-    Iterator<Integer> iter;
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+// Time Limit Exceeded  
+var divide = function(dividend, divisor) {
+    if (divisor === 0) return 0;
+    var aDividend = Math.abs(dividend);
+    var aDivisor = Math.abs(divisor);
 
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    this.iter = iterator;
-	    if (iter.hasNext()) {
-	        this.next = iter.next();
-	    }
-	}
+    var result = 0;
+    while (aDividend >= aDivisor) {
+        var shifts = 0;
+        while (aDividend >= (aDivisor << shifts)) {
+            shifts++;
+        }
+        result += (1 << (shifts - 1));
+        aDividend -= (aDivisor << (shifts - 1));
+    }
 
-    // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-        return this.next;
-	}
-
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-	    Integer nextCache = next;
-	    if (iter.hasNext()) {
-	        next = iter.next();
-	    } else {
-	        next = null;
-	    }
-
-	    return nextCache;
-	}
-
-	@Override
-	public boolean hasNext() {
-	    return next != null;
-	}
-}
+    if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+        return result;
+    } else {
+        return -1 * result;
+    }
+};
