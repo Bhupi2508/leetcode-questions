74db0f54867766ff0c790e5670c298049df8b151
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-16 22:53:41 +0530

    10 :::: 0647-palindromic-substrings.js leetcode question updated (2022-08-16)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,57 @@
-// 645. Set Mismatch
-// Easy 40% locked:false
-
-
-// The set S originally contains numbers from 1 to n. But unfortunately, due to
-// the data error, one of the numbers in the set got duplicated to another number
-// in the set, which results in repetition of one number and loss of another
-// number.
-
-// Given an array nums representing the data status of this set after the error.
-// Your task is to firstly find the number occurs twice and then find the number
-// that is missing. Return them in the form of an array.
-
-// Example 1:
-
-// Input: nums = [1,2,2,4]
-// Output: [2,3]
-
-// Note:
-
-// The given array size will in the range [2, 10000].
-// The given array's numbers won't have any order.
-
-
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-const findErrorNums = function(nums) {
-  const length = nums.length
-  for (let i = 0; i < length;) {
-    const num = nums[i], swap = nums[num - 1]
-    if (num !== i + 1 && num !== swap) {
-      [nums[num - 1], nums[i]] = [num, swap]
-    } else {
-      i++
-    }
-  }
-
-  let i = 0
-  while (i < length && nums[i] === ++i);
-  return [nums[i - 1], i]
-}
-
-;[
-  [1,2,2,4],                    // [2,3]
-  [6,4,2,3,1,5,8,8],            // [8,7]
-  [6,4,2,3,1,5,8,8,7],          // [8,9]
-  [1,1],                        // [1,2]
-  [2,2],                        // [2,1]
-].forEach(nums => {
-  console.log(findErrorNums(nums))
-})
-
-// Solution:
-// 先为元素找位置。
-// 在交换元素上要保证，交换的元素不同，不然会进入死循环。
-// 最后缺少的元素的位置上会出现重复的那个元素。
-// 再遍历一遍就可以找出来了。
-
-// Submission Result: Accepted
+// 647. Palindromic Substrings
+// Medium   60%
+
+// Given a string, your task is to count how many palindromic substrings in this
+// string.
+
+// The substrings with different start indexes or end indexes are counted as
+// different substrings even they consist of same characters.
+
+// Example 1:
+
+// Input: "abc"
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
+
+// Example 2:
+
+// Input: "aaa"
+// Output: 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+// Note:
+
+// The input string length won't exceed 1000.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = function(s) {
+  s = '#' + s.split('').join('#') + '#'
+  let result = 0
+  for (let i = 0, n = s.length; i < n; i++) {
+    let j = 0
+    while (i + j < n && i >= j && s[i + j] === s[i - j]) j++
+    result += Math.trunc(j / 2)
+  }
+  return result
+}
+
+;[
+  'abc',                        // 3
+  'aaa',                        // 6
+  'aaaaa',                      // 15
+].forEach(s => {
+  console.log(countSubstrings(s))
+})
+
+// Solution:
+// 处理回文字符串时，在字符串两边以及每个字符间插入间隔符号（如#）
+// 会比较好处理。
+// 因为有的字符串是以中间空字符对称的，而有的是以中间非空字符对称的。
+
+// 每到一个字符，都计算以其为中心最大的对称半径长度。
+// 因为插入了间隔符号，且间隔符号的数量占长度的一半，因此需要折半。
+
+// Submission Result: Accepted
