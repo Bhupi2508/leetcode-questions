6ec4aeb438920bfd1d6c6b19cf6e7e5b97ada6cb
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-17 22:52:02 +0530

    10 :::: 0014-longest-common-prefix.js leetcode question updated (2022-08-17)

--- dsa.js
+++ dsa.js
@@ -1,57 +1,38 @@
-// 13. Roman to Integer
-// Easy   46%
+// 14. Longest Common Prefix
+// Easy   31%
 
-// Given a roman numeral, convert it to an integer.
-
-// Input is guaranteed to be within the range from 1 to 3999.
+//Write a function to find the longest common prefix string amongst an array of
+//strings.
 
 /**
- * @param {string} s
- * @return {number}
+ * @param {string[]} strs
+ * @return {string}
  */
-const romanToInt = function(s) {
-  const Roman = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+const longestCommonPrefix = function(strs) {
+  if (strs == null || strs.length === 0) return ''
+  const n = strs.length, m = strs[0].length
+  for (let i = 0; i < m; i++) {
+    const c = strs[0][i]
+    for (let j = 1; j < n; j++) {
+      if (strs[j][i] !== c) return strs[0].substring(0, i)
+    }
   }
-
-  let result = 0
-  for (let i = s.length - 1; i >= 0; i--) {
-    const high = Roman[s[i]], low = (Roman[s[i + 1]] || 0)
-    result += (high < low ? -1 : 1) * high
-  }
-
-  return result
+  return strs[0]
 }
 
 ;[
-  'I',                          // 1
-  'IV',                         // 4
-  'MMMCMXCIX',                  // 3999
-].forEach(s => {
-  console.log(romanToInt(s))
+  [],                           // ''
+  [''],                         // ''
+  ['abc'],                      // 'abc'
+  ['a', 'b'],                   // ''
+  ['123', '123456', '1232'],    // '123'
+].forEach(strs => {
+  console.log(longestCommonPrefix(strs))
 })
 
 // Solution:
-// 难点在于辨别比标志符号代表的数小1的符号。如 4=IV，9=IX。
-
-// 用一个对象保存符号代表的数值。
-// 从字符串尾部开始向前遍历，遍历到的字符转换成其代表的数值
-// 如果该位的数值大于其后一位字符表示的数值，则直接将该值加入到结果中。
-// 否则减去该数值。
-
-// 例如
-// IV = (-1)*1 + 1*5 = 4
-// IX = (-1)*1 + 1*10 = 9
-// MMMCMXCIX = 1*1000 + 1*1000 + 1*1000 + (-1)*100 + 1*1000
-//             (-1)*10 + 1*100 + (-1)*1 + 1*10
-//           = 3000 - 100 + 1000 - 10 + 100 - 1 + 10
-//           = 3000 + 900 + 90 + 9
-//           = 3999
+// 垂直扫描，从头开始比较全部字符串中相同下标的字符是否相同
+// 若有一个不同，则返回0到该下标之间（包括0不包括该下标）的子字符串
+// 全部相同则检查下一个下标。
 
 // Submission Result: Accepted
