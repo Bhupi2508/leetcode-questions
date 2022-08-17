9de5134d3547c0b0eec2453990ead713c61f194f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-17 22:44:50 +0530

    10 :::: 179-largestNumber.js question updated

--- dsa.js
+++ dsa.js
@@ -1,50 +1,26 @@
 /**
- * Definition for binary tree
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * @param {number[]} nums
+ * @return {string}
  */
-
-/**
- * @constructor
- * @param {TreeNode} root - root of the binary search tree
- */
-// Is there a better solution?
-var BSTIterator = function(root) {
-    this.stack = [];
-    this.pushStack(root);
+var largestNumber = function(nums) {
+    nums.sort(function(a, b) {
+        var ab = a + '' + b;
+        var ba = b + '' + a;
+        if (ab < ba) return 1;
+        else if (ab > ba) return -1;
+        return 0;
+    });
+    if (nums[0] === 0) return '0';
+    return nums.join('');
 };
 
-/**
- * @this BSTIterator
- * @returns {boolean} - whether we have a next smallest number
- */
-BSTIterator.prototype.hasNext = function() {
-    return this.stack.length > 0 ? true : false;
+// this is a wrong solution e.g. [121,12] '12' < '121', then it doesn't work.
+var largestNumber = function(nums) {
+    nums.sort(function(a, b) {
+        if (a.toString() < b.toString()) return 1;
+        else if (a.toString() > b.toString()) return -1;
+        else return 0;
+    });
+    if (nums[0] === 0) return '0';
+    return nums.join('');
 };
-
-/**
- * @this BSTIterator
- * @returns {number} - the next smallest number
- */
-BSTIterator.prototype.next = function() {
-    var small = this.stack.pop();
-    if (small.right) {
-        this.pushStack(small.right);
-    }
-    return small.val;
-};
-
-BSTIterator.prototype.pushStack = function(node) {
-    while (node) {
-        this.stack.push(node);
-        node = node.left;
-    }
-};
-
-/**
- * Your BSTIterator will be called like this:
- * var i = new BSTIterator(root), a = [];
- * while (i.hasNext()) a.push(i.next());
-*/
