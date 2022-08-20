902a2b5b0bb4de70a0a4a3b6ecf0b42f5911bef7
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-20 22:54:16 +0530

    10 :::: 0938-range-sum-of-bst.js leetcode question updated (2022-08-20)

--- dsa.js
+++ dsa.js
@@ -1,106 +1,64 @@
-// 937. Reorder Data in Log Files
-// Easy   54%
+// 938. Range Sum of BST
+// Easy   80%
 
 
-// You have an array of logs.  Each log is a space delimited string of words.
-// For each log, the first word in each log is an alphanumeric identifier.  Then,
-// either:
-//     Each word after the identifier will consist only of lowercase letters, or;
-//     Each word after the identifier will consist only of digits.
-// We will call these two varieties of logs letter-logs and digit-logs.  It is
-// guaranteed that each log has at least one word after its identifier.
-// Reorder the logs so that all of the letter-logs come before any digit-log.
-// The letter-logs are ordered lexicographically ignoring identifier, with the
-// identifier used in case of ties.  The digit-logs should be put in their
-// original order.
-// Return the final order of the logs.
+// Given the root node of a binary search tree, return the sum of values of all
+// nodes with value between L and R (inclusive).
+// The binary search tree is guaranteed to have unique values.
 
 // Example 1:
-// Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit
-// dig","let3 art zero"]
-// Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5
-// 1","dig2 3 6"]
+// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+// Output: 32
+// Example 2:
+// Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+// Output: 23
 
-// Constraints:
-//     0 <= logs.length <= 100
-//     3 <= logs[i].length <= 100
-//     logs[i] is guaranteed to have an identifier, and a word after the
-// identifier.
+// Note:
+//     The number of nodes in the tree is at most 10000.
+//     The final answer is guaranteed to be less than 2^31.
 
 
 /**
- * @param {string[]} logs
- * @return {string[]}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ * this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-const reorderLogFiles = function(logs) {
-  let digs = [], lets = []
-  for (let s of logs) {
-    const spaceIndex = s.indexOf(' ')
-    const c = s[spaceIndex + 1]
-    if (c <= '9') digs.push(s)
-    else lets.push(s)
-  }
-  lets = lets.sort((a, b) => {
-    const aSpaceIndex = a.indexOf(' ')
-    const bSpaceIndex = b.indexOf(' ')
-    const aSub = a.substring(aSpaceIndex + 1)
-    const bSub = b.substring(bSpaceIndex + 1)
-    if (aSub < bSub) return -1
-    if (aSub > bSub) return 1
-
-    const aId = a.substring(0, aSpaceIndex)
-    const bId = b.substring(0, bSpaceIndex)
-    if (aId < bId) return -1
-    if (aId > bId) return 1
-    return 0
-  })
-  return lets.concat(digs)
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+const rangeSumBST = function(root, L, R) {
+  if (root == null) return 0
+  const v = root.val
+  if (v == L) return v + rangeSumBST(root.right, L, R)
+  if (v == R) return v + rangeSumBST(root.left, L, R)
+  if (v < L) return rangeSumBST(root.right, L, R)
+  if (v > R) return rangeSumBST(root.left, L, R)
+  return v + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R)
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  ['dig1 8 1 5 1','let1 art can','dig2 3 6','let2 own kit dig','let3 art zero'],
-  // ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 51","dig2 3 6"]
-  ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"],
-  // ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
-  ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a2 act car"],
-  // ["a2 act car","g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
-].forEach((logs) => {
-  console.log(reorderLogFiles(logs))
+  [[10,5,15,3,7,null,18], 7, 15],        // 32
+  [[10,5,15,3,7,13,18,1,null,6], 6, 10], // 23
+].forEach(([array, L, R]) => {
+  console.log(rangeSumBST(TreeNode.from(array), L, R))
 })
 
-const batter = function(logs) {
-  logs.sort((a, b) => {
-    const aSpaceIndex = a.indexOf(' ')
-    const bSpaceIndex = b.indexOf(' ')
-    const aFirstChar = a[aSpaceIndex + 1]
-    const bFirstChar = b[bSpaceIndex + 1]
-
-    if (aFirstChar <= '9') {
-      return bFirstChar <= '9' ? 0 : 1
-    }
-    if (bFirstChar <= '9') return -1
-
-    const aSub = a.substring(aSpaceIndex + 1)
-    const bSub = b.substring(bSpaceIndex + 1)
-    if (aSub < bSub) return -1
-    if (aSub > bSub) return 1
-
-    const aId = a.substring(0, aSpaceIndex)
-    const bId = b.substring(0, bSpaceIndex)
-    if (aId < bId) return -1
-    if (aId > bId) return 1
-    return 0
-  })
-  return logs
-}
-
 // Solution:
-// 使用 sort 函数，进行排序。
-// 在比较两个字符串的大小时，
-// 首先找到第一个空格的位置，读取空格后的一个字符，进行比较
-//  1. 若都为数字，则按原来的顺序排序
-//  2. 若有一个为数字，则该字符串排在后面
-//  3. 若都为字符，则比较第一个空格后的字符串的大小，小的在前
-//     若字符串相等，则比较空格前的字符串。
+// 使用递归
+// 若节点为 null 返回 0
+// 其余每个节点上的数，有五种情况：
+//  ----  L  -------  R  ----
+//  (1)  (2)   (3)   (4)  (5)
+// 1. val <  L     该节点不用处理，只需要处理其右子树（右子树中也许有大于或等于L的数）
+// 2. val == L     返回该节点的值和其右子树返回的值，左子树不用处理（左子树的值都小于L)
+// 3. L < val < R  返回该节点的值和其左右子树返回的值
+// 4. val == R     返回该节点的值和其左子树返回的值，右子树不用处理（右子树的值都大于R)
+// 5. val >  R     该节点不用处理，只需要处理其左子树（左子树中也许有小于或等于R的数）
 
 // Submission Result: Accepted
