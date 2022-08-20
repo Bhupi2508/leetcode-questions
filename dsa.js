dc10325f4ab85a5cefa077e16776b4e270af3559
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-20 22:52:41 +0530

    10 :::: 0119-pascals-triangle-ii.js leetcode question updated (2022-08-20)

--- dsa.js
+++ dsa.js
@@ -1,30 +1,25 @@
-// 118. Pascal's Triangle
-// Easy   39%
+// 119. Pascal's Triangle II
+// Easy   37%
 
-// Given numRows, generate the first numRows of Pascal's triangle.
+// Given an index k, return the kth row of the Pascal's triangle.
 
-// For example, given numRows = 5,
-// Return
+// For example, given k = 3,
+// Return [1,3,3,1].
 
-// [
-//      [1],
-//     [1,1],
-//    [1,2,1],
-//   [1,3,3,1],
-//  [1,4,6,4,1]
-// ]
+// Note: Could you optimize your algorithm to use only O(k) extra space?
 
 /**
- * @param {number} numRows
- * @return {number[][]}
+ * @param {number} rowIndex
+ * @return {number[]}
  */
-const generate = function(numRows) {
-  if (numRows <= 0) return []
-  const res = [[1]]
-  for (let i = 1; i < numRows; i++) {
-    res[i] = []
-    for (let j = 0; j <= i; j++) {
-      res[i][j] = (res[i - 1][j - 1] || 0) + (res[i - 1][j] || 0)
+const getRow = function(rowIndex) {
+  if (rowIndex < 0) return []
+  const res = new Array(rowIndex + 1)
+  res[0] = 1
+  for (let i = 1; i <= rowIndex; i++) {
+    res[i] = 0
+    for (let j = i; j > 0; j--) {
+      res[j] += res[j - 1]
     }
   }
   return res
@@ -32,13 +27,18 @@ const generate = function(numRows) {
 
 ;[
   -1,
-  5,
-].forEach(numRows => {
-  console.log(generate(numRows))
+  0,
+  1,
+  3,
+].forEach(rowIndex => {
+  console.log(getRow(rowIndex))
 })
 
 // Solution:
-// 初始化第一层为 1，
-// 其余的每层中的每个数都是其左右上角的两个数的和（没有数则用0表示）
+// 由于第 n 层的数组有 n + 1 个数，所以先分配该长度的数组。
+// 又由于每个数都是其左右上角的两个数的和，且格式化（左对齐）后，数i的值等于其上
+// 一层的数i加上数i-1的值，因此只需要在原数组基础上计算（保证 O(k) 的空间复杂度）
+
+// 因为需要加上前一个数，所以从后向前遍历。
 
 // Submission Result: Accepted
