cfa0856fc72c251e5b3f53758178375cf434e448
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-27 22:54:29 +0530

    11 :::: 1009-complement-of-base-10-integer.js leetcode question updated (2022-08-27)

--- dsa.js
+++ dsa.js
@@ -1,105 +1,67 @@
-// 1005. Maximize Sum Of Array After K Negations
-// Easy   51%
+// 1009. Complement of Base 10 Integer
+// Easy   60%
 
 
-// Given an array A of integers, we must modify the array in the following way:
-// we choose an i and replace A[i] with -A[i], and we repeat this process K times
-// in total.  (We may choose the same index i multiple times.)
-// Return the largest possible sum of the array after modifying it in this way.
+// Every non-negative integer N has a binary representation.  For example, 5 can
+// be represented as "101" in binary, 11 as "1011" in binary, and so on.  Note
+// that except for N = 0, there are no leading zeroes in any binary
+// representation.
+// The complement of a binary representation is the number in binary you get when
+// changing every 1 to a 0 and 0 to a 1.  For example, the complement of "101" in
+// binary is "010" in binary.
+// For a given number N in base-10, return the complement of it's binary
+// representation as a base-10 integer.
 
 // Example 1:
-// Input: A = [4,2,3], K = 1
-// Output: 5
-// Explanation: Choose indices (1,) and A becomes [4,-2,3].
+// Input: 5
+// Output: 2
+// Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2
+// in base-10.
 // Example 2:
-// Input: A = [3,-1,0,2], K = 3
-// Output: 6
-// Explanation: Choose indices (1, 2, 2) and A becomes [3,1,0,2].
+// Input: 7
+// Output: 0
+// Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0
+// in base-10.
 // Example 3:
-// Input: A = [2,-3,-1,5,-4], K = 2
-// Output: 13
-// Explanation: Choose indices (1, 4) and A becomes [2,3,-1,5,4].
+// Input: 10
+// Output: 5
+// Explanation: 10 is "1010" in binary, with complement "0101" in binary, which
+// is 5 in base-10.
 
 // Note:
-//     1 <= A.length <= 10000
-//     1 <= K <= 10000
-//     -100 <= A[i] <= 100
+//     0 <= N < 10^9
+//     This question is the same as 476:
+// https://leetcode.com/problems/number-complement/
 
 
 /**
- * @param {number[]} A
- * @param {number} K
+ * @param {number} N
  * @return {number}
  */
-const largestSumAfterKNegations = function(A, K) {
-  A.sort((a, b) => a - b)
-  for (let i = 0; i < A.length && A[i] < 0 && K > 0; i++, K--) {
-    A[i] = -A[i]
-  }
-  let result = 0, min = 100
-  for (let a of A) {
-    result += a
-    min = Math.min(min, a)
-  }
-  return result - (K % 2) * min * 2
-}
-
-const first = function(A, K) {
-  const hash = {}
-  for (let a of A) hash[a] = (hash[a] || 0) + 1
-  
-  let result = 0
-  let i = -100
-  for (; i < 0; i++) {
-    for (let j = 0; j < hash[i]; j++) {
-      result += i * (K-- > 0 ? -1 : 1)
-    }
-  }
-  i = 0
-  if (!hash[i] && K > 0 && K % 2) {
-    for (; i <= 100; i++) {
-      if (hash[i]) {
-        result -= i
-        hash[i]--
-        break
-      } else if (hash[-i]) {
-        result -= i * 2
-        break
-      }
-    }
-  }
-  for (; i <= 100; i++) {
-    for (let j = 0; j < hash[i]; j++) result += i
-  }
-  return result
+const bitwiseComplement = function(N) {
+  let X = 1
+  while (N > X) X = X * 2 + 1
+  return X - N
 }
 
 ;[
-  [[4,2,3], 1],         // 5
-  [[3,-1,0,2], 3],      // 6
-  [[2,-3,-1,5,-4], 2],  // 13
-  [[-1], 2],            // -1
-].forEach(([A, K]) => {
-  console.log(largestSumAfterKNegations(A, K))
+  0,      // 1
+  5,      // 2
+  7,      // 0
+  10,     // 5
+  8,      // 7
+].forEach((N) => {
+  console.log(bitwiseComplement(N))
 })
 
 // Solution:
-// 方法1
-// 使用桶排序
-// 1. 从小到大遍历所有负数
-//    若 K > 0，则将 -a 添加到 result 中
-//    若 K = 0，则将  a 添加到 result 中
-// 2. 遍历完负数后，若 K > 0 && K % 2 == 0 && 数组中没有 0，则需要有一个绝对值最小数，
-//    要变为其负数，添加到 result 中
-// 3. 将剩余的数添加到 result 中
+// 1. 直接
+// 使用过 N % 2 和 N >>> 1 可从左到右依次获得 N 的二进制数
+// 再利用 ^ 异或运算符来完成 01 转换
+// 最后利用 result += (0|1) * 2**i 来完成倒序二进制的值计算
 
-// 方法2
-// 原生排序
-// 1. 从小到大遍历所有负数
-//    若 K > 0，则 A[i] = -A[i]
-// 2. 计算数组中所有数的和 res
-// 3. 找出数组中最小的数
-// 4. 若 K % 2 == 1, 则 res 需要减去最小数的两倍
-// TO(nlogn)-SO(1)
+// 2. 互补
+// input + output = 111...11
+// 111...11的位数与input相同
 
 // Submission Result: Accepted
