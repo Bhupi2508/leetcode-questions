981e20800b52e6aae1683a09aa09906d22c574ce
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-28 22:54:44 +0530

    12 :::: 1221-split-a-string-in-balanced-strings.js leetcode question updated (2022-08-28)

--- dsa.js
+++ dsa.js
@@ -1,57 +1,64 @@
-// 1217. Play with Chips
-// Easy   64%
+// 1221. Split a String in Balanced Strings
+// Easy   82%
 
 
-// There are some chips, and the i-th chip is at position chips[i].
-// You can perform any of the two following types of moves any number of times
-// (possibly zero) on any chip:
-//     Move the i-th chip by 2 units to the left or to the right with a cost of
-// 0.
-//     Move the i-th chip by 1 unit to the left or to the right with a cost of 1.
-// There can be two or more chips at the same position initially.
-// Return the minimum cost needed to move all the chips to the same position (any
-// position).
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+// Given a balanced string s split it in the maximum amount of balanced strings.
+// Return the maximum amount of splitted balanced strings.
 
 // Example 1:
-// Input: chips = [1,2,3]
-// Output: 1
-// Explanation: Second chip will be moved to positon 3 with cost 1. First chip
-// will be moved to position 3 with cost 0. Total cost is 1.
+// Input: s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring
+// contains same number of 'L' and 'R'.
 // Example 2:
-// Input: chips = [2,2,2,3,3]
+// Input: s = "RLLLLRRRLR"
+// Output: 3
+// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains
+// same number of 'L' and 'R'.
+// Example 3:
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
+// Example 4:
+// Input: s = "RLRRRLLRLL"
 // Output: 2
-// Explanation: Both fourth and fifth chip will be moved to position two with
-// cost 1. Total minimum cost will be 2.
+// Explanation: s can be split into "RL", "RRRLLRLL", since each substring
+// contains an equal number of 'L' and 'R'
 
 // Constraints:
-//     1 <= chips.length <= 100
-//     1 <= chips[i] <= 10^9
+//     1 <= s.length <= 1000
+//     s[i] = 'L' or 'R'
 
 
 /**
- * @param {number[]} chips
+ * @param {string} s
  * @return {number}
  */
-const minCostToMoveChips = function(chips) {
-  let res = 0
-  for (let p of chips) {
-    if (p % 2) res++
+const balancedStringSplit = function(s) {
+  let l = r = res = 0
+  for (let c of s) {
+    if (c === 'L') l++
+    else r++
+    if (l === r) {
+      res++
+      l = r = 0
+    }
   }
-  return Math.min(res, chips.length - res)
+  return res
 }
 
 ;[
-  [1,2,3],
-  [2,2,2,3,3],
-  [1,2,3,4,5,6,7,8,9],
-].forEach((chips) => {
-  console.log(minCostToMoveChips(chips))
+  'RLRRLLRLRL',
+  'RLLLLRRRLR',
+  'LLLLRRRR',
+  'RLRRRLLRLL'
+].forEach((s) => {
+  console.log(balancedStringSplit(s))
 })
 
 // Solution:
-// 因为移动2个单元格的费用为0，所以可以将所有奇数位置移动到同一个位置 i 上，所有偶数位置也移动
-// 到同一个位置 j 上。再将 i 上的全部移动到 j 的旁边（左或右，j-1或j+1）上。
-// 比较 i 和 j 上的数量，将小的一边移动到大的一边，费用为小的一边的数量。
-// 即只需计算数组中奇数的数量和偶数的数量，并取其小者。
+// 遍历字符串时，统计 'L' 和 'R' 的个数 l 和 r，
+// 当数量相同时，拆分，并重置 l=r=0 。
 
 // Submission Result: Accepted
