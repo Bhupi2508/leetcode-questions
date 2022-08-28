8d7f08747f69e6045fea6967a121a61576ae1afe
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-28 22:54:32 +0530

    12 :::: 1030-matrix-cells-in-distance-order.js leetcode question updated (2022-08-28)

--- dsa.js
+++ dsa.js
@@ -1,71 +1,79 @@
-// 1029. Two City Scheduling
-// Easy   55%
+// 1030. Matrix Cells in Distance Order
+// Easy   64%
 
-// There are 2N people a company is planning to interview. The cost of flying the
-// i-th person to city A is costs[i][0], and the cost of flying the i-th person
-// to city B is costs[i][1].
-// Return the minimum cost to fly every person to a city such that exactly N
-// people arrive in each city.
+// We are given a matrix with R rows and C columns has cells with integer
+// coordinates (r, c), where 0 <= r < R and 0 <= c < C.
+// Additionally, we are given a cell in that matrix with coordinates (r0, c0).
+// Return the coordinates of all cells in the matrix, sorted by their distance
+// from (r0, c0) from smallest distance to largest distance.  Here, the distance
+// between two cells (r1, c1) and (r2, c2) is the Manhattan distance, |r1 - r2| +
+// |c1 - c2|.  (You may return the answer in any order that satisfies this
+// condition.)
 
 // Example 1:
-// Input: [[10,20],[30,200],[400,50],[30,20]]
-// Output: 110
-// Explanation:
-// The first person goes to city A for a cost of 10.
-// The second person goes to city A for a cost of 30.
-// The third person goes to city B for a cost of 50.
-// The fourth person goes to city B for a cost of 20.
-// The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people
-// interviewing in each city.
+// Input: R = 1, C = 2, r0 = 0, c0 = 0
+// Output: [[0,0],[0,1]]
+// Explanation: The distances from (r0, c0) to other cells are: [0,1]
+// Example 2:
+// Input: R = 2, C = 2, r0 = 0, c0 = 1
+// Output: [[0,1],[0,0],[1,1],[1,0]]
+// Explanation: The distances from (r0, c0) to other cells are: [0,1,1,2]
+// The answer [[0,1],[1,1],[0,0],[1,0]] would also be accepted as correct.
+// Example 3:
+// Input: R = 2, C = 3, r0 = 1, c0 = 2
+// Output: [[1,2],[0,2],[1,1],[0,1],[1,0],[0,0]]
+// Explanation: The distances from (r0, c0) to other cells are: [0,1,1,2,2,3]
+// There are other answers that would also be accepted as correct, such as
+// [[1,2],[1,1],[0,2],[1,0],[0,1],[0,0]].
 
 // Note:
-//     1 <= costs.length <= 100
-//     It is guaranteed that costs.length is even.
-//     1 <= costs[i][0], costs[i][1] <= 1000
+//     1 <= R <= 100
+//     1 <= C <= 100
+//     0 <= r0 < R
+//     0 <= c0 < C
 
 /**
- * @param {number[][]} costs
- * @return {number}
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
  */
-const twoCitySchedCost = function(costs) {
-  costs.sort((a, b) => a[0] - a[1] - (b[0] - b[1]))
-  let min = 0
-  for (let i = 0, l = costs.length; i < l; i++) {
-    min += costs[i][i < l / 2 ? 0 : 1]
+const allCellsDistOrder = function(R, C, r0, c0) {
+  const visited = Array(R)
+  for (let i = 0; i < R; i++) visited[i] = Array(C).fill(false)
+
+  const result = []
+
+  const queue = [[r0, c0]]
+  while (queue.length > 0) {
+    const cell = queue.shift()
+    const r = cell[0], c = cell[1]
+
+    if (r < 0 || r >= R || c < 0 || c >= C) continue
+    if (visited[r][c]) continue
+
+    result.push([r, c])
+    visited[r][c] = true
+
+    queue.push([r, c - 1])
+    queue.push([r, c + 1])
+    queue.push([r - 1, c])
+    queue.push([r + 1, c])
   }
-  return min
+  return result
 }
 
 ;[
-  [
-    [10, 20],
-    [30, 200],
-    [400, 50],
-    [30, 20],
-  ], // 110
-  [
-    [1, 10],
-    [2, 20],
-  ], // 12
-  [
-    [1, 10],
-    [2, 20],
-    [3, 30],
-    [4, 5],
-  ], // 10 + 2 + 3 + 5 = 20
-].forEach(costs => {
-  console.log(twoCitySchedCost(costs))
+  [1, 2, 0, 0],
+  [2, 2, 0, 1],
+  [2, 3, 1, 2],
+].forEach(([R, C, r0, c0]) => {
+  console.log(allCellsDistOrder(R, C, r0, c0))
 })
 
 // Solution:
-// 方法1：
-// 使用 countA,countB 来记录到两个城市的人数
-// 按费用差，从大到小排序，遍历时去费用小的城市，直到某个城市的人到达一半
-// TO(nlogn)-SO(n)
-
-// 方法2：（1的改进）
-// 按 costs[i][0] - costs[i][1] 从小到大排序
-// 前一半人去城市a, 后一半去城市b
-// TO(nlogn)-SO(n)
+// 1. 按照曼哈顿距离使用内部排序即可
+// 2. BFS 遍历，使用标记矩阵和先进先出队列来完成广度遍历来完成
 
 // Submission Result: Accepted
