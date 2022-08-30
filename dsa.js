1b14c727f24d0841ec69dd333b11a21bd1d9a6b1
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-08-30 22:53:36 +0530

    12 :::: 0598-range-addition-ii.js leetcode question updated (2022-08-30)

--- dsa.js
+++ dsa.js
@@ -1,58 +1,61 @@
-// 594. Longest Harmonious Subsequence
-// Easy   41%
+// 598. Range Addition II
+// Easy 48% locked:false
 
+// 598. Range Addition II
 
-// We define a harmonious array is an array where the difference between its
-// maximum value and its minimum value is exactly 1.
+// Given an m * n matrix M initialized with all 0's and several update
+// operations.
 
-// Now, given an integer array, you need to find the length of its longest
-// harmonious subsequence among all its possible subsequences.
+// Operations are represented by a 2D array, and each operation is represented
+// by an array with two positive integers a and b, which means M[i][j] should be
+// added by one for all 0 <= i < a and 0 <= j < b.
+
+// You need to count and return the number of maximum integers in the matrix
+// after performing all the operations.
 
 // Example 1:
 
-// Input: [1,3,2,2,5,2,3,7]
-// Output: 5
-// Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+// Input:
+// m = 3, n = 3
+// operations = [[2,2],[3,3]]
+// Output: 4
+// Explanation:
+// Initially, M =
+// [[0, 0, 0],
+//  [0, 0, 0],
+//  [0, 0, 0]]
+
+// After performing [2,2], M =
+// [[1, 1, 0],
+//  [1, 1, 0],
+//  [0, 0, 0]]
+
+// After performing [3,3], M =
+// [[2, 2, 1],
+//  [2, 2, 1],
+//  [1, 1, 1]]
+
+// So the maximum integer in M is 2, and there are four of it in M. So return 4.
 
 // Note:
-// The length of the input array will not exceed 20,000.
+
+//     The range of m and n is [1,40000].
+//     The range of a is [1,m], and the range of b is [1,n].
+//     The range of operations size won't exceed 10,000.
 
 
 /**
- * @param {number[]} nums
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
  * @return {number}
  */
-const findLHS = function(nums) {
-  nums.sort((a, b) => a - b)
-  let result = 0, prev = 0, curr = 0
-  for (let i = 0, n = nums.length; i < n; i++) {
-    const diff = i ? nums[i] - nums[i - 1] : 0
-    prev = diff ? (diff === 1 ? curr : 0) : prev
-    curr = diff ? 1 : curr + 1
-    if (prev) result = Math.max(result, prev + curr)
+const maxCount = function(m, n, ops) {
+  let a = m, b = n
+  for (let pair of ops) {
+    if (a > pair[0]) a = pair[0]
+    if (b > pair[1]) b = pair[1]
   }
-  return result
+
+  return a * b
 }
-
-;[
-  [1,3,2,2,5,2,3,7],            // 5
-  [],                           // 0
-  [1,1,1,1],                    // 0
-].forEach(nums => {
-  console.log(findLHS(nums))
-})
-
-// Solution:
-
-// 思路：
-// 1. 先排序
-// 2. 遍历排序后的数组
-// 3. 记录前一个数的个数，和当前数的个数
-// 4. 如果当前数和前一个数相差 1，则记录，否则继续
-
-// 实现：
-// 关键在于 prev 的记录，只有当当前数与前一个数相差 1 时才设置为前一个数的个数，
-// 否则记录为 0。
-// 而当 prev 为 0 时，说明数字不满足要求。
-
-// Submission Result: Accepted
