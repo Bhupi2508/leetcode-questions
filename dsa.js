801288d0c1a9de855cbe05e8df4ed97cfddae307
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-03 22:52:48 +0530

    12 :::: 0155-min-stack.js leetcode question updated (2022-09-03)

--- dsa.js
+++ dsa.js
@@ -1,65 +1,86 @@
-// 147. Insertion Sort List
-// Medium   40%
+// 155. Min Stack
+// Easy   29%
 
 
-// Sort a linked list using insertion sort.
-// A graphical example of insertion sort. The partial sorted list (black)
-// initially contains only the first element in the list.
-// With each iteration one element (red) is removed from the input data and
-// inserted in-place into the sorted list
+// Design a stack that supports push, pop, top, and retrieving the minimum
+// element in constant time.
 
-// Algorithm of Insertion Sort:
-//     Insertion sort iterates, consuming one input element each repetition, and
-// growing a sorted output list.
-//     At each iteration, insertion sort removes one element from the input data,
-// finds the location it belongs within the sorted list, and inserts it there.
-//     It repeats until no input elements remain.
-// Example 1:
-// Input: 4->2->1->3
-// Output: 1->2->3->4
-// Example 2:
-// Input: -1->5->3->4->0
-// Output: -1->0->3->4->5
+// push(x) -- Push element x onto stack.
+
+// pop() -- Removes the element on top of the stack.
+
+// top() -- Get the top element.
+
+// getMin() -- Retrieve the minimum element in the stack.
+
+// Example:
+
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin();   --> Returns -3.
+// minStack.pop();
+// minStack.top();      --> Returns 0.
+// minStack.getMin();   --> Returns -2.
 
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- * this.val = val;
- *     this.next = null;
- * }
+ * initialize your data structure here.
  */
-const ListNode = require('../structs/ListNode')
-
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-const insertionSortList = function(head) {
-  const helper = new ListNode(0)
-  let node, temp
-  while (head) {
-    node = helper
-    while (node.next && head.val > node.next.val) node = node.next
-    temp = head
-    head = head.next
-    temp.next = node.next
-    node.next = temp
-  }
-  return helper.next
+const MinStack = function() {
+  this.stack = []
+  this.min = []
 }
 
-;[
-  [4,2,1,3],
-  [4,2,6,1,3,7,5],
-  [-1,5,3,4,0],
-].forEach((array) => {
-  console.log((insertionSortList(ListNode.from(array)) || 'null').toString())
-})
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  if (this.min.length === 0 ||  x <= this.min[this.min.length - 1]) this.min.push(x)
+  this.stack.push(x)
+}
 
-// Solution:
-// 使用一个新的链表来记录排序后的链表，使用空节点作为头部以便更改第一个节点时丢失头部。
-// 遍历输入的链表，
-// 每遍历一个节点，都将当前节点插入到新链表的适合的位置上（从前往后找位置）。
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  const result = this.stack.pop()
+  if (this.min[this.min.length - 1] === result) this.min.pop()
+  return result
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1]
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+  return this.min[this.min.length - 1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = Object.create(MinStack).createNew()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+const minStack = new MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+console.log(minStack.getMin())
+console.log(minStack.pop())
+console.log(minStack.top())
+console.log(minStack.getMin())
 
 // Submission Result: Accepted
