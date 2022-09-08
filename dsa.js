9e6f37e6b211121bcdb2504647192446fe7e174e
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-08 22:44:47 +0530

    13 :::: 151-reverseWordsInAString.js question updated

--- dsa.js
+++ dsa.js
@@ -1,30 +1,16 @@
 /**
- * @param {string[]} tokens
- * @return {number}
+ * @param {string} str
+ * @returns {string}
  */
-// seems like it is not a good solution
-var evalRPN = function(tokens) {
-    var stack = [];
-    for (var i = 0; i < tokens.length; i++) {
-        var token = tokens[i];
-        if (!isNaN(token)) {
-            stack.push(token);
-        } else {
-            var num1 = stack.pop();
-            var num2 = stack.pop();
-            var result = getResult(num2, num1, token);
-            stack.push(result);
+var reverseWords = function(str) {
+    var words = str.split(' ');
+    // the O(1) space
+    var j = 0;
+    for (var i = 0; i < words.length; i++) {
+        if (words[i] !== '') {
+            words[j] = words[i];
+            j++;
         }
     }
-
-    return Math.floor(parseInt(stack[stack.length - 1]));
-};
-
-var getResult = function(num1, num2, operator) {
-    var a = parseInt(num1);
-    var b = parseInt(num2);
-    if (operator === '+') return a + b;
-    if (operator === '-') return a - b;
-    if (operator === '/') return a / b;
-    if (operator === '*') return a * b;
+    return words.slice(0, j).reverse().join(' ');
 };
