a94688f642b3f584b0ecaaa9cdd7c29cfe8af526
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-09 22:54:35 +0530

    13 :::: 1078-occurrences-after-bigram.js leetcode question updated (2022-09-09)

--- dsa.js
+++ dsa.js
@@ -1,53 +1,54 @@
-// 1071. Greatest Common Divisor of Strings
-// Easy   54%
+// 1078. Occurrences After Bigram
+// Easy   64%
 
 
-// For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T
-// concatenated with itself 1 or more times)
-// Return the largest string X such that X divides str1 and X divides str2.
+// Given words first and second, consider occurrences in some text of the form
+// "first second third", where second comes immediately after first, and third
+// comes immediately after second.
+// For each such occurrence, add "third" to the answer, and return the answer.
 
 // Example 1:
-// Input: str1 = "ABCABC", str2 = "ABC"
-// Output: "ABC"
+// Input: text = "alice is a good girl she is a good student", first = "a",
+// second = "good"
+// Output: ["girl","student"]
 // Example 2:
-// Input: str1 = "ABABAB", str2 = "ABAB"
-// Output: "AB"
-// Example 3:
-// Input: str1 = "LEET", str2 = "CODE"
-// Output: ""
+// Input: text = "we will we will rock you", first = "we", second = "will"
+// Output: ["we","rock"]
 
 // Note:
-//     1 <= str1.length <= 1000
-//     1 <= str2.length <= 1000
-//     str1[i] and str2[i] are English uppercase letters.
+//     1 <= text.length <= 1000
+//     text consists of space separated words, where each word consists of
+// lowercase English letters.
+//     1 <= first.length, second.length <= 10
+//     first and second consist of lowercase English letters.
 
 
 /**
- * @param {string} str1
- * @param {string} str2
- * @return {string}
+ * @param {string} text
+ * @param {string} first
+ * @param {string} second
+ * @return {string[]}
  */
-const gcdOfStrings = function(str1, str2) {
-  if (str1 == str2) return str1
-  if (str1.length == str2.length) return ''
-  if (str1.length < str2.length) return gcdOfStrings(str2, str1)
-  if (str1.substring(0, str2.length) != str2) return ''
-  return gcdOfStrings(str2, str1.substring(str2.length, str1.length))
+const findOcurrences = function(text, first, second) {
+  const result = []
+  const words = text.split(' ')
+  for (let i = 0; i < words.length - 2; i++) {
+    if (words[i] === first && words[i + 1] === second) {
+      result.push(words[i + 2])
+    }
+  }
+  return result
 }
 
 ;[
-  ['ABCABC', 'ABC'],            // 'ABC'
-  ['ABABAB', 'ABAB'],           // 'AB'
-  ['LEET', 'CODE'],             // ''
-  ['AAAAAAAA', 'AAAA'],         // 'AAAA'
-  ['TTTTT', 'TTTTTTTTT'],       // 'T'
-].forEach(([str1, str2]) => {
-  console.log(gcdOfStrings(str1, str2))
+  ['alice is a good girl she is a good student', 'a', 'good'],
+  ['we will we will rock you', 'we', 'will'],
+].forEach(([text, first, second]) => {
+  console.log(findOcurrences(text, first, second))
 })
 
 // Solution:
-// 类似求最大公约数
-// 因为是字符串，所以可以使用更相减损术
-// 长字符串减短字符串，再将剩余字符串与短字符串进行相减，直到两个字符串相等为止
+// 使用 split 函数按空格拆分 text，
+// 再遍历匹配一遍，稍微注意边界问题即可。
 
 // Submission Result: Accepted
