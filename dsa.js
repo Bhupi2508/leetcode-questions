1b878bed3e43d814cb1e5cafe9291e9f740a0a89
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-11 22:54:40 +0530

    14 :::: 1184-distance-between-bus-stops.js leetcode question updated (2022-09-11)

--- dsa.js
+++ dsa.js
@@ -1,62 +1,82 @@
-// 1175. Prime Arrangements
-// Easy   51%
+// 1184. Distance Between Bus Stops
+// Easy   55%
 
 
-// Return the number of permutations of 1 to n so that prime numbers are at prime
-// indices (1-indexed.)
-// (Recall that an integer is prime if and only if it is greater than 1, and
-// cannot be written as a product of two positive integers both smaller than it.)
-// Since the answer may be large, return the answer modulo 10^9 + 7.
+// A bus has n stops numbered from 0 to n - 1 that form a circle. We know the
+// distance between all pairs of neighboring stops where distance[i] is the
+// distance between the stops number i and (i + 1) % n.
+// The bus goes along both directions i.e. clockwise and counterclockwise.
+// Return the shortest distance between the given start and destination stops.
 
 // Example 1:
-// Input: n = 5
-// Output: 12
-// Explanation: For example [1,2,5,4,3] is a valid permutation, but [5,2,3,4,1]
-// is not because the prime number 5 is at index 1.
+//
+// (0)(start) ------1------> (1)(destination)
+//  |                         |
+//  4                         2
+//  |                         |
+// (3)--------------3------> (2)
+//
+// Input: distance = [1,2,3,4], start = 0, destination = 1
+// Output: 1
+// Explanation: Distance between 0 and 1 is 1 or 9, minimum is 1.
+
 // Example 2:
-// Input: n = 100
-// Output: 682289015
+//
+// (0)(start) ------1------> (1)
+//  |                         |
+//  4                         2
+//  |                         |
+// (3)--------------3------> (2)(destination)
+//
+// Input: distance = [1,2,3,4], start = 0, destination = 2
+// Output: 3
+// Explanation: Distance between 0 and 2 is 3 or 7, minimum is 3.
+
+// Example 3:
+//
+// (0)(start) ------1------> (1)
+//  |                         |
+//  4                         2
+//  |                         |
+// (3)(destination)-3------> (2)
+//
+// Input: distance = [1,2,3,4], start = 0, destination = 3
+// Output: 4
+// Explanation: Distance between 0 and 3 is 6 or 4, minimum is 4.
 
 // Constraints:
-//     1 <= n <= 100
+//     1 <= n <= 10^4
+//     distance.length == n
+//     0 <= start, destination < n
+//     0 <= distance[i] <= 10^4
 
 
 /**
- * @param {number} n
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
  * @return {number}
  */
-const numPrimeArrangements = function(n) {
-  const isPrime = Array(n + 1).fill(true)
-  isPrime[1] = false
-  let numPrime = 0
-  for (let i = 2; i <= n; i++) {
-    if (isPrime[i]) {
-      numPrime++
-      for (let j = 2; j * i <= n; j++) isPrime[j * i] = false
-    }
-  }
-  let res = 1, MODULO = 1000000007
-  for (let i = 1; i <= numPrime; i++) res = (res * i) % MODULO
-  for (let i = 1; i <= n - numPrime; i++) res = (res * i) % MODULO
-  return res
+const distanceBetweenBusStops = function(distance, start, destination) {
+  const n = distance.length
+  let forward = back = 0
+  for (let i = start; i != destination; i = (i + 1) % n) forward += distance[i]
+  for (let i = destination; i != start; i = (i + 1) % n) back += distance[i]
+  return Math.min(forward, back)
 }
 
 ;[
-  5, // 12
-  10, // 17280
-  20, // 344376809
-  50, // 451768713
-  100, // 682289015
-].forEach((n) => {
-  console.log(numPrimeArrangements(n))
+  [[1,2,3,4], 0, 1],
+  [[1,2,3,4], 0, 2],
+  [[1,2,3,4], 0, 3],
+].forEach(([distance, start, destination]) => {
+  console.log(distanceBetweenBusStops(distance, start, destination))
 })
 
 // Solution:
-// 先用数组标记 1 到 n 中每个数是否是素数，
-// 从 2 开始遍历，当遍历到 i 时，将所有的 i*k (k>1) 的数标记为非素数。
-// 统计素数的个数。
-
-// 所有素数进行全排列，所有非素数进行全排列，
-// 最后得到答案。
+// 分别计算
+// 1）从 start 开始按数组顺序到 destination 的距离
+// 2）从 destination 开始按数组顺序到 start 的距离
+// 最后取二者中小的即可。
 
 // Submission Result: Accepted
