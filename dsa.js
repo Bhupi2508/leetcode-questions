3fcc9a3013f46b81169d396b4fc55825ec7e1f34
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-11 22:53:30 +0530

    14 :::: 0541-reverse-string-ii.js leetcode question updated (2022-09-11)

--- dsa.js
+++ dsa.js
@@ -1,44 +1,51 @@
-// 540. Single Element in a Sorted Array
-// Medium
-
-// Given a sorted array consisting of only integers where every element appears
-// twice except for one element which appears once. Find this single element
-// that appears only once.
-
-// Example 1:
-
-// Input: [1,1,2,3,3,4,4,8,8]
-// Output: 2
-
-// Example 2:
-
-// Input: [3,3,7,7,10,11,11]
-// Output: 10
-
-// Note: Your solution should run in O(log n) time and O(1) space.
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
-const singleNonDuplicate = function(nums) {
-  for (let i = 0, n = nums.length; i < n; i++) {
-    if (!(nums[i] === nums[i - 1] || nums[i] === nums[i + 1])) return nums[i]
-  }
-  return nums[0]
-}
-
-;[
-  [1,1,2,3,3,4,4,8,8],          // 2
-  [3,3,7,7,10,11,11],           // 10
-  [0],                          // 0
-  [1,1,2],                      // 2
-].forEach(nums => {
-  console.log(singleNonDuplicate(nums))
-})
-
-// Solution:
-// 因为已经排好序，因此只需要从头开始遍历数组，
-// 返回遇到第一个既不等于前一个数，也不等于后一个数的数。
-
-// Submission Result: Accepted
+// 541. Reverse String II
+// Easy   43%
+
+
+// Given a string and an integer k, you need to reverse the first k characters
+// for every 2k characters counting from the start of the string. If there are
+// less than k characters left, reverse all of them. If there are less than 2k
+// but greater than or equal to k characters, then reverse the first k characters
+// and left the other as original.
+
+// Example:
+
+// Input: s = "abcdefg", k = 2
+// Output: "bacdfeg"
+
+// Restrictions:
+
+//  The string consists of lower English letters only.
+//  Length of the given string and k will in the range [1, 10000]
+
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+const reverseStr = function(s, k) {
+  const array = []
+  for (let i = 0, n = s.length; i < n; i++) {
+    if (i % k === 0) array.push([])
+    array[array.length - 1].push(s[i])
+  }
+  for (let i = 0, n = array.length; i < n; i++ ) {
+    if (i % 2 === 0) array[i] = array[i].reverse()
+    array[i] = array[i].join('')
+  }
+  return array.join('')
+}
+
+;[
+  ['abcdefg', 2],               // 'bacdfeg'
+].forEach(args => {
+  console.log(reverseStr(...args))
+})
+
+
+// Solution:
+// 1. 先以 k 为长度分组，
+// 2. 组号为偶数的组倒置。
+
+// Submission Result: Accepted
