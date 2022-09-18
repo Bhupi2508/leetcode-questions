2f855616d97f924d1b86c24c817a298eae82071c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-18 22:54:39 +0530

    15 :::: 1171-remove-zero-sum-consecutive-nodes-from-linked-list.js leetcode question updated (2022-09-18)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,79 @@
-// 1170. Compare Strings by Frequency of the Smallest Character
-// Easy   58%
+// 1171. Remove Zero Sum Consecutive Nodes from Linked List
+// Medium   41%
 
 
-// Let's define a function f(s) over a non-empty string s, which calculates the
-// frequency of the smallest character in s. For example, if s = "dcce" then f(s)
-// = 2 because the smallest character is "c" and its frequency is 2.
-// Now, given string arrays queries and words, return an integer array answer,
-// where each answer[i] is the number of words such that f(queries[i]) < f(W),
-// where W is a word in words.
+// Given the head of a linked list, we repeatedly delete consecutive sequences of
+// nodes that sum to 0 until there are no such sequences.
+// After doing so, return the head of the final linked list.  You may return any
+// such answer.
 
+// (Note that in the examples below, all sequences are serializations of ListNode
+// objects.)
 // Example 1:
-// Input: queries = ["cbd"], words = ["zaaaz"]
-// Output: [1]
-// Explanation: On the first query we have f("cbd") = 1, f("zaaaz") = 3 so
-// f("cbd") < f("zaaaz").
+// Input: head = [1,2,-3,3,1]
+// Output: [3,1]
+// Note: The answer [1,2,1] would also be accepted.
 // Example 2:
-// Input: queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
-// Output: [1,2]
-// Explanation: On the first query only f("bbb") < f("aaaa"). On the second query
-// both f("aaa") and f("aaaa") are both > f("cc").
+// Input: head = [1,2,3,-3,4]
+// Output: [1,2,4]
+// Example 3:
+// Input: head = [1,2,3,-3,-2]
+// Output: [1]
 
 // Constraints:
-//     1 <= queries.length <= 2000
-//     1 <= words.length <= 2000
-//     1 <= queries[i].length, words[i].length <= 10
-//     queries[i][j], words[i][j] are English lowercase letters.
+//     The given linked list will contain between 1 and 1000 nodes.
+//     Each node in the linked list has -1000 <= node.val <= 1000.
 
 
 /**
- * @param {string[]} queries
- * @param {string[]} words
- * @return {number[]}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ * this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-const numSmallerByFrequency = function(queries, words) {
-  const f = (s) => s
-    .split('')
-    .sort((a, b) => a > b ? 1 : -1)
-    .reduce((c, v, i, a) => c + (v === a[0] ? 1 : 0), 0)
-  const FW = Array(11).fill(0)
-  for (let w of words) {
-    const fw = f(w)
-    for (let i = 1; i < fw; i++) FW[i]++
+const ListNode = require('../structs/ListNode')
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const removeZeroSumSublists = function(head) {
+  const h = new ListNode()
+  h.next = head
+  let subHead = h, sum = 0
+  while (subHead) {
+    sum = 0
+    let cur = subHead.next
+    while (cur) {
+      sum += cur.val
+      if (sum === 0) {
+        subHead.next = cur.next
+      }
+      cur = cur.next
+    }
+    subHead = subHead.next
   }
-  return queries.map(q => FW[f(q)])
+
+  return h.next
 }
 
+
 ;[
-  [['cbd'], ['zaaaz']],
-  [['bbb','cc'],['a','aa','aaa','aaaa']],
-].forEach(([queries, words]) => {
-  console.log(numSmallerByFrequency(queries, words))
+  [1,2,-3,3,1],  // 3->1
+  [1,2,-3,3,1,-1,2], // 3->2
+  [1,2,3,-3,4], // 1->2->4
+  [1,2,3,-3,-2], // 1
+  [-1,-2,3,4,1], // 4->1
+].forEach((array) => {
+  console.log(removeZeroSumSublists(ListNode.from(array)).toString())
 })
 
 // Solution:
-// f(s) 函数
-// 1）将字符串拆分为字符数组
-// 2）将每个字符按从小到大的顺序排序
-// 3）统计与第一个字符相同的字符的个数
+// 寻找包含第一个节点的连续和为 0 的子序列，将其删除后在寻找第二个子序列，
+// 再从更新后的链表中寻找包含第二个节点的子序列，依次寻找完所有节点。
+// TO(n*n)
 
-// 对于 words 中的每个字符串用 f(s) 计算出其频率数
-// 用 FW 表记录频率数 i 在整个 words 的频率数组中排在第几，如：
-// 对于频率数组 [1, 4, 2, 3, 2]
-// FW = [0 4 2 1 0 0 0 ...]
-//    i  0 1 2 3 4 5 6 ...
-
-// 遍历 queries 数组，对于每个字符串，通过 f(s) 函数计算出其频率，再使用 FW 表获得 words 中
-// 有几个字符串的频率大于该字符串。
+// 使用其他数据结构，可以降低时间复杂度。
 
 // Submission Result: Accepted
