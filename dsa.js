b80df4152abd7fa51874cc38cf69efc2793bd508
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-21 22:45:10 +0530

    15 :::: 64-minimumPathSum.js question updated

--- dsa.js
+++ dsa.js
@@ -1,31 +1,30 @@
 /**
- * Key: dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
- * In this question, if obstacleGrid[i][j] === 1, set dp[i][j] = 0
- *
- * @param {number[][]} obstacleGrid
+ * Dynamic programming: S[i][j] = min(S[i - 1][j], S[i][j - 1]) + grid[i][j]
+ * Iniatially, S[i][j] is the sum of first row and first column elments.
+ * Be careful, how JavaScript handles two demensional array, otherwise, you can get
+ * 'undefined' error.
+ * 
+ * @param {number[][]} grid
  * @return {number}
  */
-var uniquePathsWithObstacles = function(obstacleGrid) {
-    var path = [[]];
-    obstacleGrid[0][0] === 0 ? path[0][0] = 1 : path[0][0] = 0;
-    for (var i = 1; i < obstacleGrid.length; i++) {
-        // In JavaScript, initialize an new empty array for row i, so that you can have a
-        // two-demension array
-        path[i] = [];
-        (path[i - 1][0] === 1 && obstacleGrid[i][0] === 0) ? path[i][0] = 1 : path[i][0] = 0;
-    }
-    for (var j = 1; j < obstacleGrid[0].length; j++) {
-        (path[0][j - 1] === 1 && obstacleGrid[0][j] === 0) ? path[0][j] = 1 : path[0][j] = 0;
+var minPathSum = function(grid) {
+    var m = grid.length;
+    var n = grid[0].length;
+    var sum = [];
+    sum[0] = [grid[0][0]];
+    for (var i = 1; i < m; i++) {
+        sum[i] = [sum[i - 1][0] + grid[i][0]];
     }
 
-    for (var i = 1; i < obstacleGrid.length; i++) {
-        for (var j = 1; j < obstacleGrid[i].length; j++) {
-           if (obstacleGrid[i][j] === 0) {
-               path[i][j] = path[i - 1][j] + path[i][j - 1];
-           } else {
-               path[i][j] = 0;
-           }
+    for (var j = 1; j < n; j++) {
+        sum[0][j] = sum[0][j - 1] + grid[0][j];
+    }
+
+    for (var i = 1; i < m; i++) {
+        for (var j = 1; j < n; j++) {
+            sum[i][j] = Math.min(sum[i - 1][j], sum[i][j - 1]) + grid[i][j];
         }
     }
-    return path[path.length - 1][path[0].length - 1];
+
+    return sum[m - 1][n - 1];
 };
