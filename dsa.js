e13fa56c44f95721b4ccbdf39f1a6d4af18a6669
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-28 22:54:13 +0530

    16 :::: 0914-x-of-a-kind-in-a-deck-of-cards.js leetcode question updated (2022-09-28)

--- dsa.js
+++ dsa.js
@@ -1,53 +1,75 @@
-// 912. Sort an Array
-// Medium   63%
+// 914. X of a Kind in a Deck of Cards
+// Easy   34%
 
 
-// Given an array of integers nums, sort the array in ascending order.
+// In a deck of cards, each card has an integer written on it.
+// Return true if and only if you can choose X >= 2 such that it is possible to
+// split the entire deck into 1 or more groups of cards, where:
+//     Each group has exactly X cards.
+//     All the cards in each group have the same integer.
 
 // Example 1:
-// Input: nums = [5,2,3,1]
-// Output: [1,2,3,5]
+// Input: deck = [1,2,3,4,4,3,2,1]
+// Output: true
+// Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
 // Example 2:
-// Input: nums = [5,1,1,2,0,0]
-// Output: [0,0,1,1,2,5]
+// Input: deck = [1,1,1,2,2,2,3,3]
+// Output: false
+// Explanation: No possible partition.
+// Example 3:
+// Input: deck = [1]
+// Output: false
+// Explanation: No possible partition.
+// Example 4:
+// Input: deck = [1,1]
+// Output: true
+// Explanation: Possible partition [1,1].
+// Example 5:
+// Input: deck = [1,1,2,2,2,2]
+// Output: true
+// Explanation: Possible partition [1,1],[2,2],[2,2].
 
 // Constraints:
-//     1 <= nums.length <= 50000
-//     -50000 <= nums[i] <= 50000
+//     1 <= deck.length <= 10^4
+//     0 <= deck[i] < 10^4
 
 
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {number[]} deck
+ * @return {boolean}
  */
-const sortArray = function(nums) {
-  function qsort(lo, hi) {
-    if (lo >= hi) return
-    const k = nums[hi]
-    let i = lo, j = hi
-    while (i < j) {
-      while (i < j && nums[i] <= k) i++
-      nums[j] = nums[i]
-      while (i < j && nums[j] >= k) j--
-      nums[i] = nums[j]
-    }
-    nums[i] = k
-    qsort(lo, i - 1)
-    qsort(i + 1, hi)
+const hasGroupsSizeX = function(deck) {
+  const hash = {}
+  for (let a of deck) hash[a] = (hash[a] || 0) + 1
+
+  function gcd(a, b) {
+    return a % b ? gcd(b, a % b) : b
   }
-  qsort(0, nums.length - 1)
-  return nums
+
+  let res = 0
+  for (let key in hash) res = gcd(res, hash[key])
+  return res > 1
 }
 
 ;[
-  // [5,2,3,1],
-  // [5,1,1,2,0,0],
-  [-4,0,7,4,9,-5,-1,0,-7,-1],
-].forEach((nums) => {
-  console.log(sortArray(nums))
+  [1,2,3,4,4,3,2,1], // true
+  [1,1,1,2,2,2,3,3], // false
+  [1],               // false
+  [1,1],             // true
+  [1,1,2,2,2,2],     // true
+  [1,1,1,1,2,2,2,2,2,2], // true
+].forEach((deck) => {
+  console.log(hasGroupsSizeX(deck))
 })
 
 // Solution:
-// 快排
+// 用 hash 记录每一个数出现的次数，
+// 找出最少次数和最多次数，
+// 若最少次数小于 2, 返回 false，
+// 否则 寻找 从 2 开始到 max 中，是否有一个数为所有次数的公约数，若是则返回true。
+
+// 数论的方法（使用最大公约数）
+// 同样用 hash 记录每一个数出现的次数，
+// 求所有数的最大公约数，若该数大于 1，则返回 true , 否则false
 
 // Submission Result: Accepted
