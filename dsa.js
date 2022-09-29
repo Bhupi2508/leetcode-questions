f30c56abd434bc78bb61c26180ee250c20389fd3
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-09-29 22:44:45 +0530

    16 :::: 143-reorderList.js question updated

--- dsa.js
+++ dsa.js
@@ -5,38 +5,37 @@
  *     this.next = null;
  * }
  */
-
 /**
- * Solution: Two pointers. Slow moves one step a time, fast moves two steps a time.
- * Once fast meets slow, the steps fast moved is twice as the steps slow moved.
- * After they met, in order to move to the cycle start place, the steps fast needs to move
- * equals to the steps the head need to mve. Here is the reason:
- * n1 -> n2 -> n3 -> n4 -> n2 the cycle starts at n2, slow and fast meet at n4
- * assume n1 -> n4 needs a steps, n2 -> n4 needs b steps, n4-> n2 needs c steps.
- * when fast reach n4, it takes (a + b + c + b) steps, slow reached n4, it takes a + b,
- * a + b + c + b = 2 * (a + b), so a = c. => head && fast move at the same, when they meet, that's
- * the place where the cycle starts
- *
+ * solution: reverse the second half of the list, then merge first half and reversed second half.
  * @param {ListNode} head
- * @return {ListNode}
+ * @return {void} Do not return anything, modify head in-place instead.
  */
-var detectCycle = function(head) {
-    if (!head || !head.next) return null;
-    var slow = head;
+var reorderList = function(head) {
+    if (!head || !head.next) return;
     var fast = head;
-
-    while (slow.next && fast.next && fast.next.next) {
+    var slow = head;
+    while (fast.next && fast.next.next) {
         slow = slow.next;
         fast = fast.next.next;
-        if (slow === fast) {
-            slow = head;
-            while (slow !== fast) {
-                slow = slow.next;
-                fast = fast.next;
-            }
-            return slow;
-        }
     }
 
-    return null;
+    // reverse the second half of the list
+    var middleHead = slow;
+    var middleHeadPrev = null;
+    while (middleHead) {
+        var headNext = middleHead.next;
+        middleHead.next = middleHeadPrev;
+        middleHeadPrev = middleHead;
+        middleHead = headNext;
+    }
+    // then merge first half and reversed second half.
+    slow = head;
+    while (slow) {
+        var slowNext = slow.next;
+        slow.next = middleHeadPrev;
+        var headPrevNext = middleHeadPrev.next;
+        middleHeadPrev.next = slowNext;
+        slow = slowNext;
+        middleHeadPrev = headPrevNext;
+    }
 };
