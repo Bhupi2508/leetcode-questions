7cd50334c060fc37b6a46ffa65ccbaeb87ea5146
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-02 22:52:15 +0530

    17 :::: 0047-permutations-ii.js leetcode question updated (2022-10-02)

--- dsa.js
+++ dsa.js
@@ -1,50 +1,52 @@
-// 46. Permutations
-// Medium   44%
+// 47. Permutations II
+// Medium   33%
 
-// Given a collection of distinct numbers, return all possible permutations.
+// Given a collection of numbers that might contain duplicates, return all
+// possible unique permutations.
 
 // For example,
-// [1,2,3] have the following permutations:
+// [1,1,2] have the following unique permutations:
 
 // [
-//   [1,2,3],
-//   [1,3,2],
-//   [2,1,3],
-//   [2,3,1],
-//   [3,1,2],
-//   [3,2,1]
+//   [1,1,2],
+//   [1,2,1],
+//   [2,1,1]
 // ]
+
 
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permute = function(nums) {
+const permuteUnique = function(nums) {
+  nums.sort((a, b) => a - b)
   const result = [], n = nums.length
-  function iter(used) {
-    if (used.length === n) result.push([...used])
+  function iter(array, used) {
+    if (array.length === n) result.push([...array])
     else {
-      for (let num of nums) {
-        if (!used.includes(num)) {
-          used.push(num)
-          iter(used)
-          used.pop(num)
+      for (let i = 0; i < n; i++) {
+        if (!used[i] && !(!used[i - 1] && nums[i] === nums[i - 1])) {
+          array.push(nums[i])
+          used[i] = true
+          iter(array, used)
+          array.pop(nums[i])
+          used[i] = false
         }
       }
     }
   }
-  iter([])
+  iter([], [])
   return result
 }
 
 ;[
-  [1,2,3],
+  [1,1,2],
 ].forEach(nums => {
-  console.log(permute(nums))
+  console.log(permuteUnique(nums))
 })
 
 // Solution:
-// 使用回溯算法遍历整个数组构成的树，
-// 在叶子处，将路径加入答案中。
+// 使用一个标记数组表示，某个位置的数是否使用过。
+// 在每层中选择一个没有使用过的数，且该数在该层之前也没有使用过。
 
 // Submission Result: Accepted
