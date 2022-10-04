fb59ffcc02a551ff5773bf6285209136d14c9a84
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-04 22:52:11 +0530

    17 :::: 0037-sudoku-solver.js leetcode question updated (2022-10-04)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,79 @@
-// 36. Valid Sudoku
-// Medium   36%
+// 37. Sudoku Solver
+// Hard   30%
 
-// Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+// Write a program to solve a Sudoku puzzle by filling the empty cells.
+// Empty cells are indicated by the character '.'.
+// You may assume that there will be only one unique solution.
 
-// The Sudoku board could be partially filled, where empty cells are filled with
-// the character '.'.
+// A sudoku puzzle...
 
-
-// A partially filled sudoku which is valid.
-
-// Note: A valid Sudoku board (partially filled) is not necessarily solvable.
-// Only the filled cells need to be validated.
+// ...and its solution numbers marked in red.
 
 /**
  * @param {character[][]} board
- * @return {boolean}
+ * @return {void} Do not return anything, modify board in-place instead.
  */
-const isValidSudoku = function(board) {
-  const n = 9, trunc = Math.trunc
-  for (let i = 0; i < n; i++) {
-    const checks = [{}, {}, {}]
-    for (let j = 0; j < n; j++) {
-      const c = [
-        board[i][j],
-        board[j][i],
-        board[trunc(i / 3) * 3 + trunc(j / 3)][j % 3 + i % 3 * 3]
-      ]
-      for (let k = 0; k < 3; k++) {
-        if (c[k] !== '.') {
-          if (checks[k][c[k]]) return false
-          checks[k][c[k]] = true
+const solveSudoku = function(board) {
+  const solve = function(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === '.') {
+          for (let c = 1; c <= 9; c++) {
+            if (isValid(board, i, j, c + '')) {
+              board[i][j] = c + ''
+              if(solve(board)) return true
+              else board[i][j] = '.'
+            }
+          }
+          return false
         }
       }
     }
+    return true
   }
 
-  return true
+  const isValid = function(board, row, col, x) {
+    const trunc = Math.trunc,
+          p = trunc(row / 3) * 3,
+          q = trunc(col / 3) * 3
+    for (let i = 0; i < 9; i++) {
+      if (board[row][i] === x ||
+          board[i][col] === x ||
+          board[p + trunc(i / 3)][i % 3 + q] === x)
+        return false
+    }
+
+    return true
+  }
+
+  for (let i = 0; i < 9; i++) board[i] = board[i].split('')
+  solve(board)
+  for (let i = 0; i < 9; i++) board[i] = board[i].join('')
 }
 
 ;[
   [
-    ".87654321",
-    "2........",
-    "3........",
-    "4........",
-    "5........",
-    "6........",
-    "7........",
-    "8........",
-    "9........"
-  ],                            // true
+    "53..7....",
+    "6..195...",
+    ".98....6.",
+    "8...6...3",
+    "4..8.3..1",
+    "7...2...6",
+    ".6....28.",
+    "...419..5",
+    "....8..79"
+  ],
 ].forEach(board => {
-  console.log(isValidSudoku(board))
+  console.log(board)
+  solveSudoku(board)
+  console.log(board)
 })
 
 // Solution:
-// 分别检查行，列，宫是否合法。
-// 检查一个组是否有重复，使用哈希表来判断。
+// 深度遍历表
+// 每次碰到点（空格位），就测试该位可填的最小的数。
+// 测试时，只需测试某数在该位是否合法，而不需要测试整个表。
+// 当遇到某个位置没有任何数可填时，则回溯到上一个数，上一个数进行改变再测试。
+// 合适就继续，不行就回溯，直到将整个表填完。
 
 // Submission Result: Accepted
