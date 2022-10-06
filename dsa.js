98991d5bfa812b7ec6e8949518cd76bb8a53989b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-06 22:54:44 +0530

    17 :::: 1232-check-if-it-is-a-straight-line.js leetcode question updated (2022-10-06)

--- dsa.js
+++ dsa.js
@@ -1,64 +1,60 @@
-// 1221. Split a String in Balanced Strings
-// Easy   82%
+// 1232. Check If It Is a Straight Line
+// Easy   47%
 
 
-// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
-// Given a balanced string s split it in the maximum amount of balanced strings.
-// Return the maximum amount of splitted balanced strings.
+// You are given an array coordinates, coordinates[i] = [x, y], where [x, y]
+// represents the coordinate of a point. Check if these points make a straight
+// line in the XY plane.
+
 
 // Example 1:
-// Input: s = "RLRRLLRLRL"
-// Output: 4
-// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring
-// contains same number of 'L' and 'R'.
+// Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+// Output: true
 // Example 2:
-// Input: s = "RLLLLRRRLR"
-// Output: 3
-// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains
-// same number of 'L' and 'R'.
-// Example 3:
-// Input: s = "LLLLRRRR"
-// Output: 1
-// Explanation: s can be split into "LLLLRRRR".
-// Example 4:
-// Input: s = "RLRRRLLRLL"
-// Output: 2
-// Explanation: s can be split into "RL", "RRRLLRLL", since each substring
-// contains an equal number of 'L' and 'R'
+// Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+// Output: false
 
 // Constraints:
-//     1 <= s.length <= 1000
-//     s[i] = 'L' or 'R'
+//     2 <= coordinates.length <= 1000
+//     coordinates[i].length == 2
+//     -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+//     coordinates contains no duplicate point.
 
 
 /**
- * @param {string} s
- * @return {number}
+ * @param {number[][]} coordinates
+ * @return {boolean}
  */
-const balancedStringSplit = function(s) {
-  let l = r = res = 0
-  for (let c of s) {
-    if (c === 'L') l++
-    else r++
-    if (l === r) {
-      res++
-      l = r = 0
-    }
+const checkStraightLine = function(coordinates) {
+  const [[x0, y0], [x1, y1]] = coordinates
+  const dx = x0 - x1, dy = y0 - y1
+  for (let c of coordinates) {
+    if (dx * (y0 - c[1]) !== dy * (x0 - c[0])) return false
   }
-  return res
+  return true
 }
 
 ;[
-  'RLRRLLRLRL',
-  'RLLLLRRRLR',
-  'LLLLRRRR',
-  'RLRRRLLRLL'
-].forEach((s) => {
-  console.log(balancedStringSplit(s))
+  [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]],
+  [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]],
+  [[1,1],[2,2],[2,0]], // false
+].forEach((coordinates) => {
+  console.log(checkStraightLine(coordinates))
 })
 
 // Solution:
-// 遍历字符串时，统计 'L' 和 'R' 的个数 l 和 r，
-// 当数量相同时，拆分，并重置 l=r=0 。
+//                         x1 - x2     dx
+// 两点连成的直线的斜率为 k = -------- = ------
+//                         y1 - y2     dy
+// 比较第一个点与其余点形成的直线的斜率，若都相同则都在一条直线上，否则不在.
+// 由于 JS 不好记录分数，因此，使用两个变量来记录dx和dy，分别为x轴距离 x1-x2 和 y轴距离
+// y1-y2
+// 在比较两个斜率时，将除法转换成乘法，如
+//  x1 - x2     x1 - xi
+// --------- = --------- --> (x1-x2) * (y1-yi) = (x1-xi) * (y1-y2)
+//  y1 - y2     y1 - yi
+//
+//  --> dx * (y1-yi) = dy * (x1-xi)
+
 
 // Submission Result: Accepted
