a056ef9f9bb615ef68d62e61d2f03b19003e2a6f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-09 22:52:47 +0530

    18 :::: 0143-reorder-list.js leetcode question updated (2022-10-09)

--- dsa.js
+++ dsa.js
@@ -1,58 +1,102 @@
-// 141. Linked List Cycle
-// Easy   35%
+// 143. Reorder List
+// Medium   36%
 
 
-// Given a linked list, determine if it has a cycle in it.
+// Given a singly linked list L: L0->L1->...->Ln-1->Ln,
+// reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
+// You may not modify the values in the list's nodes, only nodes itself may be
+// changed.
+// Example 1:
+// Given 1->2->3->4, reorder it to 1->4->2->3.
+// Example 2:
+// Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
 
-// Follow up:
-// Can you solve it without using extra space?
 
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ * this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
-
 /**
  * @param {ListNode} head
- * @return {boolean}
+ * @return {void} Do not return anything, modify head in-place instead.
  */
-const hasCycle = function(head) {
-  if (head == null || head.next == null) return false
-
-  let slow = head, fast = head.next
-  while (slow !== fast) {
-    if (fast == null || fast.next == null) return false
+const reorderList = function(head) {
+  if (!head) return null
+  let fast = slow = head
+  while (fast && fast.next) {
     slow = slow.next
     fast = fast.next.next
   }
-  return true
+  let node = slow.next
+  const stack = []
+  while (node) {
+    stack.push(node)
+    node = node.next
+  }
+  slow.next = null
+  node = head
+  while (stack.length > 0) {
+    const insert = stack.pop()
+    insert.next = node.next
+    node.next = insert
+    node = node.next.next
+  }
+}
+
+const better = function(head) {
+  if (!head || !head.next) return
+
+  let slow = fast = head
+  while (fast.next && fast.next.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  const preMid = slow
+  let preCur = slow.next
+  while (preCur.next) {
+    const cur = preCur.next
+    preCur.next = cur.next
+    cur.next = preMid.next
+    preMid.next = cur
+  }
+
+  let p1 = head
+  let p2 = preMid.next
+  while (p1 !== preMid) {
+    preMid.next = p2.next
+    p2.next = p1.next
+    p1.next = p2
+    p1 = p2.next
+    p2 = preMid.next
+  }
 }
 
 const ListNode = require('../structs/ListNode')
-
-const cycle_0 = ListNode.from([0,1,2,3,4,5,6])
-const tail_0 = cycle_0.nth(6)
-tail_0.next = cycle_0
-
-const cycle_1 = ListNode.from([0,1,2,3,4,5,6])
-const tail_1 = cycle_1.nth(6)
-const nth_3 = cycle_1.nth(3)
-tail_1.next = nth_3
-
 ;[
-  ListNode.from([3,2,0,-4]),             // false
-  cycle_0,                               // true
-  cycle_1,                               // true
-].forEach(head => {
-  console.log(hasCycle(head))
+  [1,2,3,4],
+  [1,2,3,4,5],
+  [1],
+  [1,2],
+  [1,2,3],
+  [],
+].forEach((array) => {
+  const list = ListNode.from(array)
+  // reorderList(list)
+  better(list)
+  if (list) console.log(list.toString())
+  else console.log('null')
 })
 
 // Solution:
-// 方法1. 用一个标记来标记节点是否被放问过。
+// 找到后半部分，依次入栈，再出栈插入合适的位置。
 
-// 方法2. 偏差遍历。若有循环，其相同节点迟早会相遇。
+// 更好的方法
+// 1. 找到后半部分
+// 2. 将后半部分翻转
+// 3. 将后半部分插入到前半部。
 
 // Submission Result: Accepted
