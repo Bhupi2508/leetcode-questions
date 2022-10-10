bdff52c72494cff334422191ce2a052d1ad2f52c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-10 22:54:45 +0530

    18 :::: 1260-shift-2d-grid.js leetcode question updated (2022-10-10)

--- dsa.js
+++ dsa.js
@@ -1,74 +1,74 @@
-// 1252. Cells with Odd Values in a Matrix
-// Easy   78%
+// 1260. Shift 2D Grid
+// Easy   61%
 
 
-// Given n and m which are the dimensions of a matrix initialized by zeros and
-// given an array indices where indices[i] = [ri, ci]. For each pair of [ri, ci]
-// you have to increment all cells in row ri and column ci by 1.
-// Return the number of cells with odd values in the matrix after applying the
-// increment to all indices.
+// Given a 2D grid of size m x n and an integer k. You need to shift the grid k
+// times.
+// In one shift operation:
+//     Element at grid[i][j] moves to grid[i][j + 1].
+//     Element at grid[i][n - 1] moves to grid[i + 1][0].
+//     Element at grid[m - 1][n - 1] moves to grid[0][0].
+// Return the 2D grid after applying shift operation k times.
 
 // Example 1:
-// Input: n = 2, m = 3, indices = [[0,1],[1,1]]
-// Output: 6
 //
-// 0 0 0   0,1   1 2 1   1,1   1 3 1
-// 0 0 0  ---->  0 1 0  ---->  1 3 1
+//  1 2 3      9 1 2
+//  4 5 6  ->  3 4 5
+//  7 8 9      7 8 9
 //
-// Explanation: Initial matrix = [[0,0,0],[0,0,0]].
-// After applying first increment it becomes [[1,2,1],[0,1,0]].
-// The final matrix will be [[1,3,1],[1,3,1]] which contains 6 odd numbers.
-
+// Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+// Output: [[9,1,2],[3,4,5],[6,7,8]]
 // Example 2:
-// Input: n = 2, m = 2, indices = [[1,1],[0,0]]
-// Output: 0
-//
-// 0 0   1,1   0 1   0,0   2 2
-// 0 0  ---->  1 2  ---->  2 2
-//
-// Explanation: Final matrix = [[2,2],[2,2]]. There is no odd number in the final
-// matrix.
+// Input: grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4
+// Output: [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
+// Example 3:
+// Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9
+// Output: [[1,2,3],[4,5,6],[7,8,9]]
 
 // Constraints:
-//     1 <= n <= 50
+//     m == grid.length
+//     n == grid[i].length
 //     1 <= m <= 50
-//     1 <= indices.length <= 100
-//     0 <= indices[i][0] < n
-//     0 <= indices[i][1] < m
+//     1 <= n <= 50
+//     -1000 <= grid[i][j] <= 1000
+//     0 <= k <= 100
 
 
 /**
- * @param {number} n
- * @param {number} m
- * @param {number[][]} indices
- * @return {number}
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
  */
-const oddCells = function(n, m, indices) {
-  const matrix = Array(n)
-  for (let i = 0; i < n; i++) matrix[i] = Array(m).fill(0)
-  for (let indice of indices) {
-    for (let i = 0; i < m; i++) matrix[indice[0]][i]++
-    for (let i = 0; i < n; i++) matrix[i][indice[1]]++
-  }
-  let res = 0
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (matrix[i][j] % 2) res++
+const shiftGrid = function(grid, k) {
+  const m = grid.length, n = grid[0].length
+  const rowShift = Math.floor(k / n) % m, colShift = k % n
+  const res = []
+  for (let i = 0; i < m; i++) {
+    res[i] = []
+    for (let j = 0; j < n; j++) {
+      const r = (i - rowShift + Math.floor((j - colShift) / n) + m) % m
+      const c = (j - colShift + n) % n
+      res[i][j] = grid[r][c]
     }
   }
   return res
 }
 
 ;[
-  [2, 3, [[0,1],[1,1]]],
-  [2, 2, [[1,1],[0,0]]],
-].forEach(([n, m, indices]) => {
-  console.log(oddCells(n, m, indices))
+  [[[1,2,3],[4,5,6],[7,8,9]], 1],
+  [[[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], 4],
+  [[[1,2,3],[4,5,6],[7,8,9]], 9],
+].forEach(([grid, k]) => {
+  console.log(shiftGrid(grid, k))
 })
 
 // Solution:
-// 创建一个矩阵，直接执行操作，最后统计即可。
+// 水平移动距离为 colShift = k % n
+// 垂直移动距离为 rowShift = Math.floor(k / n) % m （k除以n取整为行移动距离，取m的模是
+// 因为移动m次与不移动是一样的）
 
-// TODO #1252 使用时间和空间复杂度更低的方法
+// 移动后网格 res[i][j] 的数据为 grid[r][c]
+// c = (j - colShift + n) % n，其中 +n 是为了读取向前读取时可以读到上一行的数据
+// r = (i - rowShift - Math.floor((j - colShift) / n) + m) % m
 
 // Submission Result: Accepted
