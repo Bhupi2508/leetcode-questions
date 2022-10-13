d082fdcd4578768f51d3e4f133731b7e3a95be8d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-13 22:54:28 +0530

    18 :::: 1002-find-common-characters.js leetcode question updated (2022-10-13)

--- dsa.js
+++ dsa.js
@@ -1,58 +1,63 @@
-// 1000. Minimum Cost to Merge Stones
-// Hard   40%
+// 1002. Find Common Characters
+// Easy   66%
 
 
-// There are N piles of stones arranged in a row.  The i-th pile has stones[i]
-// stones.
-// A move consists of merging exactly K consecutive piles into one pile, and the
-// cost of this move is equal to the total number of stones in these K piles.
-// Find the minimum cost to merge all piles of stones into one pile.  If it is
-// impossible, return -1.
+// Given an array A of strings made only from lowercase letters, return a list of
+// all characters that show up in all strings within the list (including
+// duplicates).  For example, if a character occurs 3 times in all strings but
+// not 4 times, you need to include that character three times in the final
+// answer.
+// You may return the answer in any order.
 
 // Example 1:
-// Input: stones = [3,2,4,1], K = 2
-// Output: 20
-// Explanation:
-// We start with [3, 2, 4, 1].
-// We merge [3, 2] for a cost of 5, and we are left with [5, 4, 1].
-// We merge [4, 1] for a cost of 5, and we are left with [5, 5].
-// We merge [5, 5] for a cost of 10, and we are left with [10].
-// The total cost was 20, and this is the minimum possible.
+// Input: ["bella","label","roller"]
+// Output: ["e","l","l"]
 // Example 2:
-// Input: stones = [3,2,4,1], K = 3
-// Output: -1
-// Explanation: After any merge operation, there are 2 piles left, and we can't
-// merge anymore.  So the task is impossible.
-// Example 3:
-// Input: stones = [3,5,1,2,6], K = 3
-// Output: 25
-// Explanation:
-// We start with [3, 5, 1, 2, 6].
-// We merge [5, 1, 2] for a cost of 8, and we are left with [3, 8, 6].
-// We merge [3, 8, 6] for a cost of 17, and we are left with [17].
-// The total cost was 25, and this is the minimum possible.
+// Input: ["cool","lock","cook"]
+// Output: ["c","o"]
 
 // Note:
-//     1 <= stones.length <= 30
-//     2 <= K <= 30
-//     1 <= stones[i] <= 100
+//     1 <= A.length <= 100
+//     1 <= A[i].length <= 100
+//     A[i][j] is a lowercase letter
 
 
 /**
- * @param {number[]} stones
- * @param {number} K
- * @return {number}
+ * @param {string[]} A
+ * @return {string[]}
  */
-const mergeStones = function(stones, K) {
-
+const commonChars = function(A) {
+  let arr = Array(26).fill(101)
+  for (let S of A) {
+    let temp = Array(26).fill(0)
+    for (let c of S) temp[c.charCodeAt(0) - 97]++
+    for (let i = 0; i < 26; i++) arr[i] = Math.min(arr[i], temp[i])
+  }
+  let res = []
+  for (let i = 0; i < 26; i++) {
+    for (let j = 0; j < arr[i]; j++) res.push(String.fromCharCode(i + 97))
+  }
+  return res
 }
 
 ;[
-  [3,2,4,1], 2,
-].forEach(() => {
-
+  ['bella','label','roller'], // ["e", "l", "l"]
+  ["cool","lock","cook"],     // ["c","o"]
+  ["bbcd","accd","abdd","aabc"], // []
+  ["daaccccd","adacbdda","abddbaba","bacbcbcb","bdaaaddc"] // ['a']
+].forEach((A) => {
+  console.log(commonChars(A))
 })
 
 // Solution:
+// 1. 前两个比较合并
+// 每次选出两个字符串的共同字符，选出后合并成新字符串，并和下一个字符串对比，直至最后一个字符串
+// 选出两个字符串的方法是使用 hash 记录第一个字符串的字符数
+// 若第二个字符串也有，则选出来
 
-// Submission Result: Accept
+// 2. 使用桶数组代替字符串
+// 用长度为26的数组，来记录字符的个数
+// 后一个字符串的数组和前一个比较，取其小者，替换前一个
+// 最后得到的桶数组则为字符的个数
+
+// Submission Result: Accepted
