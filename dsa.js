089bafe4ec3eac43671428712d7b1c3896420571
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-15 22:54:41 +0530

    18 :::: 1189-maximum-number-of-balloons.js leetcode question updated (2022-10-15)

--- dsa.js
+++ dsa.js
@@ -1,68 +1,47 @@
-// 1185. Day of the Week
-// Easy   64%
+// 1189. Maximum Number of Balloons
+// Easy   61%
 
 
-// Given a date, return the corresponding day of the week for that date.
-// The input is given as three integers representing the day, month and year
-// respectively.
-// Return the answer as one of the following values {"Sunday", "Monday",
-// "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+// Given a string text, you want to use the characters of text to form as many
+// instances of the word "balloon" as possible.
+// You can use each character in text at most once. Return the maximum number of
+// instances that can be formed.
 
 // Example 1:
-// Input: day = 31, month = 8, year = 2019
-// Output: "Saturday"
+// Input: text = "nlaebolko"
+// Output: 1
 // Example 2:
-// Input: day = 18, month = 7, year = 1999
-// Output: "Sunday"
+// Input: text = "loonbalxballpoon"
+// Output: 2
 // Example 3:
-// Input: day = 15, month = 8, year = 1993
-// Output: "Sunday"
+// Input: text = "leetcode"
+// Output: 0
 
 // Constraints:
-//     The given dates are valid dates between the years 1971 and 2100.
+//     1 <= text.length <= 10^4
+//     text consists of lower case English letters only.
 
 
 /**
- * @param {number} day
- * @param {number} month
- * @param {number} year
- * @return {string}
+ * @param {string} text
+ * @return {number}
  */
-const dayOfTheWeek = function(day, month, year) {
-  const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  if (month < 3) {
-    month += 12
-    year -= 1
-  }
-  const c = Math.floor(year / 100)
-  year = year % 100
-  // LEARN #1185 日期算出星期
-  const w = (
-    Math.floor(c / 4) - 2 * c + year + Math.floor(year / 4) +
-    Math.floor(13 * (month + 1) / 5) +
-    day - 1
-  ) % 7
-  return DAYS[Math.floor(w + 7) % 7]
-}
-
-const builtInFunc = function(day, month, year) {
-  const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  return DAYS[new Date(year, month - 1, day).getDay()]
+const maxNumberOfBalloons = function(text) {
+  const hash = { b: 0, a: 0, l: 0, o: 0, n: 0 }
+  for (let c of text) if (hash[c] >= 0) hash[c]++
+  return Math.min(hash['b'], hash['a'], Math.floor(hash['l'] / 2), Math.floor(hash['o'] / 2), hash['n'])
 }
 
 ;[
-  [1,  1, 1971], // Fri
-  [31, 8, 2019],
-  [18, 7, 1999],
-  [15, 8, 1993],
-].forEach(([day,month, year]) => {
-  console.log(dayOfTheWeek(day, month, year))
+  'nlaebolko',
+  'loonbalxballpoon',
+  'leetcode',
+].forEach((text) => {
+  console.log(maxNumberOfBalloons(text))
 })
 
 // Solution:
-// 方法一，使用内置 Date 对象的函数
-
-// 方法二，
-// TODO #1185 日期算出星期
+// 使用 hash 记录字符串中字母 'b', 'a', 'l', 'o', 'n' 的数量
+// 取其数量的最小值即可（其中 'l', 'o' 需要除 2，因为这两个字母在 'balloon' 中出现了两次）
 
 // Submission Result: Accepted
