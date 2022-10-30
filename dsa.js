c3324410ad90e50d8c6cac624b2dbce6343fcb23
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-10-30 22:53:13 +0530

    21 :::: 0437-path-sum-iii.js leetcode question updated (2022-10-30)

--- dsa.js
+++ dsa.js
@@ -1,34 +1,78 @@
-// 434. Number of Segments in a String
-// Easy   36%
+// 437. Path Sum III
+// Easy   39%
 
 
-// Count the number of segments in a string, where a segment is defined to be a
-// contiguous sequence of non-space characters.
+// You are given a binary tree in which each node contains an integer value.
 
-// Please note that the string does not contain any non-printable characters.
+// Find the number of paths that sum to a given value.
+
+// The path does not need to start or end at the root or a leaf, but it must go
+// downwards
+// (traveling only from parent nodes to child nodes).
+
+// The tree has no more than 1,000 nodes and the values are in the range
+// -1,000,000 to 1,000,000.
 
 // Example:
 
-// Input: "Hello, my name is John"
-// Output: 5
+// root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+//       10
+//      /  \
+//     5   -3
+//    / \    \
+//   3   2   11
+//  / \   \
+// 3  -2   1
+
+// Return 3. The paths that sum to 8 are:
+
+// 1.  5 -> 3
+// 2.  5 -> 2 -> 1
+// 3. -3 -> 11
 
 
 /**
- * @param {string} s
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
  * @return {number}
  */
-const countSegments = function(s) {
-  return s.split(' ').filter(w => w !== '').length
+const pathSum = function(root, sum) {
+  function iter(root, rests) {
+    if (root == null) return 0
+
+    const nRests = [sum]
+    let count = 0
+    for (let rest of rests) {
+      if (rest - root.val === 0) count++
+      nRests.push(rest - root.val)
+    }
+
+    return iter(root.left, nRests) + iter(root.right, nRests) + count
+  }
+  return iter(root, [sum])
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  "Hello, my name is John",     // 5
-].forEach(s => {
-  console.log(countSegments(s))
+  [[10,5,-3,3,2,null,11,3,-2,null,1], 8], // 3
+  [[1,-2,-3,1,3,-2,null,-1], -1],         // 4
+].forEach(([array, sum]) => {
+  const root = TreeNode.from(array)
+  console.log(pathSum(root, sum))
 })
 
 // Solution:
-// 可耻地使用了 split 函数。
-// filter 去除多余的空格产生的空字符。
+
+// 每遍历一个节点，构造一个新的数组，数组的元素是当前节点到上层节点的和再与所要
+// 的总和的差。如果差为0，则表明有一条路径。
 
 // Submission Result: Accepted
