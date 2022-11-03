d079c743165911cad4a5134fa2095ed0e804bc1c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-03 22:54:16 +0530

    21 :::: 0937-reorder-data-in-log-files.js leetcode question updated (2022-11-03)

--- dsa.js
+++ dsa.js
@@ -1,68 +1,106 @@
-// 933. Number of Recent Calls
-// Easy   71%
+// 937. Reorder Data in Log Files
+// Easy   54%
 
 
-// Write a class RecentCounter to count recent requests.
-// It has only one method: ping(int t), where t represents some time in
-// milliseconds.
-// Return the number of pings that have been made from 3000 milliseconds ago
-// until now.
-// Any ping with time in [t - 3000, t] will count, including the current ping.
-// It is guaranteed that every call to ping uses a strictly larger value of t
-// than before.
+// You have an array of logs.  Each log is a space delimited string of words.
+// For each log, the first word in each log is an alphanumeric identifier.  Then,
+// either:
+//     Each word after the identifier will consist only of lowercase letters, or;
+//     Each word after the identifier will consist only of digits.
+// We will call these two varieties of logs letter-logs and digit-logs.  It is
+// guaranteed that each log has at least one word after its identifier.
+// Reorder the logs so that all of the letter-logs come before any digit-log.
+// The letter-logs are ordered lexicographically ignoring identifier, with the
+// identifier used in case of ties.  The digit-logs should be put in their
+// original order.
+// Return the final order of the logs.
 
 // Example 1:
-// Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs =
-// [[],[1],[100],[3001],[3002]]
-// Output: [null,1,2,3,3]
+// Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit
+// dig","let3 art zero"]
+// Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5
+// 1","dig2 3 6"]
 
-// Note:
-//     Each test case will have at most 10000 calls to ping.
-//     Each test case will call ping with strictly increasing values of t.
-//     Each call to ping will have 1 <= t <= 10^9.
+// Constraints:
+//     0 <= logs.length <= 100
+//     3 <= logs[i].length <= 100
+//     logs[i] is guaranteed to have an identifier, and a word after the
+// identifier.
 
-
-
-
-const RecentCounter = function() {
-  this.list = []
-};
 
 /**
- * @param {number} t
- * @return {number}
+ * @param {string[]} logs
+ * @return {string[]}
  */
-RecentCounter.prototype.ping = function(t) {
-  this.list.unshift(t)
-  while (this.list[this.list.length - 1] < t - 3000) this.list.pop()
-  return this.list.length
-};
+const reorderLogFiles = function(logs) {
+  let digs = [], lets = []
+  for (let s of logs) {
+    const spaceIndex = s.indexOf(' ')
+    const c = s[spaceIndex + 1]
+    if (c <= '9') digs.push(s)
+    else lets.push(s)
+  }
+  lets = lets.sort((a, b) => {
+    const aSpaceIndex = a.indexOf(' ')
+    const bSpaceIndex = b.indexOf(' ')
+    const aSub = a.substring(aSpaceIndex + 1)
+    const bSub = b.substring(bSpaceIndex + 1)
+    if (aSub < bSub) return -1
+    if (aSub > bSub) return 1
 
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * var obj = new RecentCounter()
- * var param_1 = obj.ping(t)
- */
+    const aId = a.substring(0, aSpaceIndex)
+    const bId = b.substring(0, bSpaceIndex)
+    if (aId < bId) return -1
+    if (aId > bId) return 1
+    return 0
+  })
+  return lets.concat(digs)
+}
 
 ;[
-  [['RecentCounter','ping','ping','ping','ping'], [[],[1],[100],[3001],[3002]]],
-  // [null,1,2,3,3]
-].forEach(([commands, times]) => {
-  let recentCounter = null
-  for (let i = 0; i < commands.length; i++) {
-    if (commands[i] == 'RecentCounter') {
-      recentCounter = new RecentCounter()
-    } else if (commands[i] == 'ping') {
-      console.log(recentCounter.ping(times[i][0]))
-    }
-  }
+  ['dig1 8 1 5 1','let1 art can','dig2 3 6','let2 own kit dig','let3 art zero'],
+  // ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 51","dig2 3 6"]
+  ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"],
+  // ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+  ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a2 act car"],
+  // ["a2 act car","g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+].forEach((logs) => {
+  console.log(reorderLogFiles(logs))
 })
 
+const batter = function(logs) {
+  logs.sort((a, b) => {
+    const aSpaceIndex = a.indexOf(' ')
+    const bSpaceIndex = b.indexOf(' ')
+    const aFirstChar = a[aSpaceIndex + 1]
+    const bFirstChar = b[bSpaceIndex + 1]
+
+    if (aFirstChar <= '9') {
+      return bFirstChar <= '9' ? 0 : 1
+    }
+    if (bFirstChar <= '9') return -1
+
+    const aSub = a.substring(aSpaceIndex + 1)
+    const bSub = b.substring(bSpaceIndex + 1)
+    if (aSub < bSub) return -1
+    if (aSub > bSub) return 1
+
+    const aId = a.substring(0, aSpaceIndex)
+    const bId = b.substring(0, bSpaceIndex)
+    if (aId < bId) return -1
+    if (aId > bId) return 1
+    return 0
+  })
+  return logs
+}
+
 // Solution:
-// 对象内部维护一个数组 list
-// 每次 ping 时，将 t 添加到数组头部，
-// 然后从数组尾部遍历，将时间差超过 3000 的数从数组中移除，
-// 直到找到第一个不超范围的数为止，
-// 最后返回数组的长度即可。
+// 使用 sort 函数，进行排序。
+// 在比较两个字符串的大小时，
+// 首先找到第一个空格的位置，读取空格后的一个字符，进行比较
+//  1. 若都为数字，则按原来的顺序排序
+//  2. 若有一个为数字，则该字符串排在后面
+//  3. 若都为字符，则比较第一个空格后的字符串的大小，小的在前
+//     若字符串相等，则比较空格前的字符串。
 
 // Submission Result: Accepted
