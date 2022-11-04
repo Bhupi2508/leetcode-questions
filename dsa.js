d6e86d03659c171712e0971fccce7c8483cf853b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-04 22:55:05 +0530

    21 :::: 1370-increasing-decreasing-string.js leetcode question updated (2022-11-04)

--- dsa.js
+++ dsa.js
@@ -1,58 +1,90 @@
-// 1365. How Many Numbers Are Smaller Than the Current Number
-// Easy   85%
+// 1370. Increasing Decreasing String
+// Easy   75%
 
 
-// Given the array nums, for each nums[i] find out how many numbers in the array
-// are smaller than it. That is, for each nums[i] you have to count the number of
-// valid j's such that j != i and nums[j] < nums[i].
-// Return the answer in an array.
+// Given a string s. You should re-order the string using the following
+// algorithm:
+//     Pick the smallest character from s and append it to the result.
+//     Pick the smallest character from s which is greater than the last appended
+// character to the result and append it.
+//     Repeat step 2 until you cannot pick more characters.
+//     Pick the largest character from s and append it to the result.
+//     Pick the largest character from s which is smaller than the last appended
+// character to the result and append it.
+//     Repeat step 5 until you cannot pick more characters.
+//     Repeat the steps from 1 to 6 until you pick all characters from s.
+// In each step, If the smallest or the largest character appears more than once
+// you can choose any occurrence and append it to the result.
+// Return the result string after sorting s with this algorithm.
 
 // Example 1:
-// Input: nums = [8,1,2,2,3]
-// Output: [4,0,1,1,3]
-// Explanation:
-// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
-// For nums[1]=1 does not exist any smaller number than it.
-// For nums[2]=2 there exist one smaller number than it (1).
-// For nums[3]=2 there exist one smaller number than it (1).
-// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+// Input: s = "aaaabbbbcccc"
+// Output: "abccbaabccba"
+// Explanation: After steps 1, 2 and 3 of the first iteration, result = "abc"
+// After steps 4, 5 and 6 of the first iteration, result = "abccba"
+// First iteration is done. Now s = "aabbcc" and we go back to step 1
+// After steps 1, 2 and 3 of the second iteration, result = "abccbaabc"
+// After steps 4, 5 and 6 of the second iteration, result = "abccbaabccba"
 // Example 2:
-// Input: nums = [6,5,4,8]
-// Output: [2,1,0,3]
+// Input: s = "rat"
+// Output: "art"
+// Explanation: The word "rat" becomes "art" after re-ordering it with the
+// mentioned algorithm.
 // Example 3:
-// Input: nums = [7,7,7,7]
-// Output: [0,0,0,0]
+// Input: s = "leetcode"
+// Output: "cdelotee"
+// Example 4:
+// Input: s = "ggggggg"
+// Output: "ggggggg"
+// Example 5:
+// Input: s = "spo"
+// Output: "ops"
 
 // Constraints:
-//     2 <= nums.length <= 500
-//     0 <= nums[i] <= 100
+//     1 <= s.length <= 500
+//     s contains only lower-case English letters.
 
 
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {string} s
+ * @return {string}
  */
-const smallerNumbersThanCurrent = function(nums) {
-  const n = nums.length
-  const res = Array(n).fill(0)
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (nums[j] > nums[i]) res[j]++
+const sortString = function(s) {
+  const map = Array(26).fill(0)
+  for (let c of s) map[c.charCodeAt(0) - 97]++
+  let res = ''
+  while (res.length < s.length) {
+    for (let j = 0; j < 27; j++) {
+      if (map[j] > 0) {
+        res += String.fromCharCode(97 + j)
+        map[j]--
+      }
+    }
+    for (let j = 26; j >= 0; j--) {
+      if (map[j] > 0) {
+        res += String.fromCharCode(97 + j)
+        map[j]--
+      }
     }
   }
   return res
 }
 
 ;[
-  [8,1,2,2,3],
-  [6,5,4,8],
-  [7,7,7,7],
-].forEach((nums) => {
-  console.log(smallerNumbersThanCurrent(nums))
+  'aaaabbbbcccc',
+  'rat',
+  'leetcode',
+  'ggggggg',
+  'spo',
+].forEach((s) => {
+  console.log(sortString(s))
 })
 
 // Solution:
-// 初始化一个数组记录有多少数小于当前数。
-// 两两比较，大的下标加一。
+// 将每个字符的个数保存到一个数组中，
+// 该数组是根据字符值从小到大排列的。
+// 从头遍历数组，并将有计数的字符添加到答案中。
+// 再从尾遍历数组，并将有计数的字符添加到答案中。
+// 重复该过程，直到所有字符都再答案中。
 
 // Submission Result: Accepted
