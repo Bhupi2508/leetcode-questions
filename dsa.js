32abb02ca3ebd2cd74ca9a1379a5b99ed19105b1
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-09 22:52:52 +0530

    22 :::: 0198-house-robber.js leetcode question updated (2022-11-09)

--- dsa.js
+++ dsa.js
@@ -1,38 +1,44 @@
-// 191. Number of 1 Bits
+// 198. House Robber
 // Easy   39%
 
 
-// Write a function that takes an unsigned integer and returns the number of ’1'
-// bits it has (also known as the Hamming weight).
+// You are a professional robber planning to rob houses along a street. Each
+// house has a certain amount of money stashed, the only constraint stopping you
+// from robbing each of them is that adjacent houses have security system
+// connected and it will automatically contact the police if two adjacent houses
+// were broken into on the same night.
 
-// For example, the 32-bit integer ’11' has binary representation
-// 00000000000000000000000000001011, so the function should return 3.
+// Given a list of non-negative integers representing the amount of money of each
+// house, determine the maximum amount of money you can rob tonight without
+// alerting the police.
 
-// Credits:Special thanks to @ts for adding this problem and creating all test
-// cases.
+// Credits:Special thanks to @ifanchu for adding this problem and creating all
+// test cases. Also thanks to @ts for adding additional test cases.
 
 
 /**
- * @param {number} n - a positive integer
+ * @param {number[]} nums
  * @return {number}
  */
-const hammingWeight = function(n) {
-  let result = 0
-  while (n) {
-    result += n % 2
-    n = n >>> 1
+const rob = function(nums) {
+  const n = nums.length
+  for (let i = 2; i < n; i++) {
+    nums[i] += Math.max(nums[i - 2], (nums[i - 3] || 0))
   }
-  return result
+  return n ? Math.max(nums[n - 1], (nums[n - 2] || 0)) : 0
 }
 
 ;[
-  11,                           // 3
-].forEach(n => {
-  console.log(hammingWeight(n))
+  [5,4,3,2,1,4,2,4],            // 16
+  [1,7,9,4],                    // 11
+  [],                           // 0
+  [0],                          // 0
+].forEach(nums => {
+  console.log(rob(nums))
 })
 
 // Solution:
-// %2 运算获得最后一位的比特值
-// >>>1 逻辑右移
+// 利用累积法。
+// 每到一个房子i，就加上（i - 2) 和 (i - 3) 中累积的最大的那个作为当前最大收益。
 
 // Submission Result: Accepted
