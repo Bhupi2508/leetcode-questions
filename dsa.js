b01fa47e22b8a9476a70161f6e6165e758835405
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-11 22:52:21 +0530

    22 :::: 0064-minimum-path-sum.js leetcode question updated (2022-11-11)

--- dsa.js
+++ dsa.js
@@ -1,62 +1,54 @@
-// 63. Unique Paths II
-// Medium   31%
+// 64. Minimum Path Sum
+// Medium   38%
 
-// Follow up for "Unique Paths":
+// Given a m x n grid filled with non-negative numbers, find a path from top
+// left to bottom right which minimizes the sum of all numbers along its path.
 
-// Now consider if some obstacles are added to the grids. How many unique paths
-// would there be?
+// Note: You can only move either down or right at any point in time.
 
-// An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+// Example 1:
 
-// For example,
+// [[1,3,1],
+//  [1,5,1],
+//  [4,2,1]]
 
-// There is one obstacle in the middle of a 3x3 grid as illustrated below.
-
-// [
-//   [0,0,0],
-//   [0,1,0],
-//   [0,0,0]
-// ]
-
-// The total number of unique paths is 2.
-
-// Note: m and n will be at most 100.
+// Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes
+// the sum.
 
 /**
- * @param {number[][]} obstacleGrid
+ * @param {number[][]} grid
  * @return {number}
  */
-const uniquePathsWithObstacles = function(obstacleGrid) {
-  const n = obstacleGrid[0].length
-  const dp = Array(n).fill(0)
-  dp[0] = 1
-  for (let row of obstacleGrid) {
-    for (let i = 0; i < n; i++) {
-      if (row[i] === 1) dp[i] = 0
-      else if (i > 0) dp[i] += dp[i - 1]
+const minPathSum = function(grid) {
+  const m = grid.length, n = grid[0].length
+  const dp = Array(n)
+  dp[0] = grid[0][0]
+  for (let i = 1; i < n; i++) dp[i] = dp[i - 1] + grid[0][i]
+  for (let i = 1; i < m; i++) {
+    dp[0] +=  grid[i][0]
+    for (let j = 1; j < n; j++) {
+      dp[j] = grid[i][j] + Math.min(dp[j - 1], dp[j])
     }
   }
-
   return dp[n - 1]
 }
 
 ;[
   [
-    [1],
-    [0]
+    [1, 3],
+    [2, 1]
   ],
   [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0]
+    [1,3,1],
+    [1,5,1],
+    [4,2,1]
   ],
-].forEach(obstacleGrid => {
-  console.log(uniquePathsWithObstacles(obstacleGrid))
+].forEach(grid => {
+  console.log(minPathSum(grid))
 })
 
 // Solution:
-// 在 62-unique-paths.js 的基础上加上了一些阻碍。
-// 只需要将阻碍的位置设为0即可，其他无需改变。
-// 同样是使用一个数组的动态规划。
+// 同 62-unique-paths.js 一样使用动态规划
+// 只是在其基础上多了路径权数。
 
 // Submission Result: Accepted
