fa7082b2cc34a236ca5dd7394e5089c9f2bbd0de
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-12 22:44:49 +0530

    22 :::: 17-LetterCombinationsPhoneNumber.js question updated

--- dsa.js
+++ dsa.js
@@ -1,19 +1,53 @@
 /**
- * key. binary search, why does it work?
- * explanation: https://leetcode.com/discuss/17793/find-the-maximum-by-binary-search-recursion-and-iteration
- *
- * @param {number[]} nums
- * @return {number}
+ * Backtracking. Same idea as combination.
+ * @param {string} digits
+ * @return {string[]}
  */
-var findPeakElement = function(nums) {
-    var low = 0;
-    var high = nums.length - 1;
+var letterCombinations = function(digits) {
+    var result = [];
+    var results = [];
+    if (!digits || digits.length === 0) return results;
+    combinationHelper(digits, results, result);
+    return results;
+};
 
-    while (low < high) {
-        var mid = low + Math.floor((high - low) / 2);
-        if (nums[mid+1] > nums[mid]) low = mid + 1;
-        else high = mid;
+var map = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+    "0": ""
+};
+
+var combinationHelper = function(digits, results, result) {
+    if (digits.length === 0) {
+        results.push(result.join(''));
+        return;
     }
 
-    return high;
+    var letters = map[digits.substring(0, 1)];
+    for (var i = 0; i < letters.length; i++) {
+        result.push(letters[i]);
+        combinationHelper(digits.substring(1), results, result);
+        result.pop();
+    }
+};
+
+// second try
+var combinationHelper = function(digits, results, result) {
+    if (!digits) {
+        results.push(result.join(''));
+        result = [];
+        return;
+    }
+    var letters = map[digits[0]];
+    for (var j = 0; j < letters.length; j++) {
+        result.push(letters[j]);
+        combinationHelper(digits.substring(1), results, result);
+        result.pop();
+    }
 };
