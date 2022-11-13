e4942bcbfd7246ee2abd5ffacd4d7e7734764e03
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-13 22:45:02 +0530

    23 :::: 337-houseRobberIII.js question updated

--- dsa.js
+++ dsa.js
@@ -1,34 +1,65 @@
 /**
- * Solution: use stack
- * Note, stack[stack.length - 3] !== '#' is important. for the case "9, #, #,#,# "
- * @param {string} preorder
- * @return {boolean}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var isValidSerialization = function(preorder) {
-    var stack = [];
-    var i = 0;
-    var newStr = preorder.split(',');
-    var length = newStr.length;
+/**
+ * Key: similar to every house in previous hourse rob problems, each node store two values,
+ * first value is to store the max value if the node will be robbed. The second value is the vaule
+ * if the node will not be robbed.
+ * In the first case, the value is straightforward to calculate, that is, current node's val
+ * plus the second value from it's child nodes' second value (not robbed).
+ * In the second case, because the node is not going to be robbed, its children can be robbed
+ * or not robbed. So the value is the max value of the left child's two values plus the right child's
+ * max value from the two values.
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var rob = function(root) {
+     var result = robDFS(root);
+     return Math.max(result[0], result[1]);
+ };
 
-    while (i < length) {
-        stack.push(newStr[i]);
-        while (stack.length > 2 && stack[stack.length - 1] === '#' &&
-            stack[stack.length - 2] === '#' && stack[stack.length - 3] !== '#') {
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.push('#');
-        }
-        i++;
-    }
+ var robDFS = function(root) {
+     if (!root) return [0, 0];
+     var result = [];
+     var leftResults = robDFS(root.left);
+     var rightResults = robDFS(root.right);
+     result[0] = root.val + leftResults[1] + rightResults[1];
+     result[1] = Math.max(leftResults[0], leftResults[1]) + Math.max(rightResults[0], rightResults[1]);
+     return result;
+ };
 
-    if (stack.length === 1 && stack[0] === '#') {
-        return true;
-    }
-    return false;
+
+// not right, why?
+var rob = function(root) {
+    var cachedResult = {};
+    return robHelper(root, cachedResult);
 };
 
+var robHelper = function(root, cachedResult) {
+    if (!root)  return 0;
+    if (cachedResult.hasOwnProperty(root))  {
+        return cachedResult[root];
+    }
+    var val = 0;
 
-// Testing cases
-// "9,3,4,#,#,1,#,#,2,#,6,#,#"
-// "1, #, #, #, #"
+    if (root.left !== null) {
+        val += robHelper(root.left.left, cachedResult) + robHelper(root.left.right, cachedResult);
+            console.log(root, val);
+    }
+
+    if (root.right !== null) {
+        val += robHelper(root.right.left, cachedResult) + robHelper(root.right.right, cachedResult);
+            console.log(root, val);
+
+    }
+
+    val = Math.max(val + root.val, robHelper(root.left, cachedResult) + robHelper(root.right, cachedResult));
+    cachedResult[root] = val;
+
+    return val;
+};
