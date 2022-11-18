1cd18d23732c7fd47c2181b7fe5b4e81cee265a8
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-18 22:52:31 +0530

    23 :::: 0090-subsets-ii.js leetcode question updated (2022-11-18)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,62 @@
-// 89. Gray Code
-// Medium   41%
+// 90. Subsets II
+// Medium   36%
 
-// The gray code is a binary numeral system where two successive values differ
-// in only one bit.
+// Given a collection of integers that might contain duplicates, nums, return
+// all possible subsets.
 
-// Given a non-negative integer n representing the total number of bits in the
-// code, print the sequence of gray code. A gray code sequence must begin with
-// 0.
+// Note: The solution set must not contain duplicate subsets.
 
-// For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+// For example,
+// If nums = [1,2,2], a solution is:
 
-// 00 - 0
-// 01 - 1
-// 11 - 3
-// 10 - 2
+// [
+//   [2],
+//   [1],
+//   [1,2,2],
+//   [2,2],
+//   [1,2],
+//   []
+// ]
 
-// Note: For a given n, a gray code sequence is not uniquely defined.
-
-// For example, [0,2,3,1] is also a valid gray code sequence according to the
-// above definition.
-
-// For now, the judge is able to judge based on one instance of gray code
-// sequence. Sorry about that.
 
 /**
- * @param {number} n
- * @return {number[]}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-const grayCode = function(n) {
-  if (n < 0) return []
+const subsetsWithDup = function(nums) {
+  nums.sort((a, b) => a - b)
+  const n = nums.length
+  const res = []
+  const iter = (array, m, i) => {
+    if (array.length === m) res.push([...array])
+    else {
+      for (let j = i; j < n; j++) {
+        if (j === i || nums[j] !== nums[j - 1]) {
+          array.push(nums[j])
+          iter(array, m, j + 1)
+          array.pop()
+        }
+      }
+    }
+  }
 
-  const res = [0]
-  for (let i = 0; i < n; i++) {
-    res.push(...[...res].reverse().map(v => v + (1 << i)))
+  for (let i = 0; i <= n; i++) {
+    iter([], i, 0)
   }
 
   return res
 }
 
 ;[
-  4,
-  2,
-  0,
-].forEach(n => {
-  console.log(grayCode(n))
+  [1, 2, 2],
+  [0, 2, 1, 2],
+].forEach(nums => {
+  console.log(subsetsWithDup(nums))
 })
 
 // Solution:
-
-// 0 --(逆序)--> 0 --(+1)--> 1 --(插入原数组末尾)-->
-
-// 0 1 --(逆序)--> 1 0 --(+10)--> 11 10  --(插入原数组末尾)-->
-
-// 00 01 11 10 --(逆序)--> 10 11 01 00 --(+100)--> 110 111 101 100 --(插入原数组末尾)-->
-
-// 1. 取原数组 a 的逆序, b
-// 2. b 中每个数值加上 2^(log(b.length) + 1)
-// 3. b 加到 a 的末尾
+// 先排序，使得相同的数字排在一起，以便能在选择的时候跳过相同的数字。
+// 同样像 78-subsets.js 类似的方法，即使用 DFS。
+// 区别是同一个位置上的数只替换不同的数。
 
 // Submission Result: Accepted
