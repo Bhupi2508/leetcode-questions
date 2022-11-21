5014960e05ab7fa221489907309ff38fd283ae0f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-21 22:52:41 +0530

    24 :::: 0117-populating-next-right-pointers-in-each-node-ii.js leetcode question updated (2022-11-21)

--- dsa.js
+++ dsa.js
@@ -1,40 +1,32 @@
-// 116. Populating Next Right Pointers in Each Node
-// Medium   36%
+// 117. Populating Next Right Pointers in Each Node II
+// Medium   33%
 
-// Given a binary tree
+// Follow up for problem "Populating Next Right Pointers in Each Node".
 
-// struct TreeLinkNode {
-//   TreeLinkNode *left;
-//   TreeLinkNode *right;
-//   TreeLinkNode *next;
-// }
-
-// Populate each next pointer to point to its next right node. If there is no
-// next right node, the next pointer should be set to NULL.
-
-// Initially, all next pointers are set to NULL.
+// What if the given tree could be any binary tree? Would your previous solution
+// still work?
 
 // Note:
-// - You may only use constant extra space.
-// - You may assume that it is a perfect binary tree (ie, all leaves are at the
-//   same level, and every parent has two children).
+
+// You may only use constant extra space.
 
 // For example,
-// Given the following perfect binary tree,
+// Given the following binary tree,
 
 //      1
 //    /  \
 //   2    3
-//  / \  / \
-// 4  5  6  7
+//  / \    \
+// 4   5    7
 
 // After calling your function, the tree should look like:
 
 //      1 -> NULL
 //    /  \
 //   2 -> 3 -> NULL
-//  / \  / \
-// 4->5->6->7 -> NULL
+//  / \    \
+// 4-> 5 -> 7 -> NULL
+
 
 /**
  * Definition for binary tree with next pointer.
@@ -49,25 +41,31 @@
  * @return {void} Do not return anything, modify tree in-place instead.
  */
 const connect = function(root) {
-  if (root == null) return
   let pre = root
-  let cur = null
-  while (pre.left) {
-    cur = pre
-    while (cur) {
-      cur.left.next = cur.right
-      if (cur.next) {
-        cur.right.next = cur.next.left
-      }
-      cur = cur.next
+  while (pre) {
+    while (pre && !pre.left && !pre.right) {
+      pre = pre.next
     }
-    pre = pre.left
+    if (pre) {
+      let cur = pre, tail = cur.left || cur.right
+      while (cur) {
+        if (cur.left && cur.right) {
+          tail = tail.next = cur.right
+        }
+        if (cur.next && (cur.next.left || cur.next.right)) {
+          tail = tail.next = cur.next.left || cur.next.right
+        }
+        cur = cur.next
+      }
+      pre = pre.left || pre.right
+    }
   }
 }
 
 const TreeLinkNode = require('../structs/TreeLinkNode')
 ;[
-  [1,2,3,4,5,6,7],
+  [1,2,3,4,5,null,7],
+//  [1,2,2,3,3,3,3,4,4,4,4,4,4,null,null,5,5],
 ].forEach(array => {
   const tree = TreeLinkNode.from(array)
   connect(tree)
@@ -75,11 +73,12 @@ const TreeLinkNode = require('../structs/TreeLinkNode')
 })
 
 // Solution:
-// 每一层中，都从最左边的节点开始，假设该层已经连接完毕，并开始进行下一层的连接。
-// 1. 连接当前节点的左右子节点；
-// 2. 开始连接当前节点的右子节点和下一个节点的左子节点；
-// 3. 进入下一个节点；
-// 4. 重复 1，2，3，直到该层最后一个节点；
-// 5. 进入下一层，并从第一个节点开始。
+// 每一层中，都从左边第一个有子节点的节点开始，假设该层已经连接完毕，并开始进行下一层的连接。
+// 1. 从左边开始，找到第一个有子节点的节点作为当前节点；
+// 2. 若当前节点有左右子节点，则连接左右子节点；
+// 3. 若当前的下一个节点有子节点，且有左子节点，则连接其左子节点，若无左，则连右；
+// 4. 进入下一个节点；
+// 5. 重复 2，3，4，直到该层最后一个节点；
+// 6. 进入下一层，重复 1，2，3，4，5
 
 // Submission Result: Accepted
