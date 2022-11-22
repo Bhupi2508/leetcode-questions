cd2843e0aa455b7105f3dbfc3097867073e78201
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-22 22:55:13 +0530

    24 :::: 1460-make-two-arrays-equal-by-reversing-sub-arrays.js leetcode question updated (2022-11-22)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,72 @@
-// 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
-// Easy   67%
+// 1460. Make Two Arrays Equal by Reversing Sub-arrays
+// Easy   77%
 
 
-// Given a sentence that consists of some words separated by a single space, and
-// a searchWord.
-// You have to check if searchWord is a prefix of any word in sentence.
-// Return the index of the word in sentence where searchWord is a prefix of this
-// word (1-indexed).
-// If searchWord is a prefix of more than one word, return the index of the first
-// word (minimum index). If there is no such word return -1.
-// A prefix of a string S is any leading contiguous substring of S.
+// Given two integer arrays of equal length target and arr.
+// In one step, you can select any non-empty sub-array of arr and reverse it. You
+// are allowed to make any number of steps.
+// Return True if you can make arr equal to target, or False otherwise.
 
 // Example 1:
-// Input: sentence = "i love eating burger", searchWord = "burg"
-// Output: 4
-// Explanation: "burg" is prefix of "burger" which is the 4th word in the
-// sentence.
+// Input: target = [1,2,3,4], arr = [2,4,1,3]
+// Output: true
+// Explanation: You can follow the next steps to convert arr to target:
+// 1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3]
+// 2- Reverse sub-array [4,2], arr becomes [1,2,4,3]
+// 3- Reverse sub-array [4,3], arr becomes [1,2,3,4]
+// There are multiple ways to convert arr to target, this is not the only way to
+// do so.
 // Example 2:
-// Input: sentence = "this problem is an easy problem", searchWord = "pro"
-// Output: 2
-// Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in
-// the sentence, but we return 2 as it's the minimal index.
+// Input: target = [7], arr = [7]
+// Output: true
+// Explanation: arr is equal to target without any reverses.
 // Example 3:
-// Input: sentence = "i am tired", searchWord = "you"
-// Output: -1
-// Explanation: "you" is not a prefix of any word in the sentence.
+// Input: target = [1,12], arr = [12,1]
+// Output: true
 // Example 4:
-// Input: sentence = "i use triple pillow", searchWord = "pill"
-// Output: 4
+// Input: target = [3,7,9], arr = [3,7,11]
+// Output: false
+// Explanation: arr doesn't have value 9 and it can never be converted to target.
 // Example 5:
-// Input: sentence = "hello from the other side", searchWord = "they"
-// Output: -1
+// Input: target = [1,1,1,1,1], arr = [1,1,1,1,1]
+// Output: true
 
 // Constraints:
-//     1 <= sentence.length <= 100
-//     1 <= searchWord.length <= 10
-//     sentence consists of lowercase English letters and spaces.
-//     searchWord consists of lowercase English letters.
+//     target.length == arr.length
+//     1 <= target.length <= 1000
+//     1 <= target[i] <= 1000
+//     1 <= arr[i] <= 1000
 
 
 /**
- * @param {string} sentence
- * @param {string} searchWord
- * @return {number}
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
  */
-const isPrefixOfWord = function(sentence, searchWord) {
-  let res = 0
-  for (let word of sentence.split(' ')) {
-    res++
-    if (word.indexOf(searchWord) === 0) return res
+const canBeEqual = function(target, arr) {
+  const hash = {}
+  for (let a of target) hash[a] = (hash[a] || 0) + 1
+  for (let a of arr) {
+    if (hash[a] === undefined || --hash[a] < 0) return false
   }
-  return -1
+  return true
 }
 
 ;[
-  ['i love eating burger', 'burg'], // 4
-  ['this problem is an easy problem', 'pro'], // 2
-  ['i am tired', 'you'], // -1
-  ['i use triple pillow', 'pill'], // 4
-  ['hello from the other side', 'they'], // -1
-].forEach(([sentence, searchWord]) => {
-  console.log(isPrefixOfWord(sentence, searchWord))
+  [[1,2,3,4], [2,4,1,3]],
+  [[7],[7]],
+  [[1,12],[12,1]],
+  [[3,7,9],[3,7,11]],
+  [[1,1,1,1,1],[1,1,1,1,1]],
+].forEach(([target, arr]) => {
+  console.log(canBeEqual(target, arr))
 })
 
 // Solution:
-// 使用 split 分词
-// 使用 indexOf 判断是否为前缀
+// 其实，只需要两个数组的数能一一对应，就可以通过旋转来相互转换。
+// 使用 hash 记录target数组中每个数出现的次数，
+// 再判断 arr 是否能匹配 hash 中的键值对。
+
+// 也可以排序两个数组，再按顺序遍历，看是否一一对应。不过这个的时间复杂度高了。
 
 // Submission Result: Accepted
