954cd53994c0799dbbb271ce9636f017ca261d16
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-22 22:52:51 +0530

    24 :::: 0189-rotate-array.js leetcode question updated (2022-11-22)

--- dsa.js
+++ dsa.js
@@ -1,44 +1,59 @@
-// 172. Factorial Trailing Zeroes
-// Easy   36%
+// 189. Rotate Array
+// Easy   24%
 
 
-// Given an integer n, return the number of trailing zeroes in n!.
+// Rotate an array of n elements to the right by k steps.
 
-// Note: Your solution should be in logarithmic time complexity.
+// For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
+// [5,6,7,1,2,3,4].
 
-// Credits:Special thanks to @ts for adding this problem and creating all test
-// cases.
+// Note:
+// Try to come up as many solutions as you can, there are at least 3 different
+// ways to solve this problem.
+
+// [show hint]
+
+// Hint:
+// Could you do it in-place with O(1) extra space?
+
+// Related problem: Reverse Words in a String II
+
+// Credits:Special thanks to @Freezen for adding this problem and creating all
+// test cases.
 
 
 /**
- * @param {number} n
- * @return {number}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-const trailingZeroes = function(n) {
-  let result = 0
-  for (let i = Math.trunc(Math.log(n) / Math.log(5)); i > 0; i--) {
-    result += Math.trunc(n / Math.pow(5, i))
+const rotate = function(nums, k) {
+  // js-array-features
+  // k = k % nums.length
+  // for (let i = 0; i < k; i++) nums.unshift(nums.pop())
+
+  const n = nums.length
+  k = k % n
+  for (let j = 0; j < k; j++) {
+    const num = nums[n - 1]
+    for (let i = n - 1; i > 0; i--) {
+      nums[i] = nums[i - 1]
+    }
+    nums[0] = num
   }
-  return result
 }
 
 ;[
-  0,                            // 0
-  5,                            // 1
-  10,                           // 2
-  24,                           // 4
-  25,                           // 6
-  126,                          // 31
-].forEach(n => {
-  console.log(trailingZeroes(n))
+  [[1], 0],                     // [1]
+  [[1,2,3,4], 3],               // [2,3,4,1]
+  [[1,2,3,4,5,6,7], 3],         // [5,6,7,1,2,3,4]
+  [[1,2,3,4,5,6,7,8,9,0], 3]    // [8,9,0,1,2,3,4,5,6,7]
+].forEach(args => {
+  console.log(rotate(...args))
 })
 
 // Solution:
-// 每当有一个0，说明有一个乘数为 10 （或者其他乘数相乘得到10的倍数，如 2x5）
-// 每个5的倍数的数都能与一个偶数相乘的到一个10的倍数。
-// 在n的范围内，偶数的数量远大于5的倍数。因此计算是5的倍数的个数。
-// 但是还有些数可以分解为多个5，比如 25，125等，这些数与合适的偶数相乘会产生更多的0，
-// 如25x4=100，125x8=1000，因此这些数需要额外计算。
-// 25需要算2个，125需要算3个。而这正好是对其取以5为底的对数的值。log5(25)=2,log5(125)=3。
+// 迭代k次，每次将最后一位移到最前面。
+// 或者作为一个队列，弹出最后一个，并弹入最前面。
 
 // Submission Result: Accepted
