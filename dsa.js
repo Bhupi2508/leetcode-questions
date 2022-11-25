95cc7f9b42ad45345fa1b0b2169fec6e5b48bbb8
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-25 22:55:09 +0530

    24 :::: 1403-minimum-subsequence-in-non-increasing-order.js leetcode question updated (2022-11-25)

--- dsa.js
+++ dsa.js
@@ -1,80 +1,63 @@
-// 1399. Count Largest Group
-// Easy   64%
+// 1403. Minimum Subsequence in Non-Increasing Order
+// Easy   71%
 
 
-// Given an integer n. Each number from 1 to n is grouped according to the sum of
-// its digits.
-// Return how many groups have the largest size.
+// Given the array nums, obtain a subsequence of the array whose sum of elements
+// is strictly greater than the sum of the non included elements in such
+// subsequence.
+// If there are multiple solutions, return the subsequence with minimum size and
+// if there still exist multiple solutions, return the subsequence with the
+// maximum total sum of all its elements. A subsequence of an array can be
+// obtained by erasing some (possibly zero) elements from the array.
+// Note that the solution with the given constraints is guaranteed to be unique.
+// Also return the answer sorted in non-increasing order.
 
 // Example 1:
-// Input: n = 13
-// Output: 4
-// Explanation: There are 9 groups in total, they are grouped according sum of
-// its digits of numbers from 1 to 13:
-// [1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9]. There are 4 groups
-// with largest size.
+// Input: nums = [4,3,10,9,8]
+// Output: [10,9]
+// Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum
+// of their elements is strictly greater than the sum of elements not included,
+// however, the subsequence [10,9] has the maximum total sum of its elements.
 // Example 2:
-// Input: n = 2
-// Output: 2
-// Explanation: There are 2 groups [1], [2] of size 1.
+// Input: nums = [4,4,7,6,7]
+// Output: [7,7,6]
+// Explanation: The subsequence [7,7] has the sum of its elements equal to 14
+// which is not strictly greater than the sum of elements not included (14 = 4 +
+// 4 + 6). Therefore, the subsequence [7,6,7] is the minimal satisfying the
+// conditions. Note the subsequence has to returned in non-decreasing order.
 // Example 3:
-// Input: n = 15
-// Output: 6
-// Example 4:
-// Input: n = 24
-// Output: 5
+// Input: nums = [6]
+// Output: [6]
 
 // Constraints:
-//     1 <= n <= 10^4
+//     1 <= nums.length <= 500
+//     1 <= nums[i] <= 100
 
 
 /**
- * @param {number} n
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[]}
  */
-const countLargestGroup = function(n) {
-  const counts = []
-  for (let i = 1; i <= n; i++) {
-    let j = i, k = 0
-    while (j > 0) {
-      k += j % 10
-      j = (j / 10) >>> 0
-    }
-    counts[k-1] = (counts[k-1] || 0) + 1
-  }
-  let max = Math.max(...counts)
-  let res = 0
-  for (let c of counts) if (c === max) res++
-  return res
+const minSubsequence = function(nums) {
+  nums.sort((a, b) => b - a)
+  const sum = nums.reduce((pre, v) => pre + v, 0)
+  let i = 0, subSum = 0
+  while (subSum <= sum - subSum) subSum += nums[i++]
+  return nums.slice(0, i)
 }
 
 ;[
-  13, // 4
-  2,  // 2
-  15, // 6
-  24, // 5
-].forEach((n) => {
-  console.log(countLargestGroup(n))
+  [4,3,10,9,8],
+  [4,4,7,6,7],
+  [6],
+].forEach((nums) => {
+  console.log(minSubsequence(nums))
 })
 
 // Solution:
-//  1  2  3  4  5  6  7  8  9  [10] [11]
-// 10 11 12 13 14 15 16 17 18   19
-//    20 21 22 23 24 25 26 27   28   29
-//       30 31 32 33 34 35 36
-//          40 ...
-//             50 ...
-//                60 ...
-//                   70 ...
-//                      80 ...
-//                         90 ...
-// 100 ...
-
-// 本想找一下规律的，枚举了一下，好像有点复杂，就放弃了。
-
-// 用一般思维方法
-// 先用数组计算并保存 1 到 n 的数字和
-// 再选出最大值 （其实遍历 1 到 n 时可以计算出来）
-// 最后计算最大值的个数
+// 1）排序，这样可从最大值开始遍历
+// 2）计算整个数组的总和 sum
+// 3）计算前 i 项的序列总和 subSum ，并比较剩余子序列和（sum - subSum)比较，当
+// subSum > sum-subSum 时，返回前 i 项即可。
 
 // Submission Result: Accepted
