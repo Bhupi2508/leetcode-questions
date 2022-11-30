320190f288ca5cdc09c2cd58d676259ccccadc1d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-11-30 22:54:14 +0530

    25 :::: 0925-long-pressed-name.js leetcode question updated (2022-11-30)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,74 @@
-// 922. Sort Array By Parity II
-// Easy   68%
+// 925. Long Pressed Name
+// Easy   45%
 
 
-// Given an array A of non-negative integers, half of the integers in A are odd,
-// and half of the integers are even.
-// Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is
-// even, i is even.
-// You may return any answer array that satisfies this condition.
+// Your friend is typing his name into a keyboard.  Sometimes, when typing a
+// character c, the key might get long pressed, and the character will be typed 1
+// or more times.
+// You examine the typed characters of the keyboard.  Return True if it is
+// possible that it was your friends name, with some characters (possibly none)
+// being long pressed.
 
 // Example 1:
-// Input: [4,2,5,7]
-// Output: [4,5,2,7]
-// Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+// Input: name = "alex", typed = "aaleex"
+// Output: true
+// Explanation: 'a' and 'e' in 'alex' were long pressed.
+// Example 2:
+// Input: name = "saeed", typed = "ssaaedd"
+// Output: false
+// Explanation: 'e' must have been pressed twice, but it wasn't in the typed
+// output.
+// Example 3:
+// Input: name = "leelee", typed = "lleeelee"
+// Output: true
+// Example 4:
+// Input: name = "laiden", typed = "laiden"
+// Output: true
+// Explanation: It's not necessary to long press any character.
 
-// Note:
-//     2 <= A.length <= 20000
-//     A.length % 2 == 0
-//     0 <= A[i] <= 1000
-
+// Constraints:
+//     1 <= name.length <= 1000
+//     1 <= typed.length <= 1000
+//     The characters of name and typed are lowercase letters.
 
 
 /**
- * @param {number[]} A
- * @return {number[]}
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
  */
-const sortArrayByParityII = function(A) {
-  let i = 0, j = 1, l = A.length
-  while (i < l) {
-    if (A[i] % 2 == 0) {
-      i += 2
-    } else if (A[j] % 2 == 1) {
-      j += 2
+const isLongPressedName = function(name, typed) {
+  let i = 0, j = 0
+  while (i < name.length || j < typed.length) {
+    if (name[i] == typed[j]) {
+      i++
+      j++
+    } else if (typed[j] == typed[j - 1]) {
+      j++
     } else {
-      const t = A[i]
-      A[i] = A[j]
-      A[j] = t
+      return false
     }
   }
-  return A
+  return true
 }
 
 ;[
-  [4,2,5,7], // [4, 5, 2, 7]
-  [0,0,0,0,1,1,1,1],
-  [1,1,1,0,0,0],
-  [0,1],
-  [1,0],
-  [1,0,1,0,1,0],
-].forEach((A) => {
-  console.log(sortArrayByParityII(A))
+  // ['alex', 'aaleex'],    // true
+  // ['saeed', 'ssaaedd'],  // false
+  // ['leelee', 'lleeelee'],// true
+  // ['laiden', 'laiden'],  // true
+  ['pyplrz', 'ppyypllr'],   // false
+  ['alice', 'alicee']
+].forEach(([name, typed]) => {
+  console.log(isLongPressedName(name, typed))
 })
 
 // Solution:
-// 设置两个变量 i 和 j, 分别遍历偶数下标数和奇数下标数
-// i 从 0 开始，j 从 1 开始
-// 若 A[i] 为 偶数，跳到下一个偶数下标数
-// 若 A[j] 为 奇数，跳到下一个奇数下标数
-// 否则 A[i] 和 A[j] 都违法了规则，将其交换即可
+// 使用两个变量 i 和 j 来标记遍历 name 和 typed 的字符，
+// 若两个字符相同，则遍历下一个字符，
+// 若不同，则检查 typed[j] 与 typed[j - 1] 是否相同，相同表明该字符时长按产生的，
+// 相同则遍历 typed 的下一个字符，
+// 否则 直接返回false，
+// 直到遍历完两个字符串。
 
 // Submission Result: Accepted
