6ae547b225a7eff239f4ddf91a8155e7f6c42774
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-02 22:53:04 +0530

    25 :::: 0344-reverse-string.js leetcode question updated (2022-12-02)

--- dsa.js
+++ dsa.js
@@ -1,43 +1,33 @@
-// 342. Power of Four
-// Easy   38%
+// 344. Reverse String
+// Easy   59%
 
 
-// Given an integer (signed 32 bits), write a function to check whether it is a
-// power of 4.
+// Write a function that takes a string as input and returns the string reversed.
 
 // Example:
-// Given num = 16, return true.
-// Given num = 5, return false.
-
-// Follow up: Could you solve it without loops/recursion?
-
-// Credits:Special thanks to @yukuairoy  for adding this problem and creating all
-// test cases.
+// Given s = "hello", return "olleh".
 
 
 /**
- * @param {number} num
- * @return {boolean}
+ * @param {string} s
+ * @return {string}
  */
-const isPowerOfFour = function(num) {
-  return Number.isInteger(Math.log(num) / Math.log(4))
+const reverseString = function(s) {
+  const n = s.length
+  let result = ''
+  for (let i = n - 1; i >= 0; i--) {
+    result += s[i]
+  }
+  return result
 }
 
 ;[
-  16,                           // true
-  5,                            // false
-  1,                            // true
-  0,                            // false
-  -16,                          // false
-].forEach(num => {
-  console.log(isPowerOfFour(num))
+  'hello',                      // 'olleh'
+].forEach(s => {
+  console.log(reverseString(s))
 })
 
-
 // Solution:
-// 假设 4^n = num，当 n 为整数时，num 为 以4为底数的幂
-// 两边取以4为底的对数 log_4(4 ^ n) = log_4(num)
-// n = log_4(num)
-// n = log_e(num) / log_e(4)，因为没有合适的取对数函数，只能换用 e 为底的
+// r[i] = s[n - 1 - i]
 
 // Submission Result: Accepted
