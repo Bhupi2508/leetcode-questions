e49ef6e2997da22196f85f394346261dd60a925e
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-09 22:52:21 +0530

    26 :::: 0063-unique-paths-ii.js leetcode question updated (2022-12-09)

--- dsa.js
+++ dsa.js
@@ -1,63 +1,62 @@
-// 62. Unique Paths
-// Medium   41%
+// 63. Unique Paths II
+// Medium   31%
 
-// A robot is located at the top-left corner of a m x n grid (marked 'Start' in
-// the diagram below).
+// Follow up for "Unique Paths":
 
-// The robot can only move either down or right at any point in time. The robot
-// is trying to reach the bottom-right corner of the grid (marked 'Finish' in
-// the diagram below).
+// Now consider if some obstacles are added to the grids. How many unique paths
+// would there be?
 
-// How many possible unique paths are there?
+// An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 
+// For example,
 
-// Above is a 3 x 7 grid. How many possible unique paths are there?
+// There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+// [
+//   [0,0,0],
+//   [0,1,0],
+//   [0,0,0]
+// ]
+
+// The total number of unique paths is 2.
 
 // Note: m and n will be at most 100.
 
-// easy recursion but big
-// const uniquePaths = function(m, n) {
-//   if (m === 1) return 1
-//   if (n === 1) return 1
-//   return uniquePaths(m - 1, n) + uniquePaths(m, n - 1)
-// }
-
-
 /**
- * @param {number} m
- * @param {number} n
+ * @param {number[][]} obstacleGrid
  * @return {number}
  */
-const uniquePaths = function(m, n) {
+const uniquePathsWithObstacles = function(obstacleGrid) {
+  const n = obstacleGrid[0].length
   const dp = Array(n).fill(0)
   dp[0] = 1
-  for (let i = 0; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[j] += dp[j - 1]
+  for (let row of obstacleGrid) {
+    for (let i = 0; i < n; i++) {
+      if (row[i] === 1) dp[i] = 0
+      else if (i > 0) dp[i] += dp[i - 1]
     }
   }
+
   return dp[n - 1]
 }
 
 ;[
-  [3, 7],
-  [3, 6],
-  [100, 100],
-].forEach(([m, n]) => {
-  console.log(uniquePaths(m, n))
+  [
+    [1],
+    [0]
+  ],
+  [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+  ],
+].forEach(obstacleGrid => {
+  console.log(uniquePathsWithObstacles(obstacleGrid))
 })
 
 // Solution:
-// 使用动态规划。
-// 用一个表来表示一个mxn的方格，
-// 表的每个项表示从开始位置到当前位置的全部不同路径。
-// 每个项都是上一个位置与左一个位置相加所得。
-// 最后结果为终点值，即最后一个项的值。
-
-// 对动态规划表进行优化。
-// 因为每次都是上一项加左一项。
-// 因此只需要用一个数组即可，
-// 每一项在改变之前都代表着上一项的值，只需在原来基础上加左一项即可。
-// 因为每一行的每一项只改变一次，且仅使用两次。
+// 在 62-unique-paths.js 的基础上加上了一些阻碍。
+// 只需要将阻碍的位置设为0即可，其他无需改变。
+// 同样是使用一个数组的动态规划。
 
 // Submission Result: Accepted
