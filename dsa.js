62e11cff153ffdf6ca9c854855690b7c05a196b2
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-12 22:54:46 +0530

    27 :::: 1266-minimum-time-visiting-all-points.js leetcode question updated (2022-12-12)

--- dsa.js
+++ dsa.js
@@ -1,74 +1,74 @@
-// 1260. Shift 2D Grid
-// Easy   61%
+// 1266. Minimum Time Visiting All Points
+// Easy   79%
 
 
-// Given a 2D grid of size m x n and an integer k. You need to shift the grid k
-// times.
-// In one shift operation:
-//     Element at grid[i][j] moves to grid[i][j + 1].
-//     Element at grid[i][n - 1] moves to grid[i + 1][0].
-//     Element at grid[m - 1][n - 1] moves to grid[0][0].
-// Return the 2D grid after applying shift operation k times.
+// On a plane there are n points with integer coordinates points[i] = [xi, yi].
+// Your task is to find the minimum time in seconds to visit all points.
+// You can move according to the next rules:
+//     In one second always you can either move vertically, horizontally by one
+// unit or diagonally (it means to move one unit vertically and one unit
+// horizontally in one second).
+//     You have to visit the points in the same order as they appear in the
+// array.
 
 // Example 1:
-//
-//  1 2 3      9 1 2
-//  4 5 6  ->  3 4 5
-//  7 8 9      7 8 9
-//
-// Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
-// Output: [[9,1,2],[3,4,5],[6,7,8]]
+// Input: points = [[1,1],[3,4],[-1,0]]
+// Output: 7
+// Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] ->
+// [1,2] -> [0,1] -> [-1,0]
+// Time from [1,1] to [3,4] = 3 seconds
+// Time from [3,4] to [-1,0] = 4 seconds
+// Total time = 7 seconds
 // Example 2:
-// Input: grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4
-// Output: [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
-// Example 3:
-// Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9
-// Output: [[1,2,3],[4,5,6],[7,8,9]]
+// Input: points = [[3,2],[-2,2]]
+// Output: 5
 
 // Constraints:
-//     m == grid.length
-//     n == grid[i].length
-//     1 <= m <= 50
-//     1 <= n <= 50
-//     -1000 <= grid[i][j] <= 1000
-//     0 <= k <= 100
+//     points.length == n
+//     1 <= n <= 100
+//     points[i].length == 2
+//     -1000 <= points[i][0], points[i][1] <= 1000
 
 
 /**
- * @param {number[][]} grid
- * @param {number} k
- * @return {number[][]}
+ * @param {number[][]} points
+ * @return {number}
  */
-const shiftGrid = function(grid, k) {
-  const m = grid.length, n = grid[0].length
-  const rowShift = Math.floor(k / n) % m, colShift = k % n
-  const res = []
-  for (let i = 0; i < m; i++) {
-    res[i] = []
-    for (let j = 0; j < n; j++) {
-      const r = (i - rowShift + Math.floor((j - colShift) / n) + m) % m
-      const c = (j - colShift + n) % n
-      res[i][j] = grid[r][c]
-    }
+const minTimeToVisitAllPoints = function(points) {
+  let res = 0
+  for (let i = 1; i < points.length; i++) {
+    const p1 = points[i - 1], p2 = points[i]
+    res += Math.max(Math.abs(p1[0] - p2[0]), Math.abs(p1[1] - p2[1]))
   }
   return res
 }
 
 ;[
-  [[[1,2,3],[4,5,6],[7,8,9]], 1],
-  [[[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], 4],
-  [[[1,2,3],[4,5,6],[7,8,9]], 9],
-].forEach(([grid, k]) => {
-  console.log(shiftGrid(grid, k))
+  [[1,1],[3,4],[-1,0]], // 7
+  [[3,2],[-2,2]], // 5
+].forEach((points) => {
+  console.log(minTimeToVisitAllPoints(points))
 })
 
 // Solution:
-// 水平移动距离为 colShift = k % n
-// 垂直移动距离为 rowShift = Math.floor(k / n) % m （k除以n取整为行移动距离，取m的模是
-// 因为移动m次与不移动是一样的）
+// 按顺序计算每两个相邻点间移动所需的时间，最后加起来得到答案
+// 计算两点间移动所需时间只需计算 x-轴 和 y-轴 中距离最大的那一个值即可。
+// 因为对角线移动可以移动任意一个（x-轴 + y-轴）
+// 先按对角线移动（x-距离或y-距离）中较小的一个的时间，再水平或垂直移动到下一个点。
 
-// 移动后网格 res[i][j] 的数据为 grid[r][c]
-// c = (j - colShift + n) % n，其中 +n 是为了读取向前读取时可以读到上一行的数据
-// r = (i - rowShift - Math.floor((j - colShift) / n) + m) % m
+// x-轴距离 > y-轴距离
+// .
+// \
+//  \
+//   \
+//    \____.
+
+// y-轴距离 > x-轴距离
+// .
+//  \
+//   \
+//   |
+//   |
+//   .
 
 // Submission Result: Accepted
