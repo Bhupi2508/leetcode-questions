4755ce9e1b4c7d07136864fc6109dc4d57e1b66c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-12 22:44:48 +0530

    27 :::: 153-findMinimumInRotatedSortedArray.js question updated

--- dsa.js
+++ dsa.js
@@ -1,38 +1,44 @@
 /**
- * Solution: Two dp arrays: maxDP tracks the Maximum product at index i,
- * minDP tracks the Minimum product at index i, either of them can be negative Numbers
- * so if nums[i + 1] is negative, then maxDP[i+1] can be a product of minDP[i] * nums[i+1]
- * @see the for loop body
+ * Binary search. Be careful about the boundaries.
+ * If the first element is larger than the last one, then we compute the element in the middle,
+ * and compare it with the first element. If value of the element in the middle is larger than
+ * the first element, we know the rotation is at the second half of this array.
+ * Else, it is in the first half in the array.
  *
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function(nums) {
-    if (nums.length === 0) return 0;
-    var result = nums[0];
-    var maxDP = [nums[0]];
-    var minDP = [nums[0]];
+var findMin = function(nums) {
+    var low = 0;
+    var high = nums.length - 1;
 
-    for (var i = 1; i < nums.length; i++) {
-        maxDP[i] = Math.max(nums[i], maxDP[i - 1] * nums[i], minDP[i - 1] * nums[i]);
-        minDP[i] = Math.min(nums[i], minDP[i - 1] * nums[i], maxDP[i - 1] * nums[i]);
-        result = Math.max(maxDP[i], result);
+    while (low <= high) {
+        if (nums[low] <= nums[high]) return nums[low];
+        var mid = low + Math.floor((high - low) / 2);
+        if (nums[mid] >= nums[low]) low = mid + 1;
+        else high = mid;
     }
 
-    return result;
+    return nums[0];
 };
 
-// 2nd try
-var maxProduct = function(nums) {
-    var min = [nums[0]];
-    var max = [nums[0]];
-    var maxProduct = nums[0];
+// second try
+var findMin = function(nums) {
+    var lo = 0;
+    var hi = nums.length - 1;
 
-    for (var i = 1; i < nums.length; i++) {
-        min.push(Math.min(nums[i], min[i - 1] * nums[i], max[i - 1] * nums[i]));
-        max.push(Math.max(nums[i], min[i - 1] * nums[i], max[i - 1] * nums[i]));
-        maxProduct = Math.max(maxProduct, max[i]);
+    while (lo <= hi) {
+        if (nums[lo] < nums[hi]) return nums[lo];
+        var mid = lo + Math.floor((hi - lo) / 2);
+        if (nums[lo] <= nums[mid]) {
+            // this case, minimun must exist in the second half
+            lo = mid + 1;
+        } else {
+            // this case, mininum can be mid itself or must exist in the first half.
+            hi = mid;
+        }
     }
 
-    return maxProduct;
+    // if hi < lo, then nums[hi] is the minum.
+    return nums[hi];
 };
