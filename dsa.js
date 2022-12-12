a3d470ef997e33448674005cb4e7ebd24f705c9d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-12 22:53:35 +0530

    27 :::: 0581-shortest-unsorted-continuous-subarray.js leetcode question updated (2022-12-12)

--- dsa.js
+++ dsa.js
@@ -1,65 +1,67 @@
-// 575. Distribute Candies
-// Easy   58%
+// 581. Shortest Unsorted Continuous Subarray
+// Easy   29%
 
-//         Given an integer array with even length, where different numbers in
-// this array represent different kinds of candies. Each number means one candy
-// of the corresponding kind. You need to distribute these candies equally in
-// number to brother and sister. Return the maximum number of kinds of candies
-// the sister could gain.
+
+// Given an integer array, you need to find one continuous subarray that if you
+// only sort this subarray in ascending order, then the whole array will be
+// sorted in ascending order, too.
+
+// You need to find the shortest such subarray and output its length.
 
 // Example 1:
 
-// Input: candies = [1,1,2,2,3,3]
-// Output: 3
-// Explanation:
-// There are three different kinds of candies (1, 2 and 3), and two candies for
-// each kind.
-// Optimal distribution: The sister has candies [1,2,3] and the brother has
-// candies [1,2,3], too.
-// The sister has three different kinds of candies.
-
-// Example 2:
-
-// Input: candies = [1,1,2,3]
-// Output: 2
-// Explanation: For example, the sister has candies [2,3] and the brother has
-// candies [1,1].
-// The sister has two different kinds of candies, the brother has only one kind
-// of candies.
+// Input: [2, 6, 4, 8, 10, 9, 15]
+// Output: 5
+// Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the
+// whole array sorted in ascending order.
 
 // Note:
 
-// The length of the given array is in range [2, 10,000], and will be even.
-// The number in given array is in range [-100,000, 100,000].
+// Then length of the input array is in range [1, 10,000].
+// The input array may contain duplicates, so ascending order here means <=.
 
 
 /**
- * @param {number[]} candies
+ * @param {number[]} nums
  * @return {number}
  */
-const distributeCandies = function(candies) {
-  const n = candies.length, hash = {}
-  let result = 0
-  for (let i = 0; i < n; i++) {
-    if (!hash[candies[i]]) {
-      hash[candies[i]] = true
-      result++
+const findUnsortedSubarray = function(nums) {
+  const n = nums.length
+  let head = 10000, tail = 10000
+  for (let i = 1; i < n; i++) {
+    if (nums[i - 1] > nums[i]) {
+      let j = i, max = nums[i - 1]
+      while (nums[--j] > nums[i]) max = Math.max(max, nums[j])
+      head = Math.min(head, j)
+      tail = i
+      ;[nums[j + 1], nums[i]] = [nums[i], max]
     }
   }
-  return Math.min(result, Math.floor(n / 2))
+  return tail - head
 }
 
 ;[
-  [1,1,2,2,3,3],                // 3
-  [1,1,2,3],                    // 2
-].forEach(candies => {
-  console.log(distributeCandies(candies))
+  [2, 6, 4, 8, 10, 9, 15],      // 5
+  [1,2,3,4],                    // 0
+  [2,3,3,2,4],                  // 3
+  [1,3,2,2,2],                  // 4
+  [2,1,1,1,1],                  // 5
+  [1,3,5,2,4],                  // 4
+].forEach(nums => {
+  console.log(findUnsortedSubarray(nums))
 })
 
-
 // Solution:
-// 要分给妹妹最多不同种类的糖，
-// 首先要看看这些糖中一共有多少种糖，
-// 再各个种类的糖中选一个且不超过总糖数的一半就好了。
+
+// 1. 遍历一遍，遇到 [i - 1] > [i]，就替换，并记录
+// 失败，反例:例子3
+
+// 2. 遍历一遍，但过程中遇到 [i - 1] > [i]时，
+// 使用类似插入排序的方法寻找其应该插入的位置 j + 1
+// 在寻找过程中，还记录一个最大值 max
+// 并在最后，将 [j+1] 替换为 [i]，[i] 替换为 max
+
+// 这样，在迭代过程中，乱序被整理成了虚拟的顺序，即
+// 一个子序列的开头为该子序列的最小值，而结尾为该子序列的最大值。
 
 // Submission Result: Accepted
