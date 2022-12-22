d5bc6aca43c269158c5cf7b142aa2a52178cc86a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-22 22:45:11 +0530

    28 :::: 73-setMatrixZeroes.js question updated

--- dsa.js
+++ dsa.js
@@ -1,20 +1,46 @@
 /**
- * Key: use stack
- * @param {string} path
- * @return {string}
+ * Key: use first row to store the zero state for each column exclude the first col,
+ * use first col to store the zero state for each row exclude the first row
+ * use two variable to mark whether the first row and the first column has zeros.
+ * set the matrix with zeros with the states.
+ *
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var simplifyPath = function(path) {
-    var stack = [];
-    var paths = path.split('/');
-    for (var i = 0, length = paths.length; i < length; i++) {
-        if (paths[i] === '..') {
-            stack.pop();
-        } else if (paths[i] === '.') {
-            continue;
-        } else if (paths[i] !== '') {
-            stack.push(paths[i]);
+var setZeroes = function(matrix) {
+    var firstRowZero = false;
+    var firstColZero = false;
+    var rows = matrix.length;
+    var cols = matrix[0].length;
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (matrix[i][j] === 0) {
+                if (i === 0) firstRowZero = true;
+                if (j === 0) firstColZero = true;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
         }
     }
-    // if '/../'
-    return stack.length === 0 ? '/' : '/' + stack.join('/');
+
+    for (var i = 1; i < rows; i++) {
+        for (var j = 1; j < cols; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (firstRowZero) {
+        for (var i = 0; i < cols; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+
+    if (firstColZero) {
+        for (var i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+    }
 };
