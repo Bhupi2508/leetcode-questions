8d549dbf2d68d5ce91ad2d4c99fcf8f8f99df8f2
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-23 22:44:55 +0530

    28 :::: 241-differentWaysAddParentheses.js question updated

--- dsa.js
+++ dsa.js
@@ -1,24 +1,32 @@
 /**
- * Key: Start from top right, if target is less than the element, move left (col--);
- * if target is greater than the element, move down (row++);
- * O(m+n);
- *
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
+ * @param {string} input
+ * @return {number[]}
  */
-var searchMatrix = function(matrix, target) {
-    var row = 0;
-    var col = matrix[0].length - 1;
-
-    while (row <= matrix.length - 1 && col >=0) {
-        if (target < matrix[row][col]) {
-            col--;
-        } else if (target > matrix[row][col]) {
-            row++;
-        } else {
-            return true;
+// slow, we can use dp to cache some results
+var diffWaysToCompute = function(input) {
+    var results = [];
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] === '-' || input[i] === '+' || input[i] === '*') {
+            console.log(input);
+            var inputLeft = diffWaysToCompute(input.substring(0, i));
+            var inputRight = diffWaysToCompute(input.substring(i + 1));
+            for (var j = 0; j < inputLeft.length; j++) {
+                for (var k = 0; k < inputRight.length; k++) {
+                    var result ;
+                    if (input[i] === '-') {
+                        result = parseInt(inputLeft[j]) - parseInt(inputRight[k]);
+                    } else if (input[i] === '+') {
+                        result = parseInt(inputLeft[j]) + parseInt(inputRight[k]);
+                    } else if (input[i] === '*') {
+                        result = parseInt(inputLeft[j]) * parseInt(inputRight[k]);
+                    }
+                    results.push(result);
+                }
+            }
         }
     }
-    return false;
+
+    if (results.length === 0) results.push(parseInt(input));
+
+    return results;
 };
