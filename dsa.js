badb7d69441cd6f94c6a19b9aafb320b472fe984
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-26 22:52:17 +0530

    29 :::: 0051-n-queens.js leetcode question updated (2022-12-26)

--- dsa.js
+++ dsa.js
@@ -1,47 +1,93 @@
-// 50. Pow(x, n)
-// Medium   26%
+// 51. N-Queens
+// Hard   31%
 
-// Implement pow(x, n).
+// The n-queens puzzle is the problem of placing n queens on an n×n chessboard
+// such that no two queens attack each other.
 
-// Example 1:
+// Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-// Input: 2.00000, 10
-// Output: 1024.00000
+// Each solution contains a distinct board configuration of the n-queens'
+// placement, where 'Q' and '.' both indicate a queen and an empty space
+// respectively.
 
-// Example 2:
+// For example,
+// There exist two distinct solutions to the 4-queens puzzle:
 
-// Input: 2.10000, 3
-// Output: 9.26100
+// [
+//  [".Q..",  // Solution 1
+//   "...Q",
+//   "Q...",
+//   "..Q."],
 
+//  ["..Q.",  // Solution 2
+//   "Q...",
+//   "...Q",
+//   ".Q.."]
+// ]
 
 /**
- * @param {number} x
  * @param {number} n
- * @return {number}
+ * @return {string[][]}
  */
-const myPow = function(x, n) {
-  if (n === 0) return 1
-  if (n < 0) {
-    x = 1 / x
-    n = -n
+const solveNQueens = function(n) {
+  const result = []
+  function iter(rows) {
+    if (rows.length === n) {
+      addSolution(rows)
+      return
+    }
+    for (let i = 0; i < n; i++) {
+      rows.push(i)
+      if (check(rows)) iter(rows)
+      rows.pop(i)
+    }
   }
-  return n % 2 ? myPow(x * x, Math.trunc(n / 2)) * x : myPow(x * x, n / 2)
+  function check(rows) {
+    for (let i = rows.length - 1, j = i - 1; j >= 0; j--) {
+      if (rows[i] === rows[j]) return false
+      if (rows[i] - i === rows[j] - j) return false
+      if (rows[i] + i === rows[j] + j) return false
+    }
+    return true
+  }
+  function addSolution(rows) {
+    const solution = Array(n)
+    for (let i = 0; i < n; i++) {
+      let string = ''
+      for (let j = 0; j < n; j++) {
+        string += rows[i] === j ? 'Q' : '.'
+      }
+      solution[i] = string
+    }
+    result.push(solution)
+  }
+
+  iter([])
+  return result
 }
 
 ;[
-  [2.00000, 10],
-  [2.10000, 3],
-  [34.00515, -3],
-].forEach(args => {
-  console.log(myPow(...args), Math.pow(...args))
+  4,
+  5,
+].forEach(n => {
+  console.log(solveNQueens(n))
 })
 
 // Solution:
-// 使用递归求解。
-// 不过要注意一下几点：
-// 1. 幂数为奇数时，该如何递归；
-// 2. 幂数为负数时，该如何递归；
-// 3. 终止情况。
-// 4. 是否考虑幂数为分数的情况。
+// 每个摆放方法，都只需要用一个数组来表示（因为棋盘中每行只能放一个皇后）
+// 数组下标表示行号，数值表示列号（都从0开始）
+
+// 使用试探的方法来递归地解决，试探错误则回溯。
+// 每次试着填写一个数字，若合适，则填写下一个数字，否则换一个数字来填写。
+// 直到数组填满，这个填满的数组就是一种摆放方法。
+
+// 检查函数，每次只需检查已填的数字
+// 需要检查4种情况：
+// 1. 每行只能放一个皇后。因为每行用一个数组位置表示，因此确保了每行一个。
+// 2. 每列只能放一个皇后。因为每次添加一个数之前，前面的数都是合法的，因此只需要
+//    检查新加的数是否合法。所以，检查新加的数是否出现过，即列号是否出现过。
+// 3. “\”方向斜线只能放一个皇后。若在该同斜线上的皇后，则它们 *下标与值的差* 是
+//    相同的。
+// 4. “/”方向斜线只能放一个皇后。这时检查它们 *下标与值的和* 是否相同。
 
 // Submission Result: Accepted
