4fc2c3cb2daaf82e2d72c430cf625a2aa30de366
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-26 22:44:56 +0530

    29 :::: 247-topKFrequentElements.js question updated

--- dsa.js
+++ dsa.js
@@ -1,44 +1,37 @@
 /**
-* question: http://www.programcreek.com/2014/08/leetcode-shortest-word-distance-iii/
-*
-*/
-
-function shortestWordDist(words, word1, word2) {
-  var index1 = -1;
-  var index2 = -1;
-  var dist = Number.MAX_VALUE; // for some reason in JavaScript, manually set max.
-
-  words.forEach(function(word, index) {
-    if (word1 === word2) {
-      if (word1 === word) {
-        if (index1 > index2) {
-          index2 = index;
+ * Solution: 1. priority queue, O(n*log(k)), however JavaScript doesn't natively support heap
+* 2. bucket sort
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    var map = {};
+    for (var i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(nums[i])) {
+            map[nums[i]] += 1;
         } else {
-          index1 = index;
+            map[nums[i]] = 1;
         }
-      }
-    } else {
-      if (word === word1) {
-        index1 = index;
-      }
-
-      if (word === word2) {
-        index2 = index;
-      }
     }
 
-    if (index1 >= 0 && index2 >= 0) {
-      dist = Math.min(Math.abs(index1 - index2), dist);
+    var bucket = [];
+    for (var key in map) {
+        var keyVal = parseInt(key, 10);
+        bucket[map[key]] ? bucket[map[key]].push(keyVal) : bucket[map[key]] = [keyVal];
     }
-  })
 
-  return dist;
-}
+    var result = [];
 
-// test cases
-var words = ["practice", "makes", "perfect", "coding", "makes"];
-var word1 = 'practice';
-var word2 = 'coding';
-var word3 = 'makes';
-// console.log(shortestWordDist(words, word1, word2));
-console.log(shortestWordDist(words, word3, word3));
+    for (var j = bucket.length - 1; j > 0; j--) {
+        if (bucket[j]) {
+            result = result.concat(bucket[j]);
+        }
+
+        if (result.length >= k) break;
+    }
+
+    return result;
+
+};
