95cf11bab043170788b6cd701e46f101b4c6629d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-27 22:52:28 +0530

    29 :::: 0082-remove-duplicates-from-sorted-list-ii.js leetcode question updated (2022-12-27)

--- dsa.js
+++ dsa.js
@@ -1,42 +1,56 @@
-// 81. Search in Rotated Sorted Array II
-// Medium 32% locked:false
+// 82. Remove Duplicates from Sorted List II
+// Medium 29% locked:false
 
-// Follow up for "Search in Rotated Sorted Array":
-// What if duplicates are allowed?
+// Given a sorted linked list, delete all nodes that have duplicate numbers,
+// leaving only distinct numbers from the original list.
 
-// Would this affect the run-time complexity? How and why?
-
-// Suppose an array sorted in ascending order is rotated at some pivot unknown
-// to you beforehand.
-
-// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-
-// Write a function to determine if a given target is in the array.
-
-// The array may contain duplicates.
+// For example,
+// Given 1->2->3->3->4->4->5, return 1->2->5.
+// Given 1->1->1->2->3, return 2->3.
 
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {boolean}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-const search = function(nums, target) {
-  const n = nums.length
-  if (n === 0) return false
 
-  const binary = (i, j) => {
-    if (i > j) return false
-    const mid = (i + j) >> 1
-    if (target < nums[mid]) {
-      return binary(i, mid - 1) ||
-        ((nums[mid] >= nums[j]) ? binary(mid + 1, j) : false)
-    } else if (target > nums[mid]) {
-      return binary(mid + 1, j) ||
-        ((nums[mid] <= nums[i]) ? binary(i, mid - 1) : false)
-    } else return true
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function(head) {
+  if (head === null) return head
+
+  const start = new ListNode()
+  start.next = head
+
+  let p = start, q = p.next
+  while (q !== null && q.next !== null) {
+    if (p.next.val !== q.next.val) {
+      p = p.next
+      q = q.next
+    } else {
+      while (q.next !== null && p.next.val === q.next.val) q = q.next
+      q = q.next
+      p.next = q
+    }
   }
 
-  return binary(0, n - 1)
+  return start.next
 }
 
-console.log(search([4, 4, 4, 5, 1, 1, 1, 2, 2, 2, 2], 3))
+const ListNode = require('../structs/ListNode')
+;[
+  [3,3,4,4],       // []
+  [1,2,3,3,4,4,5], // [1,2,5]
+  [1,1,1,2,3],     // [2,3]
+].forEach((array) => {
+  console.log((deleteDuplicates(ListNode.from(array)) || '').toString())
+})
+
+// Solution:
+// TODO: #82 solution
+
+// Submission Result: Accepted
