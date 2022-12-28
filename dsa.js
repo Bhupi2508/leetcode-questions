bdd76540663c410e5c879da593dc914528229781
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2022-12-28 22:52:59 +0530

    29 :::: 0257-binary-tree-paths.js leetcode question updated (2022-12-28)

--- dsa.js
+++ dsa.js
@@ -1,51 +1,59 @@
-// 242. Valid Anagram
-// Easy   46%
+// 257. Binary Tree Paths
+// Easy   39%
 
 
-// Given two strings s and t, write a function to determine if t is an anagram of
-// s.
+// Given a binary tree, return all root-to-leaf paths.
 
-// For example,
-// s = "anagram", t = "nagaram", return true.
-// s = "rat", t = "car", return false.
+// For example, given the following binary tree:
 
-// Note:
-// You may assume the string contains only lowercase alphabets.
+//    1
+//  /   \
+// 2     3
+//  \
+//   5
 
-// Follow up:
-// What if the inputs contain unicode characters? How would you adapt your
-// solution to such case?
+// All root-to-leaf paths are:
+
+// ["1->2->5", "1->3"]
+
+// Credits:Special thanks to @jianchao.li.fighter for adding this problem and
+// creating all test cases.
 
 
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
  */
-const isAnagram = function(s, t) {
-  if (s.length !== t.length) return false
-  const hash = {}
-  for (let c of s) hash[c] = (hash[c] || 0) + 1
-  for (let c of t) {
-    if (!hash[c]) return false
-    hash[c]--
+
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+const binaryTreePaths = function(root) {
+  const array = []
+  function traversal(root, string) {
+    string += (string === '' ? '' : '->') + root.val
+    if (root.left) traversal(root.left, string, array)
+    if (root.right) traversal(root.right, string, array)
+    if (!root.left && !root.right) array.push(string)
   }
-  return true
+  if (root) traversal(root, '')
+  return array
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  ['anagram', 'nagaram'],       // true
-  ['rat', 'car'],               // false
-  ['ab', 'a'],                  // false
-  ['aacc', 'ccac'],             // false
-].forEach(args => {
-  console.log(isAnagram(...args))
+  [1,2,3,null,5],      // ['1->2->5', '1->3']
+].forEach((array) => {
+  console.log(binaryTreePaths(TreeNode.from(array)))
 })
 
+
 // Solution:
-// 若字符串的长度不同，那一定 false
-// 用哈希保存 s 字符串的那个出现字符及其次数。
-// 对于 t 字符串中的每个字符，判断其是否在哈希中出现，且其哈希值是否大于0，
-// 若不是则返回 false，如果全部通过则返回 true 。
+// 递归一遍树，带上字符参数，每遍历一个节点，添加一个值。
+// 若是叶子节点，再加完值之后，将字符串添加到数组中。
 
 // Submission Result: Accepted
