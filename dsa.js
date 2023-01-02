bdf17fa1e11df4d36f540f0f10c17f557153a2f6
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-02 22:53:35 +0530

    30 :::: 0589-n-ary-tree-preorder-traversal.js leetcode question updated (2023-01-02)

--- dsa.js
+++ dsa.js
@@ -1,67 +1,62 @@
-// 581. Shortest Unsorted Continuous Subarray
-// Easy   29%
+// 589. N-ary Tree Preorder Traversal
+// Easy   71%
 
 
-// Given an integer array, you need to find one continuous subarray that if you
-// only sort this subarray in ascending order, then the whole array will be
-// sorted in ascending order, too.
+// Given an n-ary tree, return the preorder traversal of its nodes' values.
+// Nary-Tree input serialization is represented in their level order traversal,
+// each group of children is separated by the null value (See examples).
 
-// You need to find the shortest such subarray and output its length.
+// Follow up:
+// Recursive solution is trivial, could you do it iteratively?
 
 // Example 1:
+// Input: root = [1,null,3,2,4,null,5,6]
+// Output: [1,3,5,6,2,4]
+// Example 2:
+// Input: root =
+// [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
 
-// Input: [2, 6, 4, 8, 10, 9, 15]
-// Output: 5
-// Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the
-// whole array sorted in ascending order.
-
-// Note:
-
-// Then length of the input array is in range [1, 10,000].
-// The input array may contain duplicates, so ascending order here means <=.
+// Constraints:
+//     The height of the n-ary tree is less than or equal to 1000
+//     The total number of nodes is between [0, 10^4]
 
 
 /**
- * @param {number[]} nums
- * @return {number}
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *this.val = val;
+ *    this.children = children;
+ * };
  */
-const findUnsortedSubarray = function(nums) {
-  const n = nums.length
-  let head = 10000, tail = 10000
-  for (let i = 1; i < n; i++) {
-    if (nums[i - 1] > nums[i]) {
-      let j = i, max = nums[i - 1]
-      while (nums[--j] > nums[i]) max = Math.max(max, nums[j])
-      head = Math.min(head, j)
-      tail = i
-      ;[nums[j + 1], nums[i]] = [nums[i], max]
+
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+const preorder = function(root) {
+  const res = []
+  function preorder(node) {
+    if (node) {
+      res.push(node.val)
+      for (let c of node.children) {
+        preorder(c)
+      }
     }
   }
-  return tail - head
+  preorder(root)
+  return res
 }
 
 ;[
-  [2, 6, 4, 8, 10, 9, 15],      // 5
-  [1,2,3,4],                    // 0
-  [2,3,3,2,4],                  // 3
-  [1,3,2,2,2],                  // 4
-  [2,1,1,1,1],                  // 5
-  [1,3,5,2,4],                  // 4
-].forEach(nums => {
-  console.log(findUnsortedSubarray(nums))
+  [1,null,3,2,4,null,5,6],
+].forEach(() => {
+
 })
 
 // Solution:
-
-// 1. 遍历一遍，遇到 [i - 1] > [i]，就替换，并记录
-// 失败，反例:例子3
-
-// 2. 遍历一遍，但过程中遇到 [i - 1] > [i]时，
-// 使用类似插入排序的方法寻找其应该插入的位置 j + 1
-// 在寻找过程中，还记录一个最大值 max
-// 并在最后，将 [j+1] 替换为 [i]，[i] 替换为 max
-
-// 这样，在迭代过程中，乱序被整理成了虚拟的顺序，即
-// 一个子序列的开头为该子序列的最小值，而结尾为该子序列的最大值。
+// 1. 前序遍历，递归即可。
+// 2. 使用栈
+// TODO: #589 多叉树的构造函数
 
 // Submission Result: Accepted
