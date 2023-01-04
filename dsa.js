e2d801b00fecb0275253bfb53960d21aaddb4c4b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-04 22:53:37 +0530

    30 :::: 0605-can-place-flowers.js leetcode question updated (2023-01-04)

--- dsa.js
+++ dsa.js
@@ -1,78 +1,57 @@
-// 599. Minimum Index Sum of Two Lists
-// Easy   46%
+// 605. Can Place Flowers
+// Easy   30%
 
 
-// Suppose Andy and Doris want to choose a restaurant for dinner, and they both
-// have a list of favorite restaurants represented by strings.
+// Suppose you have a long flowerbed in which some of the plots are planted and
+// some are not. However, flowers cannot be planted in adjacent plots - they
+// would compete for water and both would die.
 
-// You need to help them find out their common interest with the least list index
-// sum. If there is a choice tie between answers, output all of them with no
-// order requirement. You could assume there always exists an answer.
+// Given a flowerbed (represented as an array containing 0 and 1, where 0 means
+// empty and 1 means not empty), and a number n, return if n new flowers can be
+// planted in it without violating the no-adjacent-flowers rule.
 
 // Example 1:
 
-// Input:
-// ["Shogun", "Tapioca Express", "Burger King", "KFC"]
-// ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
-// Output: ["Shogun"]
-// Explanation: The only restaurant they both like is "Shogun".
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: True
 
 // Example 2:
 
-// Input:
-// ["Shogun", "Tapioca Express", "Burger King", "KFC"]
-// ["KFC", "Shogun", "Burger King"]
-// Output: ["Shogun"]
-// Explanation: The restaurant they both like and have the least index sum is
-// "Shogun" with index sum 1 (0+1).
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: False
 
 // Note:
 
-// The length of both lists will be in the range of [1, 1000].
-// The length of strings in both lists will be in the range of [1, 30].
-// The index is starting from 0 to the list length minus 1.
-// No duplicates in both lists.
+// The input array won't violate no-adjacent-flowers rule.
+// The input array size is in the range of [1, 20000].
+// n is a non-negative integer which won't exceed the input array size.
 
 
 /**
- * @param {string[]} list1
- * @param {string[]} list2
- * @return {string[]}
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
  */
-const findRestaurant = function(list1, list2) {
-  const n = list1.length, m = list2.length
-  let result = [], index = Infinity
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (list1[i] === list2[j]) {
-        if (i + j <= index) {
-          if (i + j < index) result = []
-          result.push(list1[i])
-          index = i + j
-        }
-        break
-      }
+const canPlaceFlowers = function(flowerbed, n) {
+  for (let i = 0, len = flowerbed.length; i < len; i++) {
+    if (!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]) {
+      flowerbed[i] = 1
+      n--
     }
   }
-  return result
+  return n <= 0
 }
 
 ;[
-  [
-    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
-    ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
-  ],                            // ['Shogun']
-  [
-    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
-    ["KFC", "Shogun", "Burger King"]
-  ],                            // ['Shogun']
+  [[1,0,0,0,1], 1],             // true
+  [[1,0,0,0,1], 2],             // false
+  [[1,0,1], 0],                 // true
+  [[0,0,1,0,1], 1],             // true
 ].forEach(args => {
-  console.log(findRestaurant(...args))
+  console.log(canPlaceFlowers(...args))
 })
 
 // Solution:
-// 遍历 list1，每个元素 list1[i] 都再 list2 中找，找到第一个 list2[j]，
-// 比较记录中的 index 和 i + j，如小于 index 则将 result 清空再添加，
-// 如等于，则添加。
+// 不要空想，直接去种一遍。
 
 // Submission Result: Accepted
