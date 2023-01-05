6ba9216719b0003203899b8322de8e9bc43912b5
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-05 22:53:44 +0530

    30 :::: 0671-second-minimum-node-in-a-binary-tree.js leetcode question updated (2023-01-05)

--- dsa.js
+++ dsa.js
@@ -1,48 +1,37 @@
-// 669. Trim a Binary Search Tree
-// Easy 58% locked:false
+// 671. Second Minimum Node In a Binary Tree
+// Easy 41% locked:false
 
 
-// Given a binary search tree and the lowest and highest boundaries as L and R,
-// trim the tree so that all its elements lies in [L, R] (R >= L). You might need
-// to change the root of the tree, so the result should return the new root of
-// the trimmed binary search tree.
+// Given a non-empty special binary tree consisting of nodes with the
+// non-negative value, where each node in this tree has exactly two or zero
+// sub-node. If the node has two sub-nodes, then this node's value is the smaller
+// value among its two sub-nodes.
+
+// Given such a binary tree, you need to output the second minimum value in the
+// set made of all the nodes' value in the whole tree.
+
+// If no such second minimum value exists, output -1 instead.
 
 // Example 1:
 
 // Input:
-//     1
+//     2
 //    / \
-//   0   2
-
-//   L = 1
-//   R = 2
-
-// Output:
-//     1
-//       \
-//        2
+//   2   5
+//      / \
+//     5   7
+// Output: 5
+// Explanation: The smallest value is 2, the second smallest value is 5.
 
 // Example 2:
 
 // Input:
-//     3
-//    / \
-//   0   4
-//    \
-//     2
-//    /
-//   1
-
-//   L = 1
-//   R = 3
-
-// Output:
-//       3
-//      /
-//    2
-//   /
-//  1
-
+//   2
+//  / \
+// 2   2
+// Output: -1
+// Explanation: The smallest value is 2, but there isn't any second smallest
+// value.
 
 /**
  * Definition for a binary tree node.
@@ -54,35 +43,46 @@
 
 /**
  * @param {TreeNode} root
- * @param {number} L
- * @param {number} R
- * @return {TreeNode}
+ * @return {number}
  */
-const trimBST = function(root, L, R) {
-  if (root == void 0) return null
-  const val = root.val
-  if (val < L) return trimBST(root.right, L, R)
-  if (val > R) return trimBST(root.left, L, R)
-  root.left = trimBST(root.left, L, val)
-  root.right = trimBST(root.right, val, R)
-  return root
+const findSecondMinimumValue = function(root) {
+  if (root == void 0 || root.left == void 0) return -1
+
+  const left = root.val === root.left.val ? findSecondMinimumValue(root.left) : root.left.val,
+        right = root.val === root.right.val ? findSecondMinimumValue(root.right) : root.right.val
+
+  if (left === -1) return right
+  if (right === -1) return left
+  return Math.min(left, right)
 }
 
 const TreeNode = require('../structs/TreeNode')
 ;[
-  [[1, 0, 2], 1, 2],
-  [[3, 0, 4, null, 2, null, null, 1], 1, 3],
-].forEach(([array, L, R]) => {
-  console.log(trimBST(TreeNode.from(array), L, R))
+  [2, 2, 5, null, null, 5, 7],                // 5
+  [2, 2, 2],                                  // -1
+  [2, 2, 2, 2, 2, 2, 2, null, null, 5, 2],    // 5
+  [],                                         // -1
+  [1,1,3,1,1,3,4,3,1,1,1,3,8,4,8,3,3,1,6,2,1],// 2
+].forEach((array) => {
+  console.log(findSecondMinimumValue(TreeNode.from(array)))
 })
 
-
 // Solution:
-// 递归求解。
-// 如果节点小于 L ，节点及其右子树是不可能在范围内的，递归寻找左子树符合要求的。
-// 如果节点大于 R ，同理。
-// 如果节点在范围内，则递归获得其符合要求的左右子节点。
-// 最后递归的进行要返回根节点，或null。
-// 思路很清晰。
+
+// 关键信息：
+// 1. 非空二叉树
+// 2. 每个节点的两个子节点要么都为空，要么都存在
+// 3. 如果两子节点存在，则该节点的值为子节点中小的一个的值
+
+// 要找整棵树第二小的数
+// 若结果在第二层，就很明显。
+// 若结果在第二层以下，则会出现多层的数与根值相同的情况。
+// 并非看到的第一个比根值大的数就是结果。
+
+// 既然是树，自然是首先想到递归求解
+// 要考虑的情况有2：
+// 1. 根节点为空，或左右子节点为空，则返回 -1，说明没有第二小的数
+// 2. 左（右）子节点与根同，且存在子节点，则需要检查递归寻找该节点的第二小，
+//    再与右（左）一个节点比较，取小的。
 
 // Submission Result: Accepted
