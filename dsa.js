0f15bba0ddeb109a4cc68bdeafc872e07c8b0ad4
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-08 22:54:30 +0530

    31 :::: 1018-binary-prefix-divisible-by-5.js leetcode question updated (2023-01-08)

--- dsa.js
+++ dsa.js
@@ -1,66 +1,62 @@
-// 1013. Partition Array Into Three Parts With Equal Sum
-// Easy   57%
+// 1018. Binary Prefix Divisible By 5
+// Easy   47%
 
 
-// Given an array A of integers, return true if and only if we can partition the
-// array into three non-empty parts with equal sums.
-// Formally, we can partition the array if we can find indexes i+1 < j with (A[0]
-// + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... +
-// A[A.length - 1])
-
+// Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to
+// A[i] interpreted as a binary number (from most-significant-bit to
+// least-significant-bit.)
+// Return a list of booleans answer, where answer[i] is true if and only if N_i
+// is divisible by 5.
 // Example 1:
-// Input: A = [0,2,1,-6,6,-7,9,1,2,0,1]
-// Output: true
-// Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+// Input: [0,1,1]
+// Output: [true,false,false]
+// Explanation:
+// The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in base-10. 
+// Only the first number is divisible by 5, so answer[0] is true.
 // Example 2:
-// Input: A = [0,2,1,-6,6,7,9,-1,2,0,1]
-// Output: false
+// Input: [1,1,1]
+// Output: [false,false,false]
 // Example 3:
-// Input: A = [3,3,6,5,-2,2,5,1,-9,4]
-// Output: true
-// Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+// Input: [0,1,1,1,1,1]
+// Output: [true,false,false,false,true,false]
+// Example 4:
+// Input: [1,1,1,0,1]
+// Output: [false,false,false,false,false]
 
-// Constraints:
-//     3 <= A.length <= 50000
-//     -10^4 <= A[i] <= 10^4
+// Note:
+//     1 <= A.length <= 30000
+//     A[i] is 0 or 1
 
 
 /**
  * @param {number[]} A
- * @return {boolean}
+ * @return {boolean[]}
  */
-const canThreePartsEqualSum = function(A) {
-  let sum = 0
-  for (let a of A) sum += a
-  if (sum % 3 !== 0) return false
-
-  let part = 0, partSum = sum / 3
-  for (let i = 0; i < A.length; i++) {
-    partSum -= A[i]
-    if (partSum === 0) {
-      partSum = sum / 3
-      part++
-    }
+const prefixesDivBy5 = function(A) {
+  const result = []
+  let num = 0
+  for (let i of A) {
+    num = (num * 2 + i) % 5
+    result.push(num === 0)
   }
-  return part > 2
+  return result
 }
 
 ;[
-  [0,2,1,-6,6,-7,9,1,2,0,1],     // true
-  [0,2,1,-6,6,7,9,-1,2,0,1],     // false
-  [3,3,6,5,-2,2,5,1,-9,4],       // true
-  [10,-10,10,-10,10,-10,10,-10], // true
+  [0,1,1],      // [true, false, false]
+  [1,1,1],      // [false,false, false]
+  [0,1,1,1,1,1],// [true,false,false,false,true,false]
+  [1,1,1,0,1],  // [false,false,false,false,false]
+  [1,1,0,0,0,1,0,0,1], // [false,false,false,false,false,false,false,false,false]
+  [1,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,1,1,1,1,1,1,0,0,0,1,0,1,1,1,1,0,1,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,1,1,0,0,1,1,1],
 ].forEach((A) => {
-  console.log(canThreePartsEqualSum(A))
+  console.log(prefixesDivBy5(A))
 })
 
 // Solution:
-// 因为要分为相等的3个部分，
-// 所以所有数的和 sum 必须为3的倍数。
-// 若 sum % 3 不为 0，则返回 false
-// 遍历一遍数组
-// 若前n个数的和为 sum / 3，这 n 个数就为一个部分
-// 然后找下一个部分
-// 只要找到两个部分即可，因为剩余的就是最后一个部分
+// 二进制数转十进制，从前遍历只需前面的值乘 2 再加上该位置的数即可
+// 本题关键在于数组的长度非常大时，得到的十进制数也十分大，超出了 % 运算符正常运算的范围
+// 这就需要一些取模运算的法则了
+// <= to be continued
 
 // Submission Result: Accepted
