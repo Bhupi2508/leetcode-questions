c9f84eb33fbc5d3fb1627766fd55967687e3c795
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-19 22:45:05 +0530

    32 :::: 48-rotateImage.js question updated

--- dsa.js
+++ dsa.js
@@ -1,65 +1,23 @@
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * Key: because it is a n*n matrix, rotate from outer to inner level by level.
+ * 
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
  */
-// First try, Memory Limit Exceeded
-var permuteUnique = function(nums) {
-    if (nums.length === 0) return nums;
-    nums.sort(function(a, b) {
-        a - b;
-    });
-    var results = [];
-    permutation(results, nums, 0);
-    return results;
-};
+var rotate = function(matrix) {
+    var diagonalStart = 0;
+    var length = matrix.length - 1;
 
-var permutation = function(results, nums, start) {
-    if (start >= nums.length) {
-        results.push(nums.slice());
-        return;
-    }
-
-    for (var i = start; i < nums.length; i++) {
-        if (i > start && nums[i] === nums[i - 1]) continue;
-        swap(nums, i, start);
-        permutation(results, nums, start + 1);
-        swap(nums, i, start);
-    }
-};
-
-var swap = function(nums, i, j) {
-    if (i === j) return;
-    var tmp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = tmp;
-};
-
-
-// A iterative version, accepted, 140ms, beats 100%
-// same idea as permutation,
-// but, sort first, then if there is number is same as the previous one, skip
-// for example: nums=[1,2,2]
-// step1: [1]
-// step2: inserst 2 to [1], two possibilities: [2,1], [1,2]
-// step3: inserst 2 to [1,2] and [2,1]? but there is a 2 in each array, so skip this number and return
-var permuteUnique = function(nums) {
-    if (nums.length === 0) return nums;
-    nums.sort(function(a, b) {
-        return a - b;
-    });
-    var results = [[nums[0]]];
-    for (var i = 1; i < nums.length; i++) {
-        var newResults = [];
-        for (var m = 0; m < results.length; m++) {
-            for (var j = 0; j <= i; j++) {
-                var list = results[m].slice();
-                list.splice(j, 0, nums[i]);
-                newResults.push(list);
-                if (results[m][j] === nums[i]) break;
-            }
+    while (diagonalStart <= length / 2) {
+        var i = diagonalStart;
+        if (i + i >= length) break;
+        for (var j = diagonalStart; j < length - i; j++) {
+            var tmp = matrix[i][j];
+            matrix[i][j] = matrix[length - j][i];
+            matrix[length - j][i] = matrix[length - i][length - j];
+            matrix[length - i][length - j] = matrix[j][length - i];
+            matrix[j][length - i] = tmp;
         }
-        results = newResults;
+        diagonalStart++;
     }
-
-    return results;
 };
