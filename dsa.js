24ed31da83720f560e90c3600e58af8a97f20cc4
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-20 22:53:28 +0530

    32 :::: 0532-k-diff-pairs-in-an-array.js leetcode question updated (2023-01-20)

--- dsa.js
+++ dsa.js
@@ -1,69 +1,86 @@
-// 530. Minimum Absolute Difference in BST
-// Easy   47%
+// 532. K-diff Pairs in an Array
+// Easy   28%
 
 
-// Given a binary search tree with non-negative values, find the minimum absolute
-// difference between values of any two nodes.
+// Given an array of integers and an integer k, you need to find the number of
+// unique k-diff pairs in the array. Here a k-diff pair is defined as an integer
+// pair (i, j), where i and j are both numbers in the array and their absolute
+// difference is k.
 
-// Example:
+// Example 1:
 
-// Input:
+// Input: [3, 1, 4, 1, 5], k = 2
+// Output: 2
+// Explanation: There are two 2-diff pairs in the array, (1, 3) and (3,
+// 5).Although we have two 1s in the input, we should only return the number of
+// unique pairs.
 
-//    1
-//     \
-//      3
-//     /
-//    2
+// Example 2:
 
-// Output:
-// 1
+// Input:[1, 2, 3, 4, 5], k = 1
+// Output: 4
+// Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4)
+// and (4, 5).
 
-// Explanation:
-// The minimum absolute difference is 1, which is the difference between 2 and 1
-// (or between 2 and 3).
+// Example 3:
+
+// Input: [1, 3, 1, 5, 4], k = 0
+// Output: 1
+// Explanation: There is one 0-diff pair in the array, (1, 1).
 
 // Note:
-// There are at least two nodes in this BST.
+
+// The pairs (i, j) and (j, i) count as the same pair.
+// The length of the array won't exceed 10,000.
+// All the integers in the given input belong to the range: [-1e7, 1e7].
 
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *   this.val = val;
- *   this.left = this.right = null;
- * }
- */
-
-/**
- * @param {TreeNode} root
+ * @param {number[]} nums
+ * @param {number} k
  * @return {number}
  */
-const getMinimumDifference = function(root) {
-  function inorder(root, array) {
-    if (root != void 0) {
-      inorder(root.left, array)
-      array.push(root.val)
-      inorder(root.right, array)
-    }
-    return array
-  }
-  const array = inorder(root, [])
-  let result = Infinity
-  for (let i = 1, n = array.length; i < n; i++) {
-    result = Math.min(array[i] - array[i - 1], result)
+const findPairs = function(nums, k) {
+  nums.sort((a, b) => a - b)
+  const hash = {}, exist = {}
+  let result = 0
+  for (let num of nums) {
+    if (hash[num] != void 0 &&
+        exist[num + hash[num]] == void 0) exist[num + hash[num]] = result++
+    if (hash[num + k] == void 0) hash[num + k] = num
   }
   return result
 }
 
-const TreeNode = require('../structs/TreeNode')
 ;[
-  [1,null,3,2], // 1
-].forEach((array) => {
-  console.log(getMinimumDifference(TreeNode.from(array)))
+  [[3,1,4,1,5], 2],             // 2
+  [[1,2,3,4,5], 1],             // 4
+  [[1,3,1,5,4], 0],             // 1
+  [[0,0,0], 0],                 // 1
+].forEach(args => {
+  console.log(findPairs(...args))
 })
 
 // Solution:
-// 因为树是 BST，所以使用中序遍历就能得到一个排好序的节点数组。
-// 比较数组中每个元素与上一个元素的差，选择差最小的一个。
+
+// 先排序，有重复的元素要处理时，且不能顺利的一遍过时，只能先牺牲一点点时间，先
+// 排序，这样比较好处理。
+
+// 利用两个哈希表，分别记录 待满足的对 和 已满足的对。
+
+// 满足的对要满足两元素的差绝对值为 k。因此 k < 0 时，这样的对是不存在的。k 必须
+// 大于或等于 0。这样我们可以想象，在一条线上，其间隔为 k。
+
+// 而他们又是排好序的，因此只要在 待满足的对集合 中加入 其后间隔为 k 的对即可。
+// 无须考虑前面的，如果前面存在，到这个元素时，就应该已经存在 已满足的对的集合 里了。
+
+// 已满足的对 中，只需要记录对的两元素之和即可。
+// 因为间隔为 k 的任意两个数的和都是不同的。
+// 证明：
+// 设两个元素分别为 a，b，其满足 |a - b| = k。
+// 根据已排好序，每次待满足的且可能满足 a 值和 b 值，都会满足 a => b。
+// 因此 a - b = k， b = a - k
+// 两数之和 s = a + b = a + (a - k) = a * 2 - k
+// s 随 a 变化而变化。
 
 // Submission Result: Accepted
