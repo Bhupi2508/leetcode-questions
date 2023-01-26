a203c30bce7a073ee972efc13a63adb000b3548c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-26 22:54:41 +0530

    33 :::: 1185-day-of-the-week.js leetcode question updated (2023-01-26)

--- dsa.js
+++ dsa.js
@@ -1,82 +1,68 @@
-// 1184. Distance Between Bus Stops
-// Easy   55%
+// 1185. Day of the Week
+// Easy   64%
 
 
-// A bus has n stops numbered from 0 to n - 1 that form a circle. We know the
-// distance between all pairs of neighboring stops where distance[i] is the
-// distance between the stops number i and (i + 1) % n.
-// The bus goes along both directions i.e. clockwise and counterclockwise.
-// Return the shortest distance between the given start and destination stops.
+// Given a date, return the corresponding day of the week for that date.
+// The input is given as three integers representing the day, month and year
+// respectively.
+// Return the answer as one of the following values {"Sunday", "Monday",
+// "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
 
 // Example 1:
-//
-// (0)(start) ------1------> (1)(destination)
-//  |                         |
-//  4                         2
-//  |                         |
-// (3)--------------3------> (2)
-//
-// Input: distance = [1,2,3,4], start = 0, destination = 1
-// Output: 1
-// Explanation: Distance between 0 and 1 is 1 or 9, minimum is 1.
-
+// Input: day = 31, month = 8, year = 2019
+// Output: "Saturday"
 // Example 2:
-//
-// (0)(start) ------1------> (1)
-//  |                         |
-//  4                         2
-//  |                         |
-// (3)--------------3------> (2)(destination)
-//
-// Input: distance = [1,2,3,4], start = 0, destination = 2
-// Output: 3
-// Explanation: Distance between 0 and 2 is 3 or 7, minimum is 3.
-
+// Input: day = 18, month = 7, year = 1999
+// Output: "Sunday"
 // Example 3:
-//
-// (0)(start) ------1------> (1)
-//  |                         |
-//  4                         2
-//  |                         |
-// (3)(destination)-3------> (2)
-//
-// Input: distance = [1,2,3,4], start = 0, destination = 3
-// Output: 4
-// Explanation: Distance between 0 and 3 is 6 or 4, minimum is 4.
+// Input: day = 15, month = 8, year = 1993
+// Output: "Sunday"
 
 // Constraints:
-//     1 <= n <= 10^4
-//     distance.length == n
-//     0 <= start, destination < n
-//     0 <= distance[i] <= 10^4
+//     The given dates are valid dates between the years 1971 and 2100.
 
 
 /**
- * @param {number[]} distance
- * @param {number} start
- * @param {number} destination
- * @return {number}
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
  */
-const distanceBetweenBusStops = function(distance, start, destination) {
-  const n = distance.length
-  let forward = back = 0
-  for (let i = start; i != destination; i = (i + 1) % n) forward += distance[i]
-  for (let i = destination; i != start; i = (i + 1) % n) back += distance[i]
-  return Math.min(forward, back)
+const dayOfTheWeek = function(day, month, year) {
+  const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  if (month < 3) {
+    month += 12
+    year -= 1
+  }
+  const c = Math.floor(year / 100)
+  year = year % 100
+  // LEARN #1185 日期算出星期
+  const w = (
+    Math.floor(c / 4) - 2 * c + year + Math.floor(year / 4) +
+    Math.floor(13 * (month + 1) / 5) +
+    day - 1
+  ) % 7
+  return DAYS[Math.floor(w + 7) % 7]
+}
+
+const builtInFunc = function(day, month, year) {
+  const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  return DAYS[new Date(year, month - 1, day).getDay()]
 }
 
 ;[
-  [[1,2,3,4], 0, 1],
-  [[1,2,3,4], 0, 2],
-  [[1,2,3,4], 0, 3],
-].forEach(([distance, start, destination]) => {
-  console.log(distanceBetweenBusStops(distance, start, destination))
+  [1,  1, 1971], // Fri
+  [31, 8, 2019],
+  [18, 7, 1999],
+  [15, 8, 1993],
+].forEach(([day,month, year]) => {
+  console.log(dayOfTheWeek(day, month, year))
 })
 
 // Solution:
-// 分别计算
-// 1）从 start 开始按数组顺序到 destination 的距离
-// 2）从 destination 开始按数组顺序到 start 的距离
-// 最后取二者中小的即可。
+// 方法一，使用内置 Date 对象的函数
+
+// 方法二，
+// TODO #1185 日期算出星期
 
 // Submission Result: Accepted
