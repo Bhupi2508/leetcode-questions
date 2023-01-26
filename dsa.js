60320a3bdcd65a90e0e62986c901e13b9dd2ef93
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-26 22:45:12 +0530

    33 :::: 77-combinations.js question updated

--- dsa.js
+++ dsa.js
@@ -1,28 +1,26 @@
 /**
- * Key: move all 2s to the end, and move all 0s to the beginning.
- * Use two pointers to track start and end respectively.
- * Move 2s first.
+ * Same idea as permutation
  *
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
  */
-var sortColors = function(nums) {
-    var start = 0;
-    var end = nums.length - 1;
-    for (var i = 0; i <= end; i++) {
-        while (nums[i] === 2 && i < end) {
-            swap(nums, i, end);
-            end--;
-        }
-        while (nums[i] === 0 && i > start) {
-            swap(nums, i, start);
-            start++;
-        }
-    }
-};
+ var combine = function(n, k) {
+     var result = [];
+     var results = [];
+     combineHelper(n, k, 1, result, results);
+     return results;
+ };
 
-var swap = function(nums, i, j) {
-    var tmp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = tmp;
-};
+ var combineHelper = function(n, k, start, result, results) {
+     if (k === 0) {
+         results.push(result.slice());
+         return results;
+     }
+
+     for (var i = start; i <=n; i++) {
+         result.push(i);
+         combineHelper(n, k - 1, i + 1, result, results);
+         result.pop(i);
+     }
+ };
