b45845d08ba58bce899655e53bd6923ee4ce555f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-27 22:53:29 +0530

    33 :::: 0540-single-element-in-a-sorted-array.js leetcode question updated (2023-01-27)

--- dsa.js
+++ dsa.js
@@ -1,56 +1,44 @@
-// 538. Convert BST to Greater Tree
-// Easy   49%
-
-
-// Given a Binary Search Tree (BST), convert it to a Greater Tree such that every
-// key of the original BST is changed to the original key plus sum of all keys
-// greater than the original key in BST.
-
-// Example:
-
-// Input: The root of a Binary Search Tree like this:
-//               5
-//             /   \
-//            2     13
-
-// Output: The root of a Greater Tree like this:
-//              18
-//             /   \
-//           20     13
-
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *   this.val = val;
- *   this.left = this.right = null;
- * }
- */
-
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-const convertBST = function(root) {
-  function iter(root, sum) {
-    if (root == void 0) return sum
-    root.val += iter(root.right, sum)
-    return iter(root.left, root.val)
-  }
-  iter(root, 0)
-  return root
-}
-
-const TreeNode = require('../structs/TreeNode')
-;[
-  [5,2,13],            // [18,20,13]
-].forEach((array) => {
-  console.log(convertBST(TreeNode.from(array)))
-})
-
-// Solution:
-// 对树使用右边优先的中序遍历，
-// 并且每次都让后一个被遍历的节点的值加上前一个的。
-// 使用参数 sum 作为前一个值进行传递。
-
-// Submission Result: Accepted
+// 540. Single Element in a Sorted Array
+// Medium
+
+// Given a sorted array consisting of only integers where every element appears
+// twice except for one element which appears once. Find this single element
+// that appears only once.
+
+// Example 1:
+
+// Input: [1,1,2,3,3,4,4,8,8]
+// Output: 2
+
+// Example 2:
+
+// Input: [3,3,7,7,10,11,11]
+// Output: 10
+
+// Note: Your solution should run in O(log n) time and O(1) space.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNonDuplicate = function(nums) {
+  for (let i = 0, n = nums.length; i < n; i++) {
+    if (!(nums[i] === nums[i - 1] || nums[i] === nums[i + 1])) return nums[i]
+  }
+  return nums[0]
+}
+
+;[
+  [1,1,2,3,3,4,4,8,8],          // 2
+  [3,3,7,7,10,11,11],           // 10
+  [0],                          // 0
+  [1,1,2],                      // 2
+].forEach(nums => {
+  console.log(singleNonDuplicate(nums))
+})
+
+// Solution:
+// 因为已经排好序，因此只需要从头开始遍历数组，
+// 返回遇到第一个既不等于前一个数，也不等于后一个数的数。
+
+// Submission Result: Accepted
