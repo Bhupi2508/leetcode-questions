5a6d3de66daed2582727582731b6d1e1b7f58912
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-27 22:52:42 +0530

    33 :::: 0121-best-time-to-buy-and-sell-stock.js leetcode question updated (2023-01-27)

--- dsa.js
+++ dsa.js
@@ -1,66 +1,60 @@
-// 120. Triangle
-// Medium   34%
+// 121. Best Time to Buy and Sell Stock
+// Easy   41%
 
-// Given a triangle, find the minimum path sum from top to bottom. Each step you
-// may move to adjacent numbers on the row below.
+// Say you have an array for which the ith element is the price of a given stock
+// on day i.
 
-// For example, given the following triangle
+// If you were only permitted to complete at most one transaction (ie, buy one
+// and sell one share of the stock), design an algorithm to find the maximum
+// profit.
 
-// [
-//      [2],
-//     [3,4],
-//    [6,5,7],
-//   [4,1,8,3]
-// ]
+// Example 1:
 
-// The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+// Input: [7, 1, 5, 3, 6, 4]
+// Output: 5
 
-// Note: Bonus point if you are able to do this using only O(n) extra space,
-// where n is the total number of rows in the triangle.
+// max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger
+// than buying price)
+
+// Example 2:
+
+// Input: [7, 6, 4, 3, 1]
+// Output: 0
+
+// In this case, no transaction is done, i.e. max profit = 0.
 
 /**
- * @param {number[][]} triangle
+ * @param {number[]} prices
  * @return {number}
  */
-const minimumTotal = function(triangle) {
-  const n = triangle.length
-  if (n === 0) return 0
-
-  const sum = Array(n)
-  sum[0] = triangle[0][0]
-  for (let i = 1; i < n; i++) {
-    sum[i] = triangle[i][i] + sum[i - 1]
-    for (let j = i - 1; j > 0; j--) {
-      sum[j] = triangle[i][j] + Math.min(sum[j], sum[j - 1])
-    }
-    sum[0] += triangle[i][0]
+const maxProfit = function(prices) {
+  if (prices.length === 0) return 0
+  let max = 0, minPrice = prices[0]
+  for (let price of prices) {
+    if (price < minPrice) minPrice = price
+    if (price - minPrice > max) max = price - minPrice
   }
-
-  return Math.min(...sum)
+  return max
 }
 
+
 ;[
-  [
-    [2],
-    [3,4],
-    [6,5,7],
-    [4,1,8,3]
-  ]
-].forEach(triangle => {
-  console.log(minimumTotal(triangle))
+  [7, 1, 5, 3, 6, 4],
+  [7, 6, 4, 3, 1],
+].forEach(prices => {
+  console.log(maxProfit(prices))
 })
 
+
 // Solution:
-// 使用一个数组记录从顶层到某层的中每个数的最小路径和。
+// 设置两个变量
+// max 表示最大的收益
+// minPrice 表示从第一天开始时到当前天中的最小价格
 
-// 在迭代中，上一层每个数的最小路径和已经确定，现在来计算到该层的每个数的最小路
-// 径和，由于只能从上一层的两个相邻数选择，因此选最小的一个相加即可（边缘数只有
-// 一个，直接加）
+// 遍历数组
+// 若数比 minPrice 小，则更新 minPrice
+// 若数减去 minPrice 比 max 大，则更新 max
 
-// 如上例子，每遍历一层后的结果：
-// 1          [2]
-// 2        [5, 6]
-// 3     [11, 10, 13]
-// 4   [15, 11, 18, 16]
+// 最后返回 max 作为答案
 
 // Submission Result: Accepted
