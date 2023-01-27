95ebe19592fe34c745236e3594d08f25a1322911
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-01-27 22:52:55 +0530

    33 :::: 0226-invert-binary-tree.js leetcode question updated (2023-01-27)

--- dsa.js
+++ dsa.js
@@ -1,105 +1,53 @@
-// 225. Implement Stack using Queues
-// Easy   33%
+// 226. Invert Binary Tree
+// Easy   52%
 
+//         Invert a binary tree.
 
-// Implement the following operations of a stack using queues.
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
 
-// push(x) -- Push element x onto stack.
+// to
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
 
-// pop() -- Removes the element on top of the stack.
-
-// top() -- Get the top element.
-
-// empty() -- Return whether the stack is empty.
-
-// Notes:
-// You must use only standard operations of a queue -- which means only push to
-// back, peek/pop from front, size, and is empty operations are valid.
-
-// Depending on your language, queue may not be supported natively. You may
-// simulate a queue by using a list or deque (double-ended queue), as long as you
-// use only standard operations of a queue.
-
-// You may assume that all operations are valid (for example, no pop or top
-// operations will be called on an empty stack).
-
-// Credits:Special thanks to @jianchao.li.fighter for adding this problem and all
-// test cases.
+// Trivia:
+// This problem was inspired by this original tweet by Max Howell:
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you
+// can’t invert a binary tree on a whiteboard so fuck off.
 
 
 /**
- * Initialize your data structure here.
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
  */
-const MyStack = function() {
-  this.queue = []
-  this.size = 0
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function(root) {
+  if (root == null) return root
+  ;[root.left, root.right] = [invertTree(root.right), invertTree(root.left)]
+  return root
 }
 
-/**
- * Push element x onto stack.
- * @param {number} x
- * @return {void}
- */
-MyStack.prototype.push = function(x) {
-  this.queue.push(x)
-  this.size = this.queue.length
-}
-
-/**
- * Removes the element on top of the stack and returns that element.
- * @return {number}
- */
-MyStack.prototype.pop = function() {
-  const tmp = []
-  for (let i = this.size; i > 1; i--) {
-    tmp.push(this.queue.shift())
-  }
-
-  const result = this.queue.shift()
-  this.queue = tmp
-  this.size = tmp.length
-
-  return result
-}
-
-/**
- * Get the top element.
- * @return {number}
- */
-MyStack.prototype.top = function() {
-  const tmp = []
-  for (let i = this.size; i > 1; i--) {
-    tmp.push(this.queue.shift())
-  }
-
-  const result = this.queue.shift()
-  tmp.push(result)
-  this.queue = tmp
-
-  return result
-}
-
-/**
- * Returns whether the stack is empty.
- * @return {boolean}
- */
-MyStack.prototype.empty = function() {
-  return this.size === 0
-}
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * var obj = Object.create(MyStack).createNew()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.empty()
- */
-
+const TreeNode = require('../structs/TreeNode')
+;[
+  [4,2,7,1,3,6,9],
+].forEach((array) => {
+  console.log(invertTree(TreeNode.from(array)))
+})
 
 // Solution:
-// 栈和队列的 push 及 empty 函数的实现是相同的。
-// 栈的 pop 和 top 函数的实现需要用一个新的暂时队列来完成。
-// 将原队列的元素pop到暂时队列中，再去最后一个做回返回值。
+// 遍历每个节点，并交换其左右子树。
 
 // Submission Result: Accepted
