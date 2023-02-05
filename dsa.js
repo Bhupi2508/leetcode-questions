fa130223c591b265cbc6990770cd20be48bb8c16
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-05 22:54:37 +0530

    35 :::: 1128-number-of-equivalent-domino-pairs.js leetcode question updated (2023-02-05)

--- dsa.js
+++ dsa.js
@@ -1,67 +1,52 @@
-// 1122. Relative Sort Array
-// Easy   67%
+// 1128. Number of Equivalent Domino Pairs
+// Easy   47%
 
 
-// Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all
-// elements in arr2 are also in arr1.
-// Sort the elements of arr1 such that the relative ordering of items in arr1 are
-// the same as in arr2.  Elements that don't appear in arr2 should be placed at
-// the end of arr1 in ascending order.
+// Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] =
+// [c, d] if and only if either (a==c and b==d), or (a==d and b==c) - that is,
+// one domino can be rotated to be equal to another domino.
+// Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and
+// dominoes[i] is equivalent to dominoes[j].
 
 // Example 1:
-// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
-// Output: [2,2,2,1,4,3,3,9,6,7,19]
+// Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+// Output: 1
 
 // Constraints:
-//     arr1.length, arr2.length <= 1000
-//     0 <= arr1[i], arr2[i] <= 1000
-//     Each arr2[i] is distinct.
-//     Each arr2[i] is in arr1.
+//     1 <= dominoes.length <= 40000
+//     1 <= dominoes[i][j] <= 9
 
 
 /**
- * @param {number[]} arr1
- * @param {number[]} arr2
- * @return {number[]}
+ * @param {number[][]} dominoes
+ * @return {number}
  */
-const relativeSortArray = function(arr1, arr2) {
-  const slot = []
-  for (let i = 0; i < arr1.length; i++) {
-    const a = arr1[i]
-    if (slot[a]) {
-      slot[a] += 1
-    } else {
-      slot[a] = 1
-    }
+const numEquivDominoPairs = function(dominoes) {
+  let n = 0
+  const hash = {}
+  for (let dominoe of dominoes) {
+    const small = Math.min(dominoe[0], dominoe[1])
+    const large = Math.max(dominoe[0], dominoe[1])
+    const key = small + '$' + large
+    n += hash[key] || 0
+    hash[key] = (hash[key] || 0) + 1
   }
-  const result = []
-  for (let i = 0; i < arr2.length; i++) {
-    const a = arr2[i]
-    for (let j = 0; j < slot[a]; j++) {
-      result.push(a)
-    }
-    slot[a] = 0
-  }
-  for (let i = 0; i < slot.length; i++) {
-    if (slot[i] > 0) {
-      for (let j = 0; j < slot[i]; j++) {
-        result.push(i)
-      }
-    }
-  }
-  return result
+  return n
 }
 
 ;[
-  [[2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]],
-].forEach(([arr1, arr2]) => {
-  console.log(relativeSortArray(arr1, arr2))
+  [[1,2],[2,1],[3,4],[5,6]],            // 1
+  [[1,2],[2,1],[3,4],[5,6],[2,1],[1,2]],// 6
+].forEach((dominoes) => {
+  console.log(numEquivDominoPairs(dominoes))
 })
 
 // Solution:
-// 利用桶排序的思想
-// 1.将arr1的每个数放到该数字的桶中，桶里的数字代表该数的个数
-// 2.按照arr2中的数a去取桶中的数n，将n个a插入到答案中
-// 3.最后将桶中剩余的数按顺序插入到答案中
+// 方法一：每个对都和其后的全部对比较一次，在过程中计数即可。
+// O(n^2) 的时间复杂度
+
+// 方法二：利用 hash 表来记录相同项，
+// 将每个对的数排序，比如小的在前大的在后，用特殊符号分隔，确保生成的字符串相同
+// O(n) 的时间复杂度，O(n) 的空间复杂度
 
 // Submission Result: Accepted
