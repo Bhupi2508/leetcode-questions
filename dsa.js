0b11a884be3914f5c9595fa6117bfe543d02a656
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-09 22:53:57 +0530

    35 :::: 0771-jewels-and-stones.js leetcode question updated (2023-02-09)

--- dsa.js
+++ dsa.js
@@ -1,66 +1,52 @@
-// 766. Toeplitz Matrix
-// Easy   64%
+// 771. Jewels and Stones
+// Easy 82%
 
+// You're given strings J representing the types of stones that are jewels, and
+// S representing the stones you have.  Each character in S is a type of stone
+// you have.  You want to know how many of the stones you have are also jewels.
 
-// A matrix is Toeplitz if every diagonal from top-left to bottom-right has the
-// same element.
-// Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+// The letters in J are guaranteed distinct, and all characters in J and S are
+// letters. Letters are case sensitive, so "a" is considered a different type
+// of stone from "A".
 
 // Example 1:
-// Input:
-// matrix = [
-//   [1,2,3,4],
-//   [5,1,2,3],
-//   [9,5,1,2]
-// ]
-// Output: True
-// Explanation:
-// In the above grid, the diagonals are:
-// "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
-// In each diagonal all elements are the same, so the answer is True.
-// Example 2:
-// Input:
-// matrix = [
-//   [1,2],
-//   [2,2]
-// ]
-// Output: False
-// Explanation:
-// The diagonal "[1, 2]" has different elements.
-// Note:
-//     matrix will be a 2D array of integers.
-//     matrix will have a number of rows and columns in range [1, 20].
-//     matrix[i][j] will be integers in range [0, 99].
-// Follow up:
-//     What if the matrix is stored on disk, and the memory is limited such that
-// you can only load at most one row of the matrix into the memory at once?
-//     What if the matrix is so large that you can only load up a partial row
-// into the memory at once?
 
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+
+// Input: J = "z", S = "ZZ"
+// Output: 0
+
+// Note:
+//   - S and J will consist of letters and have length at most 50.
+//   - The characters in J are distinct.
 
 /**
- * @param {number[][]} matrix
- * @return {boolean}
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
  */
-const isToeplitzMatrix = function(matrix) {
-  const n = matrix.length, m = matrix[0].length
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < m; j++) {
-      if (matrix[i][j] !== matrix[i - 1][j - 1]) return false
-    }
+const numJewelsInStones = function(J, S) {
+  const hash = {}
+  for (let j of J) hash[j] = true
+  let result = 0
+  for (let s of S) {
+    if (hash[s]) result++
   }
-  return true
+  return result
 }
 
 ;[
-  [[1,2,3,4],[5,1,2,3],[9,5,1,2]], // true
-  [[1,2],[2,2]],                   // false
-].forEach((matrix) => {
-  console.log(isToeplitzMatrix(matrix))
+  ['aA', 'aAAbbbb'],
+  ['z', 'ZZ'],
+].forEach(args => {
+  console.log(numJewelsInStones(...args))
 })
 
 // Solution:
-// 1. 普通方法：
-// 除了第一行和第一列外，每个数与其左上角上的数比较，不同则返回 false
+// 使用一个哈希表保存宝石种类,
+// 再判断 S 中的每个字符代表的是否宝石。
 
 // Submission Result: Accepted
