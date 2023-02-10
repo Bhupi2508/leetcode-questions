b39caa3ee007032fc8ecc37d3e289b00265f0bdd
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-10 22:45:13 +0530

    35 :::: 79-wordSearch.js question updated

--- dsa.js
+++ dsa.js
@@ -1,27 +1,31 @@
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
  */
-var subsets = function(nums) {
-    nums.sort(function(a, b) {
-       return a - b;
-    });
-    var result = [];
-    var results = [result];
-    subsetsHelper(0, nums, result, results);
-    return results;
-};
-
-var subsetsHelper = function(start, nums, result, results) {
-    if (start === nums.length) {
-        return;
+var exist = function(board, word) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
+            if (existHelper(board, word, i, j, 0)) return true;
+        }
     }
 
-    for (var i = start; i < nums.length; i++) {
-        result.push(nums[i]);
-        results.push(result.slice());
-        // start from next element i, not the next element of 'start'
-        subsetsHelper(i + 1, nums, result, results);
-        result.pop(nums[i]);
+    return false;
+};
+
+// if the word[k] existing in board, keep searching up, down, left, right 
+var existHelper = function(board, word, i, j, k) {
+    if (k === word.length) return true;
+    if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) return false;
+    if (board[i][j] === word[k]) {
+        var tmp = board[i][j];
+        board[i][j] = '#';
+        if (existHelper(board, word, i + 1, j, k + 1) ||
+            existHelper(board, word, i - 1, j, k + 1) ||
+            existHelper(board, word, i, j + 1, k + 1) ||
+            existHelper(board, word, i, j - 1, k + 1)) {
+              return true;
+        }
+        board[i][j] = tmp;
     }
 };
