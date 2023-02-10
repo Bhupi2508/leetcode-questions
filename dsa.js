1c6571debc10701ae7b3fa0fa4a382cd1127745d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-10 22:53:17 +0530

    35 :::: 0455-assign-cookies.js leetcode question updated (2023-02-10)

--- dsa.js
+++ dsa.js
@@ -1,49 +1,69 @@
-// 453. Minimum Moves to Equal Array Elements
+// 455. Assign Cookies
 // Easy   47%
 
 
-// Given a non-empty integer array of size n, find the minimum number of moves
-// required to make all array elements equal, where a move is incrementing n - 1
-// elements by 1.
+// Assume you are an awesome parent and want to give your children some cookies.
+// But, you should give each child at most one cookie. Each child i has a greed
+// factor gi, which is the minimum size of a cookie that the child will be
+// content with; and each cookie j has a size sj. If sj >= gi, we can assign the
+// cookie j to the child i, and the child i will be content. Your goal is to
+// maximize the number of your content children and output the maximum number.
 
-// Example:
+// Note:
+// You may assume the greed factor is always positive.
+// You cannot assign more than one cookie to one child.
 
-// Input:
-// [1,2,3]
+// Example 1:
 
-// Output:
-// 3
+// Input: [1,2,3], [1,1]
 
-// Explanation:
-// Only three moves are needed (remember each move increments two elements):
+// Output: 1
 
-// [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+// Explanation: You have 3 children and 2 cookies. The greed factors of 3
+// children are 1, 2, 3.
+// And even though you have 2 cookies, since their size is both 1, you could only
+// make the child whose greed factor is 1 content.
+// You need to output 1.
+
+// Example 2:
+
+// Input: [1,2], [1,2,3]
+
+// Output: 2
+
+// Explanation: You have 2 children and 3 cookies. The greed factors of 2
+// children are 1, 2.
+// You have 3 cookies and their sizes are big enough to gratify all of the
+// children,
+// You need to output 2.
 
 
 /**
- * @param {number[]} nums
+ * @param {number[]} g
+ * @param {number[]} s
  * @return {number}
  */
-const minMoves = function(nums) {
-  nums.sort((a, b) => b - a)
+const findContentChildren = function(g, s) {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+  const m = g.length, n = s.length
   let result = 0
-  for (let i = 1, n = nums.length; i < n; i++) {
-    result += (nums[i - 1] - nums[i]) * i
+  for (let i = 0; result < m && i < n; i++) {
+    if (g[result] <= s[i]) result++
   }
   return result
 }
 
 ;[
-  [1,2,3],                      // 3
-  [1,3,6],                      // 7
-  [3,2,7,5],                    // 9
-].forEach(nums => {
-  console.log(minMoves(nums))
+  [[1,2,3], [1,1]],             // 1
+  [[1,2], [1,2,3]],             // 2
+  [[4,4,2,3], [1,1,1]],         // 0
+].forEach(args => {
+  console.log(findContentChildren(...args))
 })
 
 // Solution:
-// 先按降序排序，使元素不断向前一个元素变化。
-// 每完成一个元素的变化，该元素之前的元素都会变成一样的元素。
-// 假设当前元素与前一个元素的差为 a，且该元素前有 k 个元素，则需要变化 a x k 次。
+// 给两个数组排序，这样孩子就能从最小贪婪的开始满足，饼干可以从最小的开始分发。
+// 孩子能得到满足贪婪的最小的饼干。
 
 // Submission Result: Accepted
