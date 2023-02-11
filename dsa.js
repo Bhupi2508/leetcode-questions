6228c45de8eff04ca6638173e346e61e44276eb5
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-11 22:52:35 +0530

    35 :::: 0103-binary-tree-zigzag-level-order-traversal.js leetcode question updated (2023-02-11)

--- dsa.js
+++ dsa.js
@@ -1,8 +1,9 @@
-// 102. Binary Tree Level Order Traversal
-// Medium 40% locked:false
+// 103. Binary Tree Zigzag Level Order Traversal
+// Medium 35% locked:false
 
-// Given a binary tree, return the level order traversal of its nodes' values.
-// (ie, from left to right, level by level).
+// Given a binary tree, return the zigzag level order traversal of its nodes'
+// values. (ie, from left to right, then right to left for the next level and
+// alternate between).
 
 // For example:
 // Given binary tree [3,9,20,null,null,15,7],
@@ -13,13 +14,14 @@
 //     /  \
 //    15   7
 
-// return its level order traversal as:
+// return its zigzag level order traversal as:
 
 // [
 //   [3],
-//   [9,20],
+//   [20,9],
 //   [15,7]
 // ]
+
 
 /**
  * Definition for a binary tree node.
@@ -29,16 +31,20 @@
  * }
  */
 
+
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const levelOrder = function(root) {
+const zigzagLevelOrder = function(root) {
   const res = []
   function iter(root, level) {
     if (root) {
       if (!res[level]) res[level] = []
-      res[level].push(root.val)
+
+      if (level % 2) res[level].unshift(root.val)
+      else res[level].push(root.val)
+
       iter(root.left, level + 1)
       iter(root.right, level + 1)
     }
@@ -50,12 +56,13 @@ const levelOrder = function(root) {
 
 const TreeNode = require('../structs/TreeNode')
 ;[
-  [3,9,20,null,null,15,7], // [ [ 3 ], [ 9, 20 ], [ 15, 7 ] ]
+  [3,9,20,null,null,15,7], // [ [ 3 ], [ 20, 9 ], [ 15, 7 ] ]
 ].forEach(array => {
-  console.log(levelOrder(TreeNode.from(array)))
+  console.log(zigzagLevelOrder(TreeNode.from(array)))
 })
 
 // Solution:
 // 递归遍历，遍历过程中，带上层级参数 level ，根据 level 来插入对应数组。
+// 插入的时候，根据 level 的奇偶性来判断插入头或尾。
 
 // Submission Result: Accepted
