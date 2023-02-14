08d1c91a9c14a8f667190d70327b1226e6b23641
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-14 22:53:02 +0530

    36 :::: 0292-nim-game.js leetcode question updated (2023-02-14)

--- dsa.js
+++ dsa.js
@@ -1,64 +1,39 @@
-// 290. Word Pattern
-// Easy   33%
+// 292. Nim Game
+// Easy   55%
 
 
-// Given a pattern and a string str, find if str follows the same pattern.
+// You are playing the following Nim Game with your friend: There is a heap of
+// stones on the table, each time one of you take turns to remove 1 to 3 stones.
+// The one who removes the last stone will be the winner. You will take the first
+// turn to remove the stones.
 
-//  Here follow means a full match, such that there is a bijection between a
-// letter in pattern and a non-empty word in str.
+// Both of you are very clever and have optimal strategies for the game. Write a
+// function to determine whether you can win the game given the number of stones
+// in the heap.
 
-// Examples:
+// For example, if there are 4 stones in the heap, then you will never win the
+// game: no matter 1, 2, or 3 stones you remove, the last stone will always be
+// removed by your friend.
 
-// pattern = "abba", str = "dog cat cat dog" should return true.
-// pattern = "abba", str = "dog cat cat fish" should return false.
-// pattern = "aaaa", str = "dog cat cat dog" should return false.
-// pattern = "abba", str = "dog dog dog dog" should return false.
-
-// Notes:
-// You may assume pattern contains only lowercase letters, and str contains
-// lowercase letters separated by a single space.
-
-// Credits:Special thanks to @minglotus6 for adding this problem and creating all
-// test cases.
+// Credits:Special thanks to @jianchao.li.fighter for adding this problem and
+// creating all test cases.
 
 
 /**
- * @param {string} pattern
- * @param {string} str
+ * @param {number} n
  * @return {boolean}
  */
-const wordPattern = function(pattern, str) {
-  str = str.split(' ')
-  const n = pattern.length, m = str.length
-  if (n !== m) return false
-
-  const hash = {}
-  for (let i = 0; i < n; i++) {
-    if (!hash[pattern[i]]) {
-      if (hash['A' + str[i]]) return false
-      hash[pattern[i]] = str[i]
-      hash['A' + str[i]] = [pattern[i]]
-    }
-    if (hash[pattern[i]] !== str[i]) return false
-  }
-  return true
+const canWinNim = function(n) {
+  return !!(n % 4)
 }
 
 ;[
-  ['abba', 'dog cat cat dog'],  // true
-  ['abba', 'dog cat cat fish'], // false
-  ['aaaa', 'dog cat cat dog'],  // false
-  ['abba', 'dog dog dog dog'],  // false
-  ['abc', 'b c a'],             // true
-].forEach(args => {
-  console.log(wordPattern(...args))
+  4,                            // false
+].forEach(n => {
+  console.log(canWinNim(n))
 })
 
-
 // Solution:
-// 先将 str 字符串变为字符串数组。
-// 在遍历 str 和 pattern 时，一边建立双射连接一边匹配。
-// 为了避免 pattern 的某个字符与 str 中的某个字符 相同，
-// 因此在建立 str 的映射时，让其加上一个不会出现的字符，以便区分。
+// 如果是 4 的倍数，就不可能赢。
 
 // Submission Result: Accepted
