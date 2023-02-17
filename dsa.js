3a42bab6946d9ce8c7beddf13170a7cd8df129e2
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-17 22:52:10 +0530

    36 :::: 0034-find-first-and-last-position-of-element-in-sorted-array.js leetcode question updated (2023-02-17)

--- dsa.js
+++ dsa.js
@@ -1,63 +1,65 @@
-// 33. Search in Rotated Sorted Array
-// Medium   32%
+// 34. Search for a Range
+// Medium   31%
 
-// Suppose an array sorted in ascending order is rotated at some pivot unknown
-// to you beforehand.
+// Given an array of integers sorted in ascending order, find the starting and
+// ending position of a given target value.
 
-// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+// Your algorithm's runtime complexity must be in the order of O(log n).
 
-// You are given a target value to search. If found in the array return its
-// index, otherwise return -1.
+// If the target is not found in the array, return [-1, -1].
 
-// You may assume no duplicate exists in the array.
+// For example,
+// Given [5, 7, 7, 8, 8, 10] and target value 8,
+// return [3, 4].
 
 /**
  * @param {number[]} nums
  * @param {number} target
- * @return {number}
+ * @return {number[]}
  */
-const search = function(nums, target) {
-  const iter = (i, j) => {
-    if (i > j) return -1
-
-    const mid = (i + j) >> 1,
-          rotate = nums[mid] > nums[j] // Is rotate point in the back of mid ?
-    if (target < nums[mid]) {
-      return Math.max(iter(i, mid - 1), rotate ? iter(mid + 1, j) : -1)
-    } else if (target > nums[mid]) {
-      return Math.max(rotate ? -1 : iter(i, mid - 1), iter(mid + 1, j))
-    } else return mid
+const searchRange = function(nums, target) {
+  const n = nums.length, result = [-1, -1]
+  let i = 0, j = n - 1
+  while (i < j) {
+    const mid = (i + j) >> 1
+    if (nums[mid] < target) i = mid + 1
+    else j = mid
   }
+  if (nums[i] !== target) return result
 
-  return iter(0, nums.length - 1)
+  result[0] = i
+  j = n - 1
+  while (i < j) {
+    const mid = ((i + j) >> 1) + 1
+    if (nums[mid] > target) j = mid - 1
+    else i = mid
+  }
+  result[1] = j
+  return result
 }
 
 ;[
-  [[4,5,6,7,0,1,2], 0],         // 4
+  [[5, 7, 7, 8, 8, 10], 8],     // [3, 4]
+  [[5, 7, 7, 8, 8, 10], 9],     // [-1, -1]
 ].forEach(args => {
-  console.log(search(...args))
+  console.log(searchRange(...args))
 })
 
 // Solution:
-// 因为已经排好序，因此使用二分查找法，
-// 由于是旋转过，需要考虑两种情况，
-// 1. 旋转中心在中点后面
-// 2. 旋转中心在中点前面
+// 使用两次二分查找法，分别找出左位置和右位置。
 
-// 通过比较中点数与最后一个数的大小来区分这两种情况
-// 中点数大，则是情况1； 若小，则是情况2。
+// 找最左位置：
+// 每次检查中位数。
+// 若小于则从其右边一位开始再找；
+// 若大于或等于，则从该位及其左边的开始再找；
+// 直到左右边界相等。
+// 因为要找最左的目标数，找到等于时还不能结束，
+// 左边可能还存在目标数，所以等于时还要继续找。
+// 第一次找到目标数后，其右边界就必然一直都会是目标数
+// 左边界会一直排除非目标数，当左右边界重合时，其位置就是最左的
 
-// 在不断递归的过程中，还会出现查找数组是没有旋转过的。
-
-// 而二分查找又有3种情况
-// 1. 中点数等于目标数
-// 2.      大于
-// 3.      小于
-
-// 小于+情况1，需要分别递归左右两边
-// 小于+情况2，只需要递归左边
-
-// 大于+情况1，只需要递归右边
-// 大于+情况2，需要分别递归左右两边
+// 找最右位置：
+// 其左边界从前面找到的最左位置开始，因为前面的都没有必要再看了。
+// 其余与找最左位置类似。
 
 // Submission Result: Accepted
