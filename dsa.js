a380e93a77fd28a173e535690db3b4a8ff1be47c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-23 22:44:40 +0530

    37 :::: 12-integerToRoman.js question updated

--- dsa.js
+++ dsa.js
@@ -1,50 +1,59 @@
 /**
- * Definition for binary tree with next pointer.
- * function TreeLinkNode(val) {
- *     this.val = val;
- *     this.left = this.right = this.next = null;
- * }
+ * @param {number} num
+ * @return {string}
  */
-
-/**
- * Key: if a node is a parent's left child, then this node's next is its parent's right child node.
- * if a node is a parent node's right child, then this node's next is its parent's next node's left node.
- * (the parent node should have next child). The trick part is how to write code.
- *
- * @param {TreeLinkNode} root
- * @return {void} Do not return anything, modify tree in-place instead.
- */
-var connect = function(root) {
-    if (!root) return;
-    while (root.left) {
-        // track every level, starts from the left
-        var levelP = root;
-        while (levelP) {
-            levelP.left.next = levelP.right;
-            if (levelP.next) levelP.right.next = levelP.next.left;
-            levelP = levelP.next;
-        }
-        root = root.left;
-    }
+var intToRoman = function(num) {
+    var M = ["", "M", "MM", "MMM"];
+    var C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    var X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    var I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    return M[Math.floor(num/1000)] + C[Math.floor((num%1000)/100)] + X[Math.floor((num%100)/10)] + I[num%10];
 };
 
-// second try, slow, but easy understanding
-// the space is not constant
-var connect = function(root) {
-    if (!root) return;
-    root.next = null;
-    var queue = [root];
-    while (queue.length > 0) {
-        var node = queue.shift();
-        if (node.left) {
-            node.left.next = node.right;
-            queue.push(node.left);
+
+// this is awkward because the order of object keys is not reserved
+var intToRoman = function(num) {
+    var map = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    };
+    var romanStr = '';
+
+    Object.keys(map).sort(function(a, b) {
+        return b - a;
+    }).forEach(function (val) {
+        while (num >= val) {
+            num -= val;
+            romanStr += map[val];
         }
-        if (node.right) {
-            if (node.next) {
-                node.right.next = node.next.left;
-            }
-            queue.push(node.right);
+    });
+
+    return romanStr;
+};
+
+// although you can use two arrays to do it.
+var intToRoman = function(num) {
+    var vals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var strs = ['M', 'CM', 'D', 'CD', 'C','XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var romanStr = '';
+
+    vals.forEach(function (val, index) {
+        while (num >= val) {
+            num -= val;
+            romanStr += strs[index];
         }
-    }
+    });
+
+    return romanStr;
 };
