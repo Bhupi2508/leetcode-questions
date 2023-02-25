03f5a5681a97087b5613d612de2b61c56aec8716
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-25 22:53:03 +0530

    37 :::: 0326-power-of-three.js leetcode question updated (2023-02-25)

--- dsa.js
+++ dsa.js
@@ -1,62 +1,39 @@
-// 303. Range Sum Query - Immutable
-// Easy   30%
+// 326. Power of Three
+// Easy   40%
 
 
-// Given an integer array nums, find the sum of the elements between indices i
-// and j (i ≤ j), inclusive.
+// Given an integer, write a function to determine if it is a power of three.
 
-// Example:
+// Follow up:
+// Could you do it without using any loop / recursion?
 
-// Given nums = [-2, 0, 3, -5, 2, -1]
-
-// sumRange(0, 2) -> 1
-// sumRange(2, 5) -> -1
-// sumRange(0, 5) -> -3
-
-// Note:
-
-// You may assume that the array does not change.
-// There are many calls to sumRange function.
+// Credits:Special thanks to @dietpepsi for adding this problem and creating all
+// test cases.
 
 
 /**
- * @param {number[]} nums
+ * @param {number} n
+ * @return {boolean}
  */
-const NumArray = function(nums) {
-  for (let i = 1, n = nums.length; i < n; i++) {
-    nums[i] += nums[i - 1]
-  }
-  this.nums = nums
+const isPowerOfThree = function(n) {
+  return n > 0 && Math.pow(3, Math.trunc(Math.log(n) / Math.log(3))) === n
 }
 
-/**
- * @param {number} i
- * @param {number} j
- * @return {number}
- */
-NumArray.prototype.sumRange = function(i, j) {
-  return i ? this.nums[j] - this.nums[i - 1] : this.nums[j]
-}
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * var obj = Object.create(NumArray).createNew(nums)
- * var param_1 = obj.sumRange(i,j)
- */
-
-
-const obj = new NumArray([-2, 0, 3, -5, 2, -1])
 ;[
-  [0, 2],                       // 1
-  [2, 5],                       // -1
-  [0, 5],                       // -3
-].forEach(args => {
-  console.log(obj.sumRange(...args))
+  0,                            // false
+  3,                            // true
+  81,                           // true
+].forEach(n => {
+  console.log(isPowerOfThree(n))
 })
 
-// Solution:
 
-// 计算多次某个数组的某子序列和时，可以只保存当前数的累积。
-// 求 i->j 的和时，只需要计算 0->j 的累积 减去 0->i 的累积。
+// Solution:
+// 假设 3^m = n_um，当 m 为整数时，n_um 为 以4为底数的幂
+// 两边取以3为底的对数 log_3(3 ^ m) = log_3(n)
+// m = log_3(n)
+// m = log_e(n) / log_e(3)，因为没有合适的取对数函数，只能换用 e 为底的
+// 但是这样得到的 n，和正确的结果有极小的误差。
+// 因此需要对 m 取整再比较 3^m 是否等于 n
 
 // Submission Result: Accepted
