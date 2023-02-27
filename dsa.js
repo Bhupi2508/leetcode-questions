7b56dfa4dc184f4904481ece266706d776d4256d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-27 22:53:36 +0530

    38 :::: 0590-n-ary-tree-postorder-traversal.js leetcode question updated (2023-02-27)

--- dsa.js
+++ dsa.js
@@ -1,8 +1,8 @@
-// 589. N-ary Tree Preorder Traversal
+// 590. N-ary Tree Postorder Traversal
 // Easy   71%
 
 
-// Given an n-ary tree, return the preorder traversal of its nodes' values.
+// Given an n-ary tree, return the postorder traversal of its nodes' values.
 // Nary-Tree input serialization is represented in their level order traversal,
 // each group of children is separated by the null value (See examples).
 
@@ -11,11 +11,11 @@
 
 // Example 1:
 // Input: root = [1,null,3,2,4,null,5,6]
-// Output: [1,3,5,6,2,4]
+// Output: [5,6,3,2,4,1]
 // Example 2:
 // Input: root =
 // [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
-// Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+// Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
 
 // Constraints:
 //     The height of the n-ary tree is less than or equal to 1000
@@ -24,7 +24,7 @@
 
 /**
  * // Definition for a Node.
- * function Node(val, children) {
+ * function Node(val,children) {
  *this.val = val;
  *    this.children = children;
  * };
@@ -34,17 +34,14 @@
  * @param {Node} root
  * @return {number[]}
  */
-const preorder = function(root) {
+const postorder = function(root) {
+  if (root === null) return []
   const res = []
-  function preorder(node) {
-    if (node) {
-      res.push(node.val)
-      for (let c of node.children) {
-        preorder(c)
-      }
-    }
+  function postorder(node) {
+    for (let c of node.children) postorder(c)
+    res.push(node.val)
   }
-  preorder(root)
+  postorder(root)
   return res
 }
 
@@ -55,8 +52,8 @@ const preorder = function(root) {
 })
 
 // Solution:
-// 1. 前序遍历，递归即可。
+// 1. 后序遍历，递归
 // 2. 使用栈
-// TODO: #589 多叉树的构造函数
+// TODO: #590 多叉树的构造函数
 
 // Submission Result: Accepted
