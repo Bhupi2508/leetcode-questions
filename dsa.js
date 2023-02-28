ad34b2ed7ea9c7199741e99d18f03efe719cdc13
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-02-28 22:53:29 +0530

    38 :::: 0538-convert-bst-to-greater-tree.js leetcode question updated (2023-02-28)

--- dsa.js
+++ dsa.js
@@ -1,60 +1,56 @@
-// 537. Complex Number Multiplication
-// Medium   63%
+// 538. Convert BST to Greater Tree
+// Easy   49%
 
-// Given two strings representing two complex numbers.
 
-// You need to return a string representing their multiplication. Note i^2 = -1
-// according to the definition.
+// Given a Binary Search Tree (BST), convert it to a Greater Tree such that every
+// key of the original BST is changed to the original key plus sum of all keys
+// greater than the original key in BST.
 
-// Example 1:
+// Example:
 
-// Input: "1+1i", "1+1i"
-// Output: "0+2i"
-// Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it
-// to the form of 0+2i.
+// Input: The root of a Binary Search Tree like this:
+//               5
+//             /   \
+//            2     13
 
-// Example 2:
-
-// Input: "1+-1i", "1+-1i"
-// Output: "0+-2i"
-// Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it
-// to the form of 0+-2i.
-
-// Note:
-
-// The input strings will not have extra blank.
-// The input strings will be given in the form of a+bi, where the integer a and b
-// will both belong to the range of [-100, 100]. And the output should be also in
-// this form.
+// Output: The root of a Greater Tree like this:
+//              18
+//             /   \
+//           20     13
 
 
 /**
- * @param {string} a
- * @param {string} b
- * @return {string}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
  */
-const complexNumberMultiply = function(a, b) {
-  function getRI(s) {
-    const RI = s.slice(0, s.length - 1).split('+')
-    return [parseInt(RI[0]), parseInt(RI[1])]
-  }
 
-  const [aR, aI] = getRI(a), [bR, bI] = getRI(b)
-  return (aR * bR - aI * bI) + '+' + (aR * bI + aI * bR) + 'i'
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const convertBST = function(root) {
+  function iter(root, sum) {
+    if (root == void 0) return sum
+    root.val += iter(root.right, sum)
+    return iter(root.left, root.val)
+  }
+  iter(root, 0)
+  return root
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  ['1+1i', '1+1i'],             // '0+2i'
-  ['1+-1i', '1+-1i'],           // '0+-2i'
-].forEach(args => {
-  console.log(complexNumberMultiply(...args))
+  [5,2,13],            // [18,20,13]
+].forEach((array) => {
+  console.log(convertBST(TreeNode.from(array)))
 })
 
 // Solution:
-//   (a+bi)(c+di)
-// = a*c + a*di + bi*c + bi*di
-// = a*c + (a*d)i + (b*c)i + (i*i)*b*d
-// = a*c + (a*d)i + (b*c)i - b*d
-// = (a*c - b*d) + (a*d + b*c)i
+// 对树使用右边优先的中序遍历，
+// 并且每次都让后一个被遍历的节点的值加上前一个的。
+// 使用参数 sum 作为前一个值进行传递。
 
 // Submission Result: Accepted
