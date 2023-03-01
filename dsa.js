e0e3f95e9a1268efa2475184ad3fbc1df84606fa
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-01 22:54:47 +0530

    38 :::: 1287-element-appearing-more-than-25-in-sorted-array.js leetcode question updated (2023-03-01)

--- dsa.js
+++ dsa.js
@@ -1,52 +1,82 @@
-// 1281. Subtract the Product and Sum of Digits of an Integer
-// Easy   85%
+// 1287. Element Appearing More Than 25% In Sorted Array
+// Easy   60%
 
 
-// Given an integer number n, return the difference between the product of its
-// digits and the sum of its digits.
+// Given an integer array sorted in non-decreasing order, there is exactly one
+// integer in the array that occurs more than 25% of the time.
+// Return that integer.
 
 // Example 1:
-// Input: n = 234
-// Output: 15
-// Explanation:
-// Product of digits = 2 * 3 * 4 = 24
-// Sum of digits = 2 + 3 + 4 = 9
-// Result = 24 - 9 = 15
-// Example 2:
-// Input: n = 4421
-// Output: 21
-// Explanation:
-// Product of digits = 4 * 4 * 2 * 1 = 32
-// Sum of digits = 4 + 4 + 2 + 1 = 11
-// Result = 32 - 11 = 21
+// Input: arr = [1,2,2,6,6,6,6,7,10]
+// Output: 6
 
 // Constraints:
-//     1 <= n <= 10^5
+//     1 <= arr.length <= 10^4
+//     0 <= arr[i] <= 10^5
 
 
 /**
- * @param {number} n
+ * @param {number[]} arr
  * @return {number}
  */
-const subtractProductAndSum = function(n) {
-  let p = 1, s = 0, d = 0
-  while (n) {
-    d = n % 10
-    p *= d
-    s += d
-    n = Math.floor(n / 10)
+const findSpecialInteger = function(arr) {
+  const minLength = Math.floor(arr.length / 4)
+  for (let i = 0; i < arr.length - minLength; i++) {
+    if (arr[i] === arr[i + minLength]) return arr[i]
   }
-  return p - s
+  return arr[0]
+}
+
+const recursion = function(arr) {
+  const n = arr.length
+  if (n === 1) return arr[0]
+
+  const minLength = Math.floor(n / 4) + 1
+  
+  function find(p, q) {
+    if (q === p || q - p < minLength - 1) return -1
+    if (q - p === minLength - 1) return arr[p] === arr[q] ? arr[p] : -1
+
+    const mid = Math.floor((p + q) / 2)
+
+    const left = find(p, mid)
+    if (left > 0) return left
+
+    const right = find(mid + 1, q)
+    if (right > 0) return right
+
+    for (let i = 0; i < minLength; i++) {
+      if (arr[mid - i] === arr[mid - i + minLength - 1]) {
+        return arr[mid - i]
+      }
+    }
+    return -1
+  }
+
+  return find(0, n - 1)
 }
 
 ;[
-  234, // 15
-  4421, // 21
-].forEach((n) => {
-  console.log(subtractProductAndSum(n))
+  [1],                         // 1
+  [0,0,1],                     // 0
+  [0,1,1,2],                   // 1
+  [1,2,3,3],                   // 3
+  [1,2,2,6,6,6,6,7,10],        // 6
+  [0,0,0,1,1,1,2,2,2,3,3,3,3], // 3
+  [1,1,2,2,3,3,3,3]            // 3
+].forEach((arr) => {
+  console.log(findSpecialInteger(arr))
 })
 
 // Solution:
-// 从数的末位开始算即可。
+// 算法一： 遍历一遍
+// 先根据 arr 长度计算 minLength (至少需要几个数是相同的)
+// 因为数组已经排序，
+// 遍历数组时，比较 arr[i] 和 arr[i + minLength]，相同即可返回该数
+
+// 算法二： 递归
+// 强行二分递归，算法难度增加，时间空间复杂度也不好计算
+// 编码时间增加，运行速度都不一定增加
+// 算法能力增强前，尽量写这样的算法
 
 // Submission Result: Accepted
