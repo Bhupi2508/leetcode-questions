1339ce353dc591e6e7b23a391c55daa2b70b846d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-01 22:53:12 +0530

    38 :::: 0414-third-maximum-number.js leetcode question updated (2023-03-01)

--- dsa.js
+++ dsa.js
@@ -1,67 +1,66 @@
-// 413. Arithmetic Slices
-// Medium   55%
-
-
-// A sequence of number is called arithmetic if it consists of at least three
-// elements and if the difference between any two consecutive elements is the
-// same.
-
-// For example, these are arithmetic sequence:
-
-// 1, 3, 5, 7, 9
-// 7, 7, 7, 7
-// 3, -1, -5, -9
-
-// The following sequence is not arithmetic.
-
-// 1, 1, 2, 5, 7
-
-
-// A zero-indexed array A consisting of N numbers is given. A slice of that
-// array is any pair of integers (P, Q) such that 0 <= P < Q < N.
-
-// A slice (P, Q) of array A is called arithmetic if the sequence:
-// A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means
-// that P + 1 < Q.
-
-// The function should return the number of arithmetic slices in the array A.
-
-// Example:
-
-// A = [1, 2, 3, 4]
-
-// return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3,
-// 4] itself.
-
-
-/**
- * @param {number[]} A
- * @return {number}
- */
-const numberOfArithmeticSlices = function(A) {
-  const n = A.length
-  let result = 0
-  for (let i = 1; i < n; i++) {
-    let count = 0
-    while (A[i] - A[i - 1] === A[i + 1] - A[i]) {
-      count++
-      i++
-    }
-    result += ((count + 1) * count / 2)
-  }
-  return result
-}
-
-;[
-  [1, 2, 3, 4],                 // 3
-  [1, 2, 3, 4, 5, 6, 9, 12],    // 11
-].forEach(A => {
-  console.log(numberOfArithmeticSlices(A))
-})
-
-// Solution:
-// 计算每个能够组成算术数列的最长子数组。
-// 该最长子数组中能够找到 (1+a)*a/2 个算术数列子数组，其中 a为长度减2。
-// 按照该思想实现该函数。
-
-// Submission Result: Accepted
+// 414. Third Maximum Number
+// Easy   27%
+
+
+// Given a non-empty array of integers, return the third maximum number in this
+// array. If it does not exist, return the maximum number. The time complexity
+// must be in O(n).
+
+// Example 1:
+
+// Input: [3, 2, 1]
+
+// Output: 1
+
+// Explanation: The third maximum is 1.
+
+// Example 2:
+
+// Input: [1, 2]
+
+// Output: 2
+
+// Explanation: The third maximum does not exist, so the maximum (2) is returned
+// instead.
+
+// Example 3:
+
+// Input: [2, 2, 3, 1]
+
+// Output: 1
+
+// Explanation: Note that the third maximum here means the third maximum distinct
+// number.
+// Both numbers with value 2 are both considered as second maximum.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const thirdMax = function(nums) {
+  let first = null, second = null, third = null
+  for (let i = 0, n = nums.length; i < n; i++) {
+    const num = nums[i]
+    if (num === first || num === second || num === third) continue
+    if (third === null || num > third) third = num
+    if (second === null || num > second) [third, second] = [second, num]
+    if (first === null || num > first) [second, first] = [first, num]
+  }
+  return third === null ? first : third
+}
+
+;[
+  [3, 2, 1],                    // 1
+  [1, 2],                       // 2
+  [2, 2, 3, 1],                 // 1
+].forEach(nums => {
+  console.log(thirdMax(nums))
+})
+
+// Solution:
+// 用三个变量保存前三个最大的数。
+// 这三个变量像是一个优先队列一样，数字进来会把最小的数挤出去。
+
+
+// Submission Result: Accepted
