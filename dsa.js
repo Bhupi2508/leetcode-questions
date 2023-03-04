e0cce28e3c2f4424e819c7dbcce042bb729ecafa
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-04 22:52:12 +0530

    38 :::: 0038-count-and-say.js leetcode question updated (2023-03-04)

--- dsa.js
+++ dsa.js
@@ -1,79 +1,65 @@
-// 37. Sudoku Solver
-// Hard   30%
+// 38. Count and Say
+// Easy   35%
 
-// Write a program to solve a Sudoku puzzle by filling the empty cells.
-// Empty cells are indicated by the character '.'.
-// You may assume that there will be only one unique solution.
+// The count-and-say sequence is the sequence of integers with the first five
+// terms as following:
 
-// A sudoku puzzle...
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
 
-// ...and its solution numbers marked in red.
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+
+// Given an integer n, generate the nth term of the count-and-say sequence.
+
+// Note: Each term of the sequence of integers will be represented as a string.
+
+// Example 1:
+
+// Input: 1
+// Output: "1"
+
+// Example 2:
+
+// Input: 4
+// Output: "1211"
+
 
 /**
- * @param {character[][]} board
- * @return {void} Do not return anything, modify board in-place instead.
+ * @param {number} n
+ * @return {string}
  */
-const solveSudoku = function(board) {
-  const solve = function(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === '.') {
-          for (let c = 1; c <= 9; c++) {
-            if (isValid(board, i, j, c + '')) {
-              board[i][j] = c + ''
-              if(solve(board)) return true
-              else board[i][j] = '.'
-            }
-          }
-          return false
-        }
+const countAndSay = function(n) {
+  if (n < 1) return ''
+  let result = '1'
+  while (--n) {
+    let t = '', first = -1
+    for (let i = 0, m = result.length; i < m; i++) {
+      if (result[i] !== result[i + 1]) {
+        t += (i - first) + result[i]
+        first = i
       }
     }
-    return true
+    result = t
   }
-
-  const isValid = function(board, row, col, x) {
-    const trunc = Math.trunc,
-          p = trunc(row / 3) * 3,
-          q = trunc(col / 3) * 3
-    for (let i = 0; i < 9; i++) {
-      if (board[row][i] === x ||
-          board[i][col] === x ||
-          board[p + trunc(i / 3)][i % 3 + q] === x)
-        return false
-    }
-
-    return true
-  }
-
-  for (let i = 0; i < 9; i++) board[i] = board[i].split('')
-  solve(board)
-  for (let i = 0; i < 9; i++) board[i] = board[i].join('')
+  return result
 }
 
 ;[
-  [
-    "53..7....",
-    "6..195...",
-    ".98....6.",
-    "8...6...3",
-    "4..8.3..1",
-    "7...2...6",
-    ".6....28.",
-    "...419..5",
-    "....8..79"
-  ],
-].forEach(board => {
-  console.log(board)
-  solveSudoku(board)
-  console.log(board)
+  1,                            // '1'
+  2,                            // '11'
+  3,                            // '21'
+  4,                            // '1211'
+  5,                            // '111221'
+].forEach(n => {
+  console.log(countAndSay(n))
 })
 
 // Solution:
-// 深度遍历表
-// 每次碰到点（空格位），就测试该位可填的最小的数。
-// 测试时，只需测试某数在该位是否合法，而不需要测试整个表。
-// 当遇到某个位置没有任何数可填时，则回溯到上一个数，上一个数进行改变再测试。
-// 合适就继续，不行就回溯，直到将整个表填完。
+// 每次数一遍字符串。
 
 // Submission Result: Accepted
