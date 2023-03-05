935372bc6df9b61fb714bbf5d8ecb589b01b1750
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-05 22:52:06 +0530

    39 :::: 0023-merge-k-sorted-lists.js leetcode question updated (2023-03-05)

--- dsa.js
+++ dsa.js
@@ -1,51 +1,52 @@
-// 22. Generate Parentheses
-// Medium   45%
+// 23. Merge k Sorted Lists
+// Hard   27%
 
-// Given n pairs of parentheses, write a function to generate all combinations
-// of well-formed parentheses.
-
-// For example, given n = 3, a solution set is:
-
-// [
-//   "((()))",
-//   "(()())",
-//   "(())()",
-//   "()(())",
-//   "()()()"
-// ]
+// Merge k sorted linked lists and return it as one sorted list. Analyze and
+// describe its complexity.
 
 /**
- * @param {number} n
- * @return {string[]}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-const generateParenthesis = function(n) {
-  const result = []
-  function iter(s, open, close) {
-    if (close === 0) result.push(s)
-    if (open < close) iter(s + ')', open, close - 1)
-    if (open > 0) iter(s + '(', open - 1, close)
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+const mergeKLists = function(lists) {
+  if (lists == null || lists.length === 0) return null
+
+  const mergeTwoLists = (l1, l2) => {
+    if (l1 == null) return l2
+    if (l2 == null) return l1
+
+    if (l1.val > l2.val) {
+      l2.next = mergeTwoLists(l1, l2.next)
+      return l2
+    } else{
+      l1.next = mergeTwoLists(l1.next, l2)
+      return l1
+    }
   }
-  iter('', n, n)
-  return result
+
+  while(lists.length > 1) lists.push(mergeTwoLists(lists.shift(), lists.shift()))
+  return lists.shift()
 }
 
+const ListNode = require('../structs/ListNode')
 ;[
-  0,
-  1,
-  2,
-  3,
-].forEach(n => {
-  console.log(generateParenthesis(n))
+  [[1, 3], [0, 1], []],
+].forEach((lists) => {
+  lists = lists.map(l => ListNode.from(l))
+  console.log((mergeKLists(lists) || '').toString())
 })
 
 // Solution:
-// 从构造结果来看，在每个结果字符串中的前k个字符中，
-// 其左括号的数量都大于或等于其右括号的数量。
-// 因此构造过程中，只有左括号的数量超过右括号的数量时，才能添加右括号。
-
-// 使用一个分叉的迭代函数。
-// 参数为叠加的字符串和剩余的左右括号数量。
-// 只有剩余的右括号大于剩余的左括号时，添加右括号。
-// 否则只添加左括号。
+// 分治思想，在两个链表结合的时候，使用21-merge-two-sorted-lists.js中的方法。
+// 这里使用队列来实现分治。
+// 队列中的前两个链表结合成一个，并插入到队列末尾。
 
 // Submission Result: Accepted
