466f680ede9688ce5b02b5f8bebfded91be7e0d9
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-08 22:44:44 +0530

    39 :::: 139-wordBreak.js question updated

--- dsa.js
+++ dsa.js
@@ -1,26 +1,40 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {string} s
+ * @param {set<string>} wordDict
+ *   Note: wordDict is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {boolean}
  */
-// accepted, but not perfect
-var singleNumber = function(nums) {
-  var appears = {};
-  for (var i = 0; i < nums.length; i++) {
-    if (appears[nums[i]]) {
-      delete appears[nums[i]];
-    } else {
-      appears[nums[i]] = 1;
-    }
-  }
 
-  return parseInt(Object.keys(appears)[0]);
+// recursion, not accepted, time exceeds limits. O(N^2)
+var wordBreak = function(s, wordDict) {
+    return helper(s, wordDict, 0);
 };
 
-// a better solution: use XOR. XOR same number is 0
-var singleNumber = function(nums) {
-    var result = 0;
-    for (var i = 0; i < nums.length; i++) {
-        result = nums[i] ^ result;
+var helper = function(s, wordDict, start) {
+    if (start === s.length) return true;
+    // also let ... of is an ES6 feature
+    for (var word of wordDict) {
+        var wLength = word.length;
+        if (s.substring(start, start + wLength) === word) {
+            if (helper(s, wordDict, start + wLength)) return true;
+        }
     }
-    return result;
+    return false;
+};
+
+// Dynamic, accepted
+var wordBreak = function(s, wordDict) {
+    var canBreak = [true];
+    for (var i = 0; i < s.length; i++) {
+        if (!canBreak[i]) continue;
+        for (var word of wordDict) {
+            var wLength = word.length;
+            if (canBreak[i + wLength]) continue;
+            if (s.substring(i, i + wLength) === word) {
+                canBreak[i + wLength]  = true;
+            }
+        }
+    }
+    return canBreak[s.length] ? true : false;
 };
