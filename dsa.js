11d636ed7dc141a1399caed272c243fda34b4742
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-12 22:52:39 +0530

    40 :::: 0114-flatten-binary-tree-to-linked-list.js leetcode question updated (2023-03-12)

--- dsa.js
+++ dsa.js
@@ -1,26 +1,36 @@
-// 113. Path Sum II
-// Medium   34%
+// 114. Flatten Binary Tree to Linked List
+// Medium   35%
 
-// Given a binary tree and a sum, find all root-to-leaf paths where each path's
-// sum equals the given sum.
+// Given a binary tree, flatten it to a linked list in-place.
 
-// For example:
-// Given the below binary tree and sum = 22,
+// For example,
+// Given
 
-//               5
-//              / \
-//             4   8
-//            /   / \
-//           11  13  4
-//          /  \    / \
-//         7    2  5   1
+//     1
+//    / \
+//   2   5
+//  / \   \
+// 3   4   6
 
-// return
+// The flattened tree should look like:
 
-// [
-//    [5,4,11,2],
-//    [5,8,4,5]
-// ]
+//  1
+//   \
+//    2
+//     \
+//      3
+//       \
+//        4
+//         \
+//          5
+//           \
+//            6
+
+// click to show hints.
+//   Hints:
+
+// If you notice carefully in the flattened tree, each node's right child points
+// to the next node of a pre-order traversal.
 
 /**
  * Definition for a binary tree node.
@@ -32,35 +42,57 @@
 
 /**
  * @param {TreeNode} root
- * @param {number} sum
- * @return {number[][]}
+ * @return {void} Do not return anything, modify root in-place instead.
  */
-const pathSum = function(root, sum) {
-  const res = [], path = []
-  function iter (root, sum) {
-    if (root == null) return
-    if (root.val === sum && root.left == null && root.right == null) {
-      res.push([...path, root.val])
-    } else {
-      path.push(root.val)
-      sum -= root.val
-      iter(root.left, sum)
-      iter(root.right, sum)
-      path.pop()
+const flatten = function(root) {
+  if (root == null) return
+  function iter(tree) {
+    let tail = tree
+    const left = tree.left
+    const right = tree.right
+    if (left != null) {
+      tree.left = null
+      tree.right = left
+      tail = iter(left)
     }
+    if (right != null) {
+      tail.right = right
+      tail = iter(right)
+    }
+    return tail
   }
-  iter(root, sum)
-  return res
+  iter(root)
 }
 
 const TreeNode = require('../structs/TreeNode')
 ;[
-  [[5,4,8,11,null,13,4,7,2,null,null,5,1], 22], // [[5,4,11,2],[5,8,4,5]]
-].forEach(([array, sum]) => {
-  console.log(pathSum(TreeNode.from(array), sum))
+  [1,2,5,3,4,null,6],
+].forEach(array => {
+  const tree = TreeNode.from(array)
+  flatten(tree)
+  console.log(traverRight(tree))
 })
 
+function traverRight(root) {
+  let s = ''
+  while (root) {
+    s += (s ? '->' : s) + root.val
+    root = root.right
+  }
+  return s
+}
+
 // Solution:
-// 使用递归和回溯，将结果记录下来。
+// 采用前序递归遍历的方法。
+// 在递归过程中，已知当前根节点不为空，先保留左右子节点的指针，并将根节点保留在
+// 一个 tail 变量中。
+
+// 1. 若左子节点不为空，则设置左子节点为空，根的右子节点指向左子节点，然后递归平
+// 铺左子树，并将 tail 设置为平铺后的最后一个节点。
+
+// 2. 若右子节点不为空，则将其设置为 tail 的右子节点，然后递归平铺右子树，并将
+// tail 设置为平铺后的最后一个节点
+
+// 最后返回 tail 变量（即该树的最后一个右子节点）
 
 // Submission Result: Accepted
