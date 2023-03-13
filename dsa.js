2253929820a1c9bdcc58b50fde14a1b02a6a20e8
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-13 22:52:32 +0530

    40 :::: 0091-decode-ways.js leetcode question updated (2023-03-13)

--- dsa.js
+++ dsa.js
@@ -1,62 +1,51 @@
-// 90. Subsets II
-// Medium   36%
+// 91. Decode Ways
+// Medium 19% locked:false
 
-// Given a collection of integers that might contain duplicates, nums, return
-// all possible subsets.
+// A message containing letters from A-Z is being encoded to numbers using the
+// following mapping:
 
-// Note: The solution set must not contain duplicate subsets.
+// 'A' -> 1
+// 'B' -> 2
+// ...
+// 'Z' -> 26
+
+// Given an encoded message containing digits, determine the total number of
+// ways to decode it.
 
 // For example,
-// If nums = [1,2,2], a solution is:
+// Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 
-// [
-//   [2],
-//   [1],
-//   [1,2,2],
-//   [2,2],
-//   [1,2],
-//   []
-// ]
-
+// The number of ways decoding "12" is 2.
 
 /**
- * @param {number[]} nums
- * @return {number[][]}
+ * @param {string} s
+ * @return {number}
  */
-const subsetsWithDup = function(nums) {
-  nums.sort((a, b) => a - b)
-  const n = nums.length
-  const res = []
-  const iter = (array, m, i) => {
-    if (array.length === m) res.push([...array])
-    else {
-      for (let j = i; j < n; j++) {
-        if (j === i || nums[j] !== nums[j - 1]) {
-          array.push(nums[j])
-          iter(array, m, j + 1)
-          array.pop()
+const numDecodings = function(s) {
+  if (s.length === 0) return 0
+
+  const set = s.split(/\D/).filter(v => v.length !== 0)
+
+  let res = 0
+  for (let ns of set) {
+    const n = ns.length
+    let i = 0, prev = 0, curr = ns[0] === '0' ? 0 : 1
+    for (; i < n && ns[i] !== '0'; i++) {
+      if (ns[i + 1] === '0') {
+        i++
+        prev = curr
+      } else if (ns[i - 1] !== '0') {
+        if (i === 0 || ns[i - 1] + ns[i] - '0' <= 26) {
+          [curr, prev] = [curr + prev, curr]
+        } else {
+          prev = curr
         }
       }
     }
-  }
-
-  for (let i = 0; i <= n; i++) {
-    iter([], i, 0)
+    res += i === n ? curr : 0
   }
 
   return res
 }
 
-;[
-  [1, 2, 2],
-  [0, 2, 1, 2],
-].forEach(nums => {
-  console.log(subsetsWithDup(nums))
-})
-
-// Solution:
-// 先排序，使得相同的数字排在一起，以便能在选择的时候跳过相同的数字。
-// 同样像 78-subsets.js 类似的方法，即使用 DFS。
-// 区别是同一个位置上的数只替换不同的数。
-
-// Submission Result: Accepted
+console.log(numDecodings("1234212011"))
