360a874df84be233cf665d666cf55899f9a8d23a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-15 22:55:10 +0530

    40 :::: 1413-minimum-value-to-get-positive-step-by-step-sum.js leetcode question updated (2023-03-15)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,67 @@
-// 1408. String Matching in an Array
-// Easy   60%
+// 1413. Minimum Value to Get Positive Step by Step Sum
+// Easy   65%
 
 
-// Given an array of string words. Return all strings in words which is substring
-// of another word in any order.
-// String words[i] is substring of words[j], if can be obtained removing some
-// characters to left and/or right side of words[j].
+// Given an array of integers nums, you start with an initial positive value
+// startValue.
+// In each iteration, you calculate the step by step sum of startValue plus
+// elements in nums (from left to right).
+// Return the minimum positive value of startValue such that the step by step sum
+// is never less than 1.
 
 // Example 1:
-// Input: words = ["mass","as","hero","superhero"]
-// Output: ["as","hero"]
-// Explanation: "as" is substring of "mass" and "hero" is substring of
-// "superhero".
-// ["hero","as"] is also a valid answer.
+// Input: nums = [-3,2,-3,4,2]
+// Output: 5
+// Explanation: If you choose startValue = 4, in the third iteration your step by
+// step sum is less than 1.
+//                 step by step sum
+//                 startValue = 4 | startValue = 5 | nums
+//                   (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+//                   (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+//                   (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+//                   (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+//                   (4 +2 ) = 6  | (5 +2 ) = 7    |   2
 // Example 2:
-// Input: words = ["leetcode","et","code"]
-// Output: ["et","code"]
-// Explanation: "et", "code" are substring of "leetcode".
+// Input: nums = [1,2]
+// Output: 1
+// Explanation: Minimum start value should be positive.
 // Example 3:
-// Input: words = ["blue","green","bu"]
-// Output: []
+// Input: nums = [1,-2,-3]
+// Output: 5
 
 // Constraints:
-//     1 <= words.length <= 100
-//     1 <= words[i].length <= 30
-//     words[i] contains only lowercase English letters.
-//     It's guaranteed that words[i] will be unique.
+//     1 <= nums.length <= 100
+//     -100 <= nums[i] <= 100
 
 
 /**
- * @param {string[]} words
- * @return {string[]}
+ * @param {number[]} nums
+ * @return {number}
  */
-const stringMatching = function(words) {
-  let res = new Set()
-  for (let i = 1; i < words.length; i++) {
-    for (let j = 0; j < i; j++) {
-      let a = words[i], b = words[j]
-      if (a.length > b.length) [a, b] = [b, a]
-      if (b.indexOf(a) > -1) res.add(a)
+const minStartValue = function(nums) {
+  let res = sum = 1
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+    if (sum <= 0) {
+      res += -sum + 1
+      sum = 1
     }
   }
-  return [...res]
+  return res
 }
 
 ;[
-  ['mass','as','hero','superhero'],
-  ['leetcode', 'et', 'code'],
-  ['blue', 'grenn', 'bu'],
-  ["leetcoder","leetcode","od","hamlet","am"],
-  ["jak","yjakdn","tj","yyjakdn"], //["jak","yjakdn"]
-].forEach((words) => {
-  console.log(stringMatching(words))
+  [-3,2,-3,4,2], // 5
+  [1,2], // 1
+  [1,-2,-3], // 5
+  [-2,-3], // 6
+].forEach((nums) => {
+  console.log(minStartValue(nums))
 })
 
 // Solution:
-// 两两遍历，看长的字符串是否包含短的字符串。
-// 使用 Set 集合去重
+// 初始值为 res=1，当前总和为 sum=1
+// 遍历数组，计算当前总和
+// 当 sum <= 0 时，改变初始值，使 sum = 1
 
 // Submission Result: Accepted
