86308f25e11cd393bebda7bc8ddec14ec10c407c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-15 22:53:21 +0530

    40 :::: 0485-max-consecutive-ones.js leetcode question updated (2023-03-15)

--- dsa.js
+++ dsa.js
@@ -1,66 +1,42 @@
-// 482. License Key Formatting
-// Easy   43%
+// 485. Max Consecutive Ones
+// Easy   53%
 
 
-// You are given a license key represented as a string S which consists only
-// alphanumeric character and dashes. The string is separated into N+1 groups by
-// N dashes.
-// Given a number K, we would want to reformat the strings such that each group
-// contains exactly K characters, except for the first group which could be
-// shorter than K, but still must contain at least one character. Furthermore,
-// there must be a dash inserted between two groups and all lowercase letters
-// should be converted to uppercase.
-// Given a non-empty string S and a number K, format the string according to the
-// rules described above.
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+
 // Example 1:
-// Input: S = "5F3Z-2e-9-w", K = 4
-// Output: "5F3Z-2E9W"
-// Explanation: The string S has been split into two parts, each part has 4
-// characters.
-// Note that the two extra dashes are not needed and can be removed.
-// Example 2:
-// Input: S = "2-5g-3-J", K = 2
-// Output: "2-5G-3J"
-// Explanation: The string S has been split into three parts, each part has 2
-// characters except the first part as it could be shorter as mentioned above.
+
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+//     The maximum number of consecutive 1s is 3.
+
 // Note:
-// The length of string S will not exceed 12,000, and K is a positive integer.
-// String S consists only of alphanumerical characters (a-z and/or A-Z and/or
-// 0-9) and dashes(-).
-// String S is non-empty.
+
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
 
 
 /**
- * @param {string} S
- * @param {number} K
- * @return {string}
+ * @param {number[]} nums
+ * @return {number}
  */
-const licenseKeyFormatting = function(S, K) {
-  let res = ''
-  for (let i = S.length - 1, count = 0; i >= 0; i--) {
-    if (S[i] !== '-') {
-      if (count === 0 && res !== '') res = '-' + res
-      res = S[i].toUpperCase() + res
-      count = (count + 1) % K
-    }
+const findMaxConsecutiveOnes = function(nums) {
+  let result = 0, consecutive = 0
+  for (let num of nums) {
+    consecutive = num ? consecutive + 1 : 0
+    if (consecutive > result) result = consecutive
   }
-  return res
+  return result
 }
 
 ;[
-  ['5F3Z-2e-9-w', 4],
-  ['2-5g-3-J', 2],
-  ['1de0w-2nM-3MED', 3],
-  ["--a-a-a-a--", 2],
-].forEach(([S, K]) => {
-  console.log(licenseKeyFormatting(S, K))
+  [1,1,0,1,1,1],                // 3
+].forEach(nums => {
+  console.log(findMaxConsecutiveOnes(nums))
 })
 
 // Solution:
-// !!注意多个连续 - 符号的情况，别添加多余的 - 符号。
-
-// 使用 count 来记录当前一组的长度（不用也行，可以通过 res 计算）
-// 从后向前遍历，忽略 - 符号，每遍历一个字符，都转成大写，并添加到 res 头部。
-// 当 count == K 时，添加一个 - 符号到 res 头部，注意首尾处。
+// ...One pass, by a variable consecutive
 
 // Submission Result: Accepted
