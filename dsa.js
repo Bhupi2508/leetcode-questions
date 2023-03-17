46502232d9ec790ec657f1c298f586e2dc8d4a40
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-03-17 22:52:45 +0530

    40 :::: 0129-sum-root-to-leaf-numbers.js leetcode question updated (2023-03-17)

--- dsa.js
+++ dsa.js
@@ -1,19 +1,43 @@
-// 128. Longest Consecutive Sequence
-// Hard 37% locked:false
+// 129. Sum Root to Leaf Numbers
+// Medium 37% locked:false
 
-// Given an unsorted array of integers, find the length of the longest
-// consecutive elements sequence.
+// Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+// could represent a number.
+
+// An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+// Find the total sum of all root-to-leaf numbers.
 
 // For example,
-// Given [100, 4, 200, 1, 3, 2],
-// The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
-// Your algorithm should run in O(n) complexity.
+//   1
+//  / \
+// 2   3
+
+// The root-to-leaf path 1->2 represents the number 12.
+// The root-to-leaf path 1->3 represents the number 13.
+
+// Return the sum = 12 + 13 = 25.
 
 /**
- * @param {number[]} nums
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
  * @return {number}
  */
-const longestConsecutive = function(nums) {
-   
+const sumNumbers = function(root) {
+  if (root === null) return 0
+  const recursion = (tree, val) => {
+    const cur = val * 10 + tree.val
+    if (!tree.left && !tree.right) return cur
+    return (tree.left ? recursion(tree.left, cur) : 0) +
+      (tree.right ? recursion(tree.right, cur) : 0)
+  }
+
+  return recursion(root, 0)
 }
