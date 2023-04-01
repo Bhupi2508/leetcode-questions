11d30b6923c330fecf03f6a03e7bfc826d3b443f
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-01 22:53:15 +0530

    42 :::: 0443-string-compression.js leetcode question updated (2023-04-01)

--- dsa.js
+++ dsa.js
@@ -1,46 +1,94 @@
-// 442. Find All Duplicates in an Array
-// Medium   56%
+// 443. String Compression
+// Easy   37%
 
 
-// Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements
-// appear twice and others appear once.
+// Given an array of characters, compress it in-place.
 
-// Find all the elements that appear twice in this array.
+// The length after compression must always be smaller than or equal to the
+// original array.
 
-// Could you do it without extra space and in O(n) runtime?
+// Every element of the array should be a character (not int) of length 1.
 
-// Example:
+// After you are done modifying the input array in-place, return the new length
+// of the array.
+
+// Follow up:
+// Could you solve it using only O(1) extra space?
+
+// Example 1:
 
 // Input:
-// [4,3,2,7,8,2,3,1]
+// ["a","a","b","b","c","c","c"]
 
 // Output:
-// [2,3]
+// Return 6, and the first 6 characters of the input array should be:
+// ["a","2","b","2","c","3"]
+
+// Explanation:
+// "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+
+// Example 2:
+
+// Input:
+// ["a"]
+
+// Output:
+// Return 1, and the first 1 characters of the input array should be: ["a"]
+
+// Explanation:
+// Nothing is replaced.
+
+// Example 3:
+
+// Input:
+// ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+
+// Output:
+// Return 4, and the first 4 characters of the input array should be:
+// ["a","b","1","2"].
+
+// Explanation:
+// Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb"
+// is replaced by "b12".
+// Notice each digit has it's own entry in the array.
+
+// Note:
+
+// All characters have an ASCII value in [35, 126].
+// 1 <= len(chars) <= 1000.
 
 
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {character[]} chars
+ * @return {number}
  */
-const findDuplicates = function(nums) {
-  const n = nums.length
-  for (let i = 0; i < n;) {
-    const num = nums[i]
-    if (nums[num - 1] !== num) {
-      [nums[i], nums[num - 1]] = [nums[num - 1], num]
-    } else i++
+const compress = function(chars) {
+  for (let i = 1, count = 1; i <= chars.length; i++) {
+    if (chars[i] === chars[i - 1]) count++
+    else {
+      if (count > 1) {
+        i -= (count - 1)
+        chars.splice(i, count - 1, ...count.toString().split(''))
+        i += Math.trunc(Math.log10(count)) + 1
+      }
+      count = 1
+    }
   }
-  return nums.filter((v, i) => v !== i + 1)
+
+  return chars.length
 }
 
 ;[
-  [4,3,2,7,8,2,3,1],            // [2,3]
-].forEach(nums => {
-  console.log(findDuplicates(nums))
+  ["a","a","b","b","c","c","c"], // 6 ["a","2","b","2","c","3"]
+  ["a"],                         // 1 ["a"]
+  ["a","b","b","b","b","b","b","b","b","b","b","b","b"], // 4 ["a","b","1","2"]
+  ["a","b","b","b","c","c","c","a","a","c"], // 8 ["a","b","3","c","3","a","2","c"]
+].forEach(chars => {
+  console.log(compress(chars))
 })
 
 // Solution:
-// 将每个数送回正确的位置，如果该位置没有被相同的数字占领的话。
-// 再遍历归位后的数组，若数与位置不符，则时重复的。
+// 用变量 count 为连续相同的字符计数。
+// 如果遇到一个不同的，则将计数转换成字符。
 
 // Submission Result: Accepted
