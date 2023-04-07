3edea25ee50eb293d65ed4623b5fb6de603d4a18
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-07 22:45:02 +0530

    43 :::: 34-searchforRange.js question updated

--- dsa.js
+++ dsa.js
@@ -1,15 +1,31 @@
 /**
- * Solution: f(i) = f(i/2) + i % 2;
- * the bit nums = the bit nums of half of the number, and plus 1 if the last digit of the bit number is 1.
+ * Key: Two rounds of binary search, 1) first the left index, 2) find the right index
  *
- * @param {number} num
+ * @param {number[]} nums
+ * @param {number} target
  * @return {number[]}
  */
-var countBits = function(num) {
-    var result = [0];
-    for (var i = 1; i <= num; i++) {
-        result[i] = result[i >> 1] + (i & 1);
+var searchRange = function(nums, target) {
+    var lo = 0;
+    var hi = nums.length - 1;
+    var result = [-1, -1];
+
+    while (lo < hi) {
+        var mid = lo + Math.floor((hi - lo) / 2);
+        if (nums[mid] < target) lo = mid + 1;
+        else hi = mid;
     }
+    if (nums[lo] !== target) return result;
+    else result[0] = lo;
+
+    hi = nums.length - 1;
+    while (lo < hi) {
+        // this +1 is very important!!! Make mid biased to the right
+        var mid = lo + Math.floor((hi - lo) / 2) + 1;
+        if (nums[mid] > target) hi = mid - 1;
+        else lo = mid;
+    }
+    result[1] = lo;
 
     return result;
 };
