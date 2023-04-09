cb23092fbf7b908b2e99a1ca1c3297a290316f8a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-09 22:44:56 +0530

    44 :::: 244-shortestWordDistII.js question updated

--- dsa.js
+++ dsa.js
@@ -1,32 +1,48 @@
 /**
- * @param {string} input
- * @return {number[]}
- */
-// slow, we can use dp to cache some results
-var diffWaysToCompute = function(input) {
-    var results = [];
-    for (var i = 0; i < input.length; i++) {
-        if (input[i] === '-' || input[i] === '+' || input[i] === '*') {
-            console.log(input);
-            var inputLeft = diffWaysToCompute(input.substring(0, i));
-            var inputRight = diffWaysToCompute(input.substring(i + 1));
-            for (var j = 0; j < inputLeft.length; j++) {
-                for (var k = 0; k < inputRight.length; k++) {
-                    var result ;
-                    if (input[i] === '-') {
-                        result = parseInt(inputLeft[j]) - parseInt(inputRight[k]);
-                    } else if (input[i] === '+') {
-                        result = parseInt(inputLeft[j]) + parseInt(inputRight[k]);
-                    } else if (input[i] === '*') {
-                        result = parseInt(inputLeft[j]) * parseInt(inputRight[k]);
-                    }
-                    results.push(result);
-                }
-            }
-        }
+* question: http://www.programcreek.com/2014/07/leetcode-shortest-word-distance-ii-java/
+*
+*/
+
+function ShortestWordDist(words) {
+  this.hashMap = {};
+
+  var map = this.hashMap;
+
+  words.forEach(function(word, index) {
+    if (word in map) {
+      map[word].push(index);
+    } else {
+      map[word] = [index];
     }
+  });
+}
 
-    if (results.length === 0) results.push(parseInt(input));
+ShortestWordDist.prototype.shortest = function(word1, word2) {
+  var list1 = this.hashMap[word1];
+  var list2 = this.hashMap[word2];
+  var dist = Number.MAX_VALUE;
 
-    return results;
+  for (var i = 0, j = 0; i < list1.length && j < list2.length;) {
+    var index1 = list1[i];
+    var index2 = list2[j];
+
+    dist = Math.min(Math.abs(index1 - index2), dist);
+    if (index1 < index2) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return dist;
 };
+
+// test cases
+var ws = ["practice", "makes", "perfect", "coding", "makes", "google", "coding", "apple", "alpha"];
+var words = new ShortestWordDist(ws);
+
+var word1 = 'practice';
+var word2 = 'coding';
+var word3 = 'apple';
+console.log(words.shortest(word1, word2));
+console.log(words.shortest(word2, word3));
