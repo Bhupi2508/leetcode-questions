d066a7adefa174f8a16d8c1a29edfa0f9a5c38c3
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-09 22:53:25 +0530

    44 :::: 0513-find-bottom-left-tree-value.js leetcode question updated (2023-04-09)

--- dsa.js
+++ dsa.js
@@ -1,52 +1,95 @@
-// 509. Fibonacci Number
-// Easy   67%
+// 513. Find Bottom Left Tree Value
+// Medium   56%
 
 
-// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the
-// Fibonacci sequence, such that each number is the sum of the two preceding
-// ones, starting from 0 and 1. That is,
-// F(0) = 0,   F(1) = 1
-// F(N) = F(N - 1) + F(N - 2), for N > 1.
-// Given N, calculate F(N).
+// Given a binary tree, find the leftmost v in the last row of the tree.
 
 // Example 1:
-// Input: 2
-// Output: 1
-// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
-// Example 2:
-// Input: 3
-// Output: 2
-// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
-// Example 3:
-// Input: 4
-// Output: 3
-// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+// Input:
+
+//     2
+//    / \
+//   1   3
+
+// Output:
+// 1
+
+//   Example 2:
+
+// Input:
+
+//         1
+//        / \
+//       2   3
+//      /   / \
+//     4   5   6
+//        /
+//       7
+
+// Output:
+// 7
 
 // Note:
-// 0 &le; N &le; 30.
+// You may assume the tree (i.e., the given root node) is not NULL.
 
 
 /**
- * @param {number} N
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
  * @return {number}
  */
-const fib = function(N) {
-  if (N === 0) return 0
-  let p = 0, q = 1
-  while (--N > 0) [p, q] = [q, p + q]
-  return q
+const findBottomLeftValue = function(root) {
+  const queue = [root]
+  while (queue.length) {
+    root = queue.shift()
+    if (root.right) queue.push(root.right)
+    if (root.left) queue.push(root.left)
+  }
+  return root.val
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  2,
-  3,
-  4,
-].forEach((N) => {
-  console.log(fib(N))
+  [2,1,3],                             // 1
+  [1,2,3,4,null,5,6,null,null,7],      // 7
+  [1,null,1],                          // 1
+  [0,null,-1],                         // -1
+  [1,2,3,4,null,5,6,null,null,null,7], // 7
+].forEach(array => {
+  console.log(findBottomLeftValue(TreeNode.from(array)))
 })
 
 // Solution:
-// 直接迭代计算即可。
-// 递归的话会重复计算。
+
+// 解法一
+// 利用递归来遍历树，
+// 叶子节点或空节点返回其值和高度值为0的对象或结构体
+// 中间节点比较左右子树返回的对象的高度值，返回高度值大的对象，
+// 并将对象的高度加一。
+
+function iter(root) {
+  if (!root || (!root.left && !root.right)) return { v: root && root.val, h: 0 }
+
+  const left = iter(root.left), right = iter(root.right)
+  return (left.v == null || right.h > left.h)
+    ? { v: right.v, h: right.h + 1 }
+  : { v: left.v, h: left.h + 1 }
+}
+
+
+// 解法二
+// 用队列实现BFS。
+// 初始时将根节点入队，
+// 每次从头出队一个节点，
+// 按右到左的顺序检查该节点的子节点，若有子节点，则排入队尾，
+// 这样遍历，在最后将会遍历到最后一层的最左的一个节点。
 
 // Submission Result: Accepted
