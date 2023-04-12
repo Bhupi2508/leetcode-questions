363ca60563bcf89043f8f4e1d5991cf2d0141093
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-12 22:52:37 +0530

    44 :::: 0106-construct-binary-tree-from-inorder-and-postorder-traversal.js leetcode question updated (2023-04-12)

--- dsa.js
+++ dsa.js
@@ -1,14 +1,14 @@
-// 105. Construct Binary Tree from Preorder and Inorder Traversal
+// 106. Construct Binary Tree from Inorder and Postorder Traversal
 // Medium   32%
 
-// Given preorder and inorder traversal of a tree, construct the binary tree.
+// Given inorder and postorder traversal of a tree, construct the binary tree.
 
 // Note: You may assume that duplicates do not exist in the tree.
 
 // For example, given
 
-// preorder = [3,9,20,15,7]
 // inorder = [9,3,15,20,7]
+// postorder = [9,15,7,20,3]
 
 // Return the following binary tree:
 
@@ -28,34 +28,33 @@
  */
 
 /**
- * @param {number[]} preorder
  * @param {number[]} inorder
+ * @param {number[]} postorder
  * @return {TreeNode}
  */
-const buildTree = function(preorder, inorder) {
-  const n = preorder.length
-  if (n <= 0) return null
+const buildTree = function(inorder, postorder) {
+  const n = inorder.length
+  if (n === 0) return null
 
-  const root = new TreeNode(preorder[0])
-  const i = inorder.indexOf(preorder[0])
-  root.left = buildTree(preorder.slice(1, i + 1), inorder.slice(0, i))
-  root.right = buildTree(preorder.slice(i + 1, n), inorder.slice(i + 1, n))
+  const root = new TreeNode(postorder[n - 1])
+  const i = inorder.indexOf(postorder[n - 1])
+  root.left = buildTree(inorder.slice(0, i), postorder.slice(0, i))
+  root.right = buildTree(inorder.slice(i + 1, n), postorder.slice(i, n - 1))
   return root
 }
 
 ;[
-  [[3,9,20,15,7], [9,3,15,20,7]],
-  [[1, 2], [1, 2]],
+  [[9,3,15,20,7], [9,15,7,20,3]],
 ].forEach(args => {
   console.log(buildTree(...args))
 })
 
 // Solution:
-// 前序遍历的顺序是，根节点-左子树-右子树
 // 中序遍历的顺序是，左子树-根节点-右子树
+// 后序遍历的顺序是，左子树-右子树-根节点
 
-// 1. 前序数组的第一个节点作为根节点；
+// 1. 后序数组的最后一个节点作为根节点；
 // 2. 找出根节点在中序数组中的位置，由此计算出左子树和右子树的长度；
-// 3. 分割出前序和中序数组中的左右子树，并分别递归生成左右子树。
+// 3. 分割出后序和中序数组中的左右子树，并分别递归生成左右子树。
 
 // Submission Result: Accepted
