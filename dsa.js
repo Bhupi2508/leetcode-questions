d5084d6fb3b85eaa0c599d34eec2a67692aa5364
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-20 22:52:14 +0530

    45 :::: 0044-wildcard-matching.js leetcode question updated (2023-04-20)

--- dsa.js
+++ dsa.js
@@ -1,53 +1,59 @@
-// 43. Multiply Strings
-// Medium   27%
+// 44. Wildcard Matching
+// Hard 20% locked:false
 
-// Given two non-negative integers num1 and num2 represented as strings, return
-// the product of num1 and num2.
+// Implement wildcard pattern matching with support for '?' and '*'.
 
-// Note:
+// '?' Matches any single character.
+// '*' Matches any sequence of characters (including the empty sequence).
 
-// 1. The length of both num1 and num2 is < 110.
-// 2. Both num1 and num2 contains only digits 0-9.
-// 3. Both num1 and num2 does not contain any leading zero.
-// 4. You must not use any built-in BigInteger library or convert the inputs to
-// integer directly.
+// The matching should cover the entire input string (not partial).
+
+// The function prototype should be:
+// bool isMatch(const char *s, const char *p)
+
+// Some examples:
+// isMatch("aa","a") ? false
+// isMatch("aa","aa") ? true
+// isMatch("aaa","aa") ? false
+// isMatch("aa", "*") ? true
+// isMatch("aa", "a*") ? true
+// isMatch("ab", "?*") ? true
+// isMatch("aab", "c*a*b") ? false
 
 
 /**
- * @param {string} num1
- * @param {string} num2
- * @return {string}
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
  */
-const multiply = function(num1, num2) {
-  const n = num1.length, m = num2.length
-  const result = Array(n + m).fill(0)
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = m - 1; j >= 0; j--) {
-      const product = num1[i] * num2[j] + result[i + j + 1]
-      result[i + j + 1] = product % 10
-      result[i + j] += Math.trunc(product / 10)
-    }
+const isMatch = function(s, p) {
+  const n = s.length, m = p.length
+  let i = 0, j = 0, match = 0, starIdx = -1
+  while (i < n){
+    if (j < m  && (p[j] === '?' || s[i] === p[j])) {
+      i++
+      j++
+    } else if (j < m && p[j] === '*') {
+      starIdx = j
+      match = i
+      j++
+    } else if (starIdx !== -1){
+      j = starIdx + 1
+      match++
+      i = match
+    } else return false
   }
-  while (result[0] === 0 && result.length > 1) result.shift()
-  return result.join('')
+
+  while (j < m && p[j] === '*') j++
+
+  return j === m
 }
 
-;[
-  ['13', '17'],                 // 221
-  ['98', '98'],                 // 8904
-  ['0', '0'],                   // 0
-  ['123', '456'],               // 56088
-].forEach(args => {
-  console.log(multiply(...args))
-})
-
-// Solution:
-// 先分配足够的空间来保存每一位数。
-// 两个数的长度分别为 n 和 m，则其乘积的最长长度为 n+m。
-// 因此分配 n+m 个空间就足够了。
-
-// 运算的方式类似于竖式计算。
-// 每次将乘积的个位保留在对应位置，进位保存在其前一个位置。
-// 每次算两个一位数乘积时，都加上其对应位置原来保留的数。
-
-// Submission Result: Accepted
+console.log(isMatch('aa', 'a'), false)
+console.log(isMatch('aa', 'aa'), true)
+console.log(isMatch('aaa', 'aa'), false)
+console.log(isMatch('aa', '*'), true)
+console.log(isMatch('aa', 'a*'), true)
+console.log(isMatch('ab', '?*'), true)
+console.log(isMatch('ad', 'c*a*b*d'), false)
+console.log(isMatch('aaa', 'a*a'), true)
