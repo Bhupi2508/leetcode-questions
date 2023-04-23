d2765e8b3b20754918607a2c5d136e3042b8ac9b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-23 22:53:59 +0530

    46 :::: 0804-unique-morse-code-words.js leetcode question updated (2023-04-23)

--- dsa.js
+++ dsa.js
@@ -1,60 +1,87 @@
-// 796. Rotate String
-// Easy   50%
+// 804. Unique Morse Code Words
+// Easy   76%
 
 
-// We are given two strings, A and B.
-// A shift on A consists of taking string A and moving the leftmost character to
-// the rightmost position. For example, if A = 'abcde', then it will be 'bcdea'
-// after one shift on A. Return True if and only if A can become B after some
-// number of shifts on A.
-// Example 1:
-// Input: A = 'abcde', B = 'cdeab'
-// Output: true
-// Example 2:
-// Input: A = 'abcde', B = 'abced'
-// Output: false
+// International Morse Code defines a standard encoding where each letter is
+// mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps
+// to "-...", "c" maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is
+// given below:
+
+// [
+//   '.-',  // a
+//   '-...',// b
+//   '-.-.',
+//   '-..',
+//   '.',   // e
+//   '..-.',
+//   '--.', // g
+//   '....',
+//   '..',  // i
+//   '.---',
+//   '-.-',
+//   '.-..',
+//   '--',
+//   '-.',  // n
+//   '---',
+//   '.--.',
+//   '--.-',
+//   '.-.',
+//   '...',
+//   '-',
+//   '..-',
+//   '...-',
+//   '.--',
+//   '-..-',
+//   '-.--',
+//   '--..',// z
+// ]
+
+// Now, given a list of words, each word can be written as a concatenation of the
+// Morse code of each letter. For example, "cba" can be written as "-.-.-....-",
+// (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a
+// concatenation, the transformation of a word.
+// Return the number of different transformations among all words we have.
+// Example:
+// Input: words = ["gin", "zen", "gig", "msg"]
+// Output: 2
+// Explanation:
+// The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+// There are 2 different transformations, "--...-." and "--...--.".
 // Note:
-//     A and B will have length at most 100.
+//     The length of words will be at most 100.
+//     Each words[i] will have length in range [1, 12].
+//     words[i] will only consist of lowercase letters.
 
 
 /**
- * @param {string} A
- * @param {string} B
- * @return {boolean}
+ * @param {string[]} words
+ * @return {number}
  */
-const rotateString = function(A, B) {
-  if (A === B) return true
-  if (A.length !== B.length) return false
-  let i = 0, j = 0, n = A.length
-  while ((i = B.indexOf(A[0], j)) > 0) {
-    if (B.substring(i, n) + B.substring(0, i) === A) return true
-    j = i + 1
+const uniqueMorseRepresentations = function(words) {
+  const MORSE = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+  const set = new Set()
+  for (let word of words) {
+    let s = ''
+    for (let c of word) {
+      s += MORSE[c.charCodeAt(0) - 97]
+    }
+    set.add(s)
   }
-  return false
-}
-
-const amazing = function(A, B) {
-  return A.length === B.length && (A + A).includes(B)
+  return set.size
 }
 
 ;[
-  ['abcde', 'cdeab'], // true
-  ['abcde', 'abced'], // false
-  ['abcde', 'abcde'], // true
-  ['abcde', 'abcd'],  // false
-  ['abcabe', 'beabca'], // true
-  ['abcabe', 'bcabea'], // true
-].forEach(([A, B]) => {
-  console.log(rotateString(A, B))
-  console.log(rotateString(A, B))
+  ['gin', 'zen', 'gig', 'msg'],
+].forEach((words) => {
+  console.log(uniqueMorseRepresentations(words))
 })
 
 // Solution:
-// 平凡的方法
-// 找到 A 的第一个字符的在 B 中的位置，找到后重新拆分并拼接 B 字符串，并判断新字符串是否与 A
-// 相同，相同返回 true，不同则寻找下一个出现的位置进行判断，直到 B 中再无该字符。
-
-// 令人赞叹的方法!!!
-// 判断 A+A 中是否包含有 B
+// 用 Set 去重。
 
 // Submission Result: Accepted
