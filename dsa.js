367e7c086b8d2562be0d0f5c3a627323ee191e35
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-25 22:54:02 +0530

    46 :::: 0832-flipping-an-image.js leetcode question updated (2023-04-25)

--- dsa.js
+++ dsa.js
@@ -1,76 +1,77 @@
-// 830. Positions of Large Groups
-// Easy   49%
+// 832. Flipping an Image
+// Easy   75%
 
 
-// In a string S of lowercase letters, these letters form consecutive groups of
-// the same character.
-// For example, a string like S = "abbxxxxzyy" has the groups "a", "bb", "xxxx",
-// "z" and "yy".
-// Call a group large if it has 3 or more characters.  We would like the starting
-// and ending positions of every large group.
-// The final answer should be in lexicographic order.
-
+// Given a binary matrix A, we want to flip the image horizontally, then invert
+// it, and return the resulting image.
+// To flip an image horizontally means that each row of the image is reversed.
+// For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+// To invert an image means that each 0 is replaced by 1, and each 1 is replaced
+// by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
 // Example 1:
-// Input: "abbxxxxzzy"
-// Output: [[3,6]]
-// Explanation: "xxxx" is the single large group with starting  3 and ending
-// positions 6.
+// Input: [[1,1,0],[1,0,1],[0,0,0]]
+// Output: [[1,0,0],[0,1,0],[1,1,1]]
+// Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+// Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 // Example 2:
-// Input: "abc"
-// Output: []
-// Explanation: We have "a","b" and "c" but no large group.
-// Example 3:
-// Input: "abcdddeeeeaabbbcd"
-// Output: [[3,5],[6,9],[12,14]]
-
-// Note:  1 <= S.length <= 1000
+// Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+// Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+// Explanation: First reverse each row:
+// [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+// Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+// Notes:
+//     1 <= A.length = A[0].length <= 20
+//     0 <= A[i][j] <= 1
 
 
 /**
- * @param {string} S
+ * @param {number[][]} A
  * @return {number[][]}
  */
-const largeGroupPositions = function(S) {
-  const res = []
-  for (let i = 1, j = 0, l = S.length; i <= l; i++) {
-    if (S[j] !== S[i]) {
-      if (i - j >= 3) res.push([j, i - 1])
-      j = i
+const flipAndInvertImage = function(A) {
+  const N = A.length
+  for (let i = 0; i < N; i++) {
+    for (let j = 0, k = N - 1; j <= k; j++, k--) {
+      if (k === j) A[i][j] ^= 1
+      else {
+        const t = A[i][j]
+        A[i][j] = A[i][k] ^ 1
+        A[i][k] = t ^ 1
+      }
     }
   }
-  return res
+  return A
 }
 
-const universal = function(S) {
-  const res = []
-  for (let i = 0, j = 0; i < S.length; i = j) {
-    while (j < S.length && S[i] === S[j]) j++
-    if (j - i >= 3) res.push([i, j - 1])
+const awesome = function(A) {
+  const N = A.length
+  for (let row of A) {
+    for (let i = 0; i * 2 < N; i++) {
+      if (row[i] == row[N - i - 1]) row[i] = row[N - i - 1] ^= 1
+    }
   }
-  return res
+  return A
 }
 
 ;[
-  'abbxxxxzzy', // [[3,6]]
-  'abc',        // []
-  'abcdddeeeeaabbbcd', // [[3,5],[6,9],[12,14]]
-  'aaabccc',
-  'aaaa',
-  'aa'
-].forEach((S) => {
-  console.log(largeGroupPositions(S))
-  console.log(universal(S))
+  [[1,1,0],[1,0,1],[0,0,0]],
+  [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]],
+].forEach((A) => {
+  // console.log(flipAndInvertImage(A))
+  console.log(awesome(A))
 })
 
 // Solution:
-// 方法1
-// i 遍历 数组，j 记录连续字符的头部，
-// 当进入下一组连续字符时，计算 i 到 j 是否大于等于 3，是则记录，并设置 j，否则只设置 j 。
-// 利用 JS 数组尾部之后的值为 undefined 来判断最后一组。
+// 1 1 0    <|>    0 1 1   0->1    1 0 0
+// 1 0 1  ------>  1 0 1  ------>  0 1 0
+// 0 0 0           0 0 0   1->0    1 1 1
 
-// 方法2 （更普遍）
-// i 记录连续字符的头部，j 遍历数组直到下一个组的头部，
-// 计算 i 到 j 是否大于等于 3，是则记录
-// 设置 i = j
+// 每行镜像转换，并同时将 0/1 转换
+// 注意中间的转换
+
+// 神级操作
+// 镜像转换时，需要交换的两个数 a 和 b
+// 当 a==b 时，(即 a=1&&b=1 或 a=0&&b=0)，最终的到的结果为 a==b==^a
+// 当 a!=b 时，最终得到的结果不变。
 
 // Submission Result: Accepted
