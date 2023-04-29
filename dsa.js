4da3a67def506152513b33b89c5bcdc15bde63f5
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-04-29 22:52:07 +0530

    46 :::: 0025-reverse-nodes-in-k-group.js leetcode question updated (2023-04-29)

--- dsa.js
+++ dsa.js
@@ -1,13 +1,21 @@
-// 24. Swap Nodes in Pairs
-// Medium   38%
+// 25. Reverse Nodes in k-Group
+// Hard   30%
 
-// Given a linked list, swap every two adjacent nodes and return its head.
+// Given a linked list, reverse the nodes of a linked list k at a time and
+// return its modified list.
+
+// k is a positive integer and is less than or equal to the length of the linked
+// list. If the number of nodes is not a multiple of k then left-out nodes in
+// the end should remain as it is.
+
+// You may not alter the values in the nodes, only nodes itself may be changed.
+
+// Only constant memory is allowed.
 
 // For example,
-// Given 1->2->3->4, you should return the list as 2->1->4->3.
-
-// Your algorithm should use only constant space. You may not modify the values
-// in the list, only nodes itself can be changed.
+// Given this linked list: 1->2->3->4->5
+// For k = 2, you should return: 2->1->4->3->5
+// For k = 3, you should return: 3->2->1->4->5
 
 /**
  * Definition for singly-linked list.
@@ -20,29 +28,48 @@
 
 /**
  * @param {ListNode} head
+ * @param {number} k
  * @return {ListNode}
  */
-const swapPairs = function(head) {
-  if (!head || !head.next) return head
-  const second = head.next
-  head.next = swapPairs(second.next)
-  second.next = head
-  return second
+const reverseKGroup = function(head, k) {
+  let tail = head, count = 0
+  while (tail && ++count < k) tail = tail.next
+
+  if (count === k) {
+    tail.next = reverseKGroup(tail.next, k)
+
+    while (tail !== head) {
+      const tmp = head
+      head = head.next
+      tmp.next = tail.next
+      tail.next = tmp
+    }
+  }
+
+  return head
 }
 
 const ListNode = require('../structs/ListNode')
 ;[
-  [1, 2],                       // [2, 1]
-  [1],                          // [1]
-  [1, 2, 3, 4],                 // [2, 1, 4, 3]
-].forEach((array) => {
-  console.log((swapPairs(ListNode.from(array)) || '').toString())
+  [[1, 2, 3, 4, 5], 2],  // [2,1,4,3,5]
+  [[1, 2, 3, 4, 5], 3],  // [3,2,1,4,5]
+  [[1, 2, 3, 4], 2],     // [2,1,4,3]
+  [[1, 2, 3, 4], 3],     // [3,2,1,4]
+].forEach(([array, k]) => {
+  console.log((reverseKGroup(ListNode.from(array), k) || '').toString())
 })
 
 // Solution:
-// 使用递归迭代，每两个节点交换一次。
-// 保存第二个节点。
-// 将第一个节点的下一个节点置为第二个节点之后的链表交换后返回的值。
-// 再将第二个节点的下一个节点置为第一个节点，并将第二个节点作为头节点返回。
+// 使用递归结合 *头插法* 求解。
+// 1. 看当前链表长度时候大于或等于k。
+// 2. 若小于k，直接返回该链表。
+// 3. 若大于或等于，则将第k个节点后的链表进行递归，返回的链表返回原来的位置。
+// 4. 每个组（即当前链表中的第一个节点到第k个节点）中使用头插法。
+// 5. 返回头节点。
+
+// 头插入过程中，
+// 每次用一个临时变量保存头节点，而头节点变量指向下一个节点。
+// 以保存的最初的尾节点为头，将临时变量指向的节点插入到头之后。
+// 直到头节点和尾节点指向同一个节点。
 
 // Submission Result: Accepted
