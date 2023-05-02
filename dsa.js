cdd605386f5d950c585d772d056811367ed2b7d3
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-02 22:52:09 +0530

    47 :::: 0031-next-permutation.js leetcode question updated (2023-05-02)

--- dsa.js
+++ dsa.js
@@ -1,108 +1,56 @@
-// 30. Substring with Concatenation of All Words
-// Hard 22% locked:false
+// 31. Next Permutation
+// Medium   28%
 
-// You are given a string, s, and a list of words, words, that are all of the
-// same length. Find all starting indices of substring(s) in s that is a
-// concatenation of each word in words exactly once and without any intervening
-// characters.
+// Implement next permutation, which rearranges numbers into the
+// lexicographically next greater permutation of numbers.
 
-// For example, given:
-// s: "barfoothefoobarman"
-// words: ["foo", "bar"]
+// If such arrangement is not possible, it must rearrange it as the lowest
+// possible order (ie, sorted in ascending order).
 
-// You should return the indices: [0,9]. (order does not matter).
+// The replacement must be in-place, do not allocate extra memory.
+
+// Here are some examples. Inputs are in the left-hand column and its
+//   corresponding outputs are in the right-hand column.
+
+// 1,2,3 → 1,3,2
+// 3,2,1 → 1,2,3
+// 1,1,5 → 1,5,1
 
 /**
- * @param {string} s
- * @param {string[]} words
- * @return {number[]}
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-const findSubstring = function(s, words) {
-  const n = s.length, m = words.length
-  if (m === 0) return []
-  const result = [], len = words[0].length
+const nextPermutation = function(nums) {
+  let i = nums.length - 1
+  while (i > 0 && nums[i - 1] >= nums[i]) i--
 
-  const iter = (i, include) => {
-    let count = 0
-    include.forEach(v => count++)
-    if (count === m) return true
-    const str = s.substr(i, len)
-    let index = words.indexOf(str)
-    while (index !== -1 && include[index] !== void 0) {
-      index = words.indexOf(str, index + 1)
-    }
-    if (index !== -1) {
-      include[index] = true
-      return iter(i + len, include)
-    } else {
-      return false
-    }
+  if (i > 0) {
+    let p = i - 1, q = i
+    while (nums[p] < nums[q + 1]) q++
+    ;[nums[p], nums[q]] = [nums[q], nums[p]]
   }
 
-  for (let i = 0; i < n && n - i >= m * len; i++) {
-    if (iter(i, new Array(m)) === true) result.push(i)
+  for (let j = nums.length - 1; i < j; i++, j--) {
+    [nums[i], nums[j]] = [nums[j], nums[i]]
   }
-
-  return result
 }
 
-// console.log(findSubstring('barfoothefoobarman', ['foo', 'bar'])) // [0, 9]
-// console.log(findSubstring("bccbcc", ["bc","cc","cb"])) // [0]
-// console.log(findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"])) // [8]
+;[
+  [2, 3, 1],                    // [3, 1, 2]
+  [1, 2, 3],                    // [1, 3, 2]
+  [3, 2, 1],                    // [1, 2, 3]
+  [1, 1, 5],                    // [1, 5, 1]
+  [5, 1, 1],                    // [1, 1, 5]
+  [3, 6, 5, 4],                 // [4, 3, 5, 6]
+].forEach(nums => {
+  nextPermutation(nums)
+  console.log(nums)
 
-// travel all the words combinations to maintain a window
-// there are wl(word len) times travel
-// each time, n/wl words, mostly 2 times travel for each word
-// one left side of the window, the other right side of the window
-// so, time complexity O(wl * 2 * N/wl) = O(2N)
-const Other = function(s, words) {
-//vector<int> findSubstring(string s, vector<string> &words)
-  const ans = []
-  const n = s.length, cnt = words.length
-  if (n <= 0 || cnt <= 0) return ans
+})
 
-  const dict = {}
-  for (let i = 0; i < cnt; ++i) {
-    dict[words[i]] = dict[words[i]] ? dict[words[i]] + 1 : 1
-  }
+// Solution:
+// 从数组末尾开始找第一个小于其后一个数的数的位置。
+// 之后这个位置的数与其后大于该数中的最小的一个数交换。
+// 最后翻转该位置后的所有数。
 
-  // travel all sub string combinations
-  const wl = words[0].length
-  for (let i = 0; i < wl; ++i) {
-    let left = i, count = 0, tdict = {}
-    for (let j = i; j <= n - wl; j += wl) {
-      const str = s.substr(j, wl)
-      // a valid word, accumulate results
-      if (dict[str]) {
-        tdict[str] = tdict[str] ? tdict[str] + 1 : 1
-        if (tdict[str] <= dict[str]) count++
-        else {
-          // a more word, advance the window left side possiablly
-          while (tdict[str] > dict[str]) {
-            const str1 = s.substr(left, wl)
-            tdict[str1]--
-            if (tdict[str1] < dict[str1]) count--
-            left += wl
-          }
-        }
-        // come to a result
-        if (count == cnt) {
-          ans.push(left)
-          // advance one word
-          tdict[s.substr(left, wl)]--
-          count--
-          left += wl
-        }
-      }
-      // not a valid word, reset all vars
-      else {
-        tdict = {}
-        count = 0
-        left = j + wl
-      }
-    }
-  }
-
-  return ans
-}
-console.log(Other("wordgoodgoodgoodbestword", ["word","good","best","good"])) // [8]
+// Submission Result: Accepted
