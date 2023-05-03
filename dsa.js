3d9da3742ad8f303827232dffc58456a3d69611c
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-03 22:53:24 +0530

    47 :::: 0506-relative-ranks.js leetcode question updated (2023-05-03)

--- dsa.js
+++ dsa.js
@@ -1,44 +1,48 @@
-// 504. Base 7
-// Easy   44%
+// 506. Relative Ranks
+// Easy   46%
 
 
-// Given an integer, return its base 7 string representation.
+// Given scores of N athletes, find their relative ranks and the people with the
+// top three highest scores, who will be awarded medals: "Gold Medal", "Silver
+// Medal" and "Bronze Medal".
 
 // Example 1:
 
-// Input: 100
-// Output: "202"
-
-// Example 2:
-
-// Input: -7
-// Output: "-10"
+// Input: [5, 4, 3, 2, 1]
+// Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+// Explanation: The first three athletes got the top three highest scores, so
+// they got "Gold Medal", "Silver Medal" and "Bronze Medal". For the left two
+// athletes, you just need to output their relative ranks according to their
+// scores.
 
 // Note:
-// The input will be in range of [-1e7, 1e7].
+
+// N is a positive integer and won't exceed 10,000.
+// All the scores of athletes are guaranteed to be unique.
 
 
 /**
- * @param {number} num
- * @return {string}
+ * @param {number[]} nums
+ * @return {string[]}
  */
-const convertToBase7 = function(num) {
-  let result = '', p = Math.abs(num)
-  while (p) {
-    result = (p % 7) + result
-    p = Math.trunc(p / 7)
-  }
-  return num ? (num < 0 ? '-' : '') + result : '0'
+const findRelativeRanks = function(nums) {
+  const medals = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
+  const rank = nums.slice()
+        .sort((a, b) => b - a)
+        .reduce((obj, n, i) => (obj[n] = i + 1) && obj, {})
+  return nums.map(v => medals[rank[v] - 1] || rank[v] + '')
 }
 
 ;[
-  100,                          // '202'
-  -7,                           // '-10'
-].forEach(num => {
-  console.log(convertToBase7(num))
+  [5,4,3,2,1],
+  [10,3,8,9,4],
+].forEach(nums => {
+  console.log(findRelativeRanks(nums))
 })
 
 // Solution:
-// 取余运算和除法运算。
+// 先排序，得到那个元素的名次，
+// 根据名次赋予相应的字符串。
+// 主要是编码方面，如何更简洁吧。
 
 // Submission Result: Accepted
