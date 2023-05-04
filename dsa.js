8d972e48cc00159790808e0b9aac911f559884eb
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-04 22:44:48 +0530

    47 :::: 16-3sumClosest.js question updated

--- dsa.js
+++ dsa.js
@@ -1,44 +1,37 @@
 /**
- * Binary search. Be careful about the boundaries.
- * If the first element is larger than the last one, then we compute the element in the middle,
- * and compare it with the first element. If value of the element in the middle is larger than
- * the first element, we know the rotation is at the second half of this array.
- * Else, it is in the first half in the array.
+ * Same idea as 3 sum, just check the abs value of every new 3 sum result and target
+ * use a variable 'result' to track minimum result.
  *
  * @param {number[]} nums
+ * @param {number} target
  * @return {number}
  */
-var findMin = function(nums) {
-    var low = 0;
-    var high = nums.length - 1;
+var threeSumClosest = function(nums, target) {
+    nums.sort(function(a, b) {
+        return a - b;
+    });
 
-    while (low <= high) {
-        if (nums[low] <= nums[high]) return nums[low];
-        var mid = low + Math.floor((high - low) / 2);
-        if (nums[mid] >= nums[low]) low = mid + 1;
-        else high = mid;
-    }
+    var result = nums[0] + nums[1] + nums[nums.length - 1];
 
-    return nums[0];
-};
+    for (var i = 0; i < nums.length - 2; i++) {
+        var lo = i + 1;
+        var hi = nums.length - 1;
+        while (lo < hi) {
+            var sum = nums[i] + nums[lo] + nums[hi];
+            if (Math.abs(sum - target) < Math.abs(result - target)) {
+                result = sum;
+                if (result === target) return result;
+            }
 
-// second try
-var findMin = function(nums) {
-    var lo = 0;
-    var hi = nums.length - 1;
+            if (sum > target) {
+                hi--;
+            } else {
+                lo++;
+            }
 
-    while (lo <= hi) {
-        if (nums[lo] < nums[hi]) return nums[lo];
-        var mid = lo + Math.floor((hi - lo) / 2);
-        if (nums[lo] <= nums[mid]) {
-            // this case, minimun must exist in the second half
-            lo = mid + 1;
-        } else {
-            // this case, mininum can be mid itself or must exist in the first half.
-            hi = mid;
         }
     }
 
-    // if hi < lo, then nums[hi] is the minum.
-    return nums[hi];
+    return result;
+
 };
