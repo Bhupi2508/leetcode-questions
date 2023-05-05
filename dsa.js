7719553b9db89a1b70e80f297188818db2845f62
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-05 22:53:03 +0530

    47 :::: 0303-range-sum-query-immutable.js leetcode question updated (2023-05-05)

--- dsa.js
+++ dsa.js
@@ -1,72 +1,62 @@
-// 299. Bulls and Cows
-// Medium   36%
+// 303. Range Sum Query - Immutable
+// Easy   30%
 
 
-// You are playing the following Bulls and Cows game with your friend: You write
-// down a number and ask your friend to guess what the number is. Each time your
-// friend makes a guess, you provide a hint that indicates how many digits in
-// said guess match your secret number exactly in both digit and position (called
-// "bulls") and how many digits match the secret number but locate in the wrong
-// position (called "cows"). Your friend will use successive guesses and hints to
-// eventually derive the secret number.
-// Write a function to return a hint according to the secret number and friend's
-// guess, use A to indicate the bulls and B to indicate the cows.
-// Please note that both secret number and friend's guess may contain duplicate
-// digits.
+// Given an integer array nums, find the sum of the elements between indices i
+// and j (i ≤ j), inclusive.
 
-// Example 1:
-// Input: secret = "1807", guess = "7810"
-// Output: "1A3B"
-// Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+// Example:
 
-// Example 2:
-// Input: secret = "1123", guess = "0111"
-// Output: "1A1B"
-// Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
+// Given nums = [-2, 0, 3, -5, 2, -1]
 
-// Note: You may assume that the secret number and your friend's guess only
-// contain digits, and their lengths are always equal.
+// sumRange(0, 2) -> 1
+// sumRange(2, 5) -> -1
+// sumRange(0, 5) -> -3
+
+// Note:
+
+// You may assume that the array does not change.
+// There are many calls to sumRange function.
+
 
 /**
- * @param {string} secret
- * @param {string} guess
- * @return {string}
+ * @param {number[]} nums
  */
-const getHint = function(secret, guess) {
-  const digits = Array(10).fill(0)
-  let bulls = 0, cows = 0
-  for (let i = 0; i < secret.length; i++) {
-    const si = secret[i], gi = guess[i]
-    if (si === gi) {
-      bulls++
-    } else {
-      if (digits[si] < 0) cows++
-      digits[si]++
-      if (digits[gi] > 0) cows++
-      digits[gi]--
-    }
+const NumArray = function(nums) {
+  for (let i = 1, n = nums.length; i < n; i++) {
+    nums[i] += nums[i - 1]
   }
-  return bulls + 'A' + cows + 'B'
+  this.nums = nums
 }
 
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+  return i ? this.nums[j] - this.nums[i - 1] : this.nums[j]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+
+
+const obj = new NumArray([-2, 0, 3, -5, 2, -1])
 ;[
-  ['', ''],               // 0A0B
-  ['1807', '7810'],       // 1A3B
-  ['1123', '0111'],       // 1A1B
-  ['011101', '101010'],   // 1A4B
-].forEach((args) => {
-  console.log(getHint(...args))
+  [0, 2],                       // 1
+  [2, 5],                       // -1
+  [0, 5],                       // -3
+].forEach(args => {
+  console.log(obj.sumRange(...args))
 })
 
 // Solution:
-// 方法一：
-// 1. 使用一个长度为 10 的数组记录每个数在 secret 出现的次数；
-// 2. 再遍历一遍，寻找 bulls, 以及看 guess 中有多少数字出现在了 secret
-//   中（包括 bulls)；
-// 3. 得到 bulls 和 cows。
 
-// 方法二：
-// 使用一个长度为 10 的数组 digits 记录数字的信息；在 secret 中出现，则用正数，
-// 在 guess 中出现，则用负数。
+// 计算多次某个数组的某子序列和时，可以只保存当前数的累积。
+// 求 i->j 的和时，只需要计算 0->j 的累积 减去 0->i 的累积。
 
 // Submission Result: Accepted
