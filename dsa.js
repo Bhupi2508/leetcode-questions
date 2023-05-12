7acbdadbaad0c62f6445adcf2498668f26a394aa
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-12 22:54:33 +0530

    48 :::: 1037-valid-boomerang.js leetcode question updated (2023-05-12)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,45 @@
-// 1033. Moving Stones Until Consecutive
-// Easy   39%
+// 1037. Valid Boomerang
+// Easy   38%
 
 
-// Three stones are on a number line at positions a, b, and c.
-// Each turn, you pick up a stone at an endpoint (ie., either the lowest or
-// highest position stone), and move it to an unoccupied position between those
-// endpoints.  Formally, let's say the stones are currently at positions x, y, z
-// with x < y < z.  You pick up the stone at either position x or position z, and
-// move that stone to an integer position k, with x < k < z and k != y.
-// The game ends when you cannot make any more moves, ie. the stones are in
-// consecutive positions.
-// When the game ends, what is the minimum and maximum number of moves that you
-// could have made?  Return the answer as an length 2 array: answer =
-// [minimum_moves, maximum_moves]
+// A boomerang is a set of 3 points that are all distinct and not in a straight
+// line.
+// Given a list of three points in the plane, return whether these points are a
+// boomerang.
 
 // Example 1:
-// Input: a = 1, b = 2, c = 5
-// Output: [1,2]
-// Explanation: Move the stone from 5 to 3, or move the stone from 5 to 4 to 3.
+// Input: [[1,1],[2,3],[3,2]]
+// Output: true
 // Example 2:
-// Input: a = 4, b = 3, c = 2
-// Output: [0,0]
-// Explanation: We cannot make any moves.
-// Example 3:
-// Input: a = 3, b = 5, c = 1
-// Output: [1,2]
-// Explanation: Move the stone from 1 to 4; or move the stone from 1 to 2 to 4.
+// Input: [[1,1],[2,2],[3,3]]
+// Output: false
 
 // Note:
-//     1 <= a <= 100
-//     1 <= b <= 100
-//     1 <= c <= 100
-//     a != b, b != c, c != a
-
-
-
+//     points.length == 3
+//     points[i].length == 2
+//     0 <= points[i][j] <= 100
 
 /**
- * @param {number} a
- * @param {number} b
- * @param {number} c
- * @return {number[]}
+ * @param {number[][]} points
+ * @return {boolean}
  */
-const numMovesStones = function(a, b, c) {
-  [a, b, c] = [a, b, c].sort((a, b) => a - b)
-  let min = 2, p = b - a, q = c - b
-  if (p === 1 && q === 1) min = 0
-  else if (p === 1 || q === 1 || p === 2 || q === 2) min = 1
-  return [min, c - a - 2]
+const isBoomerang = function(points) {
+  return (
+    (points[0][0] - points[1][0]) * (points[0][1] - points[2][1]) !=
+    (points[0][0] - points[2][0]) * (points[0][1] - points[1][1])
+  )
 }
 
 ;[
-  [1, 2, 5],  // [1,2]
-  [4, 3, 2],  // [0,0]
-  [3, 5, 1],  // [1,2]
-  [5, 1, 10], // [2,7]
-].forEach(([a, b, c]) => {
-  console.log(numMovesStones(a, b, c))
+  [[1, 1],[2, 3],[3, 2],], // true
+  [[1,1],[2,2],[3,3]],     // false
+  [[0,0],[1,0],[2,2]],     // true
+  [[0,1],[0,1],[2,1]],     // false  
+].forEach(points => {
+  console.log(isBoomerang(points))
 })
 
 // Solution:
-// 假设 a < b < c
-// 最大移动值为 c - a - 2
-// 最小移动值为
-// 1) b - a = 1 && c - b = 1, 0
-// 2) b - a = 1 || c - b = 1 || b - a = 2 || c - b = 2, 1
-// 3) 2
+// 判断是否为直线，使用点差法即可
 
 // Submission Result: Accepted
