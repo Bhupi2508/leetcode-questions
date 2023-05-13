da65a31baba2670474caabfbd3c4c1841d76a4a7
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-13 22:51:58 +0530

    48 :::: 0002-add-two-numbers.js leetcode question updated (2023-05-13)

--- dsa.js
+++ dsa.js
@@ -1,45 +1,55 @@
-// 1. Two Sum
-// Easy  35%
+// 2. Add Two Numbers
+// Medium  27%
 
-// Given an array of integers, return indices of the two numbers such that they
-// add up to a specific target.
+// You are given two non-empty linked lists representing two non-negative
+// integers. The digits are stored in reverse order and each of their nodes
+// contain a single digit. Add the two numbers and return it as a linked list.
 
-// You may assume that each input would have exactly one solution, and you may
-// not use the same element twice.
+// You may assume the two numbers do not contain any leading zero, except the
+// number 0 itself.
 
-// Example:
-
-// Given nums = [2, 7, 11, 15], target = 9,
-
-// Because nums[0] + nums[1] = 2 + 7 = 9,
-// return [0, 1].
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
 
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-const twoSum = function(nums, target) {
-  const hash = {}
-  for (let i = 0, n = nums.length; i < n; i++) {
-    if (hash[nums[i]] != null) return [hash[nums[i]], i]
-    hash[target - nums[i]] = i
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+  const result = new ListNode()
+  let p = l1, q = l2, node = result, carry = 0
+  while (p || q || carry) {
+    const sum = (p ? p.val : 0) + (q ? q.val : 0) + carry
+    node.next = new ListNode(sum % 10)
+    carry = Math.trunc(sum / 10)
+    node = node.next
+    if (p) p = p.next
+    if (q) q = q.next
   }
-  return [-1, -1]
+  return result.next
 }
 
-
+const ListNode = require('../structs/ListNode')
 ;[
-  [[2, 7, 11, 15], 9]           // [0, 1]
-].forEach(args => {
-  console.log(twoSum(...args))
+  [[9, 9], [1]],                // [0, 0, 1]
+  [[2, 4, 3], [5, 6, 4]],       // [7, 0, 8]
+].forEach(([A, B]) => {
+  const l1 = ListNode.from(A),
+        l2 = ListNode.from(B)
+  console.log((addTwoNumbers(l1, l2) || '').toString())
 })
 
+
 // Solution:
-// 使用哈希表，以每位数的互补数为键名，该数的位置为值。
-// 如果一个数对应等于一个键名，说明找到了互补数。
-// （哈希的应用）
-// 时间复杂度 O(n)
-// 空间复杂度 O(n)
+// 保存一个进位位，每次相加都带上进位位。
 
 // Submission Result: Accepted
