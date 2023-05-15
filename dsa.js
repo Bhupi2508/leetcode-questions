1e351f23a2d7462c9fa701d1365051276498ab4b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-15 22:54:03 +0530

    49 :::: 0844-backspace-string-compare.js leetcode question updated (2023-05-15)

--- dsa.js
+++ dsa.js
@@ -1,115 +1,120 @@
-// 840. Magic Squares In Grid
-// Easy   37%
+// 844. Backspace String Compare
+// Easy   47%
 
 
-// A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9
-// such that each row, column, and both diagonals all have the same sum.
-// Given an grid of integers, how many 3 x 3 "magic square" subgrids are there?
-// (Each subgrid is contiguous).
-
+// Given two strings S and T, return if they are equal when both are typed into
+// empty text editors. # means a backspace character.
+// Note that after backspacing an empty text, the text will continue empty.
 // Example 1:
-// Input: [[4,3,8,4],
-//         [9,5,1,9],
-//         [2,7,6,2]]
-// Output: 1
-// Explanation:
-// The following subgrid is a 3 x 3 magic square:
-// 438
-// 951
-// 276
-// while this one is not:
-// 384
-// 519
-// 762
-// In total, there is only one magic square inside the given grid.
+// Input: S = "ab#c", T = "ad#c"
+// Output: true
+// Explanation: Both S and T become "ac".
+// Example 2:
+// Input: S = "ab##", T = "c#d#"
+// Output: true
+// Explanation: Both S and T become "".
+// Example 3:
+// Input: S = "a##c", T = "#a#c"
+// Output: true
+// Explanation: Both S and T become "c".
+// Example 4:
+// Input: S = "a#c", T = "b"
+// Output: false
+// Explanation: S becomes "c" while T becomes "b".
 // Note:
-//     1 <= grid.length <= 10
-//     1 <= grid[0].length <= 10
-//     0 <= grid[i][j] <= 15
+//     1 <= S.length <= 200
+//     1 <= T.length <= 200
+//     S and T only contain lowercase letters and '#' characters.
+// Follow up:
+//     Can you solve it in O(N) time and O(1) space?
 
 
 /**
- * @param {number[][]} grid
- * @return {number}
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
  */
-const numMagicSquaresInside = function(grid) {
-  const circle = '29438167'
-  const pos = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
-  function isMagicSquares(i, j) {
-    if (grid[i][j] !== 5) return false
-    let a = b = circle.indexOf(grid[i - 1][j - 1])
-    if (a % 2 === 1) return false
-    let passA = true, passB = true
-    for (let p of pos) {
-      const n = grid[i + p[0]][j + p[1]]
-      if (n != circle[a]) passA = false
-      if (n != circle[b]) passB = false
-      a = (a + 1) % 8
-      b = (b + 7) % 8
-    }
-    return passA || passB
-  }
-  const n = grid.length, m = grid[0].length
-  let res = 0
-  for (let i = 1; i < n - 1; i++) {
-    for (let j = 1; j < m - 1; j++) {
-      if (isMagicSquares(i, j)) res++
+const backspaceCompare = function(S, T) {
+  let countS = 0, countT = 0
+  let i = S.length - 1, j = T.length - 1
+  while (i >= 0 || j >= 0) {
+    if (S[i] === '#') {
+      countS++
+      i--
+    } else if (countS > 0) {
+      countS--
+      i--
+    } else if (T[j] === '#') {
+      countT++
+      j--
+    } else if (countT > 0) {
+      countT--
+      j--
+    } else if (countS === 0 && countT === 0) {
+      if (S[i] !== T[j]) return false
+      i--
+      j--
     }
   }
-  return res
+  return (i < 0 && j < 0) || i === j
 }
 
 ;[
-  [[4,3,8,4],[9,5,1,9],[2,7,6,2]],
-].forEach((grid) => {
-  console.log(numMagicSquaresInside(grid))
+  ['ab#c', 'ad#c'], // true
+  ['ab##', 'c#d#'], // true
+  ['a##c', '#a#c'], // true
+  ['a#c', 'b'], // false
+  ['ab#cab##ab#c', 'ad#cc#d#ad#c'], // true
+  ['ab#c', 'xad#c'], // false
+  ['xab#c', 'ad#c'], // false
+  ['bxj##tw', 'bxo#j##tw'], // true
+].forEach(([S, T]) => {
+  console.log(backspaceCompare(S, T))
 })
 
-// Solution:
-// 1 + 2 + ... + 9 = 45
-// 45 / 3 = 15
-// 行、列、对角线的和都为 15
-// 3 个数的和为 15 的组合的规律
 
-const set = new Set()
-for (let i = 1; i <= 9; i++) {
-  for (let j = 1; j <= 9; j++) {
-    if (i === j) continue
-    const k = 15 - i - j
-    if (i === k || j === k || k <= 0 || k > 9) continue
-    const a = [i, j, k]
-    a.sort((a, b) => a - b)
-    set.add(a.join(' '))
+// Solution:
+// 最简单的方法：使用两个栈
+// 使用栈来获取最终输入的字符串。
+// 遇到一般字母，则字符入栈；遇到 ‘#’，则出栈一个字符。
+
+// 使用一个栈
+// 用栈来获取 S 字符串的最终输入字符串。
+// 从 T 的末尾遍历，遇到字符则与栈顶比较，不同则返回 false；遇到 ‘#’，则记录，用来跳过字符。
+// 遍历完后判断栈是否为空。
+
+// TO(N)-SO(1)
+// 分析使用一个栈的方法，进一步思考，尝试从两个字符串末尾开始遍历。
+// countS\countT 分别记录当前 ‘#’ 的个数，若其中一个大于0，则该字符串忽略一个字符，个数减一
+// 若都为0，则判断两个字符是否相等
+
+
+const twoStack = function(S, T) {
+  const stackA = [], stackB = []
+  for (let c of S) {
+    if (c === '#') stackA.pop()
+    else stackA.push(c)
   }
+  for (let c of T) {
+    if (c === '#') stackB.pop()
+    else stackB.push(c)
+  }
+  return stackA.join('') === stackB.join('')
 }
 
-// 组合有
-// Set {
-//   '1 5 9',
-//   '1 6 8',
-//   '2 4 9',
-//   '2 5 8',
-//   '2 6 7',
-//   '3 4 8',
-//   '3 5 7',
-//   '4 5 6'
-// }
-
-// 在四个边的数只需要出现在 2 个组合内，所以有 1，3，7，9
-// 在四个角的数需要在 3 个组合内，所以只能是 2，4，6，8 (所有偶数)
-// 中间的数必须出现在 4 个组合内，只能是 5
-
-// 159，258，357，456
-
-// 能组成的矩阵有
-// 2 9 4
-// 7 5 3
-// 6 1 8
-// 及其旋转和镜像等
-// 共 8 个
-
-// 因此
-// 1，判断中间是否为 5
-// 2，判断周围的环是否与环 2-9-4-3-8-1-6-7(或逆环) 相同
+const oneStack = function(S, T) {
+  const stack = []
+  for (let c of S) {
+    if (c === '#') stack.pop()
+    else stack.push(c)
+  }
+  let count = 0
+  for (let i = T.length - 1; i >= 0; i--) {
+    if (T[i] === '#') count++
+    else if (count > 0) count--
+    else if (T[i] !== stack.pop()) return false
+  }
+  return stack.length === 0
+}
 
 // Submission Result: Accepted
