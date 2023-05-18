3fcbf429cfc41a77ae185b8eaedc707c98e62024
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-18 22:44:57 +0530

    49 :::: 260-singleNumberIII.js question updated

--- dsa.js
+++ dsa.js
@@ -1,37 +1,20 @@
 /**
- * Solution: 1. priority queue, O(n*log(k)), however JavaScript doesn't natively support heap
-* 2. bucket sort
- *
  * @param {number[]} nums
- * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    var map = {};
+var singleNumber = function(nums) {
+    var appears = {};
     for (var i = 0; i < nums.length; i++) {
-        if (map.hasOwnProperty(nums[i])) {
-            map[nums[i]] += 1;
+        if (appears[nums[i]]) {
+            delete appears[nums[i]];
         } else {
-            map[nums[i]] = 1;
+            appears[nums[i]] = 1;
         }
     }
 
-    var bucket = [];
-    for (var key in map) {
-        var keyVal = parseInt(key, 10);
-        bucket[map[key]] ? bucket[map[key]].push(keyVal) : bucket[map[key]] = [keyVal];
+    var arr = [];
+    for (var key in appears) {
+        arr.push(parseInt(key));
     }
-
-    var result = [];
-
-    for (var j = bucket.length - 1; j > 0; j--) {
-        if (bucket[j]) {
-            result = result.concat(bucket[j]);
-        }
-
-        if (result.length >= k) break;
-    }
-
-    return result;
-
+    return arr;
 };
