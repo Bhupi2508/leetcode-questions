2f3677bfcb082bf885d77ac37f8a51dfbeaff15b
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-23 22:55:08 +0530

    50 :::: 1394-find-lucky-integer-in-an-array.js leetcode question updated (2023-05-23)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,59 @@
-// 1389. Create Target Array in the Given Order
-// Easy   83%
+// 1394. Find Lucky Integer in an Array
+// Easy   66%
 
 
-// Given two arrays of integers nums and index. Your task is to create target
-// array under the following rules:
-//     Initially target array is empty.
-//     From left to right read nums[i] and index[i], insert at index index[i] the
-// value nums[i] in target array.
-//     Repeat the previous step until there are no elements to read in nums and
-// index.
-// Return the target array.
-// It is guaranteed that the insertion operations will be valid.
+// Given an array of integers arr, a lucky integer is an integer which has a
+// frequency in the array equal to its value.
+// Return a lucky integer in the array. If there are multiple lucky integers
+// return the largest of them. If there is no lucky integer return -1.
 
 // Example 1:
-// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
-// Output: [0,4,1,3,2]
-// Explanation:
-// nums       index     target
-// 0            0        [0]
-// 1            1        [0,1]
-// 2            2        [0,1,2]
-// 3            2        [0,1,3,2]
-// 4            1        [0,4,1,3,2]
+// Input: arr = [2,2,3,4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] ==
+// 2.
 // Example 2:
-// Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
-// Output: [0,1,2,3,4]
-// Explanation:
-// nums       index     target
-// 1            0        [1]
-// 2            1        [1,2]
-// 3            2        [1,2,3]
-// 4            3        [1,2,3,4]
-// 0            0        [0,1,2,3,4]
+// Input: arr = [1,2,2,3,3,3]
+// Output: 3
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
 // Example 3:
-// Input: nums = [1], index = [0]
-// Output: [1]
+// Input: arr = [2,2,2,3,3]
+// Output: -1
+// Explanation: There are no lucky numbers in the array.
+// Example 4:
+// Input: arr = [5]
+// Output: -1
+// Example 5:
+// Input: arr = [7,7,7,7,7,7,7]
+// Output: 7
 
 // Constraints:
-//     1 <= nums.length, index.length <= 100
-//     nums.length == index.length
-//     0 <= nums[i] <= 100
-//     0 <= index[i] <= i
+//     1 <= arr.length <= 500
+//     1 <= arr[i] <= 500
 
 
 /**
- * @param {number[]} nums
- * @param {number[]} index
- * @return {number[]}
+ * @param {number[]} arr
+ * @return {number}
  */
-const createTargetArray = function(nums, index) {
-  const res = []
-  for (let i = 0; i < nums.length; i++) {
-    res.splice(index[i], 0, nums[i])
-  }
-  return res
+const findLucky = function(arr) {
+  const frequency = Array(501).fill(0)
+  for (let i of arr) frequency[i]++
+  for (let i = 500; i > 0; i--) if (frequency[i] === i) return i
+  return -1
 }
 
 ;[
-  [[0,1,2,3,4], [0,1,2,2,1]],
-  [[1,2,3,4,0], [0,1,2,3,0]],
-  [[1], [0]],
-].forEach(([nums, index]) => {
-  console.log(createTargetArray(nums, index))
+  [2,2,3,4], // 2
+  [1,2,2,3,3,3], // 3
+  [2,2,2,3,3], // -1
+  [5], // -1
+  [7,7,7,7,7,7,7], // 7
+].forEach((arr) => {
+  console.log(findLucky(arr))
 })
 
 // Solution:
-// 使用 splice 函数
+// 使用一个数组计数，再遍历该数组找到最大的幸运数。
 
 // Submission Result: Accepted
