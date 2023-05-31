ecbe2c0b49848d596c09d60dbee8f3f2e19ec710
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-31 22:54:08 +0530

    51 :::: 0884-uncommon-words-from-two-sentences.js leetcode question updated (2023-05-31)

--- dsa.js
+++ dsa.js
@@ -1,75 +1,51 @@
-// 883. Projection Area of 3D Shapes
-// Easy   67%
+// 884. Uncommon Words from Two Sentences
+// Easy   62%
 
 
-// On a N * N grid, we place some 1 * 1 * 1 cubes that are axis-aligned with the
-// x, y, and z axes.
-// Each value v = grid[i][j] represents a tower of v cubes placed on top of grid
-// cell (i, j).
-// Now we view the projection of these cubes onto the xy, yz, and zx planes.
-// A projection is like a shadow, that maps our 3 dimensional figure to a 2
-// dimensional plane.
-// Here, we are viewing the "shadow" when looking at the cubes from the top, the
-// front, and the side.
-// Return the total area of all three projections.
+// We are given two sentences A and B.  (A sentence is a string of space
+// separated words.  Each word consists only of lowercase letters.)
+// A word is uncommon if it appears exactly once in one of the sentences, and
+// does not appear in the other sentence.
+// Return a list of all uncommon words.
+// You may return the list in any order.
 
 // Example 1:
-// Input: [[2]]
-// Output: 5
+// Input: A = "this apple is sweet", B = "this apple is sour"
+// Output: ["sweet","sour"]
 // Example 2:
-// Input: [[1,2],[3,4]]
-// Output: 17
-// Explanation:
-// Here are the three projections ("shadows") of the shape made with each
-// axis-aligned plane.
-// Example 3:
-// Input: [[1,0],[0,2]]
-// Output: 8
-// Example 4:
-// Input: [[1,1,1],[1,0,1],[1,1,1]]
-// Output: 14
-// Example 5:
-// Input: [[2,2,2],[2,1,2],[2,2,2]]
-// Output: 21
+// Input: A = "apple apple", B = "banana"
+// Output: ["banana"]
 
 // Note:
-//     1 <= grid.length = grid[0].length <= 50
-//     0 <= grid[i][j] <= 50
+//     0 <= A.length <= 200
+//     0 <= B.length <= 200
+//     A and B both contain only spaces and lowercase letters.
 
 
 /**
- * @param {number[][]} grid
- * @return {number}
+ * @param {string} A
+ * @param {string} B
+ * @return {string[]}
  */
-const projectionArea = function(grid) {
-  let res = 0
-  const N = grid.length
-  for (let i = 0; i < N; i++) {
-    let p = 0, q = 0
-    for (let j = 0; j < N; j++) {
-      if (grid[i][j] > 0) res++
-      p = Math.max(p, grid[i][j])
-      q = Math.max(q, grid[j][i])
-    }
-    res += p + q
+const uncommonFromSentences = function(A, B) {
+  const hash = {}
+  for (let w of (A + ' ' + B).split(' ')) hash[w] = (hash[w] || 0) + 1
+  const res = []
+  for (let key in hash) {
+    if (hash[key] === 1) res.push(key)
   }
   return res
 }
 
 ;[
-  [[2]], // 5
-  [[1,2],[3,4]], // 17
-  [[1,0],[0,2]], // 8
-  [[1,1,1],[1,0,1],[1,1,1]], // 14
-  [[2,2,2],[2,1,2],[2,2,2]], // 21
-].forEach((grid) => {
-  console.log(projectionArea(grid))
+  ['this apple is sweet', 'this apple is sour'], // ['sweet', 'sour'],
+  ['apple apple', 'banana'], // ['banana']
+].forEach(([A, B]) => {
+  console.log(uncommonFromSentences(A, B))
 })
 
 // Solution:
-// 顶视图的面积为 网格的方块大于0的个数，
-// 正视图的面积为 每列最大的数的和，
-// 侧视图的面积为 每行最大的数的和，
-// 遍历一遍矩阵，分别计算以上值即可。
+// 用 hashMap 来记录 A 和 B 每个单词出现的次数，
+// 次数为 1 的添加到答案中。
 
 // Submission Result: Accepted
