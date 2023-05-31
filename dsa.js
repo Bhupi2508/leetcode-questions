a5decd00e7df165b11ee85bd5771c3e46af69547
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-05-31 22:53:10 +0530

    51 :::: 0404-sum-of-left-leaves.js leetcode question updated (2023-05-31)

--- dsa.js
+++ dsa.js
@@ -1,81 +1,52 @@
-// 401. Binary Watch
-// Easy   44%
+// 404. Sum of Left Leaves
+// Easy   47%
 
 
-// A binary watch has 4 LEDs on the top which represent the hourset (0-11), and the
-// 6 LEDs on the bottom represent the minuteset (0-59).
-
-// Each LED represents a zero or one, with the least significant bit on the
-// right.
-
-// For example, the above binary watch reads "3:25".
-
-// Given a non-negative integer n which represents the number of LEDs that are
-// currently on, return all possible times the watch could represent.
+// Find the sum of all left leaves in a given binary tree.
 
 // Example:
 
-// Input: n = 1
-// Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08",
-// "0:16", "0:32"]
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
 
-// Note:
-
-// The order of output does not matter.
-// The hour must not contain a leading zero, for example "01:00" is not valid, it
-// should be "1:00".
-// The minute must be consist of two digits and may contain a leading zero, for
-// example "10:2" is not valid, it should be "10:02".
+// There are two left leaves in the binary tree, with values 9 and 15
+// respectively. Return 24.
 
 
 /**
- * @param {number} num
- * @return {string[]}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *   this.val = val;
+ *   this.left = this.right = null;
+ * }
  */
-const readBinaryWatch = function(num) {
-  if (num < 0 || num > 10) return []
 
-  function iter(num, layer, count=0, array=[]) {
-    if (layer >= 0) {
-      if (num === 0) {
-        array.push(count)
-      } else {
-        array = iter(num, layer - 1, count, array)
-        array = iter(num - 1, layer - 1, count + (1 << (layer - 1)), array)
-      }
-    }
-    return array
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = function(root) {
+  function iter(root, isLeft) {
+    if (root == null) return 0
+    if (isLeft && !root.left && !root.right) return root.val
+    return iter(root.left, true) + iter(root.right, false)
   }
-
-  const result = [], n = Math.min(num, 4)
-  for (let i = 0; i <= n; i++) {
-    const hours = iter(i, 4), minutes = iter(num - i, 6)
-    console.log(hours, minutes);
-    for (let hour of hours) {
-      if (hour < 12) {
-        for (let minute of minutes) {
-          if (minute < 60) {
-            result.push(hour + ':' + (minute < 10 ? '0' + minute : minute))
-          }
-        }
-      }
-    }
-  }
-  return result
+  return iter(root, false)
 }
 
+const TreeNode = require('../structs/TreeNode')
 ;[
-  1,
-].forEach(num => {
-  console.log(readBinaryWatch(num))
+  [3,9,20,null,null,15,7], // 24
+].forEach((array) => {
+  console.log(sumOfLeftLeaves(TreeNode.from(array)))
 })
 
 // Solution:
-// 用一个函数来计算 n 盏灯能组成什么数字。
-// 该函数用类似抉择数一样的结构来生成可能组成的数。
-
-// 先分配小时和分钟各占的灯数。
-// 分别用占用的灯数来生成数组。
-// 两个数组中的数两两组合。
+// 在遍历过程中加上一个是否为左子节点的标记。
+// 如果是左子节点，且是叶子节点，则返回该节点的值。
+// 否则返回子节点遍历到的左叶子节点的值的总和。
 
 // Submission Result: Accepted
