2dab0bd67d818f662ee9a36b7521a74c18439c7a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-01 22:54:01 +0530

    51 :::: 0830-positions-of-large-groups.js leetcode question updated (2023-06-01)

--- dsa.js
+++ dsa.js
@@ -1,65 +1,76 @@
-// 824. Goat Latin
-// Easy   61%
+// 830. Positions of Large Groups
+// Easy   49%
 
 
-// A sentence S is given, composed of words separated by spaces. Each word
-// consists of lowercase and uppercase letters only.
-// We would like to convert the sentence to "Goat Latin" (a made-up language
-// similar to Pig Latin.)
-// The rules of Goat Latin are as follows:
-//     If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end
-// of the word.
-//     For example, the word 'apple' becomes 'applema'.
-
-//     If a word begins with a consonant (i.e. not a vowel), remove the first
-// letter and append it to the end, then add "ma".
-//     For example, the word "goat" becomes "oatgma".
-
-//     Add one letter 'a' to the end of each word per its word index in the
-// sentence, starting with 1.
-//     For example, the first word gets "a" added to the end, the second word
-// gets "aa" added to the end and so on.
-// Return the final sentence representing the conversion from S to Goat Latin.
+// In a string S of lowercase letters, these letters form consecutive groups of
+// the same character.
+// For example, a string like S = "abbxxxxzyy" has the groups "a", "bb", "xxxx",
+// "z" and "yy".
+// Call a group large if it has 3 or more characters.  We would like the starting
+// and ending positions of every large group.
+// The final answer should be in lexicographic order.
 
 // Example 1:
-// Input: "I speak Goat Latin"
-// Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+// Input: "abbxxxxzzy"
+// Output: [[3,6]]
+// Explanation: "xxxx" is the single large group with starting  3 and ending
+// positions 6.
 // Example 2:
-// Input: "The quick brown fox jumped over the lazy dog"
-// Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa
-// hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+// Input: "abc"
+// Output: []
+// Explanation: We have "a","b" and "c" but no large group.
+// Example 3:
+// Input: "abcdddeeeeaabbbcd"
+// Output: [[3,5],[6,9],[12,14]]
 
-// Notes:
-//     S contains only uppercase, lowercase and spaces. Exactly one space between
-// each word.
-//     1 <= S.length <= 150.
+// Note:  1 <= S.length <= 1000
 
 
 /**
  * @param {string} S
- * @return {string}
+ * @return {number[][]}
  */
-const toGoatLatin = function(S) {
-  const words = S.split(' ')
-
-  const VOWEL = 'aeiouAEIOU'
-  let res = ''
-  for (let i = 0; i < words.length; i++) {
-    res += ' ' +
-      (VOWEL.includes(words[i][0]) ? words[i] : words[i].substring(1) + words[i][0])
-      + 'm' + 'a'.repeat(i + 2)
+const largeGroupPositions = function(S) {
+  const res = []
+  for (let i = 1, j = 0, l = S.length; i <= l; i++) {
+    if (S[j] !== S[i]) {
+      if (i - j >= 3) res.push([j, i - 1])
+      j = i
+    }
   }
-  return res.substring(1)
+  return res
+}
+
+const universal = function(S) {
+  const res = []
+  for (let i = 0, j = 0; i < S.length; i = j) {
+    while (j < S.length && S[i] === S[j]) j++
+    if (j - i >= 3) res.push([i, j - 1])
+  }
+  return res
 }
 
 ;[
-  'I speak Goat Latin',
-  'The quick brown fox jumped over the lazy dog',
+  'abbxxxxzzy', // [[3,6]]
+  'abc',        // []
+  'abcdddeeeeaabbbcd', // [[3,5],[6,9],[12,14]]
+  'aaabccc',
+  'aaaa',
+  'aa'
 ].forEach((S) => {
-  console.log(toGoatLatin(S))
+  console.log(largeGroupPositions(S))
+  console.log(universal(S))
 })
 
 // Solution:
-// 根据题意直接转换每个字符串即可
+// 方法1
+// i 遍历 数组，j 记录连续字符的头部，
+// 当进入下一组连续字符时，计算 i 到 j 是否大于等于 3，是则记录，并设置 j，否则只设置 j 。
+// 利用 JS 数组尾部之后的值为 undefined 来判断最后一组。
+
+// 方法2 （更普遍）
+// i 记录连续字符的头部，j 遍历数组直到下一个组的头部，
+// 计算 i 到 j 是否大于等于 3，是则记录
+// 设置 i = j
 
 // Submission Result: Accepted
