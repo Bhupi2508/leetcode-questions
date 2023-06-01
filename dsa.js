fcfab1456529dc119954c2bfdcaf6b88b90ed20a
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-01 22:54:22 +0530

    51 :::: 0970-powerful-integers.js leetcode question updated (2023-06-01)

--- dsa.js
+++ dsa.js
@@ -1,70 +1,116 @@
-// 969. Pancake Sorting
-// Medium   67%
+// 970. Powerful Integers
+// Easy   40%
 
 
-// Given an array A, we can perform a pancake flip: We choose some positive
-// integer k <= A.length, then reverse the order of the first k elements of A.
-// We want to perform zero or more pancake flips (doing them one after another in
-// succession) to sort the array A.
-// Return the k-values corresponding to a sequence of pancake flips that sort A.
-// Any valid answer that sorts the array within 10 * A.length flips will be
-// judged as correct.
+// Given two positive integers x and y, an integer is powerful if it is equal to
+// x^i + y^j for some integers i >= 0 and j >= 0.
+// Return a list of all powerful integers that have value less than or equal to
+// bound.
+// You may return the answer in any order.  In your answer, each value should
+// occur at most once.
 
 // Example 1:
-// Input: [3,2,4,1]
-// Output: [4,2,4,3]
+// Input: x = 2, y = 3, bound = 10
+// Output: [2,3,4,5,7,9,10]
 // Explanation:
-// We perform 4 pancake flips, with k values 4, 2, 4, and 3.
-// Starting state: A = [3, 2, 4, 1]
-// After 1st flip (k=4): A = [1, 4, 2, 3]
-// After 2nd flip (k=2): A = [4, 1, 2, 3]
-// After 3rd flip (k=4): A = [3, 2, 1, 4]
-// After 4th flip (k=3): A = [1, 2, 3, 4], which is sorted.
+// 2 = 2^0 + 3^0
+// 3 = 2^1 + 3^0
+// 4 = 2^0 + 3^1
+// 5 = 2^1 + 3^1
+// 7 = 2^2 + 3^1
+// 9 = 2^3 + 3^0
+// 10 = 2^0 + 3^2
 // Example 2:
-// Input: [1,2,3]
-// Output: []
-// Explanation: The input is already sorted, so there is no need to flip
-// anything.
-// Note that other answers, such as [3, 3], would also be accepted.
+// Input: x = 3, y = 5, bound = 15
+// Output: [2,4,6,8,10,14]
 
 // Note:
-//     1 <= A.length <= 100
-//     A[i] is a permutation of [1, 2, ..., A.length]
+//     1 <= x <= 100
+//     1 <= y <= 100
+//     0 <= bound <= 10^6
 
 
 /**
- * @param {number[]} A
+ * @param {number} x
+ * @param {number} y
+ * @param {number} bound
  * @return {number[]}
  */
-const pancakeSort = function(A) {
-  const res = []
-
-  function flip(k) {
-    for (let i = 0, j = k - 1; i < j; i++, j--) {
-      [A[i], A[j]] = [A[j], A[i]]
+const powerfulIntegers = function(x, y, bound) {
+  const set = new Set()
+  for (let a = 1; a < bound; a *= x) {
+    for (let b = 1; a + b < bound; b *= y) {
+      set.add(a + b)
+      if (y === 1) break
     }
+    if (x === 1) break
   }
-
-  for (let x = A.length, i; x > 1; x--) {
-    for (i = 0; A[i] !== x; i++) {}
-    flip(i + 1)
-    res.push(i + 1)
-    flip(x)
-    res.push(x)
-  }
-  return res
+  return [...set]
 }
 
 ;[
-  [3,2,4,1],
-  [1,2,3],
-].forEach((A) => {
-  console.log(pancakeSort(A))
+  [2, 3, 10], // [2,3,4,5,7,9,10]
+  [3, 5, 15], // [2,4,6,8,10,14]
+  [1, 1, 0],  // []
+  [1, 1, 1000000], // [2]
+  [1, 2, 1000000], // [2, 3, 5, 9, ...more 16 items]
+].forEach(([x, y, bound]) => {
+  console.log(powerfulIntegers(x, y, bound))
 })
 
 // Solution:
-// 找到 数组中最大的数的位置 i, 翻转前 i+1 个数，将最大的数转到了第一位，
-// 再翻转整个数组，使得最大的数在数组的最后面。
-// 完成后，再对未排序的数进行相同的操作。直到整个数组排好序。
+// 想法1
+// 根据相加的两个数的不同值，可以想象出一个杨辉（帕斯卡）三角
+// 如
+//             1+1=2
+//         /          \
+//      2+1=3        1+3=4
+//     /     \     %      \
+//   4+1=5    2+3=5     1+9=10
+//  /     \  %     \   %     \
+// 8+1=9  4+3=7    2+9=11     82
+// 65   11     13       83
+// 可以遍历 a+b 小于 bound 的节点，使用归并排序的思想得到两个子节点返回的数组，
+// 并将当前节点的 a+b 添加到合并后的数组的头部，因为其值必小于左右的最小值。
+// 为了避免重复遍历，所以只有根节点和左节点才遍历左右子节点，右节点只遍历右子节点。
+// 注意相同的值的合并。
+// 当 bound 很大时，递归的层数就很大，如 x=1,y=1,bound=1000000 时， 就爆栈了。
+// x=1 || y=1 的情况单独解决即可
+
+const idea1 = function() {
+  if (bound < 2) return []
+  if (x === 1 && y === 1) return [2]
+  if (x === 1 || y === 1) {
+    let a = x = Math.max(x, y)
+    let res = [2]
+    while (a <= bound) {
+      res.push(a + 1)
+      a *= x
+    }
+    return res
+  }
+  function dfs(a, b, isSplit) {
+    if (a + b > bound) return []
+    let left = []
+    if (isSplit) left = dfs(a * x, b, true)
+    const right = dfs(a, b * y, false)
+    const res = [a + b]
+    for (let i = 0, j = 0; i < left.length || j < right.length;) {
+      if (i == left.length || left[i] > right[j]) res.push(right[j++])
+      else if (j == right.length || left[i] < right[j]) res.push(left[i++])
+      else {
+        res.push(left[i++])
+        j++
+      }
+    }
+    return res
+  }
+  return dfs(1, 1, true)
+}
+// 回头发现，结果并不需要排序，即只需要排除重复数据即可
+
+// 若不需要排序
+// 1. 使用 hash 来确定数使用存在
+// 2. es6 中的 set
 
 // Submission Result: Accepted
