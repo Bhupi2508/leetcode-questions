1659e08345766a71ef70f84190e29daa2699c838
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-06 22:53:50 +0530

    52 :::: 0706-design-hashmap.js leetcode question updated (2023-06-06)

--- dsa.js
+++ dsa.js
@@ -1,96 +1,106 @@
-// 705. Design HashSet
+// 706. Design HashMap
 // Easy   59%
 
 
-// Design a HashSet without using any built-in hash table libraries.
+// Design a HashMap without using any built-in hash table libraries.
 // To be specific, your design should include these functions:
-//     add(value): Insert a value into the HashSet.
-//     contains(value) : Return whether the value exists in the HashSet or not.
-//     remove(value): Remove a value in the HashSet. If the value does not exist
-// in the HashSet, do nothing.
+//     put(key, value) : Insert a (key, value) pair into the HashMap. If the
+// value already exists in the HashMap, update the value.
+//     get(key): Returns the value to which the specified key is mapped, or -1 if
+// this map contains no mapping for the key.
+//     remove(key) : Remove the mapping for the value key if this map contains
+// the mapping for the key.
 // Example:
-// MyHashSet hashSet = new MyHashSet();
-// hashSet.add(1);
-// hashSet.add(2);
-// hashSet.contains(1);    // returns true
-// hashSet.contains(3);    // returns false (not found)
-// hashSet.add(2);
-// hashSet.contains(2);    // returns true
-// hashSet.remove(2);
-// hashSet.contains(2);    // returns false (already removed)
+// MyHashMap hashMap = new MyHashMap();
+// hashMap.put(1, 1);
+// hashMap.put(2, 2);
+// hashMap.get(1);            // returns 1
+// hashMap.get(3);            // returns -1 (not found)
+// hashMap.put(2, 1);          // update the existing value
+// hashMap.get(2);            // returns 1
+// hashMap.remove(2);          // remove the mapping for 2
+// hashMap.get(2);            // returns -1 (not found)
 // Note:
-//     All values will be in the range of [0, 1000000].
+//     All keys and values will be in the range of [0, 1000000].
 //     The number of operations will be in the range of [1, 10000].
-//     Please do not use the built-in HashSet library.
+//     Please do not use the built-in HashMap library.
 
 
 /**
  * Initialize your data structure here.
  */
-const MyHashSet = function() {
-  this.set = []
+const MyHashMap = function() {
+  this.map = []
 };
 
 /**
+ * value will always be non-negative.
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+  this.map[key] = value
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+  return this.map[key] === undefined ? -1 : this.map[key]
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key
  * @param {number} key
  * @return {void}
  */
-MyHashSet.prototype.add = function(key) {
-  this.set[key] = true
+MyHashMap.prototype.remove = function(key) {
+  this.map[key] = -1
 };
 
 /**
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function(key) {
-  this.set[key] = false
-};
-
-/**
- * Returns true if this set contains the specified element
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function(key) {
-  return !!this.set[key]
-};
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * var obj = new MyHashSet()
- * obj.add(key)
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
  * obj.remove(key)
- * var param_3 = obj.contains(key)
  */
 
 ;[
+  // [
+  //   ['MyHashMap','put','put','get','get','put','get', 'remove', 'get'],
+  //   [[],[1,1],[2,2],[1],[3],[2,1],[2],[2],[2]],
+  // ],
   [
-    ['MyHashSet','add','add','contains','contains','add','contains','remove','contains'],
-    [[],[1],[2],[1],[3],[2],[2],[2],[2]],
-  ],
+    ["MyHashMap","remove","put","put","put","put","put","put","get","put","put"],
+    [[],[2],[3,11],[4,13],[15,6],[6,15],[8,8],[11,0],[11],[1,10],[12,14]],
+  ]
 ].forEach(([commands, data]) => {
-  let myHashSet = null
+  let myHashMap = null
   for (let i = 0; i < commands.length; i++) {
     switch(commands[i]) {
-      case 'MyHashSet':
-        myHashSet = new MyHashSet()
+      case 'MyHashMap':
+        myHashMap = new MyHashMap()
         break
-      case 'add':
-        myHashSet.add(data[i][0])
+      case 'put':
+        myHashMap.put(...data[i])
         break
-      case 'contains':
-        console.log(myHashSet.contains(data[i][0]))
+      case 'get':
+        console.log(myHashMap.get(data[i][0]))
         break
       case 'remove':
-        myHashSet.remove(data[i][0])
+        myHashMap.remove(data[i][0])
         break
     }
   }
 })
 
 // Solution:
-// 只能用数组来实现了
-// 其实JS数组也是一种特殊的hash
+// 用数组模拟。
+// 但是空间复杂度极大
+// JS 的对象又是 hash 实现的。
 
 // Submission Result: Accepted
