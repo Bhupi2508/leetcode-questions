8750b3c403b67c3eb96af1ae35721736d8fca3fd
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-07 22:54:04 +0530

    52 :::: 0852-peak-index-in-a-mountain-array.js leetcode question updated (2023-06-07)

--- dsa.js
+++ dsa.js
@@ -1,61 +1,57 @@
-// 849. Maximize Distance to Closest Person
-// Easy   42%
+// 852. Peak Index in a Mountain Array
+// Easy   69%
 
 
-// In a row of seats, 1 represents a person sitting in that seat, and 0
-// represents that the seat is empty.
-// There is at least one empty seat, and at least one person sitting.
-// Alex wants to sit in the seat such that the distance between him and the
-// closest person to him is maximized.
-// Return that maximum distance to closest person.
+// Let's call an array A a mountain if the following properties hold:
+//  - A.length >= 3
+//  - There exists some 0 < i < A.length - 1 such that
+//    A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+
+// Given an array that is definitely a mountain, return any i such that
+//    A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+
 // Example 1:
-// Input: [1,0,0,0,1,0,1]
-// Output: 2
-// Explanation:
-// If Alex sits in the second open seat (seats[2]), then the closest person has
-// distance 2.
-// If Alex sits in any other open seat, the closest person has distance 1.
-// Thus, the maximum distance to the closest person is 2.
+// Input: [0,1,0]
+// Output: 1
+
 // Example 2:
-// Input: [1,0,0,0]
-// Output: 3
-// Explanation:
-// If Alex sits in the last seat, the closest person is 3 seats away.
-// This is the maximum distance possible, so the answer is 3.
+// Input: [0,2,1,0]
+// Output: 1
+
 // Note:
-//     1 <= seats.length <= 20000
-//     seats contains only 0s or 1s, at least one 0, and at least one 1.
+//     3 <= A.length <= 10000
+//     0 <= A[i] <= 10^6
+//     A is a mountain, as defined above.
 
 
 /**
- * @param {number[]} seats
+ * @param {number[]} A
  * @return {number}
  */
-const maxDistToClosest = function(seats) {
-  let res = seats.indexOf(1)
-  let count = 0
-  for (let i = res + 1; i < seats.length; i++) {
-    if (seats[i] === 0) count++
-    else {
-      res = Math.max(res, Math.ceil(count / 2))
-      count = 0
+const peakIndexInMountainArray = function(A) {
+  function recursive(i, j) {
+    const mid = (i + j) >> 1
+    if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+      return mid
+    } else if (A[mid] < A[mid + 1]) {
+      return recursive(mid + 1, j)
+    } else {
+      return recursive(i, mid - 1)
     }
   }
-  return Math.max(res, count)
+  return recursive(1, A.length - 1)
 }
 
 ;[
-  [1,0,0,0,1,0,1], // 2
-  [1,0,0,0],       // 3
-  [1,0],           // 1
-  [1,0,0,0,1,1,1], // 2
-].forEach((seats) => {
-  console.log(maxDistToClosest(seats))
+  [0, 1, 0], // 1
+  [0, 2, 1, 0], // 1
+  [0,1,2,3,4,5,6,7,4,3,2], // 7
+  [0,10,9,8,7,6,5,4,3,2,1,0], // 1
+].forEach((A) => {
+  console.log(peakIndexInMountainArray(A))
 })
 
 // Solution:
-// Alex 有两种类型的位置可以坐：
-// 1. 最两侧空的位置。若有空位，其最大距离为空位置的数量
-// 2. 空位置位于两人之间。其最大距离为空位置的数量的一半（向上取整）
+// 二分查找法。
 
 // Submission Result: Accepted
