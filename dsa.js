de5a97fcd6d85af5fdd9ed9cc29de2640fbe7285
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-12 22:52:23 +0530

    53 :::: 0069-sqrtx.js leetcode question updated (2023-06-12)

--- dsa.js
+++ dsa.js
@@ -1,87 +1,43 @@
-// 68. Text Justification
-// Hard   19%
+// 69. Sqrt(x)
+// Easy   28%
 
-// Given an array of words and a length L, format the text such that each line
-// has exactly L characters and is fully (left and right) justified.
+// Implement int sqrt(int x).
 
-// You should pack your words in a greedy approach; that is, pack as many words
-// as you can in each line. Pad extra spaces ' ' when necessary so that each
-// line has exactly L characters.
+// Compute and return the square root of x.
 
-// Extra spaces between words should be distributed as evenly as possible. If
-// the number of spaces on a line do not divide evenly between words, the empty
-// slots on the left will be assigned more spaces than the slots on the right.
+// x is guaranteed to be a non-negative integer.
 
-// For the last line of text, it should be left justified and no extra space is
-// inserted between words.
+// Example 1:
 
-// For example,
-// words: ["This", "is", "an", "example", "of", "text", "justification."]
-// L: 16.
+// Input: 4
+// Output: 2
 
-// Return the formatted lines as:
+// Example 2:
 
-// [
-//    "This    is    an",
-//    "example  of text",
-//    "justification.  "
-// ]
-
-// Note: Each word is guaranteed not to exceed L in length.
-
-// click to show corner cases.
-// Corner Cases:
-
-// A line other than the last line might contain only one word. What should
-// you do in this case? In this case, that line should be left-justified.
+// Input: 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we want to return
+// an integer, the decimal part will be truncated.
 
 /**
- * @param {string[]} words
- * @param {number} maxWidth
- * @return {string[]}
+ * @param {number} x
+ * @return {number}
  */
-const fullJustify = function(words, maxWidth) {
-  const result = [], n = words.length
-  for (let i = 0, j = 0; i < n; i += j, j = 0) {
-    let width = 0
-    while (i + j < n && width + words[i + j].length + j <= maxWidth) {
-      width += words[i + j].length
-      j++
-    }
-    let string = '', spaces = maxWidth - width
-    for (let k = 0; k < j - 1; k++) {
-      string += words[i + k]
-      if (i + j >= n) {
-        string += ' '
-        spaces -= 1
-      } else {
-        const repeat = spaces > 0 ? Math.ceil(spaces / (j - k - 1)) : 0
-        spaces -= repeat
-        string += ' '.repeat(repeat)
-      }
-    }
-    string += words[i + j - 1] + ' '.repeat(spaces)
-    result.push(string)
-  }
-
-  return result
+const mySqrt = function(x) {
+  let r = x
+  while (r * r > x) r = ((r + x / r) / 2) | 0
+  return r
 }
 
 ;[
-  [["This", "is", "an", "example", "of", "text", "justification."], 16],
-  [['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1],
-  [["What","must","be","shall","be."], 12],
-  [["a"], 2],
-].forEach(([words, maxWidth]) => {
-  console.log(fullJustify(words, maxWidth))
+  4,                            // 2
+  8,                            // 2
+  100,                          // 10
+].forEach(x => {
+  console.log(mySqrt(x));
 })
 
 // Solution:
-// 每个单词间必须有空格隔开，最小为1，
-// 每次加一个字符串的长度，这些加起来的长度加上最小空格数小于或等于 maxWidth。
-// 当数量最大时，将这些单词构造出句子。
-// 此时，单词将的间隔的计算为
-// 剩下空格数除以剩下单词数，并向上取整。
-// 若为最后一行，则单词间只加一个空格，并将剩下的所有空格加到末尾。
+// 牛顿迭代法的特殊情况（精度为1）
 
 // Submission Result: Accepted
