4c7e709e9c47783c8f37281e9f511edb096c5004
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-14 22:52:14 +0530

    53 :::: 0045-jump-game-ii.js leetcode question updated (2023-06-14)

--- dsa.js
+++ dsa.js
@@ -1,59 +1,53 @@
-// 44. Wildcard Matching
-// Hard 20% locked:false
+// 45. Jump Game II
+// Hard   26%
 
-// Implement wildcard pattern matching with support for '?' and '*'.
+// Given an array of non-negative integers, you are initially positioned at the
+// first index of the array.
 
-// '?' Matches any single character.
-// '*' Matches any sequence of characters (including the empty sequence).
+// Each element in the array represents your maximum jump length at that
+// position.
 
-// The matching should cover the entire input string (not partial).
+// Your goal is to reach the last index in the minimum number of jumps.
 
-// The function prototype should be:
-// bool isMatch(const char *s, const char *p)
+// For example:
+// Given array A = [2,3,1,1,4]
 
-// Some examples:
-// isMatch("aa","a") ? false
-// isMatch("aa","aa") ? true
-// isMatch("aaa","aa") ? false
-// isMatch("aa", "*") ? true
-// isMatch("aa", "a*") ? true
-// isMatch("ab", "?*") ? true
-// isMatch("aab", "c*a*b") ? false
+// The minimum number of jumps to reach the last index is 2. (Jump 1 result from
+// index 0 to 1, then 3 steps to the last index.)
 
+// Note:
+// You can assume that you can always reach the last index.
 
 /**
- * @param {string} s
- * @param {string} p
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-const isMatch = function(s, p) {
-  const n = s.length, m = p.length
-  let i = 0, j = 0, match = 0, starIdx = -1
-  while (i < n){
-    if (j < m  && (p[j] === '?' || s[i] === p[j])) {
-      i++
-      j++
-    } else if (j < m && p[j] === '*') {
-      starIdx = j
-      match = i
-      j++
-    } else if (starIdx !== -1){
-      j = starIdx + 1
-      match++
-      i = match
-    } else return false
+const jump = function(nums) {
+  let result = 0, via = 0, max = 0
+  for (let i = 0; i < nums.length - 1; i++) {
+    max = Math.max(max, i + nums[i])
+    if (via === i) {
+      result++
+      via = max
+    }
   }
 
-  while (j < m && p[j] === '*') j++
-
-  return j === m
+  return result
 }
 
-console.log(isMatch('aa', 'a'), false)
-console.log(isMatch('aa', 'aa'), true)
-console.log(isMatch('aaa', 'aa'), false)
-console.log(isMatch('aa', '*'), true)
-console.log(isMatch('aa', 'a*'), true)
-console.log(isMatch('ab', '?*'), true)
-console.log(isMatch('ad', 'c*a*b*d'), false)
-console.log(isMatch('aaa', 'a*a'), true)
+;[
+  [3,2,1],                      // 1
+  [2,3,1,1,4],                  // 2
+  [1,3,2,3,0,1,2,1,1],          // 4
+  [1,1,1,1],                    // 3
+  [10,9,8,7,6,5,4,3,2,1,1,0],   // 2
+].forEach(nums => {
+  console.log(jump(nums))
+})
+
+// Solution:
+// 使用贪心算法。
+// 每次都选择一个最远的进行试跳，若过程中还遇到更远的，先保存。
+// 因为被试跳的位置总是先会被遍历，因此，在跳到更远的位置前，都会进行更新。
+
+// Submission Result: Accepted
