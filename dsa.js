53405aa955a311042960fcacbc12ed318c66b058
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-18 22:53:52 +0530

    54 :::: 0720-longest-word-in-dictionary.js leetcode question updated (2023-06-18)

--- dsa.js
+++ dsa.js
@@ -1,59 +1,84 @@
-// 717. 1-bit and 2-bit Characters
-// Easy   52%
+
+// 720. Longest Word in Dictionary
+// Easy   39%
 
 
-// We have two special characters. The first character can be represented by one
-// bit 0. The second character can be represented by two bits (10 or 11).
-
-// Now given a string represented by several bits. Return whether the last
-// character must be a one-bit character or not. The given string will always end
-// with a zero.
+// Given a list of strings words representing an English Dictionary, find the
+// longest word in words that can be built one character at a time by other words
+// in words.  If there is more than one possible answer, return the longest word
+// with the smallest lexicographical order.
+//   If there is no answer, return the empty string.
 
 // Example 1:
 
 // Input:
-// bits = [1, 0, 0]
-// Output: True
+// words = ["w","wo","wor","worl", "world"]
+// Output: "world"
 // Explanation:
-// The only way to decode it is two-bit character and one-bit character. So the
-// last character is one-bit character.
+// The word "world" can be built one character at a time by "w", "wo", "wor", and
+// "worl".
 
 // Example 2:
 
 // Input:
-// bits = [1, 1, 1, 0]
-// Output: False
+// words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+// Output: "apple"
 // Explanation:
-// The only way to decode it is two-bit character and two-bit character. So the
-// last character is NOT one-bit character.
+// Both "apply" and "apple" can be built from other words in the dictionary.
+// However, "apple" is lexicographically smaller than "apply".
 
 // Note:
 
-// 1 <= len(bits) <= 1000.
-// bits[i] is always 0 or 1.
+// All the strings in the input will only contain lowercase letters.
+// The length of words will be in the range [1, 1000].
+// The length of words[i] will be in the range [1, 30].
 
 
 /**
- * @param {number[]} bits
- * @return {boolean}
+ * @param {string[]} words
+ * @return {string}
  */
-const isOneBitCharacter = function(bits) {
-  for (let i = 0, n = bits.length; i < n; i++) {
-    if (i === n - 1) return true
-    if (bits[i]) i++
+const longestWord = function(words) {
+  const group = {}
+  for (let word of words) {
+    const layer = word.length - 1
+    if (!group[layer]) group[layer] = []
+    group[layer].push(word)
   }
-  return false
+  console.log(group);
+
+  function iter(chars, layer) {
+    if (!group[layer]) return chars
+    let result = chars
+    for (let word of group[layer]) {
+      if (word.indexOf(chars) === 0) {
+        const next = iter(word, layer + 1)
+        if (result === chars ||
+            (next.length === result.length && result > next) ||
+            (next.length > result.length)) result = next
+      }
+    }
+    return result
+  }
+
+  return iter('', 0)
 }
 
 ;[
-  [1,0,0],                      // true
-  [1,1,1,0],                    // false
-].forEach(bits => {
-  console.log(isOneBitCharacter(bits))
+  ['w','wo','wor','worl', 'world', 'longestWord'], // 'world'
+  ['a', 'banana', 'app', 'appl', 'ap', 'apply', 'apple'], // 'apple'
+  ["b","br","bre","brea","break","breakf","breakfa","breakfas","breakfast",
+   "l","lu","lun","lunc","lunch","d","di","din","dinn","dinne","dinner"],
+  // 'breakfast'
+  ["m","mo","moc","moch","mocha","l","la","lat","latt","latte",
+   "c","ca","cat"],             // 'latte'
+].forEach(words => {
+  console.log(longestWord(words))
 })
 
 // Solution:
-// 遇到一则跳过一个下一个比特，否则不跳过。
-// 最后看是否能遍历到最后一个比特0，若能则返回true，否则false。
+// 先按字符的长度分层。
+// 从空字符开始，每次进入一层，若该层存在有以从下一层字符开头的，则添加该单词进入下一层，
+// 最后返回该找到的最长单词且字符代码和最小的单词。
 
 // Submission Result: Accepted
