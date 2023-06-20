c1db36d8aea686bea0a2f249f5a0acb453711999
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-20 22:52:04 +0530

    54 :::: 0019-remove-nth-node-from-end-of-list.js leetcode question updated (2023-06-20)

--- dsa.js
+++ dsa.js
@@ -1,96 +1,64 @@
-// 18. 4Sum
-// Medium   27%
+// 19. Remove Nth Node From End of List
+// Medium   33%
 
-// Given an array S of n integers, are there elements a, b, c, and d in S such
-// that a + b + c + d = target? Find all unique quadruplets in the array which
-// gives the sum of target.
+// Given a linked list, remove the nth node from the end of list and return its
+// head.
 
-// Note: The solution set must not contain duplicate quadruplets.
+// For example,
 
-// For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+// Given linked list: 1->2->3->4->5, and n = 2.
 
-// A solution set is:
-// [
-//   [-1,  0, 0, 1],
-//   [-2, -1, 1, 2],
-//   [-2,  0, 0, 2]
-// ]
+// After removing the second node from the end, the linked list becomes
+// 1->2->3->5.
+
+// Note:
+// Given n will always be valid.
+//   Try to do this in one pass.
 
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[][]}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-const fourSum = function(nums, target) {
-  const result = []
-  nums.sort((a, b) => a - b)
-  const iter = (i, j, next) => {
-    if (i + 3 > j) return
 
-    let lo = i + 1, hi = j - 1, s = nums[i] + nums[j]
-    const t = s + nums[lo] + nums[hi]
-    while(lo < hi) {
-      const sum = s + nums[lo] + nums[hi]
-      if (sum > target) hi--
-      else if (sum < target) lo++
-      else {
-        result.push([nums[i], nums[lo], nums[hi], nums[j]])
-        while (++lo < hi && nums[lo] === nums[lo - 1]);
-        while (lo < --hi && nums[hi] === nums[hi + 1]);
-      }
-    }
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+const removeNthFromEnd = function(head, n) {
+  const start = new ListNode()
+  let slow = start, fast = start
+  slow.next = head
 
-    if (next) {
-      if (t > target) iter(i, j - 1, true)
-      if (t < target) iter(i + 1, j, true)
-      if (t === target) {
-        iter(i, j - 1, false)
-        iter(i + 1, j, true)
-      }
-    }
+  for (let i = 0; i <= n; i++) {
+    fast = fast.next
   }
 
-  iter(0, nums.length - 1, true)
-  return result
-}
-
-const On3 = function(nums, target) {
-  const result = [], n = nums.length
-  if (n < 4) return result
-
-  nums.sort((a, b) => a - b)
-  const sum4 = (a, b, c, d) => nums[a] + nums[b] + nums[c] + nums[d]
-  for (let i = 0; i < n - 3; i++) {
-    if (sum4(i, i + 1, i + 2, i + 3) > target) break
-    if (sum4(i, n - 1, n - 2, n - 3) < target) continue
-    if (nums[i] === nums[i - 1]) continue
-
-    for(let j = i + 1; j < n - 2; j++) {
-      if(sum4(i, j, j + 1, j + 2) > target) break
-      if(sum4(i, j, n - 1, n - 2) < target) continue
-      if(j > i + 1 && nums[j] === nums[j - 1]) continue
-      let lo = j + 1, hi = n - 1
-      while (lo < hi) {
-        const sum = sum4(i, j, lo, hi)
-        if (sum < target) lo++
-        else if (sum > target) hi--
-        else {
-          result.push([nums[i], nums[j], nums[lo], nums[hi]])
-          while (++lo < hi && nums[lo] === nums[lo - 1]);
-          while (lo < --hi && nums[hi] === nums[hi + 1]);
-        }
-      }
-    }
+  while (fast) {
+    fast = fast.next
+    slow = slow.next
   }
-  return result
+  slow.next = slow.next.next
+
+  return start.next
 }
 
+const ListNode = require('../structs/ListNode')
 ;[
-  [[1, 0, -1, 0, -2, 2], 0],
-  [[-3,-1,0,2,4,5], 0],
-  [[-3,-2,-1,0,0,1,2,3], 0],
-  [[-5,-4,-3,-2,-1,0,0,1,2,3,4,5], 0],
-].forEach(args => {
-  console.log(fourSum(...args))
-  console.log(On3(...args))
+  [[1, 2], 2],      // [2]
+  [[1,2,3,4,5], 2], // [1,2,3,5]
+].forEach(([A, n]) => {
+  console.log((removeNthFromEnd(ListNode.from(A), n) || '').toString())
 })
+
+// Solution:
+// 使用前后差双前进法：
+// 遍历过程中保存两个节点，两节点之间总是相差n个节点。
+// 后一个节点从链表头（即第一个节点）开始，前一个节点从第n + 1个节点开始。
+// 当前节点前进到了链表的末尾时，后节点就到了倒数第n - 1个节点处，
+// 此时，后节点将其前一个节点删除就完成任务了。
+
+// Submission Result: Accepted
