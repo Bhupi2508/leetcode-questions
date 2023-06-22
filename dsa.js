6c0c7916447e59e5d072d1da4729c2c994fea054
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-22 22:52:03 +0530

    54 :::: 0015-3sum.js leetcode question updated (2023-06-22)

--- dsa.js
+++ dsa.js
@@ -1,38 +1,77 @@
-// 14. Longest Common Prefix
-// Easy   31%
+// 15. 3Sum
+// Medium  21%
 
-//Write a function to find the longest common prefix string amongst an array of
-//strings.
+// Given an array S of n integers, are there elements a, b, c in S such that a +
+// b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+// Note: The solution set must not contain duplicate triplets.
+
+// For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
 
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-const longestCommonPrefix = function(strs) {
-  if (strs == null || strs.length === 0) return ''
-  const n = strs.length, m = strs[0].length
-  for (let i = 0; i < m; i++) {
-    const c = strs[0][i]
-    for (let j = 1; j < n; j++) {
-      if (strs[j][i] !== c) return strs[0].substring(0, i)
+const threeSum0 = function(nums) {
+  const n = nums.length, result = []
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < n - 2; i++) {
+    if (i === 0 || nums[i] !== nums[i - 1]) {
+      let lo = i + 1, hi = n - 1
+      while (lo < hi) {
+        const sum = nums[i] + nums[lo] + nums[hi]
+        if (sum < 0) lo++
+        else if (sum > 0) hi--
+        else {
+          result.push([nums[i], nums[lo], nums[hi]])
+          while (++lo < hi && nums[lo] === nums[lo - 1]);
+          while (lo < --hi && nums[hi] === nums[hi + 1]);
+        }
+      }
     }
   }
-  return strs[0]
+  return result
 }
 
 ;[
-  [],                           // ''
-  [''],                         // ''
-  ['abc'],                      // 'abc'
-  ['a', 'b'],                   // ''
-  ['123', '123456', '1232'],    // '123'
-].forEach(strs => {
-  console.log(longestCommonPrefix(strs))
+  [0, 0, 0],
+  [-2,0,0,2,2,0],
+  [3,0,-2,-1,1,2],
+  [-1,0,1,2,-1,-4],
+].forEach(nums => {
+  console.log(threeSum0(nums))
 })
 
 // Solution:
-// 垂直扫描，从头开始比较全部字符串中相同下标的字符是否相同
-// 若有一个不同，则返回0到该下标之间（包括0不包括该下标）的子字符串
-// 全部相同则检查下一个下标。
+
+// 解法1
+// 3个数之和为0，以正负类型来看可分为4种情况：
+// 1. 全为0；
+// 2. 一个0一个正数一个负数；
+// 3. 一个正数两个负数；
+// 4. 两个正数一个负数。
+
+// 将数组中的数分为3个组，零组，正数组，负数组
+// 1) 零组有三个及其以上，则将[0,0,0]添加到结果中。
+// 2) 对于正（负）数组中的每个数，扫描负（正）数组中的数，使用 [1.Two Sum] 中的解法
+// （即使用哈希表），找出匹配的两个数，并添加到结果中。
+
+// 简单的编码中，无法排除重复的组合。
+// 若要排除重复的组合必然会增加程序的复杂性。
+
+// 解法2
+// 排序
+// 对每个数，从数组两侧开始向中间遍历，
+// 计算三个数之和
+// 若小了，说明小的数太小了，左边前进一位，
+// 若大了，说明大的数太大了，右边前进一位，
+// 否则将三个数添加到结果中，并寻找下一个组合。
+// 为了避免重复，每次找到适合的数后，左右都要前进到与原来数不同的位置上。
 
 // Submission Result: Accepted
