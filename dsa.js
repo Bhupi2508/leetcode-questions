1e7e976c22cd9717e36fcc4d7488707f3186d7c4
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-24 22:45:07 +0530

    54 :::: 54-spiralMatrix.js question updated

--- dsa.js
+++ dsa.js
@@ -1,65 +1,58 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
-// my own solution BFS, iterative
-/**
- * @param {TreeNode} root
+ * Key: Traverse from left to right, increase rowStart, means this top row has been visited.
+ * Traverse from up to down, decrease colEnd, means this right column has been visited.
+ * Traverse from right to left, decrease rowEnd, means this bottom row has been visited.
+ * Traverse from down to up, increase colStart, means this left column has been visited.
+ * if one row/column left, no circle can be formed, so
+ * when the rowStart === rowEnd, there is only this row left to visit, (left -> right)
+ * when the colStart === colEnd, there is only this column left to visit, (up -> down)
+ *
+ * @param {number[][]} matrix
  * @return {number[]}
  */
-var largestValues = function(root) {
-    var next = [root];
-    var maxArr = [];
+var spiralOrder = function(matrix) {
+    if (matrix.length === 0) return matrix;
+    var rowStart = 0;
+    var colStart = 0;
+    var rowEnd = matrix.length - 1;
+    var colEnd = matrix[rowStart].length - 1;
+    var path = [];
 
-    if (!root) {
-        return maxArr;
-    }
-
-    while (next.length > 0) {
-        var curr = next.slice();
-        var max = -Number.MAX_VALUE;
-        next = [];
-
-        while (curr.length > 0) {
-            var node = curr.shift();
-            max = Math.max(node.val, max);
-
-            if (node.left) {
-                next.push(node.left);
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        if (rowStart === rowEnd) {
+            for (var i = colStart; i <= colEnd; i++) {
+                path.push(matrix[rowStart][i]);
             }
-            if (node.right) {
-                next.push(node.right);
-            }
+            break;
         }
 
-        maxArr.push(max);
+        if (colStart === colEnd) {
+            for (var i = rowStart; i <= rowEnd; i++) {
+                path.push(matrix[i][colEnd]);
+            }
+            break;
+        }
+
+        for (var i = colStart; i <= colEnd; i++) {
+            path.push(matrix[rowStart][i]);
+        }
+        rowStart++;
+
+        for (var i = rowStart; i <= rowEnd; i++) {
+            path.push(matrix[i][colEnd]);
+        }
+        colEnd--;
+
+        for (var i = colEnd; i >= colStart; i--) {
+            path.push(matrix[rowEnd][i]);
+        }
+        rowEnd--;
+
+        for (var i = rowEnd; i >= rowStart; i--) {
+            path.push(matrix[i][colStart]);
+        }
+        colStart++;
     }
 
-    return maxArr;
+    return path;
 };
-
-// DFS
-var largestValues = function(root) {
-    var result = [];
-    helper(root, result, 0);
-    return result;
-};
-
-var helper = function(root, res, level) {
-    if (!root) {
-        return;
-    }
-
-    if (res.length === level) {
-        res.push(root.val);
-    } else {
-        res[level] = Math.max(res[level], root.val);
-    }
-
-    helper(root.left, res, level + 1);
-    helper(root.right, res, level + 1);
-}
