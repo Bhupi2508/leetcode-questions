496beef7da1a42a25cc3cf921d285dc32c547a27
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-10-03 00:00:00 +0530

    22 :::: 105-constructBinaryTree.js solution updated

--- dsa.js
+++ dsa.js
@@ -1,28 +1,38 @@
 /**
- * Key: same idea as binary tree level order traversal,
- * if the level is odd, insert the node to the begining of the level result array
- * if the level is even, insert the node to the end of the level result array
- *
- * @param {TreeNode} root
- * @return {number[][]}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var zigzagLevelOrder = function(root) {
-    var results = [];
-    helper(results, root, 0);
-    return results;
+/**
+ * e.g. preorder 12453768, inorder: 42516738,
+ * from preorder, we know the first element is the root, then find root's index in
+ * inorder, then the left side (in inorder) of root is the left subtree, and right side
+ * after root is the right subtree.
+ *
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
 };
 
-var helper = function(results, node, level) {
-    if (!node) return results;
-    if (level >= results.length) {
-        results[level] = [];
-    }
-    if (level % 2 === 0) {
-        results[level].push(node.val);
-    } else {
-        results[level].unshift(node.val);
+var helper = function(preorder, preStart, preEnd, inorder, inStart, inEnd) {
+    if (preStart > preEnd || inStart > inEnd) return null;
+    var k = 0;
+    for (var i = 0, length = inorder.length; i < length; i++) {
+        if(preorder[preStart] === inorder[i]) {
+            k = i;
+            break;
+        }
     }
 
-    helper(results, node.left, level + 1);
-    helper(results, node.right, level + 1);
+    var root = new TreeNode(preorder[preStart]);
+    // make sure inEnd - inStart === preEnd - preStart
+    root.left = helper(preorder, preStart + 1, preStart + k - inStart, inorder, inStart, k - 1);
+    root.right = helper(preorder, preEnd - inEnd + k + 1, preEnd, inorder, k + 1, inEnd);
+
+    return root;
 };
