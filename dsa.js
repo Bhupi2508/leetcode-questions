972d796aa9e1166a35b792ba4643f966d4189338
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2024-01-30 00:00:00 +0530

    39 :::: 106-constructBinaryTree.js solution updated

--- dsa.js
+++ dsa.js
@@ -6,33 +6,24 @@
  * }
  */
 /**
- * e.g. preorder 12453768, inorder: 42516738,
- * from preorder, we know the first element is the root, then find root's index in
- * inorder, then the left side (in inorder) of root is the left subtree, and right side
- * after root is the right subtree.
- *
- * @param {number[]} preorder
  * @param {number[]} inorder
+ * @param {number[]} postorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
-    return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+var buildTree = function(inorder, postorder) {
+    return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
 };
 
-var helper = function(preorder, preStart, preEnd, inorder, inStart, inEnd) {
-    if (preStart > preEnd || inStart > inEnd) return null;
-    var k = 0;
-    for (var i = 0, length = inorder.length; i < length; i++) {
-        if(preorder[preStart] === inorder[i]) {
-            k = i;
+var helper = function(inorder, inStart, inEnd, postorder, postStart, postEnd) {
+    if (inEnd < inStart || postEnd < postStart) return null;
+    var rootVal = postorder[postEnd];
+    var root = new TreeNode(rootVal);
+    for (var i = 0; i < inEnd; i++) {
+        if (inorder[i] === rootVal) {
             break;
         }
     }
-
-    var root = new TreeNode(preorder[preStart]);
-    // make sure inEnd - inStart === preEnd - preStart
-    root.left = helper(preorder, preStart + 1, preStart + k - inStart, inorder, inStart, k - 1);
-    root.right = helper(preorder, preEnd - inEnd + k + 1, preEnd, inorder, k + 1, inEnd);
-
+    root.left = helper(inorder, inStart , i - 1 , postorder, postStart, postStart + i - 1 - inStart);
+    root.right = helper(inorder, i + 1, inEnd, postorder, postEnd - inEnd + i, postEnd - 1);
     return root;
 };
